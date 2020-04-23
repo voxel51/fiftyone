f78dc60b0d94f26dc07471cf92204d325187024f
alan <alan@voxel51.com>
2020-04-23 14:06:45 -0400

    Tweak home page

--- electron/app/components/Home.tsx
+++ electron/app/components/Home.tsx
@@ -5,14 +5,13 @@ import styles from './Home.css';
 
 import { Button } from 'semantic-ui-react';
 
-const ButtonExampleButton = () => <Button>SEMANTIC UI BUTTON</Button>;
-
 export default function Home() {
   return (
     <div className={styles.container} data-tid="container">
       <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
-      <ButtonExampleButton />
+      <Button as={Link} to={routes.COUNTER}>
+        to Counter
+      </Button>
     </div>
   );
 }
