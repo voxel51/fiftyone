4ca38da2d87832cbe10e09d118c19534b014c6a4
alan <alan@voxel51.com>
2020-04-23 16:52:00 -0400

    Refactor, add tabs to overlay page

--- electron/app/components/Overview.tsx
+++ electron/app/components/Overview.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react';
+import SidebarLayout from './SidebarLayout';
 
 export default function Overview() {
-  return (
-    <Sidebar.Pushable as={Segment}>
-      <Sidebar as={Menu} icon="labeled" inverted vertical visible width="thin">
-        <Menu.Item>Dashboard Navigation</Menu.Item>
-        <Menu.Item as="a">
-          <Icon name="camera" />
-          Option 1
-        </Menu.Item>
-        <Menu.Item as="a">
-          <Icon name="zoom-in" />
-          Option 2
-        </Menu.Item>
-        <Menu.Item as="a">
-          <Icon name="random" />
-          Option 3
-        </Menu.Item>
-      </Sidebar>
+  const [tab, setTab] = useState('overview');
 
-      <Sidebar.Pusher>
-        <Segment basic>
-          <Header as="h3">Application Content</Header>
-        </Segment>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
+  return (
+    <SidebarLayout>
+      <Header as="h3">Overview: [name]</Header>
+
+      <Menu pointing secondary>
+        {['overview', 'pools', 'side-by-side', 'overlayed'].map((item) => (
+          <Menu.Item
+            key={item}
+            name={item}
+            active={tab === item}
+            onClick={() => setTab(item)}
+          />
+        ))}
+      </Menu>
+    </SidebarLayout>
   );
 }
---
+++ electron/app/components/SidebarLayout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Header, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react';
+
+export default function SidebarLayout({ children }) {
+  return (
+    <Sidebar.Pushable as={Segment}>
+      <Sidebar as={Menu} icon="labeled" inverted vertical visible width="thin">
+        <Menu.Item>Dashboard Navigation</Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="camera" />
+          Option 1
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="zoom-in" />
+          Option 2
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="random" />
+          Option 3
+        </Menu.Item>
+      </Sidebar>
+
+      <Sidebar.Pusher>
+        <Segment basic>{children}</Segment>
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
+  );
+}
