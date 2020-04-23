e0dda7c3f32e4676b802f75dea3a959ad0b6c6df
alan <alan@voxel51.com>
2020-04-23 16:18:02 -0400

    Tweak sidebar

--- electron/app/components/Overview.tsx
+++ electron/app/components/Overview.tsx
@@ -4,25 +4,19 @@ import { Header, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react';
 export default function Overview() {
   return (
     <Sidebar.Pushable as={Segment}>
-      <Sidebar
-        as={Menu}
-        icon="labeled"
-        inverted
-        vertical
-        visible
-        width="thin"
-      >
-        <Menu.Item as="a">
-          <Icon name="home" />
-          Home
-        </Menu.Item>
-        <Menu.Item as="a">
-          <Icon name="gamepad" />
-          Games
-        </Menu.Item>
+      <Sidebar as={Menu} icon="labeled" inverted vertical visible width="thin">
+        <Menu.Item>Dashboard Navigation</Menu.Item>
         <Menu.Item as="a">
           <Icon name="camera" />
-          Channels
+          Option 1
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="zoom-in" />
+          Option 2
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="random" />
+          Option 3
         </Menu.Item>
       </Sidebar>
 
