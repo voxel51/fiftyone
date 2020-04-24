08822c5612f2dfaf12d022358203fc7bbc29e8ac
Tyler Ganter <tyler@voxel51.com>
2020-04-24 12:52:30 -0400

    classier

--- fiftyone/core/dataset.py
+++ fiftyone/core/dataset.py
@@ -15,15 +15,6 @@ META_COLLECTION = "_meta"
 def list_dataset_names(database_name=None):
     return _list_collection_names(collection_type="dataset", database_name=database_name)
 
-def list_view_names(database_name=None):
-    return _list_collection_names(collection_type="view", database_name=database_name)
-
-def _list_collection_names(collection_type, database_name=None):
-    db = _get_database(name=database_name)
-    return [
-        collection_name for collection_name in db.list_collection_names()
-        if _in_meta_collection(collection_name, collection_type, database_name=database_name)
-    ]
 
 def ingest_dataset():
     pass
@@ -34,6 +25,7 @@ def load_dataset(name, database_name=None):
 
 
 class _SampleCollection(object):
+    COLLECTION_TYPE = "subclass must specify"
 
     def __init__(self, name):
         self.name = name
@@ -42,37 +34,68 @@ class _SampleCollection(object):
     def __len__(self):
         return self._c.count_documents({})
 
-    def all_samples(self):
-        # iterator?
-        pass
+    @property
+    def database_name(self):
+        raise NotImplementedError("Subclass must implement")
 
-    def index_samples_by_filehash(self):
-        index_id = None
-        return index_id
-
-    def select_samples(index, method="max-covering", num_samples=100,
-                       format="voxf.types.datasets.PytorchImageDataset"):
-        pass
-
-    def export(self):
-        '''
-        samples.export(
-           "/path/for/export", format=voxf.types.datasets.LabeledImageDataset,
-        )
-        '''
-        pass
+    # def all_samples(self):
+    #     # iterator?
+    #     pass
+    #
+    # def index_samples_by_filehash(self):
+    #     index_id = None
+    #     return index_id
+    #
+    # def select_samples(index, method="max-covering", num_samples=100,
+    #                    format="voxf.types.datasets.PytorchImageDataset"):
+    #     pass
+    #
+    # def export(self):
+    #     '''
+    #     samples.export(
+    #        "/path/for/export", format=voxf.types.datasets.LabeledImageDataset,
+    #     )
+    #     '''
+    #     pass
 
     # PRIVATE #################################################################
 
+    @property
+    def _db(self):
+        raise NotImplementedError("Subclass must implement")
+
+    def _init_collection(self):
+        raise NotImplementedError("Subclass must implement")
+
     def _get_collection(self):
         '''Get the collection backing this _SampleCollection'''
-        raise NotImplementedError("Subclass must implement")
+        if self.name in self._db.list_collection_names():
+            # make sure it's the right collection type
+            in_meta = _in_meta_collection(
+                name=self.name, collection_type=self.COLLECTION_TYPE)
+            assert in_meta, "raise a better error!"
+
+        else:
+            # add to meta collection
+            c = _get_meta_collection(database_name=self.database_name)
+            c.update_one({"collection_type": self.COLLECTION_TYPE},
+                         {"$push": {"members": self.name}})
+
+            self._init_collection()
+
+        return self._db[self.name]
 
 
 class Dataset(_SampleCollection):
+    COLLECTION_TYPE = "dataset"
+
     def __init__(self, name, database_name=None):
         self._database_name = database_name or DEFAULT_DATABASE
         super(Dataset, self).__init__(name=name)
+
+    @property
+    def database_name(self):
+        return self._database_name
 
     def get_tags(self):
         return self._c.distinct("tags")
@@ -92,15 +115,21 @@ class Dataset(_SampleCollection):
     def _db(self):
         return _get_database(self._database_name)
 
-    def _get_collection(self):
-        return _get_dataset(self.name, database_name=self._database_name)
+    def _init_collection(self):
+        pass
 
 
 class DatasetView(_SampleCollection):
+    COLLECTION_TYPE = "view"
+
     def __init__(self, dataset, tag):
         self.dataset = dataset
         self.tag = tag
         super(DatasetView, self).__init__(name=self._get_name())
+
+    @property
+    def database_name(self):
+        return self.dataset.database_name
 
     # PRIVATE #################################################################
 
@@ -111,11 +140,15 @@ class DatasetView(_SampleCollection):
     def _db(self):
         return self.dataset._db
 
-    def _get_collection(self):
-        return _get_view(
-            name=self.name, dataset_name=self.dataset.name,
-            tag=self.tag, database_name=self.dataset._database_name
-        )
+    def _init_collection(self):
+        # create view
+        self._db.command({
+            "create": self.name,
+            "viewOn": self.dataset.name,
+            "pipeline": [
+                {"$match": {"tags": self.tag}}
+            ]
+        })
 
     def _drop_view(self):
         # not sure if this is needed, but I want it available in case
@@ -129,7 +162,20 @@ def _get_database(name=None):
     return pymongo.MongoClient()[name or DEFAULT_DATABASE]
 
 
+def list_view_names(database_name=None):
+    return _list_collection_names(collection_type="view", database_name=database_name)
+
+
+def _list_collection_names(collection_type, database_name=None):
+    db = _get_database(name=database_name)
+    return [
+        collection_name for collection_name in db.list_collection_names()
+        if _in_meta_collection(collection_name, collection_type, database_name=database_name)
+    ]
+
+
 def _get_meta_collection(database_name=None):
+    '''Get the meta collection (and initialize if necessary)'''
     db = _get_database(name=database_name)
     c = db[META_COLLECTION]
     if not c.count({"collection_type": "dataset"}):
@@ -153,46 +199,3 @@ def _in_meta_collection(name, collection_type, database_name=None):
     c = _get_meta_collection(database_name=database_name)
     members = c.find_one({"collection_type": collection_type})["members"]
     return name in members
-
-
-def _get_dataset(name, database_name=None):
-    collection_type = "dataset"
-
-    db = _get_database(name=database_name)
-
-    if name in db.list_collection_names():
-        # make sure it's a dataset
-        in_meta = _in_meta_collection(name=name,
-                                      collection_type=collection_type)
-        assert in_meta, "raise a better error!"
-    else:
-        # add to meta collection
-        c = _get_meta_collection(database_name=database_name)
-        c.update_one({"collection_type": collection_type}, {"$push": {"members": name}})
-
-    return db[name]
-
-
-def _get_view(name, dataset_name, tag, database_name=None):
-    collection_type = "view"
-
-    db = _get_database(name=database_name)
-
-    if name in db.list_collection_names():
-        # make sure it's a view
-        in_meta = _in_meta_collection(name=name,
-                                      collection_type=collection_type)
-        assert in_meta, "raise a better error!"
-    else:
-        # add to meta collection
-        c = _get_meta_collection(database_name=database_name)
-        c.update_one({"collection_type": "view"}, {"$push": {"members": name}})
-
-        # create view
-        db.command({
-            "create": name,
-            "viewOn": dataset_name,
-            "pipeline": [
-                {"$match": {"tags": tag}}
-            ]
-        })
