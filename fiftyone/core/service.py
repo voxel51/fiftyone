a66447c76f64246ae575f5d7a1e64cc4ebf35cb6
Tyler Ganter <tyler@voxel51.com>
2020-04-27 17:17:44 -0400

    install fixes

--- fiftyone/core/service.py
+++ fiftyone/core/service.py
@@ -13,7 +13,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import *
-from future.utils import itervalues
 
 # pragma pylint: enable=redefined-builtin
 # pragma pylint: enable=unused-wildcard-import
@@ -30,7 +29,7 @@ class Service(object):
     """A Service is a class that implements a start method and stop method"""
 
     _DEVNULL = open(os.devnull, "wb")
-    _SUPRESS = {"stderr": _DEVNULL, "stdout": _DEVNULL}
+    _SUPPRESS = {"stderr": _DEVNULL, "stdout": _DEVNULL}
 
     def __init__(self):
         """Creates the `Service`."""
@@ -56,11 +55,11 @@ class DatabaseService(Service):
 
     def start(self):
         """Stop the `DatabaseService`."""
-        etau.call(foc.START_DB, **self._SUPRESS)
+        etau.call(foc.START_DB, **self._SUPPRESS)
 
     def stop(self):
         """Start the `DatabaseService`."""
-        etau.call(foc.STOP_DB, **self._SUPRESS)
+        etau.call(foc.STOP_DB, **self._SUPPRESS)
 
 
 class ServerService(Service):
@@ -70,11 +69,11 @@ class ServerService(Service):
 
     def start(self):
         """Stop the `ServerService`."""
-        etau.call(foc.START_SERVER, **self._SUPRESS)
+        etau.call(foc.START_SERVER, **self._SUPPRESS)
 
     def stop(self):
         """Start the `ServerService`."""
-        etau.call(foc.STOP_SERVER, **self._SUPRESS)
+        etau.call(foc.STOP_SERVER, **self._SUPPRESS)
 
 
 class AppService(Service):
@@ -84,8 +83,8 @@ class AppService(Service):
     def start(self):
         """Stop the `AppService`."""
         with etau.WorkingDir(foc.FIFTYONE_APP_DIR):
-            etau.call(foc.START_APP, **self._SUPRESS)
+            etau.call(foc.START_APP, **self._SUPPRESS)
 
     def stop(self):
         """Start the `AppService`."""
-        etau.call(foc.STOP_APP, **self._SUPRESS)
+        etau.call(foc.STOP_APP, **self._SUPPRESS)
