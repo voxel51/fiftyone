f0bb9af4b0242f4543c566747bc16ea9d43ee2de
alan <alan@voxel51.com>
2020-04-28 10:52:20 -0400

    Fix bar height

--- electron/app/components/Histogram.tsx
+++ electron/app/components/Histogram.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts";
 
 export default function Histogram({ data }) {
+  const barHeight = 30;
   return (
-    <BarChart width={250} height={200} data={data} layout="vertical">
+    <BarChart
+      width={250}
+      height={data.length * barHeight}
+      data={data}
+      layout="vertical"
+      margin={0}
+    >
       <XAxis type="number" hide />
       <YAxis type="category" dataKey="name" axisLine={false} tickLine={false} />
-      <Bar dataKey="count" fill="#8884d8">
+      <Bar dataKey="count" fill="#8884d8" barSize={barHeight}>
         <LabelList dataKey="count" position="right" />
       </Bar>
     </BarChart>
