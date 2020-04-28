f44716fd881ec1516322a01dd90265d2b6dc5489
alan <alan@voxel51.com>
2020-04-28 15:04:08 -0400

    Work around cut-off histogram labels

--- electron/app/components/Histogram.tsx
+++ electron/app/components/Histogram.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts";
 
 export default function Histogram({ data }) {
   const barHeight = 30;
+  const [rightMargin, setRightMargin] = useState(0);
+  const container = useRef(null);
+
+  useEffect(() => {
+    if (container.current && container.current.container) {
+      const nodes = container.current.container.querySelectorAll(
+        "text.recharts-label"
+      );
+      if (nodes.length) {
+        const rects = Array.from(nodes).map((node) =>
+          node.getBoundingClientRect()
+        );
+        const rightmost = rects.sort((n1, n2) => n2.right - n1.right)[0];
+        setRightMargin(rightmost.width);
+      }
+    }
+  }, [container.current, data]);
+
   return (
     <BarChart
-      width={250}
+      ref={container}
+      width={350}
       height={data.length * barHeight}
       data={data}
       layout="vertical"
-      margin={0}
+      margin={{ top: 0, left: 0, bottom: 0, right: rightMargin + 5 }}
     >
       <XAxis type="number" hide />
       <YAxis type="category" dataKey="name" axisLine={false} tickLine={false} />
