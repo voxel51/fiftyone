c8808d3c612e22478eac804169cff6d87da22d04
Benjamin Kane <ben@voxel51.com>
2020-04-30 09:31:12 -0400

    sidebar rough sketch

--- electron/app/components/Sidebar.tsx
+++ electron/app/components/Sidebar.tsx
@@ -14,17 +14,40 @@ export default (props) => {
       visible={true}
     >
       <Menu.Item as="h3">FiftyOne</Menu.Item>
-      <Menu.Item as="h5">View Info:</Menu.Item>
-      <Menu.Item as="h5">Query Info:</Menu.Item>
-      {props.update.state && props.update.state.pipeline
-        ? Object.keys(props.update.state.pipeline).map((k) => {
+      <Menu.Item as="h4">
+        {props.update.state ? (
+          <>Dataset: {props.update.state.dataset_name}</>
+        ) : (
+          <>No dataset loaded</>
+        )}
+      </Menu.Item>
+      <Menu.Item as="h5">Page Info:</Menu.Item>
+      {props.update.state && props.update.state.page
+        ? Object.keys(props.update.state.page).map((k) => {
+            const v = props.update.state.page[k];
             return (
-              <Menu.Item as="h6">
-                {String(Object.keys(props.update.state.pipeline[k])[0])}
+              <Menu.Item as="h5">
+                {k} : {v}
               </Menu.Item>
             );
           })
         : null}
+      <Menu.Item as="h5">View Info:</Menu.Item>
+      <Menu.Item as="h5">
+        {props.update.state.view_tag ? props.update.state.view_tag : "No view"}
+      </Menu.Item>
+      <Menu.Item as="h5">Query Info:</Menu.Item>
+      {props.update.state && props.update.state.query ? (
+        Object.keys(props.update.state.query).map((k) => {
+          return (
+            <Menu.Item as="h5">
+              {String(Object.keys(props.update.state.query[k])[0])}
+            </Menu.Item>
+          );
+        })
+      ) : (
+        <Menu.Item as="h5">Empty query</Menu.Item>
+      )}
     </Sidebar>
   );
 };
