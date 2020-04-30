3e2bd8e2f63b283a0f187a04c3b9baf3df69cb0c
brimoor <brimoor@umich.edu>
2020-04-30 03:27:46 -0400

    adding package init

---
+++ fiftyone/zoo/__init__.py
@@ -0,0 +1,26 @@
+"""
+The FiftyOne Dataset Zoo.
+
+This package defines a collection of open source datasets made available for
+download via FiftyOne.
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+# pragma pylint: disable=redefined-builtin
+# pragma pylint: disable=unused-wildcard-import
+# pragma pylint: disable=wildcard-import
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import *
+
+# pragma pylint: enable=redefined-builtin
+# pragma pylint: enable=unused-wildcard-import
+# pragma pylint: enable=wildcard-import
+
+import fiftyone as fo
+
+from .base import *
