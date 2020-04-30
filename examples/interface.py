b5a4f512fdfb9cfeb60da50e1650c02b2b9c8673
brimoor <brimoor@umich.edu>
2020-04-30 04:15:50 -0400

    moving config to core package

--- examples/interface.py
+++ examples/interface.py
@@ -8,11 +8,11 @@ Initial demo of the FiftyOne interface.
 
 import random
 
-import fiftyone as fo
+import fiftyone.core.config as foc
 
 # Set these to configure which ML backend will be used to provide zoo datasets
-# fo.set_config_settings(default_ml_backend="tensorflow")
-# fo.set_config_settings(default_ml_backend="torch")
+# foc.set(default_ml_backend="tensorflow")
+# foc.set(default_ml_backend="torch")
 
 import fiftyone.core.data as fod
 import fiftyone.core.datautils as fodu
