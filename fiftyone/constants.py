fd3abff922771f3fab3fe02d9eba70f7d9a4d90f
brimoor <brimoor@umich.edu>
2020-04-30 03:23:26 -0400

    adding a FiftyOne config to store settings

--- fiftyone/constants.py
+++ fiftyone/constants.py
@@ -29,6 +29,7 @@ except ImportError:
 # Directories
 FIFTYONE_DIR = os.path.abspath(os.path.dirname(__file__))
 FIFTYONE_CONFIG_DIR = os.path.join(os.path.expanduser("~"), ".fiftyone")
+FIFTYONE_CONFIG_PATH = os.path.join(FIFTYONE_CONFIG_DIR, "config.json")
 BASE_DIR = os.path.dirname(FIFTYONE_DIR)
 EXAMPLES_DIR = os.path.join(BASE_DIR, "examples")
 
