52c532b78464fce03b97565f9dfade75551357e6
brimoor <brimoor@umich.edu>
2020-04-30 03:24:59 -0400

    adding base infrastructure for DatasetZoo

---
+++ fiftyone/zoo/base.py
@@ -0,0 +1,210 @@
+"""
+Base definitions for the FiftyOne Dataset Zoo.
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+# pragma pylint: disable=redefined-builtin
+# pragma pylint: disable=unused-wildcard-import
+# pragma pylint: disable=wildcard-import
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import *
+
+# pragma pylint: enable=redefined-builtin
+# pragma pylint: enable=unused-wildcard-import
+# pragma pylint: enable=wildcard-import
+
+import logging
+import os
+
+import eta.core.datasets as etads
+import eta.core.serial as etas
+import eta.core.utils as etau
+
+import fiftyone as fo
+
+
+logger = logging.getLogger(__name__)
+
+
+# This list is updated when __init_zoo_datasets__() is called
+AVAILABLE_DATASETS = {}
+
+
+def __init_zoo_datasets__():
+    # Imports datasets made available by the TensorFlow backend
+    if fo.config.default_ml_backend == "tensorflow":
+        import fiftyone.zoo.tf  # pylint: disable=unused-import
+
+    # Imports datasets made available by the Torch backend
+    if fo.config.default_ml_backend == "torch":
+        import fiftyone.zoo.torch  # pylint: disable=unused-import
+
+
+def get_zoo_dataset(name):
+    """Returns the ZooDataset instance for the dataset of the given name.
+
+    Args:
+        name: the name of the dataset
+
+    Returns:
+        the ZooDataset instance
+    """
+    __init_zoo_datasets__()
+
+    if name.lower() not in AVAILABLE_DATASETS:
+        raise ValueError("Dataset '%s' not found in the zoo" % name)
+
+    zoo_dataset_cls = AVAILABLE_DATASETS[name.lower()]
+    return zoo_dataset_cls()
+
+
+def list_zoo_datasets():
+    """Returns the list of available datasets in the zoo."""
+    __init_zoo_datasets__()
+    return list(AVAILABLE_DATASETS.keys())
+
+
+class ZooDatasetInfo(etas.Serializable):
+    """Class containing info about a dataset in the FiftyOne Dataset Zoo."""
+
+    def __init__(
+        self, name, zoo_dataset, split, num_samples, format, labels_map=None
+    ):
+        """Creates a ZooDatasetInfo instance.
+
+        Args:
+            name: the name of the dataset
+            zoo_dataset: the ZooDataset class
+            split: the dataset split
+            num_samples: the number of samples in the dataset
+            format: the class of the dataset on disk
+            labels_map: an optional dict mapping class IDs to class labels
+        """
+        self.name = name
+        self.zoo_dataset = etau.get_class_name(zoo_dataset)
+        self.split = split
+        self.num_samples = num_samples
+        self.format = etau.get_class_name(format)
+        self.labels_map = labels_map
+
+    def attributes(self):
+        """Returns a list of class attributes to be serialized.
+
+        Returns:
+            a list of class attributes
+        """
+        _attrs = ["name", "zoo_dataset", "split", "num_samples", "format"]
+        if self.labels_map is not None:
+            _attrs.append("labels_map")
+
+        return _attrs
+
+    @classmethod
+    def from_dict(cls, d):
+        """Loads a ZooDatasetInfo from a JSON dictionary.
+
+        Args:
+            d: a JSON dictionary
+
+        Returns:
+            a ZooDatasetInfo
+        """
+        labels_map = d.get("labels_map", None)
+        return cls(
+            d["name"],
+            etau.get_class(d["zoo_dataset"]),
+            d["split"],
+            d["num_samples"],
+            etau.get_class(d["format"]),
+            labels_map=labels_map,
+        )
+
+
+class ZooDataset(object):
+    """Base class for datasets made available in the FiftyOne Dataset Zoo."""
+
+    @property
+    def name(self):
+        """The name of the dataset."""
+        raise NotImplementedError("subclasses must implement name")
+
+    @property
+    def supported_splits(self):
+        """A tuple of supported splits for the dataset, or None if the dataset
+        does not have splits.
+        """
+        raise NotImplementedError("subclasses must implement supported_splits")
+
+    @property
+    def default_split(self):
+        """The default split for the dataset, or None if the dataset does
+        not have splits.
+        """
+        raise NotImplementedError("subclasses must implement default_split")
+
+    def download_and_prepare(self, dataset_dir, split=None):
+        """Downloads the dataset and prepares it for use in the given directory
+        as an ``eta.core.datasets.LabeledDataset``.
+
+        If the ``ZooDatasetInfo`` file already exists in the directory, this
+        method assumes that the dataset is already downloaded, and does
+        nothing.
+
+        Args:
+            dataset_dir: the directory in which to construct the dataset
+            split: the dataset split to download, if applicable. If omitted,
+                the default split is downloaded
+        """
+        info_path = os.path.join(dataset_dir, "info.json")
+
+        if os.path.isfile(info_path):
+            logger.debug("ZooDatasetInfo file '%s' already exists", info_path)
+            return
+
+        if split is None:
+            split = self.default_split
+            logger.info("Using default split '%s'", split)
+
+        if split is not None and split not in self.supported_splits:
+            raise ValueError(
+                "Invalid split '%s'; supported values are %s"
+                % (split, self.supported_splits)
+            )
+
+        logger.info("Downloading dataset to '%s'", dataset_dir)
+        info = self._download_and_prepare(dataset_dir, split)
+
+        info.write_json(info_path, pretty_print=True)
+        logger.info("Dataset info written to '%s'", info_path)
+
+    def _download_and_prepare(self, dataset_dir, split):
+        raise NotImplementedError(
+            "subclasses must implement download_and_prepare()"
+        )
+
+    #
+    # @todo support other formats here? just delete?
+    #
+    def load(self, dataset_dir, split=None, download=False):
+        """Loads the dataset backed by the given directory as an
+        ``eta.core.datasets.LabeledDataset``.
+
+        Args:
+            dataset_dir: the backing directory for the dataset
+            split: the dataset split to download, if applicable. If omitted,
+                the default split is loaded
+            download: whether to download the dataset, if necessary. The
+                default is False
+
+        Returns:
+            an ``eta.core.datasets.LabeledDataset``
+        """
+        if download:
+            self.download_and_prepare(dataset_dir, split=split)
+
+        return etads.load_dataset(dataset_dir)
