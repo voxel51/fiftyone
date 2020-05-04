80971e701da77543e18435adad1e0d1d95512ee5
Benjamin Kane <ben@voxel51.com>
2020-05-04 10:00:10 -0400

    creating StateDescription serializable object for sharing GUI/SDK state

--- fiftyone/server/main.py
+++ fiftyone/server/main.py
@@ -12,6 +12,8 @@ os.environ["FIFTYONE_SERVER"] = "1"  # noqa
 from flask import Flask, request, send_file
 from flask_socketio import emit, Namespace, SocketIO
 
+import eta.core.serial as etas
+
 import fiftyone.core.dataset as fod
 import query
 
@@ -31,6 +33,49 @@ def get_sample_media():
     path = request.args.get("path")
     mime_type = request.args.get("mime_type")
     return send_file(path, mimetype=mime_type)
+
+
+class StateDescription(etas.Serializable):
+    """A StateDescription describes the shared state between the FiftyOne GUI
+    and the FiftyOne Session.
+
+    Attributes:
+        dataset: (optional) the current dataset
+        view: (optional) the current view
+        pipeline: (optional) the current pipeline (or query)
+    """
+
+    def __init__(self, dataset=None, view=None, pipeline=None):
+        """Creates a StateDescription instance.
+
+        Args:
+            dataset: (optional) the current dataset
+            view: (optional) the current view
+            pipeline: (optional) the current pipeline (or query)
+        """
+        self.dataset = dataset
+        self.view = view
+        self.pipeline = pipeline or []
+
+    @classmethod
+    def from_dict(cls, d, **kwargs):
+        """Constructs a StateDescription from a JSON dictionary.
+
+        Args:
+            d: a JSON dictionary
+
+        Returns:
+            a StateDescription
+        """
+        dataset = d.get("dataset", None)
+        if dataset is not None:
+            dataset = fod.Dataset(dataset)
+
+        view = d.get("view", None)
+
+        pipeline = d.get("pipeline", None)
+
+        return cls(dataset=dataset, view=view, pipeline=pipeline)
 
 
 class State(Namespace):
