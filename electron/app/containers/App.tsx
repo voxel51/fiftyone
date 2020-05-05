cca35fc82ebe3e02eaace72b01c22d1e0f302e19
Benjamin Kane <ben@voxel51.com>
2020-05-05 12:18:33 -0400

    load current state on app launch

--- electron/app/containers/App.tsx
+++ electron/app/containers/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { connect } from "react-redux";
 import Sidebar from "../components/Sidebar";
 import { updateState } from "../actions/update";
@@ -14,8 +14,17 @@ const mapStateToProps = (state = {}) => {
 
 function App(props: Props) {
   const { children, dispatch, update } = props;
+  const [connectionEstablished, setConnectionEstablished] = useState(false);
   const socket = getSocket("state");
-  useSubscribe(socket, "connect", () => console.log("connected"));
+  useSubscribe(socket, "connect", () => {
+    console.log("connected");
+    if (!connectionEstablished) {
+      socket.emit("get_current_state", "", (data) => {
+        dispatch(updateState(data));
+      });
+      setConnectionEstablished(true);
+    }
+  });
   useSubscribe(socket, "disconnect", () => console.log("disconnected"));
   useSubscribe(socket, "update", (data) => {
     dispatch(updateState(data));
