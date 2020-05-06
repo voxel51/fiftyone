ba6250f40eed569ce53498dceacf5acf9e3b3632
brimoor <brimoor@umich.edu>
2020-05-06 11:43:58 -0400

    adding rest of ODMLabels classes

--- fiftyone/core/odm.py
+++ fiftyone/core/odm.py
@@ -22,64 +22,111 @@ from builtins import *
 from mongoengine import *
 
 
-DEFAULT_DATABASE = "fiftyone"
+_DEFAULT_DATABASE = "fiftyone"
 
 
-_db = connect(DEFAULT_DATABASE)
+_db = connect(_DEFAULT_DATABASE)
 
 
 def drop_database():
-    _db.drop_database(DEFAULT_DATABASE)
+    """Drops the entire database."""
+    _db.drop_database(_DEFAULT_DATABASE)
 
 
 class ODMDocument(Document):
+    """Base class for documents backing
+    :class:`fiftyone.core.document.BackedByDocument` classes.
+    """
+
     meta = {"allow_inheritance": True}
 
 
 class ODMMetadata(EmbeddedDocument):
+    """Base class for storing metadata about raw data."""
+
     size_bytes = IntField()
     mime_type = StringField()
-
     meta = {"allow_inheritance": True}
 
 
 class ODMImageMetadata(ODMMetadata):
+    """Base class for storing metadata about raw images."""
+
     width = IntField()
     height = IntField()
     num_channels = IntField()
 
 
 class ODMLabels(EmbeddedDocument):
-    group = StringField()
+    """Base class for documents that back sample labels."""
 
+    group = StringField()
     meta = {"allow_inheritance": True}
 
 
 class ODMClassificationLabel(ODMLabels):
+    """Backing document for :class:`fiftyone.core.labels.ClassificationLabel`
+    instances.
+    """
+
     label = StringField()
     confidence = FloatField()
 
 
-class ODMInsight(EmbeddedDocument):
-    group = StringField()
+class ODMDetectionLabel(EmbeddedDocument):
+    """Backing document for individual detections stored in
+    :class:`fiftyone.core.labels.DetectionLabels`instances.
+    """
 
+    label = StringField()
+    confidence = FloatField()
+    bounding_box = ListField(FloatField())
+
+
+class ODMDetectionLabels(ODMLabels):
+    """Backing document for :class:`fiftyone.core.labels.DetectionLabels`
+    instances.
+    """
+
+    detections = ListField(EmbeddedDocumentField(ODMDetectionLabel))
+
+
+class ODMImageLabels(ODMLabels):
+    """Backing document for :class:`fiftyone.core.labels.ImageLabels`
+    instances.
+    """
+
+    labels = DictField()
+
+
+class ODMInsight(EmbeddedDocument):
+    """Base class for documents that back sample insights."""
+
+    group = StringField()
     meta = {"allow_inheritance": True}
 
 
 class ODMFileHashInsight(ODMInsight):
+    """Backing document for file hash insights."""
+
     file_hash = StringField()
 
 
 class ODMSample(ODMDocument):
+    """Backing document for :class:`fiftyone.core.sample.Sample` instances."""
+
     dataset = StringField()
     filepath = StringField(unique=True)
     metadata = EmbeddedDocumentField(ODMMetadata)
     tags = ListField(StringField())
     insights = ListField(EmbeddedDocumentField(ODMInsight))
     labels = ListField(EmbeddedDocumentField(ODMLabels))
-
     meta = {"allow_inheritance": True, "indexes": ["dataset", "filepath"]}
 
 
 class ODMImageSample(ODMSample):
+    """Backing document for :class:`fiftyone.core.sample.ImageSample`
+    instances.
+    """
+
     metadata = EmbeddedDocumentField(ODMImageMetadata)
