2bcf6722cc0f25af1ac218032e8913132212e6a4
Tyler Ganter <tyler@voxel51.com>
2020-05-06 13:42:07 -0400

    labels test

--- tests/labels_test.py
+++ tests/labels_test.py
@@ -9,6 +9,7 @@ import numpy as np
 import eta.core.image as etai
 
 import fiftyone.core.dataset as fod
+import fiftyone.core.labels as fol
 import fiftyone.core.odm as foo
 import fiftyone.core.sample as fos
 
@@ -23,22 +24,9 @@ if not os.path.exists(img_path):
     img = np.random.randint(0, 255, (32, 32, 3))
     etai.write(img, img_path)
 
-
-###############################################################################
-# Create a Sample
-###############################################################################
-
 sample = fos.Sample.create_new(img_path, tags=["train", "rand"])
 
-print(sample.filepath)
-print(sample.filename)
-
-###############################################################################
-# Add to Dataset
-###############################################################################
-
 sample_id = dataset.add_sample(sample)
-# sample_ids = dataset.add_samples([sample])
 
 ###############################################################################
 # Explore
@@ -50,21 +38,13 @@ print()
 print("Num samples: %d" % len(dataset))
 print()
 
-print("Tags: %s" % dataset.get_tags())
-print()
-
 print("Label Groups: %s" % dataset.get_label_groups())
-print()
-
-print("Insight Groups: %s" % dataset.get_insight_groups())
 print()
 
 ###############################################################################
 # Synchronization test
 ###############################################################################
 
-print(sample.id)
-s2 = dataset[sample.id]
-print(s2.id)
+labels = fol.ClassificationLabel.create_new(group="ground_truth", label="cow")
 
-sample.add_tag("new_tag")
+asdf = 0
