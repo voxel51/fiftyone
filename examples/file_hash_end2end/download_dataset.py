132a75061b61f9fe1c4dd47587fdc10916b635b7
Tyler Ganter <tyler@voxel51.com>
2020-05-08 10:17:41 -0400

    download cleanup

--- examples/file_hash_end2end/download_dataset.py
+++ examples/file_hash_end2end/download_dataset.py
@@ -3,9 +3,9 @@ Download CIFAR100 data and store as images and labels JSON to:
     {{fiftyone}}/examples/data/cifar100/
 
 Data is downloaded as:
-data/
-└── cifar100/
-    ├── test/                       # 10k INDEX.jpg test images
+/tmp/fiftyone/
+└── cifar100_with_duplicates/
+    ├── apple/                       # 10k INDEX.jpg test images
     ├── test_coarse.json            # coarse labels for test images
     ├── test_fine.json              # fine-grain labels for test images
     ├── train/                      # 50k INDEX.jpg train images
@@ -20,6 +20,8 @@ from tensorflow.keras.datasets import cifar100
 
 import eta.core.image as etai
 import eta.core.utils as etau
+
+DATASET_SIZE = 1000
 
 fine_labels_map = [
     "apple",
@@ -124,81 +126,18 @@ fine_labels_map = [
     "worm",
 ]
 
-coarse_2_fine_mapping = {
-    "aquatic mammals": ["beaver", "dolphin", "otter", "seal", "whale"],
-    "fish": ["aquarium_fish", "flatfish", "ray", "shark", "trout"],
-    "flowers": ["orchid", "poppy", "rose", "sunflower", "tulip"],
-    "food containers": ["bottle", "bowl", "can", "cup", "plate"],
-    "fruit and vegetables": [
-        "apple",
-        "mushroom",
-        "orange",
-        "pear",
-        "sweet_pepper",
-    ],
-    "household electrical device": [
-        "clock",
-        "computer_keyboard",
-        "lamp",
-        "telephone",
-        "television",
-    ],
-    "household furniture": ["bed", "chair", "couch", "table", "wardrobe"],
-    "insects": ["bee", "beetle", "butterfly", "caterpillar", "cockroach"],
-    "large carnivores": ["bear", "leopard", "lion", "tiger", "wolf"],
-    "large man-made outdoor things": [
-        "bridge",
-        "castle",
-        "house",
-        "road",
-        "skyscraper",
-    ],
-    "large natural outdoor scenes": [
-        "cloud",
-        "forest",
-        "mountain",
-        "plain",
-        "sea",
-    ],
-    "large omnivores and herbivores": [
-        "camel",
-        "cattle",
-        "chimpanzee",
-        "elephant",
-        "kangaroo",
-    ],
-    "medium-sized mammals": ["fox", "porcupine", "possum", "raccoon", "skunk"],
-    "non-insect invertebrates": ["crab", "lobster", "snail", "spider", "worm"],
-    "people": ["baby", "boy", "girl", "man", "woman"],
-    "reptiles": ["crocodile", "dinosaur", "lizard", "snake", "turtle"],
-    "small mammals": ["hamster", "mouse", "rabbit", "shrew", "squirrel"],
-    "trees": [
-        "maple_tree",
-        "oak_tree",
-        "palm_tree",
-        "pine_tree",
-        "willow_tree",
-    ],
-    "vehicles 1": ["bicycle", "bus", "motorcycle", "pickup_truck", "train"],
-    "vehicles 2": ["lawn_mower", "rocket", "streetcar", "tank", "tractor"],
-}
-
-fine_2_course_mapping = {
-    fine: coarse
-    for coarse, fine_list in coarse_2_fine_mapping.items()
-    for fine in fine_list
-}
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
-data_dir = os.path.join(dir_path, "data/cifar_fh_e2e")
+data_dir = os.path.join(dir_path, "/tmp/fiftyone/cifar100_with_duplicates")
 
-# if not empty, don't download
-etau.ensure_empty_dir(data_dir)
+# if not empty, delete current contents
+etau.ensure_empty_dir(data_dir, cleanup=True)
 
 (_, _), (x_test, y_test) = cifar100.load_data(label_mode="fine")
 
-x = x_test
-y = y_test
+dataset_size = min(DATASET_SIZE, 10000)
+
+x = x_test[:dataset_size, :]
+y = y_test[:dataset_size, :]
 
 for i in range(x.shape[0]):
     if random.random() > 0.95:
