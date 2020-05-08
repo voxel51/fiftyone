90e9c6231a419ef58bfe28922a4c631c253140f2
brimoor <brimoor@umich.edu>
2020-05-08 10:27:00 -0400

    linting

--- fiftyone/core/view.py
+++ fiftyone/core/view.py
@@ -61,15 +61,14 @@ class DatasetView(foc.SampleCollection):
         return next(result)["count"]
 
     def __getitem__(self, sample_id):
-        # try to find the sample_id in the pipeline
         try:
+            # Try to find `sample_id` in the pipeline
             pipeline = [{"$match": {"_id": ObjectId(sample_id)}}]
             next(self.aggregate(pipeline))
             found = True
         except StopIteration:
             found = False
 
-        # sample_id not in pipeline
         if not found:
             raise KeyError("No sample found with ID '%s'" % sample_id)
 
