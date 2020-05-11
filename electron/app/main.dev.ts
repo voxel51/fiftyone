3c3f23d194d532864dc608476f9a6e94df2d0c65
Benjamin Kane <ben@voxel51.com>
2020-05-11 10:51:36 -0400

    proposing to always fully close close app, even on macos

--- electron/app/main.dev.ts
+++ electron/app/main.dev.ts
@@ -102,11 +102,7 @@ const createWindow = async () => {
  */
 
 app.on("window-all-closed", () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on("ready", createWindow);
