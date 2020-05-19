ad525f576fd0a89bfe171b512577c026886f4d60
alan <alan@voxel51.com>
2020-05-19 15:49:12 -0400

    Switch to using voxel51-eta package and rely on it to specify its dependencies

--- setup.py
+++ setup.py
@@ -17,37 +17,20 @@ class BdistWheelCustom(bdist_wheel):
         # pure Python, so build a wheel for any Python version
         self.universal = True
 
-    def write_wheelfile(self, wheelfile_base, *args, **kwargs):
-        bdist_wheel.write_wheelfile(self, wheelfile_base, *args, **kwargs)
-        # ETA's constants.py looks up ETA's version information dynamically from
-        # site-packages/ETA-x.y.z.dist-info, which is not created when we bundle
-        # ETA with fiftyone. This is a workaround to make ETA use fiftyone's
-        # version information instead.
-        # TODO: package ETA separately to avoid this
-        constants_path = os.path.join(self.bdist_dir, "eta", "constants.py")
-        with open(constants_path, "r") as constants_file:
-            contents = constants_file.read()
-        if 'metadata("ETA")' not in contents:
-            raise ValueError("Could not rewrite %r" % constants_path)
-        contents = contents.replace('metadata("ETA")', 'metadata("fiftyone")')
-        with open(constants_path, "w") as constants_file:
-            constants_file.write(contents)
-
 
 cmdclass = {
     "bdist_wheel": BdistWheelCustom,
 }
 
 
-with open("requirements/common.txt") as reqs, open(
-    "eta/requirements/common.txt"
-) as eta_reqs:
-    requirements = eta_reqs.readlines() + reqs.readlines()
+with open("requirements/common.txt") as reqs:
+    requirements = reqs.readlines()
 
 requirements += [
     "fiftyone-brain",
     "fiftyone-db",
     "fiftyone-gui",
+    "voxel51-eta",
 ]
 
 setup(
