a6127aebb9b9822a1b4fcd84ca271eb1521d2bee
alan <alan@voxel51.com>
2020-05-22 17:03:28 -0400

    Move top-level interface to a separate file to allow importing from fiftyone-brain

---
+++ fiftyone/interface.py
@@ -0,0 +1,37 @@
+"""
+FiftyOne's basic, high-level public interface.
+
+Parts of various fiftyone sub-packages are collected here and made available
+under the top-level `fiftyone` package.
+"""
+
+import fiftyone.core.config as foc
+import fiftyone.core.service as fos
+
+_database_service = fos.DatabaseService()
+config = foc.load_config()
+
+from .core.dataset import (
+    Dataset,
+    list_dataset_names,
+    load_dataset,
+)
+from .core.labels import (
+    Label,
+    ImageLabel,
+    Classification,
+    Detection,
+    Detections,
+    ImageLabels,
+)
+from .core.metadata import (
+    Metadata,
+    ImageMetadata,
+)
+from .core.sample import Sample
+from .core.session import (
+    close_dashboard,
+    launch_dashboard,
+    Session,
+)
+from .core.view import DatasetView
