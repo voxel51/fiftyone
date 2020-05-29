0537f8533ede1862328c7f89ae80c36b52e6908b
alan <alan@voxel51.com>
2020-05-29 13:49:23 -0400

    Remove nonfunctional "Open" option

--- electron/app/menu.ts
+++ electron/app/menu.ts
@@ -217,10 +217,6 @@ export default class MenuBuilder {
         label: "&File",
         submenu: [
           {
-            label: "&Open",
-            accelerator: "Ctrl+O",
-          },
-          {
             label: "&Close",
             accelerator: "Ctrl+W",
             click: () => {
