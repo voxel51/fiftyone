19cc235dab50ef3a063d1def3cfad78647acc496
Tyler Ganter <tyler@voxel51.com>
2020-06-02 09:58:19 -0400

    insert

---
+++ tests/db_performance/README.md
@@ -0,0 +1,31 @@
+# Data Model
+
+## Libraries
+
+-   [MongoDB](https://docs.mongodb.com/)
+-   [PyMongo](https://pymongo.readthedocs.io/en/stable/)
+-   [PyMODM](https://pymodm.readthedocs.io/en/stable/)
+-   [MongoEngine](http://docs.mongoengine.org/)
+-   [MongoFrames](http://mongoframes.com/)
+
+## Desired Features
+
+-   `Dataset`s and `Sample`s are Singletons
+-   Field Validation
+-   Dynamic Schemas
+    -   which are saved to the DB
+-   Multi-process/user Synchronization
+
+...
+
+-   Indexes? …`Sample.filepath`?
+
+## Performance Benchmarking
+
+1. insert one/many
+2. update one/many
+3. delete one/many
+
+-   What is the idea batch size?
+-   How does size of documents affect this?
+-   How do indexes affect this? Is `background: true` going to help?
---
+++ tests/db_performance/performance.py
@@ -0,0 +1,337 @@
+"""
+Benchmarking packages
+
+
+  mongoengine_document_save() 1.066s per 1000 samples (batch size = 100)
+  mongoengine_document_save() 0.567s per 1000 samples (batch size = 1000)
+  mongoengine_document_save() 0.534s per 1000 samples (batch size = 10000)
+  mongoengine_document_save() 0.524s per 1000 samples (batch size = 100000)
+       pymodm_document_save() 0.421s per 1000 samples (batch size = 100)
+       pymodm_document_save() 0.436s per 1000 samples (batch size = 1000)
+       pymodm_document_save() 0.417s per 1000 samples (batch size = 10000)
+       pymodm_document_save() 0.427s per 1000 samples (batch size = 100000)
+     mongoframes_frame_save() 0.839s per 1000 samples (batch size = 100)
+     mongoframes_frame_save() 0.366s per 1000 samples (batch size = 1000)
+     mongoframes_frame_save() 0.320s per 1000 samples (batch size = 10000)
+     mongoframes_frame_save() 0.320s per 1000 samples (batch size = 100000)
+         pymongo_insert_one() 0.804s per 1000 samples (batch size = 100)
+         pymongo_insert_one() 0.314s per 1000 samples (batch size = 1000)
+         pymongo_insert_one() 0.257s per 1000 samples (batch size = 10000)
+         pymongo_insert_one() 0.254s per 1000 samples (batch size = 100000)
+         mongoengine_insert() 0.755s per 1000 samples (batch size = 100)
+         mongoengine_insert() 0.308s per 1000 samples (batch size = 1000)
+         mongoengine_insert() 0.264s per 1000 samples (batch size = 10000)
+         mongoengine_insert() 0.271s per 1000 samples (batch size = 100000)
+         pymodm_bulk_create() 0.085s per 1000 samples (batch size = 100)
+         pymodm_bulk_create() 0.075s per 1000 samples (batch size = 1000)
+         pymodm_bulk_create() 0.078s per 1000 samples (batch size = 10000)
+         pymodm_bulk_create() 0.100s per 1000 samples (batch size = 100000)
+    mongoframes_insert_many() 0.430s per 1000 samples (batch size = 100)
+    mongoframes_insert_many() 0.071s per 1000 samples (batch size = 1000)
+    mongoframes_insert_many() 0.037s per 1000 samples (batch size = 10000)
+    mongoframes_insert_many() 0.039s per 1000 samples (batch size = 100000)
+        pymongo_insert_many() 0.434s per 1000 samples (batch size = 100)
+        pymongo_insert_many() 0.057s per 1000 samples (batch size = 1000)
+        pymongo_insert_many() 0.021s per 1000 samples (batch size = 10000)
+        pymongo_insert_many() 0.017s per 1000 samples (batch size = 100000)
+
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+import fiftyone.core.odm as foo
+
+from collections import defaultdict
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pymongo import MongoClient
+import mongoengine
+import mongoframes
+import pymodm
+
+foo.drop_database()
+
+###############################################################################
+# PyMongo
+###############################################################################
+
+
+def pymongo_insert_one(n):
+    foo.drop_database()
+    client = MongoClient()
+    db = client.fiftyone
+    collection = db["test_pymongo_collection"]
+    start_time = time.time()
+    samples = [
+        {
+            "filepath": "test_%d.png" % i,
+            "tags": ["tag1", "tag2"],
+            "metadata": {"size_bytes": 512, "mime_type": ".png"},
+        }
+        for i in range(n)
+    ]
+    for sample in samples:
+        collection.insert_one(sample)
+    return time.time() - start_time
+
+
+def pymongo_insert_many(n):
+    foo.drop_database()
+    client = MongoClient()
+    db = client.fiftyone
+    collection = db["test_pymongo_collection"]
+    start_time = time.time()
+    samples = [
+        {
+            "filepath": "test_%d.png" % i,
+            "tags": [],
+            "metadata": {"size_bytes": 512, "mime_type": ".png"},
+        }
+        for i in range(n)
+    ]
+    collection.insert_many(samples)
+    return time.time() - start_time
+
+
+###############################################################################
+# PyMODM
+###############################################################################
+
+# Connect to MongoDB and call the connection "my-app".
+pymodm.connection.connect("mongodb://localhost:27017/pymodm_db")
+
+
+class PyMODMMetadata(pymodm.EmbeddedMongoModel):
+    size_bytes = pymodm.fields.IntegerField()
+    mime_type = pymodm.fields.CharField()
+
+
+class PyMODMSample(pymodm.MongoModel):
+    filepath = pymodm.fields.CharField()
+    tags = pymodm.fields.ListField(field=pymodm.fields.CharField())
+    metadata = pymodm.fields.EmbeddedDocumentField(PyMODMMetadata)
+
+
+def pymodm_document_save(n):
+    foo.drop_database()
+    start_time = time.time()
+    samples = [
+        PyMODMSample(
+            filepath="test_%d.png" % i,
+            tags=["tag1", "tag2"],
+            metadata=PyMODMMetadata(size_bytes=512, mime_type=".png"),
+        )
+        for i in range(n)
+    ]
+    for sample in samples:
+        sample.save()
+    return time.time() - start_time
+
+
+def pymodm_bulk_create(n):
+    # pylint: disable=no-member
+    foo.drop_database()
+    start_time = time.time()
+    samples = [
+        PyMODMSample(
+            filepath="test_%d.png" % i,
+            tags=["tag1", "tag2"],
+            metadata=PyMODMMetadata(size_bytes=512, mime_type=".png"),
+        )
+        for i in range(n)
+    ]
+    PyMODMSample.objects.bulk_create(samples)
+    return time.time() - start_time
+
+
+###############################################################################
+# MongoEngine
+###############################################################################
+
+
+class MongoEngineMetadata(mongoengine.EmbeddedDocument):
+    meta = {"allow_inheritance": True}
+
+    size_bytes = mongoengine.IntField()
+    mime_type = mongoengine.StringField()
+
+
+class MongoEngineSample(mongoengine.Document):
+    # The path to the data on disk
+    filepath = mongoengine.StringField()
+    # filepath = mongoengine.StringField(unique=True)
+
+    # The set of tags associated with the sample
+    tags = mongoengine.ListField()
+    # tags = mongoengine.ListField(mongoengine.StringField())
+
+    # Metadata about the sample media
+    metadata = mongoengine.EmbeddedDocumentField(
+        MongoEngineMetadata, null=True
+    )
+
+
+def mongoengine_document_save(n):
+    foo.drop_database()
+    start_time = time.time()
+    samples = [
+        MongoEngineSample(
+            filepath="test_%d.png" % i,
+            tags=["tag1", "tag2"],
+            metadata=MongoEngineMetadata(size_bytes=512, mime_type=".png"),
+        )
+        for i in range(n)
+    ]
+    for sample in samples:
+        sample.save()
+    return time.time() - start_time
+
+
+def mongoengine_insert(n):
+    # pylint: disable=no-member
+    foo.drop_database()
+    start_time = time.time()
+    samples = [
+        MongoEngineSample(
+            filepath="test_%d.png" % i,
+            tags=["tag1", "tag2"],
+            metadata=MongoEngineMetadata(size_bytes=512, mime_type=".png"),
+        )
+        for i in range(n)
+    ]
+    MongoEngineSample.objects.insert(samples)
+    return time.time() - start_time
+
+
+###############################################################################
+# MongoFrames
+###############################################################################
+
+# Connect MongoFrames to the database
+mongoframes.Frame._client = MongoClient("mongodb://localhost:27017/fiftyone")
+
+
+class MongoFramesMetadata(mongoframes.SubFrame):
+    _fields = {
+        "size_bytes",
+        "mime_type",
+    }
+
+
+class MongoFramesSample(mongoframes.Frame):
+    _fields = {
+        "filepath",
+        "tags",
+        "metadata",
+    }
+
+
+def mongoframes_frame_save(n):
+    foo.drop_database()
+    start_time = time.time()
+    samples = [
+        MongoFramesSample(
+            filepath="test_%d.png" % i,
+            tags=["tag1", "tag2"],
+            metadata=MongoFramesMetadata(size_bytes=512, mime_type=".png"),
+        )
+        for i in range(n)
+    ]
+    for sample in samples:
+        sample.insert()
+    return time.time() - start_time
+
+
+def mongoframes_insert_many(n):
+    foo.drop_database()
+    start_time = time.time()
+    samples = [
+        {
+            "filepath": "test_%d.png" % i,
+            "tags": [],
+            "metadata": MongoFramesMetadata(size_bytes=512, mime_type=".png"),
+        }
+        for i in range(n)
+    ]
+    MongoFramesSample.insert_many(samples)
+    return time.time() - start_time
+
+
+###############################################################################
+
+func_map = {
+    "insert": {
+        "pymongo": {"one": pymongo_insert_one, "many": pymongo_insert_many,},
+        "pymodm": {"one": pymodm_document_save, "many": pymodm_bulk_create,},
+        "mongoengine": {
+            "one": mongoengine_document_save,
+            "many": mongoengine_insert,
+        },
+        "mongoframes": {
+            "one": mongoframes_frame_save,
+            "many": mongoframes_insert_many,
+        },
+    }
+}
+
+###############################################################################
+
+NUM_SAMPLES = [10 ** i for i in range(0, 4)]
+OPS = ["insert"]
+packages = ["mongoengine", "pymodm", "mongoframes", "pymongo"]
+bulk = ["one", "many"]
+
+TIMES = defaultdict(lambda: defaultdict(dict))
+
+for op in OPS:
+    for b in bulk:
+        for pkg in packages:
+            func = func_map[op][pkg][b]
+            times = []
+            for n in NUM_SAMPLES:
+                if n < 1000:
+                    rounds = []
+                    for i in range(11):
+                        rounds.append(func(n))
+                    times.append(np.median(rounds) * 1000 / n)
+                else:
+                    times.append(func(n) * 1000 / n)
+                print(
+                    "%27s() %.3fs per 1000 samples (batch size = %d)"
+                    % (func.__name__, times[-1], n)
+                )
+            TIMES[op][b][pkg] = times
+
+###############################################################################
+
+
+x = np.arange(len(NUM_SAMPLES))
+width = 0.1
+
+fig, ax = plt.subplots()
+
+op = OPS[0]
+
+i = 0
+for b in bulk:
+    for pkg in packages:
+        times = TIMES[op][b][pkg]
+        L = (len(bulk) * len(packages) - 1) / 2
+        rects = ax.bar(
+            x + (i - L) * width, times, width=width, label="-".join([pkg, b])
+        )
+        i += 1
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel("Time per 1000 samples")
+ax.set_title("Database Operations grouped by Method")
+ax.set_xticks(x)
+ax.set_xticklabels(NUM_SAMPLES)
+ax.legend()
+# plt.yscale("log")
+
+plt.show()
