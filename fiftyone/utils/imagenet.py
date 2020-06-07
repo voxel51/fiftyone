0a83cfe2e3ee84a4d669e4ef32a3c6ade7382d99
brimoor <brimoor@umich.edu>
2020-06-06 23:19:05 -0400

    adding util to verify ImageNet manual downloads

---
+++ fiftyone/utils/imagenet.py
@@ -0,0 +1,68 @@
+"""
+Utilities for the ImageNet dataset.
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+# pragma pylint: disable=redefined-builtin
+# pragma pylint: disable=unused-wildcard-import
+# pragma pylint: disable=wildcard-import
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import *
+
+# pragma pylint: enable=redefined-builtin
+# pragma pylint: enable=unused-wildcard-import
+# pragma pylint: enable=wildcard-import
+
+import os
+
+
+def ensure_imagenet_manual_download(dataset_dir, split, devkit=False):
+    """Ensures that the ImageNet archive(s) for the requested split have been
+    manually downloaded to the required locations.
+
+    Args:
+        dataset_dir: the dataset directory
+        split: the split of interest. Supported values are
+            ``("train", "validation")``
+        devkit (False): whether to ensure that the devkit archive is present
+
+    Raises:
+        OSError: if the required files are not present
+    """
+    if split == "train":
+        archive_name = "ILSVRC2012_img_train.tar"
+    elif split == "validation":
+        archive_name = "ILSVRC2012_img_val.tar"
+    else:
+        raise ValueError(
+            "Unsupported split '%s'; Supported values are "
+            "('train', 'validation')"
+        )
+
+    _ensure_archive(archive_name, dataset_dir)
+
+    if devkit:
+        devkit_name = "ILSVRC2012_devkit_t12.tar.gz"
+        _ensure_archive(devkit_name, dataset_dir)
+
+
+def _ensure_archive(archive_name, dataset_dir):
+    archive_path = os.path.join(dataset_dir, archive_name)
+    if not os.path.isfile(archive_path):
+        raise OSError(
+            (
+                "Archive '%s' not found in dataset directory '%s'."
+                "\n\n"
+                "You must download the source files for the ImageNet dataset "
+                "manually to the dataset directory."
+                "\n\n"
+                "Register at http://www.image-net.org/download-images in "
+                "order to get the link to download the dataset"
+            )
+            % (archive_name, dataset_dir)
+        )
