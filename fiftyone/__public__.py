7bfafdc5d38e179b8a6b8e6722376ec0d4ffaa52
brimoor <brimoor@umich.edu>
2020-06-10 18:32:23 -0400

    adding copyright

--- fiftyone/__public__.py
+++ fiftyone/__public__.py
@@ -3,6 +3,10 @@ FiftyOne's basic, high-level public interface.
 
 Parts of various fiftyone sub-packages are collected here and made available
 under the top-level `fiftyone` package.
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
 """
 
 import fiftyone.core.config as foc
