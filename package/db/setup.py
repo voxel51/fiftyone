cee490fe482502bc91ee0b67166dac0cae1a7299
alan <alan@voxel51.com>
2020-06-15 17:50:11 -0400

    DB v0.1.2beta1 (Windows support)

--- package/db/setup.py
+++ package/db/setup.py
@@ -152,7 +152,7 @@ if LINUX_DISTRO:
 
 setup(
     name="fiftyone_db" + name_suffix,
-    version="0.1.1",
+    version="0.1.2beta1",
     description="Project FiftyOne database",
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
