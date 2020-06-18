1ce9bd46677973ba45445b95da7d293e4b1b2479
brimoor <brimoor@umich.edu>
2020-06-18 10:51:27 -0400

    removing experimental package references

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(
     author_email="info@voxel51.com",
     url="https://github.com/voxel51/fiftyone",
     license="",
-    packages=find_packages(exclude=["fiftyone.experimental"])
-    + ["fiftyone.examples"],
+    packages=find_packages() + ["fiftyone.examples"],
     package_dir={"fiftyone.examples": "examples"},
     include_package_data=True,
     exclude_package_data={
