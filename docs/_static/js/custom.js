b8412a1b0836467554b41fcfeb5831dd4446880b
alan <alan@voxel51.com>
2020-06-23 13:30:00 -0400

    Make header white

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function () {
-  var navHeight = document.querySelector(".nav__main").clientHeight;
+  var navHeight = document.querySelector(".nav__main").offsetHeight;
   var y = window.scrollY || window.pageYOffset;
   document.querySelector(".wy-nav-side").style.marginTop =
     Math.max(0, navHeight - y) + "px";
