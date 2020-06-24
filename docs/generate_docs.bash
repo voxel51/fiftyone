voxel51/fiftyone

commit 3848e32281e6ae28a5dcd978edded10c4f41b161

moving docs to source dir

docs/generate_docs.bash
@@ -40,23 +40,20 @@ THIS_DIR=$(dirname "$0")
 
 if [[ ${CLEAN_BUILD} = true ]]; then
     echo "**** Deleting existing build directories ****"
-    rm -rf "${THIS_DIR}/api"
+    rm -rf "${THIS_DIR}/source/api"
     rm -rf "${THIS_DIR}/build"
 fi
 
 
 echo "**** Generating documentation ****"
 
-#
-# The syntax here is:
-#   sphinx-apidoc [OPTIONS] -o <OUTPUT_PATH> <MODULE_PATH> [EXCLUDE_PATTERN, …]
-#
+# sphinx-apidoc [OPTIONS] -o <OUTPUT_PATH> <MODULE_PATH> [EXCLUDE_PATTERN, …]
 cd "${THIS_DIR}/.."
-sphinx-apidoc -f --no-toc -o docs/api fiftyone
+sphinx-apidoc -f --no-toc -o docs/source/api fiftyone
 
-cd docs
-make html
-cd ..
+# sphinx-build [OPTIONS] SOURCEDIR OUTPUTDIR [FILENAMES...]
+cd "${THIS_DIR}"
+sphinx-build -M html source build
 
-echo "**** Documentation complete"
+echo "**** Documentation complete ****"
 printf "To view the docs, open:\n\ndocs/build/html/index.html\n\n"

.gitignore
@@ -21,4 +21,4 @@ out/
 tmp/
 
 /docs/build/
-/docs/api/
+/docs/source/api/

docs/Makefile
@@ -1,20 +1,0 @@
-# Minimal makefile for Sphinx documentation
-#
-
-# You can set these variables from the command line, and also
-# from the environment for the first two.
-SPHINXOPTS    ?=
-SPHINXBUILD   ?= sphinx-build
-SOURCEDIR     = .
-BUILDDIR      = build
-
-# Put it first so that "make" without argument is like "make help".
-help:
-	@$(SPHINXBUILD) -M help "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
-
-.PHONY: help Makefile
-
-# Catch-all target: route all unknown targets to Sphinx using the new
-# "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
-%: Makefile
-	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)

docs/source/conf.py
@@ -1,8 +1,16 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Sphinx configuration file.
+
+For a full list of available options, see:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+import fiftyone.constants as foc
+
 
 # -- Path setup --------------------------------------------------------------
 
@@ -14,11 +22,9 @@
 # -- Project information -----------------------------------------------------
 
 project = "FiftyOne"
-copyright = "2017-2020, Voxel51, Inc"
-author = "Voxel51, Inc."
-
-# The full version, including alpha/beta/rc tags
-release = "0.1.0"
+copyright = foc.COPYRIGHT
+author = foc.AUTHOR
+release = foc.VERSION
 
 
 # -- General configuration ---------------------------------------------------
@@ -55,7 +61,7 @@ m2r_parse_relative_links = True
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["dev_guide.md"]
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------

commit 7c907621fe5b4b89253e55820e8324f9538fff36

Restore SPHINXOPTS functionality
Broken in 3848e32281e6ae28a5dcd978edded10c4f41b161

docs/generate_docs.bash
@@ -50,7 +50,7 @@ sphinx-apidoc -f --no-toc -o docs/source/api fiftyone
 
 # sphinx-build [OPTIONS] SOURCEDIR OUTPUTDIR [FILENAMES...]
 cd docs
-sphinx-build -M html source build
+sphinx-build -M html source build $SPHINXOPTS  # unquoted to allow multiple args
 
 echo "**** Documentation complete ****"
 printf "To view the docs, open:\n\ndocs/build/html/index.html\n\n"