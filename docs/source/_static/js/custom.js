7b7dc079b49ac5d31ba8984ebaa02ed154b27d1b
alan <alan@voxel51.com>
2020-06-25 13:03:31 -0400

    Replace header with voxel51 header

--- docs/source/_static/js/custom.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function () {
-  var navHeight = document.querySelector(".nav__main").clientHeight;
-  var y = window.scrollY || window.pageYOffset;
-  document.querySelector(".wy-nav-side").style.marginTop =
-    Math.max(0, navHeight - y) + "px";
-});
