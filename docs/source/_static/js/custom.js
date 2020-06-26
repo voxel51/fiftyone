c8701593c44d29a6e0907562faad1c5e7de2f066
alan <alan@voxel51.com>
2020-06-26 11:42:03 -0400

    Add some custom JS to highlight links in the right sidebar

---
+++ docs/source/_static/js/custom.js
@@ -0,0 +1,31 @@
+$(function () {
+  const sections = $(".section").sort(
+    (a, b) => $(a).offset().top - $(b).offset().top
+  );
+
+  const sectionStartThreshold =
+    $("#pytorch-page-level-bar").height() +
+    parseInt($(".section h2").css("marginTop")) +
+    10;
+
+  $(window).on("scroll", function () {
+    let currentSection = undefined;
+    for (let i = sections.length - 1; i >= 0; i--) {
+      if (
+        $(sections[i]).offset().top <
+        $(window).scrollTop() + sectionStartThreshold
+      ) {
+        currentSection = sections[i].id;
+        break;
+      }
+    }
+
+    if (currentSection) {
+      $(".pytorch-content-right .pytorch-side-scroll li")
+        .removeClass("current-section")
+        .filter(":not(:has(a.title-link))")
+        .filter(':has(a[href="#' + currentSection + '"])')
+        .addClass("current-section");
+    }
+  });
+});
