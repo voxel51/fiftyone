f31e4b8e6c34a938e79faeacf506459ca162c96c
Benjamin Kane <ben@voxel51.com>
2020-07-13 12:43:42 -0400

    debugging

---
+++ electron/.storybook/main.js
@@ -0,0 +1,30 @@
+module.exports = {
+  stories: ["../app/**/*.stories.tsx"],
+  addons: [
+    "@storybook/addon-actions/register",
+    "@storybook/addon-backgrounds/register",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-links/register",
+    "@storybook/addon-storysource",
+    "@storybook/addon-viewport/register",
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("ts-loader"),
+        },
+        // Optional
+        {
+          loader: require.resolve("react-docgen-typescript-loader"),
+        },
+      ],
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+
+    return config;
+  },
+};
