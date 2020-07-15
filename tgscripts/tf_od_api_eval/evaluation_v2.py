51e36a08ac2da21e1c9f11eec921681d6df11480
Tyler Ganter <tyler@voxel51.com>
2020-07-14 22:29:15 -0600

    finally something works

--- tgscripts/tf_od_api_eval/evaluation_v2.py
+++ tgscripts/tf_od_api_eval/evaluation_v2.py
@@ -1,21 +1,15 @@
 """"""
-import logging
-import os
-import six
-
-import numpy as np
 import pandas as pd
+from pprintpp import pprint
 from google.protobuf import text_format
 
 import sys
 
 sys.path.append("/Users/tylerganter/source/theta/tensorflow/models/research")
 
-from object_detection.metrics import io_utils
 from object_detection.metrics import oid_challenge_evaluation_utils as utils
 from object_detection.protos import string_int_label_map_pb2
 from object_detection.utils import object_detection_evaluation
-from object_detection.core import standard_fields
 
 
 ###############################################################################
@@ -30,25 +24,15 @@ OUTPUT_METRICS = "output_metrics.csv"
 ###############################################################################
 
 
-def get_mAP(eval_result):
-    keys = [key for key in eval_result if "mAP" in key]
-    assert len(keys) == 1, "unexpected number of mAP keys: %d" % len(keys)
-    return eval_result[keys[0]]
-
-
-def get_class_AP(eval_result, label_name):
-    keys = [key for key in eval_result if label_name in key]
-    assert len(keys) == 1, "unexpected number of keys: %d" % len(keys)
-    return eval_result[keys[0]]
-
-
-class Evaluator:
-    def __init__(self, class_label_map_path):
+class TensorflowObjectDetectionAPIEvaluator:
+    def __init__(self, class_label_map_path, iou_threshold=0.5):
         """
         Args:
             class_label_map_path: path to the label map .pbtxt file
         """
-        self._class_label_map, self._categories = _load_labelmap(
+        self._iou_threshold = iou_threshold
+
+        self._class_label_map, self._categories = self._load_labelmap(
             class_label_map_path
         )
         self._reverse_label_map = {
@@ -56,7 +40,9 @@ class Evaluator:
         }
 
         self._evaluator = object_detection_evaluation.OpenImagesChallengeEvaluator(
-            self._categories, evaluate_masks=False
+            self._categories,
+            evaluate_masks=False,
+            matching_iou_threshold=self._iou_threshold,
         )
 
     def evaluate_image(self, groundtruth, predictions):
@@ -91,51 +77,35 @@ class Evaluator:
         # guarantee the evaluator is cleared
         try:
             # actually evaluate the image
-            result = self._evaluate_image(predictions, prediction_dict)
+            result = self._evaluate_image(predictions)
         finally:
             self._evaluator.clear()
 
         return result
 
-    def _evaluate_image(self, predictions, prediction_dict):
+    def _evaluate_image(self, predictions):
         state, _ = self._evaluator.get_internal_state()
-        tp_idxs, fp_idxs = self._get_TP_FP(state, predictions, prediction_dict)
-        print("TP idxs: ", tp_idxs)
-        print("FP idxs: ", fp_idxs)
-
-        assert False, "ASDF"
+        tp_idxs, fp_idxs = self._get_TP_FP(state, predictions)
 
         eval_result = self._evaluator.evaluate()
-        print("mAP: ", get_mAP(eval_result))
-        for i in range(60, 80):
-            label_name = self._categories[i]["name"]
-            print(
-                "%s AP: " % label_name, get_class_AP(eval_result, label_name)
-            )
 
-    def _get_TP_FP(self, state, predictions, prediction_dict):
+        return {
+            "true_positive_indexes": tp_idxs,
+            "false_positive_indexes": fp_idxs,
+            "mAP": self._get_mAP(eval_result),
+            "AP_per_class": self._get_AP_per_class(eval_result),
+        }
+
+    def _get_TP_FP(self, state, predictions):
+        """Finds the true positive and false positive bounding boxes.
+
+        Returns:
+              true_positive_idxs, false_positive_idxs: each of which is a list
+                of row indexes for prediction rows with true positives and
+                false positives respectively.
+                To be accessed via:
+                    predictions.loc[true_positive_idxs]
         """
-            state.scores_per_class
-            state.tp_fp_labels_per_class
-
-            groundtruth_dict['groundtruth_classes']
-            groundtruth_dict['groundtruth_boxes']
-            groundtruth_dict['groundtruth_group_of']
-            groundtruth_dict['groundtruth_image_classes']
-
-            prediction_dict['detection_classes']
-            prediction_dict['detection_scores']
-            prediction_dict['detection_boxes']
-
-
-        USELESS?
-            state.num_gt_instances_per_class
-            state.num_gt_imgs_per_class
-            state.num_images_correctly_detected_per_class
-        """
-        detection_classes = prediction_dict["detection_classes"]
-        detection_scores = prediction_dict["detection_scores"]
-
         true_positive_idxs = []
         false_positive_idxs = []
 
@@ -143,11 +113,9 @@ class Evaluator:
             cur_scores = state.scores_per_class[class_label - 1]
             if not cur_scores:
                 continue
+
             for i, score in enumerate(cur_scores[0]):
                 class_name = self._reverse_label_map[class_label]
-                # match_idxs = np.where(
-                #     np.logical_and(detection_classes == class_label,
-                #                    detection_scores == score))[0]
 
                 match_idxs = predictions[
                     (predictions.Score == score)
@@ -170,37 +138,49 @@ class Evaluator:
 
         return true_positive_idxs, false_positive_idxs
 
+    def _get_mAP(self, eval_result):
+        keys = [key for key in eval_result if "mAP" in key]
+        assert len(keys) == 1, "unexpected number of mAP keys: %d" % len(keys)
+        return eval_result[keys[0]]
+
+    def _get_AP_per_class(self, eval_result):
+        d = {}
+        for key, average_precision in eval_result.items():
+            if "PerformanceByCategory" not in key:
+                continue
+            class_name = "/" + key.split("//")[-1]
+            d[class_name] = average_precision
+        return d
+
+    @staticmethod
+    def _load_labelmap(labelmap_path):
+        """Loads labelmap from the labelmap path.
+
+      Args:
+        labelmap_path: Path to the labelmap.
+
+      Returns:
+        A dictionary mapping class name to class numerical id
+        A list with dictionaries, one dictionary per category.
+      """
+
+        label_map = string_int_label_map_pb2.StringIntLabelMap()
+        with open(labelmap_path, "r") as fid:
+            label_map_string = fid.read()
+            text_format.Merge(label_map_string, label_map)
+        labelmap_dict = {}
+        categories = []
+        for item in label_map.item:
+            labelmap_dict[item.name] = item.id
+            categories.append({"id": item.id, "name": item.name})
+        return labelmap_dict, categories
+
 
 ###############################################################################
 
 
-def _load_labelmap(labelmap_path):
-    """Loads labelmap from the labelmap path.
-
-  Args:
-    labelmap_path: Path to the labelmap.
-
-  Returns:
-    A dictionary mapping class name to class numerical id
-    A list with dictionaries, one dictionary per category.
-  """
-
-    label_map = string_int_label_map_pb2.StringIntLabelMap()
-    with open(labelmap_path, "r") as fid:
-        label_map_string = fid.read()
-        text_format.Merge(label_map_string, label_map)
-    labelmap_dict = {}
-    categories = []
-    for item in label_map.item:
-        labelmap_dict[item.name] = item.id
-        categories.append({"id": item.id, "name": item.name})
-    return labelmap_dict, categories
-
-
 if __name__ == "__main__":
-    print("Reading location annotations...")
     all_location_annotations = pd.read_csv(BOUNDING_BOXES)
-    print("Reading label annotations...")
     all_label_annotations = pd.read_csv(IMAGE_LABELS)
     all_label_annotations.rename(
         columns={"Confidence": "ConfidenceImageLabel"}, inplace=True
@@ -210,13 +190,13 @@ if __name__ == "__main__":
         [all_location_annotations, all_label_annotations]
     )
 
-    print("Reading predictions...")
     all_predictions = pd.read_csv(INPUT_PREDICTIONS)
     images_processed = 0
 
-    evaluator = Evaluator(CLASS_LABELMAP)
+    evaluator = TensorflowObjectDetectionAPIEvaluator(
+        CLASS_LABELMAP, iou_threshold=0.5
+    )
 
-    print("Processing...")
     # for image_id, cur_predictions in all_predictions.groupby("ImageID"):
     for image_id in ["0032485d3a9720dc"]:
         print("Processing image %d" % images_processed)
@@ -231,7 +211,4 @@ if __name__ == "__main__":
 
         result = evaluator.evaluate_image(cur_groundtruth, cur_predictions)
 
-    # metrics = evaluator.evaluate()
-    #
-    # with open(OUTPUT_METRICS, "w") as fid:
-    #     io_utils.write_csv(fid, metrics)
+        pprint(result)
