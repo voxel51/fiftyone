084a23605105275d11d0bbe93b9c5f031e229ed7
alan <alan@voxel51.com>
2020-07-22 15:11:52 -0400

    Use material icons

--- electron/app/components/DropdownHandle.tsx
+++ electron/app/components/DropdownHandle.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import { Add, ExpandLess, ExpandMore, Remove } from "@material-ui/icons";
 
 import CellHeader from "./CellHeader";
 
 const DropdownHandle = styled(CellHeader)`
   width: 15rem;
+
+  svg {
+    font-size: 1.25em;
+  }
 `;
 
 type Props = {
@@ -13,8 +18,9 @@ type Props = {
   icon: (expanded: boolean) => ReactNode;
 };
 
-export const ArrowButton = (expanded) => (expanded ? "^" : "v");
-export const PlusMinusButton = (expanded) => (expanded ? "-" : "+");
+export const ArrowButton = (expanded) =>
+  expanded ? <ExpandLess /> : <ExpandMore />;
+export const PlusMinusButton = (expanded) => (expanded ? <Remove /> : <Add />);
 
 export default ({ label, expanded, icon = ArrowButton }: Props) => {
   return (
