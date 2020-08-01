a0af333a47a6753b30928bb9fe4dad78c104fb9a
alan <alan@voxel51.com>
2020-07-31 23:28:09 -0400

    Add support for outlined and filled tags

--- electron/app/components/Tags/Tag.stories.tsx
+++ electron/app/components/Tags/Tag.stories.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Tag from "./Tag";
+import { boolean } from "@storybook/addon-knobs";
 
 export default {
   component: Tag,
   title: "Tag",
 };
 
-export const standard = () => <Tag name="tag"></Tag>;
+export const standard = () => (
+  <Tag name="tag" selected={boolean("Selected")}></Tag>
+);
 
-export const color = () => <Tag name="tag" color="red"></Tag>;
+export const color = () => (
+  <Tag name="tag" color="red" selected={boolean("Selected")}></Tag>
+);
--- electron/app/components/Tags/Tag.tsx
+++ electron/app/components/Tags/Tag.tsx
@@ -2,31 +2,64 @@ import React from "react";
 import styled from "styled-components";
 
 const Outer = styled.div`
-  display: inline-block;
-  height: 2em;
-  line-height: 2em;
   position: relative;
+  display: inline-block;
   margin-left: 1em;
-  padding: 0 0.5em 0 0.5em;
-  color: #fff;
-  background-color: ${({ color }) => color};
-  text-decoration: none;
+  padding-right: 0.25em;
+  height: 2em;
+  line-height: 1em;
+  background-color: ${({ borderColor }) => borderColor};
 
   &:before {
-    content: "";
     position: absolute;
     top: 0;
     left: -1em;
     width: 0;
     height: 0;
-    border-color: transparent ${({ color }) => color} transparent transparent;
+    content: "";
+    border-color: transparent ${({ borderColor }) => borderColor} transparent
+      transparent;
     border-style: solid;
     border-width: 1em 1em 1em 0;
   }
 `;
 
+const Inner = styled.div`
+  position: relative;
+  display: inline-block;
+  top: 0.25em;
+  padding: 0 0.5em 0 0.5em;
+  height: 1.5em;
+  line-height: 1em;
+  color: ${({ textColor }) => textColor};
+  background-color: ${({ fillColor }) => fillColor};
+  text-decoration: none;
+
+  &:before {
+    position: absolute;
+    top: 0;
+    left: -0.75em;
+    width: 0;
+    height: 0;
+    content: "";
+    border-color: transparent ${({ fillColor }) => fillColor} transparent
+      transparent;
+    border-style: solid;
+    border-width: 0.75em 0.75em 0.75em 0;
+  }
+`;
+
 const Tag = ({ name, color = "blue", selected = false }) => {
-  return <Outer color={color}>{name}</Outer>;
+  return (
+    <Outer borderColor={color}>
+      <Inner
+        fillColor={selected ? color : "white"}
+        textColor={selected ? "white" : color}
+      >
+        {name}
+      </Inner>
+    </Outer>
+  );
 };
 
 export default Tag;
