9eb1ee7e6f1d1ce3791ca014c042b860c70dd3cc
alan <alan@voxel51.com>
2020-08-03 01:02:38 -0400

    Add grid story

--- electron/app/components/Tags/Tag.stories.tsx
+++ electron/app/components/Tags/Tag.stories.tsx
@@ -14,3 +14,18 @@ export const standard = () => (
 export const color = () => (
   <Tag name="tag" color="red" selected={boolean("Selected")}></Tag>
 );
+
+export const grid = () => (
+  <>
+    <div style={{ lineHeight: 0 }}>
+      <Tag name="tag" color="#ff0000" selected={boolean("Selected")}></Tag>
+      <Tag name="tag" color="#00ff00" selected={boolean("Selected")}></Tag>
+      <Tag name="tag" color="#0000ff" selected={boolean("Selected")}></Tag>
+    </div>
+    <div style={{ lineHeight: 0 }}>
+      <Tag name="tag" color="#ff0000" selected={boolean("Selected")}></Tag>
+      <Tag name="tag" color="#00ff00" selected={boolean("Selected")}></Tag>
+      <Tag name="tag" color="#0000ff" selected={boolean("Selected")}></Tag>
+    </div>
+  </>
+);
