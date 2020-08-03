c96150138bb9c771fd033b0c79f43c678155e3f7
Benjamin Kane <ben@voxel51.com>
2020-08-03 11:12:46 -0400

    update view bar story to use theme

--- electron/app/components/ViewBar/ViewBar.stories.tsx
+++ electron/app/components/ViewBar/ViewBar.stories.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import ViewBar from "./ViewBar";
 
+import styled from "styled-components";
+
 export default {
   component: ViewBar,
   title: "ViewBar",
 };
 
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.background};
+  padding: 10;
+  width: calc(100% - 20);
+`;
+
 export const standard = () => (
-  <div
-    style={{
-      padding: 10,
-      width: "calc(100% - 20)",
-      position: "relative",
-      background: "pink",
-    }}
-    key={0}
-  >
-    <ViewBar key={0} />
-  </div>
+  <Container>
+    <ViewBar />
+  </Container>
 );
