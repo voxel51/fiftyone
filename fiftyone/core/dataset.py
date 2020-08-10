fd43e225a172af0308b7a74782f3ae5fe0c121f8
Eric Hofesmann <ehofesmann@gmail.com>
2020-08-10 16:00:22 -0400

    only optionally return fields starting with _

--- fiftyone/core/dataset.py
+++ fiftyone/core/dataset.py
@@ -311,7 +311,9 @@ class Dataset(foc.SampleCollection, metaclass=DatasetSingleton):
         """
         return fov.DatasetView(self)
 
-    def get_field_schema(self, ftype=None, embedded_doc_type=None):
+    def get_field_schema(
+        self, ftype=None, embedded_doc_type=None, return_all=False
+    ):
         """Returns a schema dictionary describing the fields of the samples in
         the dataset.
 
@@ -322,12 +324,16 @@ class Dataset(foc.SampleCollection, metaclass=DatasetSingleton):
             embedded_doc_type (None): an optional embedded document type to
                 which to restrict the returned schema. Must be a subclass of
                 :class:``fiftyone.core.odm.BaseEmbeddedDocument``
+            return_all (False): a boolean indicating whether to return fields
+                that start with the character "_"
 
         Returns:
              a dictionary mapping field names to field types
         """
         return self._sample_doc_cls.get_field_schema(
-            ftype=ftype, embedded_doc_type=embedded_doc_type
+            ftype=ftype,
+            embedded_doc_type=embedded_doc_type,
+            return_all=return_all,
         )
 
     def add_sample_field(
@@ -1266,14 +1272,14 @@ class Dataset(foc.SampleCollection, metaclass=DatasetSingleton):
         return foo.get_db_conn()[self._collection_name]
 
     def _expand_schema(self, samples):
-        fields = self.get_field_schema()
+        fields = self.get_field_schema(return_all=True)
         for sample in samples:
             for field_name in sample.field_names:
                 if field_name not in fields:
                     self._sample_doc_cls.add_implied_field(
                         field_name, sample[field_name]
                     )
-                    fields = self.get_field_schema()
+                    fields = self.get_field_schema(return_all=True)
 
     def _sample_dict_to_doc(self, d):
         return self._sample_doc_cls.from_dict(d, extended=False)
@@ -1294,7 +1300,7 @@ class Dataset(foc.SampleCollection, metaclass=DatasetSingleton):
         )
 
     def _validate_sample(self, sample):
-        fields = self.get_field_schema()
+        fields = self.get_field_schema(return_all=True)
 
         non_existest_fields = {
             fn for fn in sample.field_names if fn not in fields
@@ -1337,7 +1343,7 @@ def _create_dataset(name, persistent=False):
     _meta = foo.DatasetDocument(
         name=name,
         sample_fields=foo.SampleFieldDocument.list_from_field_schema(
-            _sample_doc_cls.get_field_schema()
+            _sample_doc_cls.get_field_schema(return_all=True)
         ),
         persistent=persistent,
     )
@@ -1360,7 +1366,7 @@ def _load_dataset(name):
 
     _sample_doc_cls = type(name, (foo.DatasetSampleDocument,), {})
 
-    num_default_fields = len(_sample_doc_cls.get_field_schema())
+    num_default_fields = len(_sample_doc_cls.get_field_schema(return_all=True))
 
     for sample_field in _meta.sample_fields[num_default_fields:]:
         subfield = (
