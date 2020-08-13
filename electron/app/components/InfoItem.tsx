ac2d0af3a205754c8161f5e9032d5ca4f8bdc796
alan <alan@voxel51.com>
2020-08-13 17:21:23 -0400

    Remove some unused components

--- electron/app/components/InfoItem.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Menu } from "semantic-ui-react";
-
-export default ({ k, v }) => (
-  <Menu.Item as="span" style={{ userSelect: "auto" }}>
-    {k} &middot; <code>{v}</code>
-  </Menu.Item>
-);
