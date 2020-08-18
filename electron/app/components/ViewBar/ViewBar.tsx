ea35791d3a1989f408d0690a7b3621f641818571
Benjamin Kane <ben@voxel51.com>
2020-08-18 13:31:56 -0400

    fix delete, use array syntax

--- electron/app/components/ViewBar/ViewBar.tsx
+++ electron/app/components/ViewBar/ViewBar.tsx
@@ -53,7 +53,7 @@ const viewBarKeyMap = {
   VIEW_BAR_PREVIOUS: "left",
   VIEW_BAR_NEXT_STAGE: "shift+right",
   VIEW_BAR_PREVIOUS_STAGE: "shift+left",
-  VIEW_BAR_DELETE: "backspace,del",
+  VIEW_BAR_DELETE: ["del", "backspace"],
 };
 
 const ViewBar = () => {
