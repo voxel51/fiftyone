0d10f8a97c25afea036696d93a90d3dd5e98d7da
brimoor <brimoor@umich.edu>
2020-08-22 21:19:10 -0400

    adding method to expand an ImageLabels field of a dataset

--- fiftyone/utils/data/base.py
+++ fiftyone/utils/data/base.py
@@ -10,6 +10,8 @@ import os
 import eta.core.image as etai
 import eta.core.utils as etau
 
+import fiftyone.core.utils as fou
+
 
 def parse_images_dir(dataset_dir, recursive=True):
     """Parses the contents of the given directory of images.
@@ -68,3 +70,53 @@ def parse_image_classification_dir_tree(dataset_dir):
         samples.append((path, target))
 
     return samples, classes
+
+
+def expand_image_labels_field(
+    dataset,
+    label_field,
+    prefix=None,
+    multilabel=False,
+    skip_non_categorical=False,
+):
+    """Expands the :class:`fiftyone.core.labels.ImageLabels` field of the
+    dataset into per-label fields.
+
+    If ``multilabel`` is False, frame attributes will be stored in separate
+    :class:`fiftyone.core.labels.Classification` fields with names
+    ``prefix + attr.name``.
+
+    If ``multilabel`` if True, all frame attributes will be stored in a
+    :class:`fiftyone.core.labels.Classifications` field called
+    ``prefix + "attrs"``.
+
+    All objects will be stored in a ``prefix + "objs"`` field.
+
+    The ``label_field`` of the dataset will be deleted after the expansion is
+    completed.
+
+    Args:
+        prefix (None): a string prefix to prepend to each expanded field name
+        multilabel (False): whether to store frame attributes in a single
+            :class:`fiftyone.core.labels.Classifications` field
+        skip_non_categorical (False): whether to skip non-categorical frame
+            attributes (True) or cast them to strings (False)
+    """
+    # Expand image labels field
+    with fou.ProgressBar() as pb:
+        for sample in pb(dataset):
+            labels = sample[label_field]
+            if labels is None:
+                continue
+
+            sample.update_fields(
+                labels.expand(
+                    prefix=prefix,
+                    multilabel=multilabel,
+                    skip_non_categorical=skip_non_categorical,
+                )
+            )
+            sample.clear_field(label_field)
+            sample.save()
+
+    dataset.delete_sample_field(label_field)
