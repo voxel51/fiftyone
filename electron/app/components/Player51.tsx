59ed490e0507e793a618e26d77cf78c5d946edb6
alan <alan@voxel51.com>
2020-09-01 14:35:52 -0400

    Pass attributes to player51

--- electron/app/components/Player51.tsx
+++ electron/app/components/Player51.tsx
@@ -3,7 +3,13 @@ import uuid from "react-uuid";
 
 import Player51 from "../player51/build/cjs/player51.min.js";
 import clickHandler from "../utils/click.ts";
-import { RESERVED_FIELDS, VALID_SCALAR_TYPES } from "../utils/labels";
+import {
+  RESERVED_FIELDS,
+  VALID_LABEL_TYPES,
+  VALID_SCALAR_TYPES,
+  getDetectionAttributes,
+  convertAttributesToETA,
+} from "../utils/labels";
 
 const PARSERS = {
   Classification: [
@@ -21,6 +27,7 @@ const PARSERS = {
     "objects",
     (name, obj) => {
       const bb = obj.bounding_box;
+      const attrs = convertAttributesToETA(getDetectionAttributes(obj));
       return {
         type: "eta.core.objects.DetectedObject",
         name,
@@ -30,6 +37,7 @@ const PARSERS = {
           top_left: { x: bb[0], y: bb[1] },
           bottom_right: { x: bb[0] + bb[2], y: bb[1] + bb[3] },
         },
+        attrs: { attrs },
       };
     },
   ],
