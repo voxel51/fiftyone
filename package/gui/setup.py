06877f8479b948d1c4873fc73be2b23a410df1b7
alan <alan@voxel51.com>
2020-09-03 15:34:50 -0400

    Bump GUI to 0.5.3.1

--- package/gui/setup.py
+++ package/gui/setup.py
@@ -96,7 +96,7 @@ cmdclass = {
 
 setup(
     name="fiftyone_gui",
-    version="0.5.3",
+    version="0.5.3.1",
     description="FiftyOne App",
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
