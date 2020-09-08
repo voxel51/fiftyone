d85281f69ccb19bc178002845e2692b6c41638de
Benjamin Kane <ben@voxel51.com>
2020-09-08 15:10:41 -0400

    blue autocomplete text

--- electron/app/components/ViewBar/ViewStage/ViewStage.tsx
+++ electron/app/components/ViewBar/ViewStage/ViewStage.tsx
@@ -204,7 +204,7 @@ const BestMatchDiv = styled.div`
   background-color: transparent;
   border: none;
   margin: 0.5rem 0.5rem 0.5rem 0;
-  color: ${({ theme }) => theme.secondary};
+  color: ${({ theme }) => theme.brand};
   line-height: 1rem;
   border: none;
   font-weight: bold;
