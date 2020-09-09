46bd307f188d0633695920200f1bb78ec04b927b
Benjamin Kane <ben@voxel51.com>
2020-09-09 16:36:41 -0400

    client side setup

--- electron/app/components/NumericFieldFilter.tsx
+++ electron/app/components/NumericFieldFilter.tsx
@@ -5,25 +5,60 @@ import * as atoms from "../recoil/atoms";
 import * as selectors from "../recoil/selectors";
 import { NamedRangeSlider } from "./RangeSlider";
 
+const makeFilter = (fieldName, range, includeNone) => {
+  let expr,
+    rangeExpr = null;
+  let fieldStr = `$${fieldName}`;
+  if (range) {
+    rangeExpr = {
+      $and: [{ $gt: [fieldStr, range[0]] }, { $lt: [fieldStr, range[1]] }],
+    };
+  }
+  if (includeNone) {
+    expr = { $or: [rangeExpr, { $eq: [fieldStr, null] }] };
+  }
+  return {
+    kwargs: ["filter", { $expr: expr }],
+    _cls: "fiftyone.core.stages.Match",
+  };
+};
+
 const NumericFieldFilter = ({ entry }) => {
   const boundsAtom = selectors.numericFieldBounds(entry.name);
   const rangeAtom = atoms.filterNumericFieldRange(entry.name);
+  const includeNoneAtom = atoms.filterNumericFieldIncludeNone(entry.name);
+  const includeNone = useRecoilValue(includeNoneAtom);
   const [stateDescription, setStateDescription] = useRecoilState(
     atoms.stateDescription
   );
   const bounds = useRecoilValue(boundsAtom);
   const [range, setRange] = useRecoilState(rangeAtom);
   const hasBounds = bounds.every((b) => b !== null);
+  const isDefaultRange = range[0] === bounds[0] && range[1] === bounds[1];
   useEffect(() => {
     hasBounds && range.every((r) => r === null) && setRange([...bounds]);
   }, [bounds]);
-  console.log(stateDescription);
+
+  useEffect(() => {
+    hasBounds &&
+      setStateDescription({
+        ...stateDescription,
+        filter_stages: {
+          ...stateDescription.filter_stages,
+          [entry.name]:
+            isDefaultRange || !hasBounds
+              ? null
+              : makeFilter(entry.name, range, includeNone),
+        },
+      });
+  }, [range, bounds, includeNone]);
+
   return (
     <NamedRangeSlider
       color={entry.color}
       name={"Range"}
       valueName={"value"}
-      includeNoneAtom={atoms.filterNumericFieldIncludeNone(entry.name)}
+      includeNoneAtom={includeNoneAtom}
       boundsAtom={boundsAtom}
       rangeAtom={rangeAtom}
     />
