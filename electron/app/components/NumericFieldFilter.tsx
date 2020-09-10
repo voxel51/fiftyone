ee5eb7a044e91dc5b0fc19ec6cd63d46fb27a30e
Benjamin Kane <ben@voxel51.com>
2020-09-10 11:16:02 -0400

    state checks

--- electron/app/components/NumericFieldFilter.tsx
+++ electron/app/components/NumericFieldFilter.tsx
@@ -12,7 +12,7 @@ const makeFilter = (fieldName, range, includeNone) => {
   let fieldStr = `$${fieldName}`;
   if (range) {
     rangeExpr = {
-      $and: [{ $gt: [fieldStr, range[0]] }, { $lt: [fieldStr, range[1]] }],
+      $and: [{ $gte: [fieldStr, range[0]] }, { $lte: [fieldStr, range[1]] }],
     };
   }
   if (includeNone) {
@@ -44,14 +44,18 @@ const NumericFieldFilter = ({ entry }) => {
 
   useEffect(() => {
     const newState = JSON.parse(JSON.stringify(stateDescription));
+    if (range.every((e) => e === null)) return;
+    if (isDefaultRange && !(entry.name in newState.filter_stages)) return;
+    const filter = makeFilter(entry.name, range, includeNone);
+    if (
+      JSON.stringify(filter) ===
+      JSON.stringify(newState.filter_stages[entry.name])
+    )
+      return;
     if (isDefaultRange && newState.filter_stages[entry.name]) {
       delete newState.filter_stages[entry.name];
     } else {
-      newState.filter_stages[entry.name] = makeFilter(
-        entry.name,
-        range,
-        includeNone
-      );
+      newState.filter_stages[entry.name] = filter;
     }
     hasBounds &&
       socket.emit(
