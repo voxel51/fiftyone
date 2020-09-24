b240448c2672ac4992d926a192bc6d2639878470
alan <alan@voxel51.com>
2020-09-24 16:51:11 -0400

    Set height of rows so that rows with only videos display correctly

--- electron/app/components/Samples.tsx
+++ electron/app/components/Samples.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroller";
+import useMeasure from "react-use-measure";
 import { useSetRecoilState, useRecoilValue } from "recoil";
 import { Grid, Loader, Dimmer } from "semantic-ui-react";
 import Sample from "./Sample";
@@ -10,6 +11,7 @@ import * as atoms from "../recoil/atoms";
 function Samples({ setView }) {
   const port = useRecoilValue(atoms.port);
   const setCurrentSamples = useSetRecoilState(atoms.currentSamples);
+  const [containerRef, bounds] = useMeasure();
 
   const [scrollState, setScrollState] = tile(port);
   useEffect(() => {
@@ -17,27 +19,33 @@ function Samples({ setView }) {
   }, [scrollState.rows]);
 
   return (
-    <InfiniteScroll
-      pageStart={1}
-      initialLoad={true}
-      loadMore={() =>
-        !scrollState.isLoading && !scrollState.loadMore
-          ? setScrollState({ ...scrollState, loadMore: true })
-          : null
-      }
-      hasMore={scrollState.hasMore}
-      useWindow={true}
-    >
-      {scrollState.rows.map((r, i) => (
-        <Grid columns={r.samples.length} style={r.style} key={i}>
-          {r.samples.map((s, j) => (
-            <Grid.Column key={j} style={{ padding: 0, width: "100%" }}>
-              <Sample sample={s} setView={setView} />
-            </Grid.Column>
-          ))}
-        </Grid>
-      ))}
-    </InfiniteScroll>
+    <div ref={containerRef}>
+      <InfiniteScroll
+        pageStart={1}
+        initialLoad={true}
+        loadMore={() =>
+          !scrollState.isLoading && !scrollState.loadMore
+            ? setScrollState({ ...scrollState, loadMore: true })
+            : null
+        }
+        hasMore={scrollState.hasMore}
+        useWindow={true}
+      >
+        {scrollState.rows.map((r, i) => (
+          <Grid
+            columns={r.samples.length}
+            style={{ ...r.style, height: bounds.width / r.aspectRatio }}
+            key={i}
+          >
+            {r.samples.map((s, j) => (
+              <Grid.Column key={j} style={{ padding: 0, width: "100%" }}>
+                <Sample sample={s} setView={setView} />
+              </Grid.Column>
+            ))}
+          </Grid>
+        ))}
+      </InfiniteScroll>
+    </div>
   );
 }
 
