20effec2122bfc1bdb8308087fa44c082f8d80ba
Benjamin Kane <ben@voxel51.com>
2020-09-29 12:03:41 -0400

    force objectid to str conversion

--- fiftyone/server/json_util.py
+++ fiftyone/server/json_util.py
@@ -7,9 +7,25 @@ FiftyOne server json utilies.
 """
 from bson import ObjectId, json_util
 from flask.json import JSONEncoder
+from collections import OrderedDict
 
 from fiftyone.core.sample import Sample, SampleView
 from fiftyone.core.stages import ViewStage
+
+
+def _objectid_to_str(d):
+    if isinstance(d, (dict, OrderedDict)):
+        for k, v in d.items():
+            if isinstance(v, ObjectId):
+                d[k] = str(v)
+            elif isinstance(v, (dict, OrderedDict, list)):
+                _objectid_to_str(v)
+    if isinstance(d, list):
+        for idx, i in enumerate(d):
+            if isinstance(i, (dict, OrderedDict, list)):
+                _objectid_to_str(i)
+            elif isinstance(i, ObjectId):
+                d[idx] = str(i)
 
 
 class FiftyOneJSONEncoder(JSONEncoder):
@@ -42,6 +58,7 @@ class FiftyOneJSONEncoder(JSONEncoder):
     def dumps(*args, **kwargs):
         """Defined for overriding the default SocketIO `json` interface"""
         kwargs["cls"] = FiftyOneJSONEncoder
+        _objectid_to_str(args[0])
         return json_util.dumps(
             json_util.loads(
                 json_util.dumps(*args, **kwargs), parse_constant=lambda c: c
