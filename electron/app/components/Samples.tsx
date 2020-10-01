6c05afc4d016d8d766df0f7a3155b579d632c803
alan <alan@voxel51.com>
2020-10-01 09:45:08 -0400

    Add loading indicator to samples grid

--- electron/app/components/Samples.tsx
+++ electron/app/components/Samples.tsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 import useMeasure from "react-use-measure";
 import { useSetRecoilState, useRecoilValue } from "recoil";
 import { Grid, Loader, Dimmer } from "semantic-ui-react";
+import styled, { ThemeContext } from "styled-components";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 import Sample from "./Sample";
 import connect from "../utils/connect";
 import tile from "./Samples.hooks";
 import * as atoms from "../recoil/atoms";
 
 function Samples({ setView }) {
+  const theme = useContext(ThemeContext);
   const port = useRecoilValue(atoms.port);
   const setCurrentSamples = useSetRecoilState(atoms.currentSamples);
   const [containerRef, bounds] = useMeasure();
@@ -48,6 +52,19 @@ function Samples({ setView }) {
             ))}
           </Grid>
         ))}
+        {scrollState.isLoading ? (
+          <Grid columns={1}>
+            <Grid.Column
+              style={{
+                width: "100%",
+                textAlign: "center",
+                color: theme.fontDark,
+              }}
+            >
+              <CircularProgress color="inherit" />
+            </Grid.Column>
+          </Grid>
+        ) : null}
       </InfiniteScroll>
     </div>
   );
