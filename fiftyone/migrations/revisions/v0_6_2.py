4ca74ffe2a39d8f550e29ac5b517bb4e2f4d8973
Benjamin Kane <ben@voxel51.com>
2020-10-08 14:25:43 -0400

    migration work

--- fiftyone/migrations/revisions/v0_6_2.py
+++ fiftyone/migrations/revisions/v0_6_2.py
@@ -20,12 +20,18 @@ def up(db, dataset_name):
     dataset_dict = db.datasets.find_one(match_d)
     if "media_type" not in dataset_dict:
         dataset_dict["media_type"] = "image"
+
+    if dataset_dict["media_type"] == "image":
         db.datasets.update_one(
             match_d, {"$set": {"media_type": dataset_dict["media_type"]}}
         )
-
-    if dataset_dict["media_type"] == "image":
         return
+
+    dataset_dict["frames"][
+        "ftype"
+    ] = "fiftyone.core.fields.EmbeddedDocumentField"
+    dataset_dict["frames"]["subfield"] = None
+    dataset_dict["frames"]["embedded_doc_type"] = "fiftyone.core.labels.Frames"
 
     sample_coll = dataset_dict["sample_collection_name"]
     frame_coll = "frames.%s" % sample_coll
@@ -40,6 +46,9 @@ def down(db, dataset_name):
     frame_coll = "frames.%s" % sample_coll
 
     db.datasets.update_one(match_d, {"$unset": {"media_type": ""}})
+    dataset_dict["frames"]["ftype"] = "fiftyone.core.fields.ReferenceField"
+    dataset_dict["frames"]["subfield"] = "fiftyone.core.fields.Frames"
+    dataset_dict["frames"]["embedded_doc_type"] = None
 
     db[dataset_name].update({}, {"$unset": {"sample_id": 1}}, {"multi": True})
     print("down")
