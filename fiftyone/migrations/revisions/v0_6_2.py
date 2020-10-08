be17755a029b5025e525f196c510287d431caf00
Benjamin Kane <ben@voxel51.com>
2020-10-08 15:45:41 -0400

    filling out revisions

--- fiftyone/migrations/revisions/v0_6_2.py
+++ fiftyone/migrations/revisions/v0_6_2.py
@@ -1,5 +1,5 @@
 """
-FiftyOne v0.6.2 revisions
+FiftyOne v0.6.2 revision
 
 | Copyright 2017-2020, Voxel51, Inc.
 | `voxel51.com <https://voxel51.com/>`_
@@ -41,8 +41,20 @@ def up(db, dataset_name):
 
     sample_coll = dataset_dict["sample_collection_name"]
     frame_coll = "frames.%s" % sample_coll
-    for s in sample_coll.find():
-        print(s)
+    for s in db[sample_coll].find():
+        frames_d = {"frame_count": len(s["frames"]), "first_frame": None}
+        frame_updates = []
+        for frame_number_str, frame_id in s["frames"]:
+            frame_number = int(frame_number_str)
+            if frame_number == 1:
+                first_frame = db[frame_coll].find_one({"_id": frame_id})
+                frames_d["first_frame"] = first_frame
+            frame_updates.append(
+                pm.UpdateOne(
+                    {"_id": frame_id}, {"$set": {"_sample_id": s["_id"]}}
+                )
+            )
+        db[frame_coll].bulk_write(frame_updates, ordered=False)
 
 
 def down(db, dataset_name):
@@ -53,8 +65,17 @@ def down(db, dataset_name):
     frame_coll = "frames.%s" % sample_coll
 
     db.datasets.update_one(match_d, {"$unset": {"media_type": ""}})
-    dataset_dict["frames"]["ftype"] = "fiftyone.core.fields.ReferenceField"
-    dataset_dict["frames"]["subfield"] = "fiftyone.core.fields.Frames"
+    dataset_dict["frames"]["ftype"] = "fiftyone.core.fields.FramesField"
+    dataset_dict["frames"]["subfield"] = "fiftyone.core.fields.ReferenceField"
     dataset_dict["frames"]["embedded_doc_type"] = None
 
-    db[dataset_name].update({}, {"$unset": {"sample_id": 1}}, {"multi": True})
+    for s in db[sample_coll].find():
+        frames = {}
+        for f in db[frame_coll].find({"_sample_id": s["_id"]}):
+            frames[str(f["frame_number"])] = f["_id"]
+
+        db[sample_coll].update_one(
+            {"_id": s["_id"]}, {"$set": {"frames": frames}}
+        )
+
+    db[frame_coll].update({}, {"$unset": {"_sample_id": ""}}, {"multi": True})
