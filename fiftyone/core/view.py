9b3f70fb059c44246f9dbb4a917c1fab54bfcf03
brimoor <brimoor@umich.edu>
2020-10-30 12:54:51 -0400

    noting hack

--- fiftyone/core/view.py
+++ fiftyone/core/view.py
@@ -278,7 +278,7 @@ class DatasetView(foc.SampleCollection):
 
         _frames_pipeline = []
         for s in self._stages:
-            s.validate(self)
+            s.validate(self)  # @todo don't re-validate here
             _pipeline.extend(s.to_mongo(self))
 
         if pipeline is not None:
