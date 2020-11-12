6c08c04cd9aebfd24455cc3c124a6fb1a1e796ea
Benjamin Kane <ben@voxel51.com>
2020-11-12 13:07:56 -0500

    update media type field in dataset doc sample fields

--- fiftyone/migrations/revisions/v0_6_5.py
+++ fiftyone/migrations/revisions/v0_6_5.py
@@ -11,6 +11,10 @@ import pymongo as pm
 def up(db, dataset_name):
     match_d = {"name": dataset_name}
     dataset_dict = db.datasets.find_one(match_d)
+    for field in dataset_dict["sample_fields"]:
+        if field["name"] == "media_type":
+            field["name"] = "_media_type"
+    db.datasets.replace_one(match_d, dataset_dict)
     sample_coll = db[dataset_dict["sample_collection_name"]]
     sample_coll.update_many({}, {"$rename": {"media_type": "_media_type"}})
 
@@ -18,5 +22,9 @@ def up(db, dataset_name):
 def down(db, dataset_name):
     match_d = {"name": dataset_name}
     dataset_dict = db.datasets.find_one(match_d)
+    for field in dataset_dict["sample_fields"]:
+        if field["name"] == "_media_type":
+            field["name"] = "media_type"
+    db.datasets.replace_one(match_d, dataset_dict)
     sample_coll = db[dataset_dict["sample_collection_name"]]
     sample_coll.update_many({}, {"$rename": {"_media_type": "media_type"}})
