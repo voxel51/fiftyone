561be632f25c7cb9855efb687f17a0629bce8c01
Benjamin Kane <ben@voxel51.com>
2020-11-18 19:30:15 -0700

    snowpack work

---
+++ app/snowpack.config.js
@@ -0,0 +1,30 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_",
+  },
+  plugins: [
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
