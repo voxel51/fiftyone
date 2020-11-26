5af8f308b83a0b1e53acfeefaec0de496a8fcc8f
Benjamin Kane <ben@voxel51.com>
2020-11-26 13:49:30 -0700

    patch recharts-scale, remove snowpack typescript plugin errors to allow builds

--- app/snowpack.config.js
+++ app/snowpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
   plugins: [
     "@snowpack/plugin-react-refresh",
     "@snowpack/plugin-dotenv",
-    "@snowpack/plugin-typescript",
     "snowpack-plugin-mdx",
     [
       "snowpack-plugin-replace",
