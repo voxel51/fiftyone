d6ece637ab2834d97aa30786cd537823a0d9a6f2
brimoor <brimoor@umich.edu>
2020-12-02 14:05:32 -0500

    updating dataset zoo docs paths

--- fiftyone/utils/quickstart.py
+++ fiftyone/utils/quickstart.py
@@ -103,7 +103,7 @@ Here are some things you can do to explore the dataset:
 Resources:
 
 -   Using the App: https://voxel51.com/docs/fiftyone/user_guide/app.html
--   Dataset Zoo:   https://voxel51.com/docs/fiftyone/user_guide/dataset_creation/zoo.html
+-   Dataset Zoo:   https://voxel51.com/docs/fiftyone/user_guide/dataset_zoo.html
 """
 
 
@@ -151,5 +151,5 @@ Here are some things you can do to explore the dataset:
 Resources:
 
 -   Using the App: https://voxel51.com/docs/fiftyone/user_guide/app.html
--   Dataset Zoo:   https://voxel51.com/docs/fiftyone/user_guide/dataset_creation/zoo.html
+-   Dataset Zoo:   https://voxel51.com/docs/fiftyone/user_guide/dataset_zoo.html
 """
