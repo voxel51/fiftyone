86ae097c15633bf53ddb19e269e0352ceb6e712c
brimoor <brimoor@umich.edu>
2020-12-02 15:36:29 -0500

    adding MinResize transform

--- fiftyone/utils/torch.py
+++ fiftyone/utils/torch.py
@@ -15,10 +15,49 @@ import fiftyone.core.utils as fou
 fou.ensure_torch()
 import torch
 import torchvision
+from torchvision.transforms import functional as F
 from torch.utils.data import Dataset
 
 
 logger = logging.getLogger(__name__)
+
+
+class MinResize(object):
+    """Transform that resizes the PIL image or torch Tensor, if necessary, so
+    that its minimum dimensions are at least the specified size.
+
+    Args:
+        min_output_size: Desired minimum output dimensions. Can either be a
+            ``(min_height, min_width)`` tuple or a single ``min_dim``
+        interpolation (None): Optional interpolation mode. Passed directly to
+            ``torchvision.transforms.functional.resize``
+    """
+
+    def __init__(self, min_output_size, interpolation=None):
+        if isinstance(min_output_size, int):
+            min_output_size = (min_output_size, min_output_size)
+
+        self.min_output_size = min_output_size
+        self.interpolation = interpolation
+
+        self._kwargs = {}
+        if interpolation is not None:
+            self._kwargs["interpolation"] = interpolation
+
+    def __call__(self, pil_image_or_tensor):
+        if isinstance(pil_image_or_tensor, torch.Tensor):
+            h, w = list(pil_image_or_tensor.shape())[-2:]
+        else:
+            w, h = pil_image_or_tensor.size
+
+        minh, minw = self.min_output_size
+
+        if h >= minh and w >= minw:
+            return pil_image_or_tensor
+
+        alpha = max(minh / h, minw / w)
+        size = (int(round(alpha * h)), int(round(alpha * w)))
+        return F.resize(pil_image_or_tensor, size, **self._kwargs)
 
 
 class TorchImageDataset(Dataset):
