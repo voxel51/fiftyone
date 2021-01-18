e0e260c8f6f38e79d7ce30d447783564bdf5e3c8
Benjamin Kane <ben@voxel51.com>
2021-01-18 13:58:54 -0700

    lint

--- app/src/containers/SamplesContainer.tsx
+++ app/src/containers/SamplesContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import styled from "styled-components";
 
