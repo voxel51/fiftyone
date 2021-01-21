a7859c62dfc24bb5c6b7a885de7c470e68090020
Benjamin Kane <ben@voxel51.com>
2021-01-21 15:26:47 -0700

    add no colab cache header to all server requests to remove ipynb bloat

--- fiftyone/server/main.py
+++ fiftyone/server/main.py
@@ -58,6 +58,7 @@ class RequestHandler(tornado.web.RequestHandler):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        self.set_header("x-colab-notebook-cache-control", "no-cache")
 
     async def get(self):
         self.write(self.get_response())
@@ -1075,6 +1076,7 @@ class FileHandler(tornado.web.StaticFileHandler):
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS")
         self.set_header("content-length", self.get_content_size())
+        self.set_header("x-colab-notebook-cache-control", "no-cache")
 
 
 class Application(tornado.web.Application):
