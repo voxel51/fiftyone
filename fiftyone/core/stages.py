a780eb2d39d5ddd2ec4c415936933381a9124d98
Benjamin Kane <ben@voxel51.com>
2021-01-22 12:09:33 -0700

    parse empty select field fields stage correctly in app

--- fiftyone/core/stages.py
+++ fiftyone/core/stages.py
@@ -2108,7 +2108,7 @@ class SelectFields(ViewStage):
         return [
             {
                 "name": "field_names",
-                "type": "list<str>|NoneType",
+                "type": "NoneType|list<str>",
                 "default": "None",
                 "placeholder": "list,of,fields",
             }
