4dfa1a03f8dda05b7b15d4a59034c17f4c2d3603
brimoor <brimoor@umich.edu>
2021-02-04 12:10:11 -0500

    updating model zoo tests

--- tests/intensive/model_zoo_tests.py
+++ tests/intensive/model_zoo_tests.py
@@ -9,7 +9,6 @@ import unittest
 
 import fiftyone as fo
 import fiftyone.zoo as foz
-import fiftyone.zoo.models as fozm
 from fiftyone import ViewField as F
 
 
@@ -20,66 +19,96 @@ def test_all_models():
 
 
 @unittest.skip("Must be run manually")
-def test_all_embedding_models():
+def test_classification_models():
+    models = _get_models_with_tag("classification")
+    _apply_models(models)
+
+
+@unittest.skip("Must be run manually")
+def test_detection_models():
+    models = _get_models_with_tag("detection")
+    _apply_models(models)
+
+
+@unittest.skip("Must be run manually")
+def test_instance_segmentation_models():
+    models = _get_models_with_tag("instances")
+    _apply_models(models)
+
+
+@unittest.skip("Must be run manually")
+def test_semantic_segmentation_models():
+    models = _get_models_with_tag("segmentation")
+    _apply_models(models)
+
+
+@unittest.skip("Must be run manually")
+def test_keypoint_models():
+    models = _get_models_with_tag("keypoints")
+    _apply_person_keypoint_models(models)
+
+
+@unittest.skip("Must be run manually")
+def test_embedding_models():
     all_models = foz.list_zoo_models()
     _apply_embedding_models(all_models)
 
 
 @unittest.skip("Must be run manually")
-def test_classification_models():
-    manifest = fozm._load_zoo_models_manifest()
-    classification_models = [
-        model.name
-        for model in manifest.models
-        if model.has_tag("classification")
-    ]
-
-    _apply_models(classification_models)
+def test_logits_models():
+    models = _get_models_with_tag("logits")
+    _apply_models_with_logits(models, store_logits=True)
 
 
 @unittest.skip("Must be run manually")
-def test_detection_models():
-    manifest = fozm._load_zoo_models_manifest()
-    detection_models = [
-        model.name for model in manifest.models if model.has_tag("detection")
-    ]
-
-    _apply_models(detection_models)
+def test_logits_models_no_logits():
+    models = _get_models_with_tag("logits")
+    _apply_models_with_logits(models, store_logits=False)
 
 
 @unittest.skip("Must be run manually")
-def test_instance_segmentation_models():
-    manifest = fozm._load_zoo_models_manifest()
-    instance_segmentation_models = [
-        model.name for model in manifest.models if model.has_tag("instances")
-    ]
-
-    _apply_models(instance_segmentation_models)
+def test_no_confidence_thresh():
+    all_models = foz.list_zoo_models()
+    _apply_models(
+        all_models, confidence_thresh=None, pass_confidence_thresh=True
+    )
 
 
 @unittest.skip("Must be run manually")
-def test_semantic_segmentation_models():
-    manifest = fozm._load_zoo_models_manifest()
-    segmentation_models = [
-        model.name
-        for model in manifest.models
-        if model.has_tag("segmentation")
-    ]
-
-    _apply_models(segmentation_models)
+def test_confidence_thresh():
+    all_models = foz.list_zoo_models()
+    _apply_models(
+        all_models, confidence_thresh=0.5, pass_confidence_thresh=True
+    )
 
 
 @unittest.skip("Must be run manually")
-def test_keypoint_models():
-    manifest = fozm._load_zoo_models_manifest()
-    keypoint_models = [
-        model.name for model in manifest.models if model.has_tag("keypoints")
-    ]
-
-    _apply_person_keypoint_models(keypoint_models)
+def test_batch_size():
+    all_models = foz.list_zoo_models()
+    _apply_models(all_models, batch_size=5)
 
 
-def _apply_models(model_names):
+def _get_models_with_tag(tag):
+    model_names = []
+    for model_name in foz.list_zoo_models():
+        zoo_model = foz.get_zoo_model(model_name)
+        if zoo_model.has_tag(tag):
+            model_names.append(model_name)
+
+    return model_names
+
+
+def _apply_models(
+    model_names,
+    batch_size=None,
+    confidence_thresh=None,
+    pass_confidence_thresh=False,
+):
+    if pass_confidence_thresh:
+        kwargs = {"confidence_thresh": confidence_thresh}
+    else:
+        kwargs = {}
+
     dataset = foz.load_zoo_dataset(
         "coco-2017",
         split="validation",
@@ -88,18 +117,53 @@ def _apply_models(model_names):
         max_samples=10,
     )
 
-    f = lambda n: n.lower().replace("-", "_").replace(".", "_")
-
     for idx, model_name in enumerate(model_names, 1):
         print(
             "Running model %d/%d: '%s'" % (idx, len(model_names), model_name)
         )
 
         model = foz.load_zoo_model(model_name)
-        dataset.apply_model(model, label_field=f(model_name))
+
+        label_field = model_name.lower().replace("-", "_").replace(".", "_")
+
+        dataset.apply_model(
+            model, label_field=label_field, batch_size=batch_size, **kwargs
+        )
 
     session = fo.launch_app(dataset)
     session.wait()
+
+
+def _apply_models_with_logits(model_names, store_logits=True):
+    dataset = foz.load_zoo_dataset(
+        "coco-2017",
+        split="validation",
+        dataset_name=fo.get_default_dataset_name(),
+        shuffle=True,
+        max_samples=10,
+    )
+
+    label_fields = []
+    for idx, model_name in enumerate(model_names, 1):
+        print(
+            "Running model %d/%d: '%s'" % (idx, len(model_names), model_name)
+        )
+
+        model = foz.load_zoo_model(model_name)
+
+        label_field = model_name.lower().replace("-", "_").replace(".", "_")
+        dataset.apply_model(
+            model, label_field=label_field, store_logits=store_logits
+        )
+        label_fields.append(label_field)
+
+    for sample in dataset:
+        for field in label_fields:
+            if store_logits:
+                assert sample[field].logits is not None
+                assert sample[field].logits.ndim == 1
+            else:
+                assert sample[field].logits is None
 
 
 def _apply_embedding_models(model_names):
@@ -140,15 +204,15 @@ def _apply_person_keypoint_models(model_names):
         "ground_truth", F("label") == "person", only_matches=True
     ).limit(5)
 
-    f = lambda n: n.lower().replace("-", "_").replace(".", "_")
-
     for idx, model_name in enumerate(model_names, 1):
         print(
             "Running model %d/%d: '%s'" % (idx, len(model_names), model_name)
         )
 
         model = foz.load_zoo_model(model_name)
-        person_samples.apply_model(model, label_field=f(model_name))
+
+        label_field = model_name.lower().replace("-", "_").replace(".", "_")
+        person_samples.apply_model(model, label_field=label_field)
 
     session = fo.launch_app(view=person_samples)
     session.wait()
