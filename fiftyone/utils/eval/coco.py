5e7454c4c52bd24e072a12a30c8736d2538f2d83
brimoor <brimoor@umich.edu>
2021-02-08 09:33:25 -0500

    simplifying data model when evaluating detections

--- fiftyone/utils/eval/coco.py
+++ fiftyone/utils/eval/coco.py
@@ -5,19 +5,6 @@ COCO-style detection evaluation.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
-# pragma pylint: disable=redefined-builtin
-# pragma pylint: disable=unused-wildcard-import
-# pragma pylint: disable=wildcard-import
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-from builtins import *
-
-# pragma pylint: enable=redefined-builtin
-# pragma pylint: enable=unused-wildcard-import
-# pragma pylint: enable=wildcard-import
-
 import logging
 
 import numpy as np
@@ -66,50 +53,42 @@ def evaluate_detections(
     positive (FP), and false negative (FN) counts at the specified IoU are
     saved in the following top-level fields of each sample::
 
-        TP: sample.tp_iou_<iou>
-        FP: sample.fp_iou_<iou>
-        FN: sample.fn_iou_<iou>
-
-    where ``<iou> = str(iou).replace(".", "_")``.
+        TP: sample.tp
+        FP: sample.fp
+        FN: sample.fn
 
     Args:
-        samples: an iterable of :class:`fiftyone.core.sample.Sample` instances.
-            For example, this may be a :class:`fiftyone.core.dataset.Dataset`
-            or a :class:`fiftyone.core.view.DatasetView`
+        samples: a :class:`fiftyone.core.collections.SampleCollection`
         pred_field: the name of the field containing the predicted
             :class:`fiftyone.core.labels.Detections` to evaluate
         gt_field ("ground_truth"): the name of the field containing the ground
             truth :class:`fiftyone.core.labels.Detections`
-        iou (0.75): an IoU value for which to compute
-            per-detection and per-image TP/FP/FN
+        iou (0.75): an IoU value for which to compute per-detection and
+            per-image TP/FP/FN
         classwise (True): whether to match objects within the same class (True)
             or between classes (False)
         save_sample_fields (True): whether to save TP, FP, and FN counts at the
             sample-level
     """
+    processing_frames = (
+        samples.media_type == fom.VIDEO
+        and pred_field.startswith(samples._FRAMES_PREFIX)
+    )
+
     gt_key = "%s_eval" % pred_field
     pred_key = "%s_eval" % gt_field
-    eval_id = 0
-
-    iou_str = str(iou).replace(".", "_")
 
     logger.info("Evaluating detections...")
     with fou.ProgressBar() as pb:
-        for sample in pb(samples):
-            # Get image(s) to process
-            if sample.media_type == fom.VIDEO:
+        for sample in pb(samples.select_fields([gt_field, pred_field])):
+            if processing_frames:
                 images = sample.frames.values()
-                has_frames = True
             else:
                 images = [sample]
-                has_frames = False
 
-            # Initialize sample result dict tp, fp, fn = 0 for each IoU
-            sample_result_dict = {
-                "true_positives": 0,
-                "false_positives": 0,
-                "false_negatives": 0,
-            }
+            sample_tp = 0
+            sample_fp = 0
+            sample_fn = 0
 
             for image in images:
                 preds = image[pred_field]
@@ -121,15 +100,7 @@ def evaluate_detections(
                     if pred_key not in det:
                         det[pred_key] = {}
 
-                    if "matches" not in det[pred_key]:
-                        det[pred_key]["matches"] = {
-                            iou_str: {"gt_id": -1, "iou": -1}
-                        }
-
-                    else:
-                        matches = dict(det[pred_key]["matches"])
-                        matches[iou_str] = {"gt_id": -1, "iou": -1}
-                        det[pred_key]["matches"] = matches
+                    det[pred_key] = {"gt_id": -1, "iou": -1}
 
                     if classwise:
                         label = det.label
@@ -147,15 +118,7 @@ def evaluate_detections(
                     if gt_key not in det:
                         det[gt_key] = {}
 
-                    if "matches" not in det[gt_key]:
-                        det[gt_key]["matches"] = {
-                            iou_str: {"pred_id": -1, "iou": -1}
-                        }
-
-                    else:
-                        matches = dict(det[gt_key]["matches"])
-                        matches[iou_str] = {"pred_id": -1, "iou": -1}
-                        det[gt_key]["matches"] = matches
+                    det[gt_key] = {"pred_id": -1, "iou": -1}
 
                     if classwise:
                         label = det.label
@@ -224,9 +187,7 @@ def evaluate_detections(
                             best_match_iou = min([iou, 1 - 1e-10])
                             for gt_id, det_iou in pred_ious[pred.id]:
                                 gt = gt_by_id[gt_id]
-                                curr_gt_match = gt[gt_key]["matches"][iou_str][
-                                    "pred_id"
-                                ]
+                                curr_gt_match = gt[gt_key]["pred_id"]
 
                                 if "iscrowd" in gt.attributes:
                                     iscrowd = bool(
@@ -252,12 +213,11 @@ def evaluate_detections(
                                 # If the prediction was matched, store the eval
                                 # id of the pred in the gt and of the gt in the
                                 # pred
-                                gt_to_store = gt_by_id[best_match][gt_key]
-                                gt_to_store["matches"][iou_str] = {
+                                gt_by_id[best_match][gt_key] = {
                                     "pred_id": pred.id,
                                     "iou": best_match_iou,
                                 }
-                                pred[pred_key]["matches"][iou_str] = {
+                                pred[pred_key] = {
                                     "gt_id": best_match,
                                     "iou": best_match_iou,
                                 }
@@ -268,33 +228,23 @@ def evaluate_detections(
                         elif pred.label == cat:
                             false_pos += 1
 
-                sample_result_dict["true_positives"] += true_pos
-                sample_result_dict["false_positives"] += false_pos
                 false_neg = len(
-                    [
-                        g
-                        for g in dets["gts"]
-                        if g[gt_key]["matches"][iou_str]["pred_id"] == -1
-                    ]
+                    [g for g in dets["gts"] if g[gt_key]["pred_id"] == -1]
                 )
 
-                sample_result_dict["false_negatives"] += false_neg
+                sample_tp += true_pos
+                sample_fp += false_pos
+                sample_fn += false_neg
 
                 if save_sample_fields:
-                    image["tp_iou_%s" % iou_str] = true_pos
-                    image["fp_iou_%s" % iou_str] = false_pos
-                    image["fn_iou_%s" % iou_str] = false_neg
+                    image["tp"] = true_pos
+                    image["fp"] = false_pos
+                    image["fn"] = false_neg
 
-            if has_frames and save_sample_fields:
-                sample["tp_iou_%s" % iou_str] = sample_result_dict[
-                    "true_positives"
-                ]
-                sample["fp_iou_%s" % iou_str] = sample_result_dict[
-                    "false_positives"
-                ]
-                sample["fn_iou_%s" % iou_str] = sample_result_dict[
-                    "false_negatives"
-                ]
+            if processing_frames and save_sample_fields:
+                sample["tp"] = sample_tp
+                sample["fp"] = sample_fp
+                sample["fn"] = sample_fn
 
             sample.save()
 
