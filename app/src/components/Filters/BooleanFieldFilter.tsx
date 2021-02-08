4098721e6fd3220e5a37a2689ba87032aebc66d2
Benjamin Kane <ben@voxel51.com>
2021-02-08 10:50:50 -0700

    boolean filter components

--- app/src/components/Filters/BooleanFieldFilter.tsx
+++ app/src/components/Filters/BooleanFieldFilter.tsx
@@ -1,59 +1,69 @@
 import React, { useState } from "react";
-import { selectorFamily } from "recoil";
+import {
+  DefaultValue,
+  GetRecoilValue,
+  selectorFamily,
+  SetRecoilState,
+} from "recoil";
 
 import * as selectors from "../../recoil/selectors";
 import { NamedBooleanFilter } from "./StringFilter";
 import { animated, useSpring } from "react-spring";
 import useMeasure from "react-use-measure";
 
+type BooleanFilter = {
+  false: boolean;
+  true: boolean;
+  none: boolean;
+};
+
+const getFilter = (get: GetRecoilValue, path: string): BooleanFilter => {
+  return {
+    ...{
+      true: true,
+      false: true,
+      none: true,
+    },
+    ...get(selectors.filterStage(path)),
+  };
+};
+
+const setFilter = (
+  get: GetRecoilValue,
+  set: SetRecoilState,
+  path: string,
+  key: string,
+  value: boolean | DefaultValue
+) => {
+  set(selectors.filterStage(path), {
+    ...getFilter(get, path),
+    [key]: value,
+  });
+};
+
 const trueAtom = selectorFamily<boolean, string>({
   key: "filterBooleanFieldTrue",
-  get: (path) => ({ get }) => {
-    const filter = get(selectors.filterStage(path));
-    return filter?.true ?? true;
-  },
-  set: (path) => ({ get, set }, trueValue) => {
-    set(selectors.filterStage(path), {
-      true: trueValue,
-      false: get(falseAtom(path)),
-      none: get(noneAtom(path)),
-    });
-  },
+  get: (path) => ({ get }) => getFilter(get, path).true,
+  set: (path) => ({ get, set }, value) =>
+    setFilter(get, set, path, "true", value),
 });
 
 const falseAtom = selectorFamily<boolean, string>({
   key: "filterBooleanFieldFalse",
-  get: (path) => ({ get }) => {
-    const filter = get(selectors.filterStage(path));
-    return filter?.true ?? true;
-  },
-  set: (path) => ({ get, set }, falseValue) => {
-    set(selectors.filterStage(path), {
-      true: get(trueAtom(path)),
-      false: falseValue,
-      none: get(noneAtom(path)),
-    });
-  },
+  get: (path) => ({ get }) => getFilter(get, path).false,
+  set: (path) => ({ get, set }, value) =>
+    setFilter(get, set, path, "false", value),
 });
 
 const noneAtom = selectorFamily<boolean, string>({
   key: "filterBooleanFieldNone",
-  get: (path) => ({ get }) => {
-    const filter = get(filterStage(path));
-    return filter?.none ?? true;
-  },
-  set: (path) => ({ get, set }, none) => {
-    set(selectors.filterStage(path), {
-      true: get(trueAtom(path)),
-      false: get(falseAtom(path)),
-      none,
-    });
-  },
+  get: (path) => ({ get }) => getFilter(get, path).none,
+  set: (path) => ({ get, set }, value) =>
+    setFilter(get, set, path, "none", value),
 });
 
 const BooleanFieldFilter = ({ expanded, entry }) => {
   const [overflow, setOverflow] = useState("hidden");
-
   const [ref, { height }] = useMeasure();
   const props = useSpring({
     height: expanded ? height : 0,
@@ -67,8 +77,6 @@ const BooleanFieldFilter = ({ expanded, entry }) => {
   return (
     <animated.div style={{ ...props, overflow }}>
       <NamedBooleanFilter
-        name={"Values"}
-        valueName={"value"}
         color={entry.color}
         trueAtom={trueAtom(entry.path)}
         falseAtom={falseAtom(entry.path)}
--- app/src/components/Filters/BooleanFilter.tsx
+++ app/src/components/Filters/BooleanFilter.tsx
@@ -1,178 +1,20 @@
-import React, { useContext, useEffect, useRef } from "react";
-import { RecoilState, useRecoilState, useRecoilValue } from "recoil";
+import React from "react";
+import { RecoilState, useRecoilState } from "recoil";
 import { Checkbox, FormControlLabel } from "@material-ui/core";
-import styled, { ThemeContext } from "styled-components";
-import { useMachine } from "@xstate/react";
+import styled from "styled-components";
 
-import SearchResults from "../ViewBar/ViewStage/SearchResults";
-import { useOutsideClick } from "../../utils/hooks";
-
-const StringInput = styled.input`
-  width: 100%;
-  background: ${({ theme }) => theme.backgroundDark};
-  border: 1px solid #191c1f;
-  box-shadow: 0 8px 15px 0 rgba(0, 0, 0, 0.43);
-  border-radius: 2px;
-  font-size: 14px;
-  height: 2.5rem;
-  font-weight: bold;
-  padding: 0.5rem;
-  margin-bottom: 0.5rem;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-const Selected = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  margin: 0 -0.25rem;
-  padding-bottom: 0.5rem;
-  flex-wrap: wrap;
-`;
-
-const StringButton = styled.button`
-    background: ${({ theme }) => theme.background};
-    border: 2px solid #393C3F;
-    background-color: #2D3034;
-    border-radius: 11px;
-    text-align: center
-    vertical-align: middle;
-    margin: 0.5rem 0.25rem 0;
-    padding: 0 0.5rem;
-    line-height: 20px;
-    font-weight: bold;
-    cursor: pointer;
-    &:focus {
-      outline: none;
-    }
-  `;
-
-const StringFilterContainer = styled.div`
+const BooleanFilterContainer = styled.div`
   position: relative;
   margin: 0.25rem 0;
 `;
 
-type Props = {
-  valuesAtom: RecoilState<string[]>;
-  selectedValuesAtom: RecoilState<string[]>;
-  valueName: string;
-};
-
-const StringFilter = React.memo(
-  ({ valuesAtom, selectedValuesAtom, valueName }: Props) => {
-    const theme = useContext(ThemeContext);
-    const values = useRecoilValue(valuesAtom);
-    const [selectedValues, setSelectedValues] = useRecoilState(
-      selectedValuesAtom
-    );
-    const [state, send] = useMachine(stringFilterMachine);
-    const inputRef = useRef();
-
-    useEffect(() => {
-      const filtered = selectedValues.filter((c) => values.includes(c));
-      filtered.length !== selectedValues.length && setSelectedValues(filtered);
-    }, [values, selectedValues]);
-
-    useEffect(() => {
-      send({ type: "SET_VALUES", values });
-    }, [values]);
-
-    useOutsideClick(inputRef, () => send("BLUR"));
-    const { inputValue, results, currentResult, selected } = state.context;
-
-    useEffect(() => {
-      if (JSON.stringify(selected) !== JSON.stringify(selectedValues)) {
-        send({ type: "SET_SELECTED", selected: selectedValues });
-      }
-    }, [selectedValues]);
-
-    useEffect(() => {
-      if (
-        (state.event.type === "COMMIT" && state.context.valid) ||
-        state.event.type === "REMOVE" ||
-        state.event.type === "CLEAR"
-      ) {
-        setSelectedValues(state.context.selected);
-      }
-    }, [state.event]);
-
-    return (
-      <>
-        <StringFilterContainer>
-          <div ref={inputRef}>
-            <StringInput
-              value={inputValue}
-              placeholder={`+ add ${valueName}`}
-              onFocus={() => state.matches("reading") && send("EDIT")}
-              onBlur={() => {
-                state.matches("editing.searchResults.notHovering") &&
-                  send("BLUR");
-              }}
-              onChange={(e) => send({ type: "CHANGE", value: e.target.value })}
-              onKeyPress={(e) => {
-                if (e.key === "Enter") {
-                  send({ type: "COMMIT", value: e.target.value });
-                }
-              }}
-              onKeyDown={(e) => {
-                switch (e.key) {
-                  case "Escape":
-                    send("BLUR");
-                    break;
-                  case "ArrowDown":
-                    send("NEXT_RESULT");
-                    break;
-                  case "ArrowUp":
-                    send("PREVIOUS_RESULT");
-                    break;
-                }
-              }}
-            />
-            {state.matches("editing") && (
-              <SearchResults
-                results={results.filter((r) => !selected.includes(r)).sort()}
-                send={send}
-                currentResult={currentResult}
-                style={{
-                  position: "absolute",
-                  top: "0.25rem",
-                  fontSize: 14,
-                  maxHeight: 294,
-                  overflowY: "scroll",
-                }}
-              />
-            )}
-          </div>
-          {selected.length ? (
-            <Selected>
-              {selected.map((s) => (
-                <StringButton
-                  key={s}
-                  onClick={() => {
-                    send({ type: "REMOVE", value: s });
-                  }}
-                >
-                  {s + " "}
-                  <a style={{ color: theme.fontDark }}>x</a>
-                </StringButton>
-              ))}
-            </Selected>
-          ) : null}
-        </StringFilterContainer>
-      </>
-    );
-  }
-);
-
-const NamedStringFilterContainer = styled.div`
+const NamedBooleanFilterContainer = styled.div`
   padding-bottom: 0.5rem;
   margin: 3px;
   font-weight: bold;
 `;
 
-const NamedStringFilterHeader = styled.div`
+const NamedBooleanFilterHeader = styled.div`
   display: flex;
   justify-content: space-between;
 `;
@@ -186,10 +28,70 @@ const CheckboxContainer = styled.div`
   margin-top: 0.25rem;
 `;
 
+type Props = {
+  trueAtom: RecoilState<boolean>;
+  falseAtom: RecoilState<boolean>;
+  color: string;
+};
+
+const BooleanFilter = React.memo(({ trueAtom, falseAtom, color }: Props) => {
+  const [trueValue, setTrue] = useRecoilState(trueAtom);
+  const [falseValue, setFalse] = useRecoilState(falseAtom);
+
+  return (
+    <>
+      <BooleanFilterContainer>
+        <CheckboxContainer>
+          <FormControlLabel
+            label={
+              <div style={{ lineHeight: "20px", fontSize: 14 }}>
+                Filter true
+              </div>
+            }
+            control={
+              <Checkbox
+                checked={!trueValue}
+                onChange={() => setTrue(!trueValue)}
+                style={{
+                  padding: "0 5px",
+                  color,
+                }}
+              />
+            }
+          />
+        </CheckboxContainer>
+        <CheckboxContainer>
+          <FormControlLabel
+            label={
+              <div style={{ lineHeight: "20px", fontSize: 14 }}>
+                Filter false
+              </div>
+            }
+            control={
+              <Checkbox
+                checked={!falseValue}
+                onChange={() => setFalse(!falseValue)}
+                style={{
+                  padding: "0 5px",
+                  color,
+                }}
+              />
+            }
+          />
+        </CheckboxContainer>
+      </BooleanFilterContainer>
+    </>
+  );
+});
+
+const isDefault = (falseValue: boolean, trueValue: boolean, none: boolean) => {
+  return falseValue && trueValue && none;
+};
+
 type NamedProps = {
-  valuesAtom: RecoilState<string[]>;
-  selectedValuesAtom: RecoilState<string[]>;
-  includeNoneAtom: RecoilState<boolean>;
+  trueAtom: RecoilState<boolean>;
+  falseAtom: RecoilState<boolean>;
+  noneAtom: RecoilState<boolean>;
   name: string;
   valueName: string;
   color: string;
@@ -197,57 +99,56 @@ type NamedProps = {
 
 export const NamedStringFilter = React.memo(
   React.forwardRef(
-    (
-      { color, name, includeNoneAtom, ...stringFilterProps }: NamedProps,
-      ref
-    ) => {
-      const [includeNone, setIncludeNone] = useRecoilState(includeNoneAtom);
-      const [values, setValues] = useRecoilState(
-        stringFilterProps.selectedValuesAtom
+    ({ name, noneAtom, ...booleanFilterProps }: NamedProps, ref) => {
+      const [none, setNone] = useRecoilState(noneAtom);
+      const [falseValue, setFalse] = useRecoilState(
+        booleanFilterProps.falseAtom
       );
+      const [trueValue, setTrue] = useRecoilState(booleanFilterProps.trueAtom);
 
       return (
-        <NamedStringFilterContainer ref={ref}>
-          <NamedStringFilterHeader>
+        <NamedBooleanFilterContainer ref={ref}>
+          <NamedBooleanFilterHeader>
             {name}
-            {values.length || !includeNone ? (
+            {!isDefault(falseValue, trueValue, none) ? (
               <a
                 style={{ cursor: "pointer", textDecoration: "underline" }}
                 onClick={() => {
-                  setValues([]);
-                  setIncludeNone(true);
+                  setTrue(true);
+                  setFalse(true);
+                  setNone(true);
                 }}
               >
                 reset
               </a>
             ) : null}
-          </NamedStringFilterHeader>
-          <StringFilterContainer>
-            <StringFilter {...stringFilterProps} />
+          </NamedBooleanFilterHeader>
+          <BooleanFilterContainer>
+            <BooleanFilter {...booleanFilterProps} />
             <CheckboxContainer>
               <FormControlLabel
                 label={
                   <div style={{ lineHeight: "20px", fontSize: 14 }}>
-                    Filter no {stringFilterProps.valueName}
+                    Filter no {booleanFilterProps.valueName}
                   </div>
                 }
                 control={
                   <Checkbox
-                    checked={!includeNone}
-                    onChange={() => setIncludeNone(!includeNone)}
+                    checked={!none}
+                    onChange={() => setNone(!none)}
                     style={{
                       padding: "0 5px",
-                      color,
+                      color: booleanFilterProps.color,
                     }}
                   />
                 }
               />
             </CheckboxContainer>
-          </StringFilterContainer>
-        </NamedStringFilterContainer>
+          </BooleanFilterContainer>
+        </NamedBooleanFilterContainer>
       );
     }
   )
 );
 
-export default StringFilter;
+export default BooleanFilter;
