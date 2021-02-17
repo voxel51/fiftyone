2e16c9834072b23bffabafe632145823f839feb0
brimoor <brimoor@umich.edu>
2021-02-17 17:25:15 -0500

    eval keys must be valid variable names

--- fiftyone/utils/eval/base.py
+++ fiftyone/utils/eval/base.py
@@ -206,6 +206,12 @@ def validate_evaluation(samples, eval_info):
         eval_info: an :class:`EvaluationInfo`
     """
     eval_key = eval_info.eval_key
+    if not eval_key.isidentifier():
+        raise ValueError(
+            "Invalid eval_key '%s'; evaluation keys must be valid variable "
+            "names" % eval_key
+        )
+
     if eval_key not in list_evaluations(samples):
         return
 
