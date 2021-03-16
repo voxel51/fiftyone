7b730394582ad11e7aff6b35252107942090f977
brimoor <brimoor@umich.edu>
2021-03-16 18:53:36 -0400

    adding an interactive plotly heatmap

--- fiftyone/utils/plot/plotly.py
+++ fiftyone/utils/plot/plotly.py
@@ -35,23 +35,57 @@ _MAX_TRACES = 25
 
 
 def plot_confusion_matrix(
-    confusion_matrix, labels, colorscale=None, height=None, show=True
+    confusion_matrix,
+    labels,
+    ids=None,
+    colorscale=None,
+    height=None,
+    show=True,
 ):
     """Plots a confusion matrix.
+
+    If ``ids`` are provided, a :class:`PlotlyHeatmap` is returned that can be
+    used to interactively select cells and retrive their corresponding IDs.
 
     Args:
         confusion_matrix: a ``num_true x num_preds`` confusion matrix
         labels: a ``max(num_true, num_preds)`` array of class labels
+        ids (None): an optional array of same shape as ``confusion_matrix``
+            containing lists of IDs corresponding to each cell
         colorscale (None): a plotly colorscale to use
         height (None): a height for the plot, in pixels
         show (True): whether to show the plot
 
     Returns:
-        a ``plotly.graph_objects.Figure``
+        one of the following:
+
+        -   a :class:`PlotlyHeatmap`, if ``ids`` are provided
+        -   a ``plotly.graph_objects.Figure``, otherwise
     """
+    if ids is None:
+        return _plot_confusion_matrix_static(
+            confusion_matrix,
+            labels,
+            colorscale=colorscale,
+            height=height,
+            show=show,
+        )
+
+    return _plot_confusion_matrix_interactive(
+        confusion_matrix,
+        labels,
+        ids,
+        colorscale=colorscale,
+        height=height,
+        show=show,
+    )
+
+
+def _plot_confusion_matrix_static(
+    confusion_matrix, labels, colorscale=None, height=None, show=True
+):
     confusion_matrix = np.asarray(confusion_matrix)
-    num_rows = confusion_matrix.shape[0]
-    num_cols = confusion_matrix.shape[1]
+    num_rows, num_cols = confusion_matrix.shape
     zlim = [0, confusion_matrix.max()]
 
     hover_lines = [
@@ -88,6 +122,33 @@ def plot_confusion_matrix(
         figure.show()
 
     return figure
+
+
+def _plot_confusion_matrix_interactive(
+    confusion_matrix, labels, ids, colorscale=None, height=None, show=True
+):
+    confusion_matrix = np.asarray(confusion_matrix)
+    ids = np.asarray(ids)
+
+    num_rows, num_cols = confusion_matrix.shape
+    zlim = [0, confusion_matrix.max()]
+
+    plot = PlotlyHeatmap(
+        confusion_matrix,
+        ids,
+        xlabels=labels[:num_cols],
+        ylabels=labels[:num_rows],
+        zlim=zlim,
+        colorscale=colorscale,
+    )
+
+    if height is not None:
+        plot._figure.update_layout(height=height)
+
+    if show:
+        plot.show()
+
+    return plot
 
 
 def plot_pr_curve(
@@ -337,7 +398,7 @@ def scatterplot(
 
         -   a :class:`fiftyone.utils.plot.interactive.SessionPlot`, if a
             ``session`` is provided
-        -   a :class:`PlotlyPlot`, if no ``session`` is provided
+        -   a :class:`InteractiveScatter`, if no ``session`` is provided
         -   a ``plotly.graph_objects.Figure``, for 3D points or in non-notebook
             contexts
     """
@@ -397,7 +458,7 @@ def scatterplot(
 
         return figure
 
-    plot = PlotlyPlot(figure)
+    plot = InteractiveScatter(figure)
     if show:
         plot.show()
 
@@ -528,7 +589,7 @@ def location_scatterplot(
 
         return figure
 
-    plot = PlotlyPlot(figure)
+    plot = InteractiveScatter(figure)
     if show:
         plot.show()
 
@@ -539,48 +600,76 @@ def location_scatterplot(
     return SessionPlot(session, plot, link_type=link_type)
 
 
-class PlotlyPlot(InteractivePlot):
-    """Interactive plot wrapper for a Plotly figure.
+class InteractivePlotlyPlot(InteractivePlot):
+    """Base class for interactive plotly plots.
 
-    The provided ``figure`` may contain multiple traces.
+    Args:
+        widget: a ``plotly.graph_objects.FigureWidget``
+    """
+
+    def __init_(self, widget):
+        if not foc.is_notebook_context():
+            raise foc.ContextError(
+                "Interactive Plotly plots can only be used in notebooks"
+            )
+
+        self._widget = widget
+        self._handle = None
+
+        super().__init__()
+
+    def _show(self):
+        from IPython.display import display
+
+        # Create an empty display that we'll use for `freeze()` later
+        # Replacing the widget with an image in the same handle didn't work...
+        self._handle = display(display_id=True)
+        self._handle.display({"text/plain": ""}, raw=True)
+
+        display(self._widget)
+
+    def _freeze(self):
+        from IPython.display import Image
+
+        width = self._widget.layout.width
+        height = self._widget.layout.height
+        image_bytes = self._widget.to_image(
+            format="png", height=height, width=width
+        )
+
+        self._widget.close()
+        self._handle.update(Image(image_bytes))
+
+
+class InteractiveScatter(InteractivePlotlyPlot):
+    """Interactive plot wrapper for a Plotly figure containing one or more
+    scatter-type traces.
+
+    This plot responds to selection and deselection events triggered on the
+    figure's traces via plotly's lasso and box selector tools.
 
     All traces must contain IDs in their ``customdata`` attribute that identify
     the points in the traces.
 
     Args:
         figure: a ``plotly.graph_objects.Figure``
-        points (None): an optional ``num_points x 2`` array of points. If
-            provided, selection is manually performed across all visible traces
-            using these points
-        ids (None): an optional ``num_points`` array indicating the IDs of each
-            point in ``points``. Only necessary when ``points`` is provided
     """
 
-    def __init__(self, figure, points=None, ids=None):
-        if not foc.is_notebook_context():
-            raise foc.ContextError(
-                "Interactive Plotly plots can only be used in notebooks"
-            )
+    def __init__(self, figure):
+        widget = pgo.FigureWidget(figure)
+        traces = widget.data
 
-        self._widget = pgo.FigureWidget(figure)
-        self._traces = self._widget.data
+        self._traces = traces
         self._trace_ids = {}
         self._ids_to_traces = {}
         self._ids_to_inds = {}
-        self._select_callback = None
         self._callback_flags = {}
-        self._handle = None
-
-        # For manual point selection calculations
-        self._points = points
-        self._point_ids = ids
-        self._trace_inds = None
-        self._ids = None
+        self._select_callback = None
 
         self._init_traces()
         self._init_callback_flags()
 
-        super().__init__()
+        super().__init__(widget)
 
     def _init_traces(self):
         for idx, trace in enumerate(self._traces):
@@ -604,16 +693,11 @@ class PlotlyPlot(InteractivePlot):
                 _id: idx for idx, _id in enumerate(trace_ids)
             }
 
-        if self._points is not None:
-            self._trace_inds = np.zeros(len(self._points), dtype=int)
-            for trace_idx, trace in enumerate(self._traces):
-                trace_ids = set(self._trace_ids[trace_idx])
-                for point_idx, _id in enumerate(self._point_ids):
-                    if _id in trace_ids:
-                        self._trace_inds[point_idx] = trace_idx
-
     def _init_callback_flags(self):
         self._callback_flags = {t.name: False for t in self._traces}
+
+    def _register_selection_callback(self, callback):
+        self._select_callback = callback
 
     @property
     def _any_selected(self):
@@ -621,9 +705,6 @@ class PlotlyPlot(InteractivePlot):
 
     @property
     def _selected_ids(self):
-        if self._ids is not None:
-            return list(self._ids)
-
         ids = []
         for idx, trace in enumerate(self._traces):
             if trace.selectedpoints:
@@ -645,26 +726,7 @@ class PlotlyPlot(InteractivePlot):
             trace.on_selection(_on_selection)
             trace.on_deselect(_on_deselect)
 
-        from IPython.display import display
-
-        # Create an empty display that we'll use for `freeze()` later
-        # Replacing the widget with an image in the same handle didn't work...
-        self._handle = display(display_id=True)
-        self._handle.display({"text/plain": ""}, raw=True)
-
-        display(self._widget)
-
-    def _freeze(self):
-        from IPython.display import Image
-
-        width = self._widget.layout.width
-        height = self._widget.layout.height
-        image_bytes = self._widget.to_image(
-            format="png", height=height, width=width
-        )
-
-        self._widget.close()
-        self._handle.update(Image(image_bytes))
+        super()._show()
 
     def _disconnect(self):
         for trace in self._traces:
@@ -692,10 +754,6 @@ class PlotlyPlot(InteractivePlot):
             trace.update(selectedpoints=trace_inds)
 
     def _on_select(self, trace, selector=None):
-        if self._points is not None:
-            # Manually compute points within selector
-            self._manual_select(selector)
-
         if self._select_callback is None:
             return
 
@@ -720,6 +778,52 @@ class PlotlyPlot(InteractivePlot):
             self._init_callback_flags()
 
         return ready
+
+
+class ManualInteractiveScatter(InteractiveScatter):
+    """Interactive plot wrapper for a Plotly figure containing one or more
+    scatter-type traces.
+
+    This plot responds to selection and deselection events triggered on the
+    figure's traces via plotly's lasso and box selector tools.
+
+    Unlike :class:`InteractiveScatter`, this class does not require the traces
+    to store the IDs of their points in their ``customdata`` attribute.
+    Instead, the selected points are manually computed from the raw lasso/box
+    coordinates via the provided ``points`` array each time a selection event
+    occurs.
+
+    Args:
+        figure: a ``plotly.graph_objects.Figure``
+        points: a ``num_points x 2`` array of points
+        ids: a ``num_points`` array containing the IDs for ``points``
+    """
+
+    def __init__(self, figure, points, ids):
+        self._points = points
+        self._point_ids = ids
+        self._trace_inds = None
+        self._ids = None
+
+        super().__init__(figure)
+
+    @property
+    def _selected_ids(self):
+        return list(self._ids) if self._ids is not None else []
+
+    def _init_traces(self):
+        self._trace_inds = np.zeros(len(self._points), dtype=int)
+        for trace_idx, trace in enumerate(self._traces):
+            trace_ids = set(self._trace_ids[trace_idx])
+            for point_idx, _id in enumerate(self._point_ids):
+                if _id in trace_ids:
+                    self._trace_inds[point_idx] = trace_idx
+
+    def _on_select(self, trace, selector=None):
+        # Manually compute points within selector
+        self._manual_select(selector)
+
+        super()._on_select(trace, selector=selector)
 
     def _manual_select(self, selector):
         if not isinstance(selector, (pcb.LassoSelector, pcb.BoxSelector)):
@@ -749,6 +853,209 @@ class PlotlyPlot(InteractivePlot):
         mask = np.array([ind in visible_traces for ind in self._trace_inds])
 
         self._ids = self._point_ids[found & mask]
+
+
+class PlotlyHeatmap(InteractivePlotlyPlot):
+    """An interactive Plotly heatmap.
+
+    Args:
+        Z: a ``num_cols x num_rows`` array of heatmap values
+        ids: an array of same shape as ``Z`` whose elements contain lists
+            of IDs for the heatmap cells
+        xlabels (None): a ``num_rows`` array of x labels
+        ylabels (None): a ``num_cols`` array of y labels
+        colorscale (None): a plotly colorscale to use
+        grid_opacity (0.1): an opacity value for the grid points
+        bg_opacity (0.25): an opacity value for background (unselected) cells
+    """
+
+    def __init__(
+        self,
+        Z,
+        ids,
+        xlabels=None,
+        ylabels=None,
+        zlim=None,
+        colorscale=None,
+        grid_opacity=0.1,
+        bg_opacity=0.25,
+    ):
+        Z = np.asarray(Z)
+
+        if zlim is None:
+            zlim = [Z.min(), Z.max()]
+
+        self.Z = Z
+        self.ids = ids
+        self.xlabels = xlabels
+        self.ylabels = ylabels
+        self.zlim = zlim
+        self.colorscale = colorscale
+        self.grid_opacity = grid_opacity
+        self.bg_opacity = bg_opacity
+
+        self._selected_cells = []
+        self._cells_map = {}
+
+        self._figure = None
+        self._widget = None
+        self._gridw = None
+        self._selectedw = None
+        self._bgw = None
+        self._select_callback = None
+
+        self._init_cells_map()
+        self._init_heatmap()
+
+        super().__init__(self._widget)
+
+    @property
+    def _any_selected(self):
+        return bool(self._selected_cells)
+
+    @property
+    def _selected_ids(self):
+        ids = []
+        for x, y in self._selected_cells:
+            ids.extend(self.ids[x, y])
+
+        return ids
+
+    def _register_selection_callback(self, callback):
+        self._select_callback = callback
+
+    def _show(self):
+        def _on_click(trace, points, state):
+            self._on_click(points.point_inds[0])
+
+        def _on_selection(trace, points, state):
+            self._on_selection(points.point_inds)
+
+        self._bgw.on_click(_on_click)
+        self._gridw.on_selection(_on_selection)
+
+        super()._show()
+
+    def _disconnect(self):
+        self._bgw.on_click(None)
+        self._gridw.on_selection(None)
+
+    def _select_ids(self, ids):
+        if ids is None:
+            ids = []
+
+        cells = list(set(self._cells_map(_id) for _id in ids))
+        self._select(cells)
+
+    def _on_click(self, cell):
+        cell = tuple(cell)
+        num_selected = len(self._selected_cells)
+
+        self._gridw.selectedpoints = None
+
+        if (num_selected > 1) or (
+            num_selected == 1 and cell == self._selected_cells[0]
+        ):
+            self._deselect()
+        else:
+            self._select([cell])
+
+    def _on_selection(self, point_inds):
+        if point_inds:
+            x, y = np.unravel_index(point_inds, self.Z.shape)
+            cells = list(zip(x, y))
+            self._select(cells)
+        else:
+            self._deselect()
+
+    def _select(self, cells):
+        x, y = zip(*cells)
+        Zclick = np.full(self.Z.shape, None)
+        Zclick[x, y] = self.Z[x, y]
+        self._selected_cells = cells
+        self._selectedw.z = Zclick
+
+        if self._select_callback is not None:
+            self._select_callback(self.selected_ids)
+
+    def _deselect(self):
+        self._selected_cells = []
+        self._selectedw.z = self.Z
+
+        if self._select_callback is not None:
+            self._select_callback(self.selected_ids)
+
+    def _init_cells_map(self):
+        num_cols, num_rows = self.Z.shape
+        self._cells_map = {}
+        for y in range(num_cols):
+            for x in range(num_rows):
+                for _id in self.ids[y, x]:
+                    self._cells_map[_id] = (x, y)
+
+    def _init_heatmap(self):
+        Z = self.Z.copy()
+
+        num_cols, num_rows = Z.shape
+        xticks = np.arange(num_rows)
+        yticks = np.arange(num_cols)
+        X, Y = np.meshgrid(xticks, yticks)
+
+        hover_lines = [
+            "<b>count: %{z:d}</b>",
+            "truth: %{y}",
+            "predicted: %{x}",
+        ]
+        hovertemplate = "<br>".join(hover_lines) + "<extra></extra>"
+
+        grid = pgo.Scatter(
+            x=X.flatten(),
+            y=Y.flatten(),
+            opacity=self.grid_opacity,
+            mode="markers",
+            hovertemplate=None,  # no hover
+        )
+
+        selected = pgo.Heatmap(
+            z=Z,
+            zmin=self.zlim[0],
+            zmax=self.zlim[1],
+            colorbar=dict(lenmode="fraction", len=1),
+            colorscale=self.colorscale,
+            hoverinfo="skip",  # no hover, no callbacks
+        )
+
+        bg = pgo.Heatmap(
+            z=Z,
+            colorscale=self.colorscale,
+            opacity=self.bg_opacity,
+            showscale=False,
+            hovertemplate=hovertemplate,
+        )
+
+        figure = pgo.Figure([grid, selected, bg])
+
+        figure.update_layout(
+            xaxis=dict(
+                tickmode="array",
+                tickvals=xticks,
+                ticktext=self.xlabels,
+                range=[-0.5, num_rows - 0.5],
+                constrain="domain",
+            ),
+            yaxis=dict(
+                tickmode="array",
+                tickvals=yticks,
+                ticktext=self.ylabels,
+                scaleanchor="x",
+                scaleratio=1,
+            ),
+            clickmode="event",
+        )
+
+        self._figure = figure
+        self._widget = pgo.FigureWidget(figure)
+        self._gridw, self._selectedw, self._bgw = self._widget.data
 
 
 def _parse_scatter_inputs(
