d83dede2cdd1c57a122209b3324104852b913c92
hdadoun <hind.dadoun@inria.fr>
2021-03-23 18:44:53 +0100

    clean format

--- fiftyone/utils/eval/coco.py
+++ fiftyone/utils/eval/coco.py
@@ -226,8 +226,9 @@ class COCOEvaluation(DetectionEvaluation):
                 confs = [p[3] for p in tp] + [p[3] for p in fp]
                 if None in confs:
                     raise ValueError(
-                        "Average precision is a ranking based metric, detections in pred_field"
-                        " sample should have a confidence attribute "
+                        "Average precision is a ranking based metric,"
+                        " detections in pred_field should have a confidence"
+                        " attribute"
                     )
                 inds = np.argsort(-np.array(confs), kind="mergesort")
                 tp_fp = np.array(tp_fp)[inds]
