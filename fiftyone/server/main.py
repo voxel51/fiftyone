3327db732df65d9ab0f39c248fd4fa902717ed85
Benjamin Kane <ben@voxel51.com>
2021-03-24 15:14:05 -0600

    colab fix

--- fiftyone/server/main.py
+++ fiftyone/server/main.py
@@ -261,7 +261,13 @@ class PollingHandler(tornado.web.RequestHandler):
                     force_update = True
                     message = {"state": StateHandler.state}
 
-            if event in {"distributions", "page", "get_video_data"}:
+            if event in {
+                "distributions",
+                "page",
+                "get_video_data",
+                "all_tags",
+                "selected_statistics",
+            }:
                 caller = self
             elif event in {"capture", "update"}:
                 caller = client
@@ -744,7 +750,7 @@ class StateHandler(tornado.websocket.WebSocketHandler):
 
         _write_message(
             {"type": "selected_statistics", "count": count, "tags": tags},
-            app=True,
+            only=caller,
         )
 
     @classmethod
