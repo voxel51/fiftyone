5a337a286eafabf787c7942c8caac987d6611d44
Benjamin Kane <ben@voxel51.com>
2021-05-05 07:53:07 -0600

    summarizeLongStr

--- app/src/utils/generic.ts
+++ app/src/utils/generic.ts
@@ -9,3 +9,31 @@ export const isElectron = (): boolean => {
 export const isFloat = (n: number): boolean => {
   return Number(n) === n && n % 1 !== 0;
 };
+
+export const summarizeLongStr = (
+  s: string,
+  maxLen: number,
+  mode: string = "middle"
+): string => {
+  if (s.length <= maxLen) {
+    return s;
+  }
+
+  const modeLower = mode.toLowerCase();
+
+  if (modeLower === "first") {
+    return "... " + s.slice(-(maxLen - 4));
+  }
+
+  if (modeLower === "middle") {
+    const len1 = Math.ceil(0.5 * (maxLen - 5));
+    const len2 = Math.floor(0.5 * (maxLen - 5));
+    return s.slice(0, len1) + " ... " + s.slice(-len2);
+  }
+
+  if (modeLower === "last") {
+    return s.slice(0, maxLen - 4) + " ...";
+  }
+
+  throw new Error(`Unsupported mode '${mode}'`);
+};
