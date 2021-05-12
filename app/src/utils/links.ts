68c4c20785f06515859c7e407efaed24115b82fa
Benjamin Kane <ben@voxel51.com>
2021-05-12 12:34:14 -0600

    fixes

--- app/src/utils/links.ts
+++ app/src/utils/links.ts
@@ -1,5 +1,5 @@
 export const OBJECT_PATCHES =
-  "https://voxel51.com/docs/fiftyone/user_guide/app.html#view-object-patches";
+  "https://voxel51.com/docs/fiftyone/user_guide/app.html#viewing-object-patches";
 
 export const EVALUATION_PATCHES =
   "https://voxel51.com/docs/fiftyone/user_guide/app.html#viewing-evaluation-patches";
