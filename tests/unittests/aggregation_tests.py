2ae6807a9ec4f74cf16b6dfded136f44b77cf70c
brimoor <brimoor@umich.edu>
2021-06-08 15:15:24 -0400

    adding unit tests for ID aggregations

--- tests/unittests/aggregation_tests.py
+++ tests/unittests/aggregation_tests.py
@@ -376,10 +376,10 @@ class DatasetTests(unittest.TestCase):
         for _id in ids:
             self.assertIsInstance(_id, str)
 
-        object_ids = dataset.values("_id")
+        oids = dataset.values("_id")
 
-        self.assertEqual(len(object_ids), 5)
-        for oid in object_ids:
+        self.assertEqual(len(oids), 5)
+        for oid in oids:
             self.assertIsInstance(oid, ObjectId)
 
         label_ids = dataset.values("ground_truth.id")
@@ -388,10 +388,10 @@ class DatasetTests(unittest.TestCase):
         for _id in label_ids:
             self.assertIsInstance(_id, str)
 
-        label_object_ids = dataset.values("ground_truth._id")
+        label_oids = dataset.values("ground_truth._id")
 
-        self.assertEqual(len(label_object_ids), 5)
-        for oid in label_object_ids:
+        self.assertEqual(len(label_oids), 5)
+        for oid in label_oids:
             self.assertIsInstance(oid, ObjectId)
 
         frame_ids = dataset.values("frames.id")
@@ -402,12 +402,12 @@ class DatasetTests(unittest.TestCase):
             for _id in _frame_ids:
                 self.assertIsInstance(_id, str)
 
-        frame_object_ids = dataset.values("frames._id")
+        frame_oids = dataset.values("frames._id")
 
-        self.assertEqual(len(frame_object_ids), 5)
-        for _frame_object_ids in frame_object_ids:
-            self.assertEqual(len(_frame_object_ids), 4)
-            for oid in _frame_object_ids:
+        self.assertEqual(len(frame_oids), 5)
+        for _frame_oids in frame_oids:
+            self.assertEqual(len(_frame_oids), 4)
+            for oid in _frame_oids:
                 self.assertIsInstance(oid, ObjectId)
 
         frame_label_ids = dataset.values("frames.ground_truth.id")
@@ -418,13 +418,49 @@ class DatasetTests(unittest.TestCase):
             for _id in _frame_label_ids:
                 self.assertIsInstance(_id, str)
 
-        frame_label_object_ids = dataset.values("frames.ground_truth._id")
+        frame_label_oids = dataset.values("frames.ground_truth._id")
 
-        self.assertEqual(len(frame_label_object_ids), 5)
-        for _frame_label_object_ids in frame_label_object_ids:
-            self.assertEqual(len(_frame_label_object_ids), 4)
-            for oid in _frame_label_object_ids:
+        self.assertEqual(len(frame_label_oids), 5)
+        for _frame_label_oids in frame_label_oids:
+            self.assertEqual(len(_frame_label_oids), 4)
+            for oid in _frame_label_oids:
                 self.assertIsInstance(oid, ObjectId)
+
+    @drop_datasets
+    def test_object_ids(self):
+        dataset = fo.Dataset()
+        dataset.add_samples(
+            [fo.Sample(filepath="image%d.png" % i) for i in range(10)]
+        )
+
+        id_bounds = dataset.bounds("id")
+        for _id in id_bounds:
+            self.assertIsInstance(_id, str)
+
+        oid_bounds = dataset.bounds("_id")
+        for oid in oid_bounds:
+            self.assertIsInstance(oid, ObjectId)
+
+        ids = dataset.distinct("id")
+        for _id in ids:
+            self.assertIsInstance(_id, str)
+
+        oids = dataset.distinct("_id")
+        for oid in oids:
+            self.assertIsInstance(oid, ObjectId)
+
+        self.assertEqual(dataset.count("id"), 10)
+        self.assertEqual(dataset.count("_id"), 10)
+
+        id_counts = dataset.count_values("id")
+        for _id, count in id_counts.items():
+            self.assertIsInstance(_id, str)
+            self.assertEqual(count, 1)
+
+        oid_counts = dataset.count_values("_id")
+        for oid, count in oid_counts.items():
+            self.assertIsInstance(oid, ObjectId)
+            self.assertEqual(count, 1)
 
 
 if __name__ == "__main__":
