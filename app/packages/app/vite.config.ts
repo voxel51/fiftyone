0ba7b34590c034b9463dc33ec2e284192fa644fc
Benjamin Kane <ben@voxel51.com>
2021-06-22 16:47:20 -0600

    polyfill node

--- app/packages/app/vite.config.ts
+++ app/packages/app/vite.config.ts
@@ -1,11 +1,12 @@
-import path from "path";
 import { UserConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 export default {
   plugins: [
     reactRefresh({
       parserPlugins: ["classProperties", "classPrivateProperties"],
     }),
+    nodePolyfills(),
   ],
 } as UserConfig;
