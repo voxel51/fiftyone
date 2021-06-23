d32064892d437c95fbccc97ab492ef58747cddd0
Benjamin Kane <ben@voxel51.com>
2021-06-23 08:04:46 -0600

    update node version

--- install.bash
+++ install.bash
@@ -100,8 +100,8 @@ if [ ${BUILD_APP} = true ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-    nvm install v12.16.2
-    nvm use v12.16.2
+    nvm install v16.3.0
+    nvm use v16.3.0
     npm -g install yarn
     if [ -f ~/.bashrc ]; then
         source ~/.bashrc
