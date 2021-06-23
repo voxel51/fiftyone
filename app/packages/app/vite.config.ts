6928978773af7e7903ae477a46f599aa47a5a734
Benjamin Kane <ben@voxel51.com>
2021-06-23 15:58:52 -0600

    desktop config tweaks

--- app/packages/app/vite.config.ts
+++ app/packages/app/vite.config.ts
@@ -3,6 +3,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import nodePolyfills from "rollup-plugin-polyfill-node";
 
 export default {
+  base: "",
   plugins: [
     reactRefresh({
       parserPlugins: ["classProperties", "classPrivateProperties"],
