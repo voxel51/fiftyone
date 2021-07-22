1346802e03eccf5b0e1a907bf66a6fd7bba8c823
brimoor <brimoor@umich.edu>
2021-07-22 00:35:05 -0400

    adding partial import/export unit tests

---
+++ tests/unittests/import_export_tests.py
@@ -0,0 +1,603 @@
+"""
+FiftyOne import/export-related unit tests.
+
+| Copyright 2017-2021, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+import os
+import random
+import string
+import unittest
+
+import numpy as np
+
+import eta.core.image as etai
+import eta.core.utils as etau
+import eta.core.video as etav
+
+import fiftyone as fo
+
+from decorators import drop_datasets
+
+
+class ImageDatasetTests(unittest.TestCase):
+    def setUp(self):
+        temp_dir = etau.TempDir()
+        tmp_dir = temp_dir.__enter__()
+        images_dir = os.path.join(tmp_dir, "_images")
+        image_path = os.path.join(images_dir, "image.jpg")
+
+        img = np.random.randint(255, size=(480, 640, 3), dtype=np.uint8)
+        etai.write(img, image_path)
+
+        self._temp_dir = temp_dir
+        self.tmp_dir = tmp_dir
+        self.images_dir = images_dir
+        self.image_path = image_path
+
+    def tearDown(self):
+        self._temp_dir.__exit__()
+
+    def _new_image(self):
+        filepath = os.path.join(
+            self.images_dir,
+            self._new_name() + os.path.splitext(self.image_path)[1],
+        )
+
+        etau.copy_file(self.image_path, filepath)
+        return filepath
+
+    def _new_name(self):
+        return "".join(
+            random.choice(string.ascii_lowercase + string.digits)
+            for _ in range(24)
+        )
+
+    def _new_dir(self):
+        return os.path.join(self.tmp_dir, self._new_name())
+
+
+class ImageExportCoersionTests(ImageDatasetTests):
+    @drop_datasets
+    def test_field_inference(self):
+        sample = fo.Sample(
+            filepath=self._new_image(),
+            ground_truth=fo.Detections(
+                detections=[
+                    fo.Detection(
+                        label="cat", bounding_box=[0.1, 0.1, 0.4, 0.4],
+                    ),
+                    fo.Detection(
+                        label="dog", bounding_box=[0.5, 0.5, 0.4, 0.4],
+                    ),
+                ]
+            ),
+        )
+
+        dataset = fo.Dataset()
+        dataset.add_sample(sample)
+
+        #
+        # A field of appropriate type is inferred
+        #
+
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir, dataset_type=fo.types.COCODetectionDataset,
+        )
+
+        #
+        # Multiple compatible field types exist, but the first one is still
+        # chosen and used
+        #
+
+        dataset.clone_sample_field("ground_truth", "predictions")
+
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir, dataset_type=fo.types.COCODetectionDataset,
+        )
+
+    @drop_datasets
+    def test_patch_exports(self):
+        sample = fo.Sample(
+            filepath=self._new_image(),
+            ground_truth=fo.Detections(
+                detections=[
+                    fo.Detection(
+                        label="cat", bounding_box=[0.1, 0.1, 0.4, 0.4],
+                    ),
+                    fo.Detection(
+                        label="dog", bounding_box=[0.5, 0.5, 0.4, 0.4],
+                    ),
+                ]
+            ),
+        )
+
+        dataset = fo.Dataset()
+        dataset.add_sample(sample)
+
+        #
+        # No label field is provided; only images are exported
+        #
+
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir, dataset_type=fo.types.ImageDirectory,
+        )
+
+        #
+        # A detections field is provided, so the object patches are exported as
+        # a directory of images
+        #
+
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.ImageDirectory,
+            label_field="ground_truth",
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir, dataset_type=fo.types.ImageDirectory,
+        )
+
+        self.assertEqual(
+            len(dataset2), dataset.count("ground_truth.detections")
+        )
+
+        #
+        # A detections field is provided, so the object patches are exported as
+        # an image classification directory tree
+        #
+
+        export_dir3 = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir3,
+            dataset_type=fo.types.ImageClassificationDirectoryTree,
+            label_field="ground_truth",
+        )
+
+        dataset3 = fo.Dataset.from_dir(
+            dataset_dir=export_dir3,
+            dataset_type=fo.types.ImageClassificationDirectoryTree,
+        )
+
+        self.assertEqual(
+            len(dataset3), dataset.count("ground_truth.detections")
+        )
+
+    @drop_datasets
+    def test_single_label_to_lists(self):
+        sample = fo.Sample(
+            filepath=self._new_image(),
+            ground_truth=fo.Detection(
+                label="cat", bounding_box=[0.1, 0.1, 0.4, 0.4],
+            ),
+        )
+
+        dataset = fo.Dataset()
+        dataset.add_sample(sample)
+
+        #
+        # The `ground_truth` field has type `Detection`, but COCO format
+        # expects `Detections`, so the labels are automatically coerced to
+        # single-label lists
+        #
+
+        export_dir4 = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir4,
+            dataset_type=fo.types.COCODetectionDataset,
+            label_field="ground_truth",
+        )
+
+    @drop_datasets
+    def test_classification_as_detections(self):
+        sample = fo.Sample(
+            filepath=self._new_image(), animal=fo.Classification(label="cat"),
+        )
+
+        dataset = fo.Dataset()
+        dataset.add_sample(sample)
+
+        #
+        # The `animal` field is exported as detections that span entire images
+        #
+
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.COCODetectionDataset,
+            label_field="animal",
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir,
+            dataset_type=fo.types.COCODetectionDataset,
+            label_field="animal",
+        )
+
+        bounding_box = dataset2.first().animal.detections[0].bounding_box
+        self.assertTrue(np.allclose(bounding_box, [0, 0, 1, 1]))
+
+
+class UnlabeledImageDatasetTests(ImageDatasetTests):
+    def _make_dataset(self):
+        samples = [fo.Sample(filepath=self._new_image()) for _ in range(5)]
+
+        dataset = fo.Dataset()
+        dataset.add_samples(samples)
+
+        return dataset
+
+    @drop_datasets
+    def test_image_directory(self):
+        dataset = self._make_dataset()
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir, dataset_type=fo.types.ImageDirectory,
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir, dataset_type=fo.types.ImageDirectory,
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+
+
+class ImageClassificationDatasetTests(ImageDatasetTests):
+    def _make_dataset(self):
+        samples = [
+            fo.Sample(
+                filepath=self._new_image(),
+                predictions=fo.Classification(label="cat", confidence=0.9),
+            ),
+            fo.Sample(
+                filepath=self._new_image(),
+                predictions=fo.Classification(label="dog", confidence=0.95),
+            ),
+        ]
+
+        dataset = fo.Dataset()
+        dataset.add_samples(samples)
+
+        return dataset
+
+    @drop_datasets
+    def test_fiftyone_image_classification_dataset(self):
+        dataset = self._make_dataset()
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.FiftyOneImageClassificationDataset,
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir,
+            dataset_type=fo.types.FiftyOneImageClassificationDataset,
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+
+    @drop_datasets
+    def test_image_classification_directory_tree(self):
+        dataset = self._make_dataset()
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.ImageClassificationDirectoryTree,
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir,
+            dataset_type=fo.types.ImageClassificationDirectoryTree,
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+
+    @drop_datasets
+    def test_tf_image_classification_dataset(self):
+        dataset = self._make_dataset()
+
+        # Standard format
+
+        export_dir = self._new_dir()
+        images_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.TFImageClassificationDataset,
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir,
+            dataset_type=fo.types.TFImageClassificationDataset,
+            images_dir=images_dir,
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+
+        # Direct records path w/ sharding
+
+        tf_records_path = os.path.join(self._new_dir(), "tf.records")
+        tf_records_patt = tf_records_path + "-*-of-*"
+        images_dir = self._new_dir()
+
+        dataset.export(
+            dataset_type=fo.types.TFImageClassificationDataset,
+            tf_records_path=tf_records_path,
+            num_shards=2,
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_type=fo.types.TFImageClassificationDataset,
+            tf_records_path=tf_records_patt,
+            images_dir=images_dir,
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+
+
+class ImageDetectionDatasetTests(ImageDatasetTests):
+    def _make_dataset(self):
+        samples = [
+            fo.Sample(
+                filepath=self._new_image(),
+                predictions=fo.Detections(
+                    detections=[
+                        fo.Detection(
+                            label="cat", bounding_box=[0.1, 0.1, 0.4, 0.4],
+                        ),
+                        fo.Detection(
+                            label="dog", bounding_box=[0.5, 0.5, 0.4, 0.4],
+                        ),
+                    ]
+                ),
+            ),
+            fo.Sample(
+                filepath=self._new_image(),
+                predictions=fo.Detections(
+                    detections=[
+                        fo.Detection(
+                            label="cat",
+                            bounding_box=[0.1, 0.1, 0.4, 0.4],
+                            confidence=0.9,
+                            age=51,
+                            cute=True,
+                            mood="surly",
+                        ),
+                        fo.Detection(
+                            label="dog",
+                            bounding_box=[0.5, 0.5, 0.4, 0.4],
+                            confidence=0.95,
+                            age=52,
+                            cute=False,
+                            mood="derpy",
+                        ),
+                    ]
+                ),
+            ),
+        ]
+
+        dataset = fo.Dataset()
+        dataset.add_samples(samples)
+
+        return dataset
+
+    @drop_datasets
+    def test_fiftyone_image_detection_dataset(self):
+        dataset = self._make_dataset()
+
+        # Standard format
+
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.FiftyOneImageDetectionDataset,
+            label_field="predictions",
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir,
+            dataset_type=fo.types.FiftyOneImageDetectionDataset,
+            label_field="predictions",
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+        self.assertEqual(
+            dataset.count("predictions.detections"),
+            dataset2.count("predictions.detections"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.confidence"),
+            dataset2.distinct("predictions.detections.confidence"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.age"),
+            dataset2.distinct("predictions.detections.age"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.cute"),
+            dataset2.distinct("predictions.detections.cute"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.mood"),
+            dataset2.distinct("predictions.detections.mood"),
+        )
+
+        # Labels-only
+
+        export_dir = self._new_dir()
+        data_path = os.path.dirname(dataset.first().filepath)
+        labels_path = os.path.join(export_dir, "labels.json")
+
+        dataset.export(
+            dataset_type=fo.types.FiftyOneImageDetectionDataset,
+            labels_path=labels_path,
+            label_field="predictions",
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_type=fo.types.FiftyOneImageDetectionDataset,
+            data_path=data_path,
+            labels_path=labels_path,
+            label_field="predictions",
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+        self.assertEqual(
+            dataset.count("predictions.detections"),
+            dataset2.count("predictions.detections"),
+        )
+
+    @drop_datasets
+    def test_tf_object_detection_dataset(self):
+        dataset = self._make_dataset()
+
+        # Standard format
+
+        export_dir = self._new_dir()
+        images_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.TFObjectDetectionDataset,
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir,
+            dataset_type=fo.types.TFObjectDetectionDataset,
+            images_dir=images_dir,
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+
+        # Direct records path w/ sharding
+
+        tf_records_path = os.path.join(self._new_dir(), "tf.records")
+        tf_records_patt = tf_records_path + "-*-of-*"
+        images_dir = self._new_dir()
+
+        dataset.export(
+            dataset_type=fo.types.TFObjectDetectionDataset,
+            tf_records_path=tf_records_path,
+            num_shards=2,
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_type=fo.types.TFObjectDetectionDataset,
+            tf_records_path=tf_records_patt,
+            images_dir=images_dir,
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+
+    @drop_datasets
+    def test_coco_detection_dataset(self):
+        dataset = self._make_dataset()
+
+        # Standard format
+
+        export_dir = self._new_dir()
+
+        dataset.export(
+            export_dir=export_dir,
+            dataset_type=fo.types.COCODetectionDataset,
+            label_field="predictions",
+            extra_attrs=["age", "cute", "mood"],
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_dir=export_dir,
+            dataset_type=fo.types.COCODetectionDataset,
+            label_field="predictions",
+            extra_attrs=["age", "cute", "mood"],
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+        self.assertEqual(
+            dataset.count("predictions.detections"),
+            dataset2.count("predictions.detections"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.confidence"),
+            dataset2.distinct("predictions.detections.confidence"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.age"),
+            dataset2.distinct("predictions.detections.age"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.cute"),
+            dataset2.distinct("predictions.detections.cute"),
+        )
+        self.assertEqual(
+            dataset.distinct("predictions.detections.mood"),
+            dataset2.distinct("predictions.detections.mood"),
+        )
+
+        # Labels-only
+
+        export_dir = self._new_dir()
+        data_path = os.path.dirname(dataset.first().filepath)
+        labels_path = os.path.join(export_dir, "labels.json")
+
+        dataset.export(
+            dataset_type=fo.types.COCODetectionDataset,
+            labels_path=labels_path,
+            label_field="predictions",
+        )
+
+        dataset2 = fo.Dataset.from_dir(
+            dataset_type=fo.types.COCODetectionDataset,
+            data_path=data_path,
+            labels_path=labels_path,
+            label_field="predictions",
+        )
+
+        self.assertEqual(len(dataset), len(dataset2))
+        self.assertEqual(
+            dataset.count("predictions.detections"),
+            dataset2.count("predictions.detections"),
+        )
+
+
+"""
+class VideoTests(unittest.TestCase):
+    def setUp(self):
+        temp_dir = etau.TempDir()
+        tmp_dir = temp_dir.__enter__()
+        video_path = os.path.join(tmp_dir, "video.mp4")
+
+        with etav.FFmpegVideoWriter(video_path, 5, (640, 480)) as writer:
+            for _ in range(5):
+                img = np.random.randint(
+                    255, size=(480, 640, 3), dtype=np.uint8
+                )
+                writer.write(img)
+
+        self._temp_dir = temp_dir
+        self.tmp_dir = tmp_dir
+        self.video_path = video_path
+
+    def tearDown(self):
+        self._temp_dir.__exit__()
+"""
+
+
+if __name__ == "__main__":
+    fo.config.show_progress_bars = False
+    unittest.main(verbosity=2)
