c608daacf029884d21819d6bc42f711179cf57d8
Benjamin Kane <ben@voxel51.com>
2021-09-27 13:35:47 -0600

    unwind list fields recursively

--- fiftyone/core/collections.py
+++ fiftyone/core/collections.py
@@ -7256,13 +7256,13 @@ def _parse_field_name(
     omit_terminal_lists,
     allow_missing,
 ):
-    unwind_list_fields = set()
-    other_list_fields = set()
+    unwind_list_fields = []
+    other_list_fields = []
 
     # Parse explicit array references
     chunks = field_name.split("[]")
     for idx in range(len(chunks) - 1):
-        unwind_list_fields.add("".join(chunks[: (idx + 1)]))
+        unwind_list_fields.append("".join(chunks[: (idx + 1)]))
 
     # Array references [] have been stripped
     field_name = "".join(chunks)
@@ -7281,7 +7281,7 @@ def _parse_field_name(
             return "frames", True, [], [], False
 
         prefix = sample_collection._FRAMES_PREFIX
-        unwind_list_fields = {f[len(prefix) :] for f in unwind_list_fields}
+        unwind_list_fields = [f[len(prefix) :] for f in unwind_list_fields]
 
     # Validate root field, if requested
     if not allow_missing and not is_id_field:
@@ -7320,25 +7320,28 @@ def _parse_field_name(
             if omit_terminal_lists and path == field_name:
                 break
 
-            if auto_unwind:
-                unwind_list_fields.add(path)
-            elif path not in unwind_list_fields:
-                other_list_fields.add(path)
+            if auto_unwind or path not in unwind_list_fields:
+                add_to = (
+                    unwind_list_fields if auto_unwind else other_list_fields
+                )
+                _unwind_list_recursively(field_type, add_to, path)
 
     if is_frame_field:
         if auto_unwind:
-            unwind_list_fields.discard("")
+            unwind_list_fields = list(
+                filter(lambda f: f != "", unwind_list_fields)
+            )
         else:
             prefix = sample_collection._FRAMES_PREFIX
             field_name = prefix + field_name
-            unwind_list_fields = {
+            unwind_list_fields = [
                 prefix + f if f else "frames" for f in unwind_list_fields
-            }
-            other_list_fields = {
+            ]
+            other_list_fields = [
                 prefix + f if f else "frames" for f in other_list_fields
-            }
+            ]
             if "frames" not in unwind_list_fields:
-                other_list_fields.add("frames")
+                other_list_fields.append("frames")
 
     # Sorting is important here because one must unwind field `x` before
     # embedded field `x.y`
@@ -7618,3 +7621,9 @@ def _handle_existing_dirs(
         elif os.path.isfile(_labels_path):
             if overwrite:
                 etau.delete_file(_labels_path)
+
+
+def _unwind_list_recursively(field_type, add_to, path):
+    if isinstance(field_type, fof.ListField):
+        add_to.append(path)
+        _unwind_list_recursively(field_type.field, add_to, path)
