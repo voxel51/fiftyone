28c4f863dbff41f3845a3da6ec5ec072f021845d
Joseph Robinson <robinson.jo@husky.neu.edu>
2021-11-11 01:36:32 -0500

    updated fiw.py, Fixed google links; removed pass

--- fiftyone/utils/fiw.py
+++ fiftyone/utils/fiw.py
@@ -10,10 +10,9 @@ import logging
 import os
 from pathlib import Path
 
-import pandas as pd
-
 import eta.core.utils as etau
 import eta.core.web as etaw
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +28,6 @@ def download_fiw_dataset(dataset_dir, scratch_dir=None, cleanup=True):
         cleanup (True): whether to cleanup the scratch directory after
             extraction
     """
-    pass
     if scratch_dir is None:
         scratch_dir = os.path.join(dataset_dir, "scratch")
 
@@ -47,12 +45,13 @@ def download_fiw_dataset(dataset_dir, scratch_dir=None, cleanup=True):
     if cleanup:
         logger.info(f"Cleaning up {scratch_dir}")
         etau.delete_dir(scratch_dir)
+    return scratch_dir
 
 
-_IMAGES_DOWNLOAD_LINK = "https://drive.google.com/file/d/1rkrDGOjS0e_pptzRHZl5bRGq0yy5xQxQ/view?usp=sharing"
-_MD5_DATA_DOWNLOAD_LINK = "https://drive.google.com/file/d/121lbbeaiY-qM2tK9sJXWNuvMczVuwi2p/view?usp=sharing"
-_LISTS_DOWNLOAD_LINK = "https://drive.google.com/file/d/1nt22yiCfdGF7aIguUb-SJmsM_1CvcYjg/view?usp=sharing"
-_MD5_LISTS_DOWNLOAD_LINK = "https://drive.google.com/file/d/1TfGLD61WTEVazzjDMg4413mUpTBQ5GJI/view?usp=sharing"
+_IMAGES_DOWNLOAD_LINK = "1rkrDGOjS0e_pptzRHZl5bRGq0yy5xQxQ"
+_MD5_DATA_DOWNLOAD_LINK = "121lbbeaiY-qM2tK9sJXWNuvMczVuwi2p"
+_LISTS_DOWNLOAD_LINK = "1nt22yiCfdGF7aIguUb-SJmsM_1CvcYjg"
+_MD5_LISTS_DOWNLOAD_LINK = "1nt22yiCfdGF7aIguUb-SJmsM_1CvcYjg"
 
 
 def _download_images(scratch_dir):
