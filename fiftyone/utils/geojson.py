29eff04118a4e4bb19bc7643c47503dd76b59056
brimoor <brimoor@umich.edu>
2021-12-28 14:22:37 -0600

    documenting new data_path option

--- fiftyone/utils/geojson.py
+++ fiftyone/utils/geojson.py
@@ -312,6 +312,7 @@ class GeoJSONDatasetImporter(
             -   an absolute filepath specifying the location of the JSON data
                 manifest. In this case, ``dataset_dir`` has no effect on the
                 location of the data
+            -   a dict mapping filenames to absolute filepaths
 
             If None, this parameter will default to whichever of ``data/`` or
             ``data.json`` exists in the dataset directory
