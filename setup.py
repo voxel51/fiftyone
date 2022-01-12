d49c7cc6be6d9a42dd9817b3f8dab6ed5026b8fb
Benjamin Kane <ben@voxel51.com>
2022-01-12 09:03:52 -0700

    choose install requires with opencv

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ Installs FiftyOne.
 |
 """
 import os
+from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import setup, find_packages
+import re
 from wheel.bdist_wheel import bdist_wheel
 
 
@@ -38,6 +40,75 @@ def get_version():
     return VERSION
 
 
+INSTALL_REQUIRES = [
+    # third-party packages
+    "aiofiles",
+    "argcomplete",
+    "boto3",
+    "Deprecated",
+    "eventlet",
+    "future",
+    "Jinja2",
+    "kaleido",
+    "matplotlib",
+    "mongoengine==0.20.0",
+    "motor>=2.3,<3",
+    "numpy",
+    "packaging",
+    "pandas",
+    "Pillow>=6.2",
+    "plotly>=4.14,<5",
+    "pprintpp",
+    "psutil",
+    "pymongo>=3.11,<4",
+    "pytz",
+    "PyYAML",
+    "retrying",
+    "scikit-learn",
+    "scikit-image",
+    "setuptools",
+    "tabulate",
+    "tornado>=5.1.1,<7",
+    "xmltodict",
+    "universal-analytics-python3>=1.0.1,<2",
+    # internal packages
+    "voxel51-eta>=0.6.1,<0.7",
+]
+
+
+CHOOSE_INSTALL_REQUIRES = [
+    (
+        (
+            "opencv-python",
+            "opencv-contrib-python",
+            "opencv-contrib-python-headless",
+        ),
+        "opencv-python-headless",
+    )
+]
+
+
+def choose_requirement(mains, secondary):
+    chosen = secondary
+    for main in mains:
+        try:
+            name = re.split(r"[!<>=]", main)[0]
+            get_distribution(name)
+            chosen = main
+            break
+        except DistributionNotFound:
+            pass
+
+    return str(chosen)
+
+
+def get_install_requirements(install_requires, choose_install_requires):
+    for mains, secondary in choose_install_requires:
+        install_requires.append(choose_requirement(mains, secondary))
+
+    return install_requires
+
+
 EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.19.1,<0.20"]}
 
 
@@ -65,41 +136,9 @@ setup(
         "fiftyone.tutorials": "docs/source/tutorials",
     },
     include_package_data=True,
-    install_requires=[
-        # third-party packages
-        "aiofiles",
-        "argcomplete",
-        "boto3",
-        "Deprecated",
-        "eventlet",
-        "future",
-        "Jinja2",
-        "kaleido",
-        "matplotlib",
-        "mongoengine==0.20.0",
-        "motor>=2.3,<3",
-        "numpy",
-        "opencv-python-headless",
-        "packaging",
-        "pandas",
-        "Pillow>=6.2",
-        "plotly>=4.14,<5",
-        "pprintpp",
-        "psutil",
-        "pymongo>=3.11,<4",
-        "pytz",
-        "PyYAML",
-        "retrying",
-        "scikit-learn",
-        "scikit-image",
-        "setuptools",
-        "tabulate",
-        "tornado>=5.1.1,<7",
-        "xmltodict",
-        "universal-analytics-python3>=1.0.1,<2",
-        # internal packages
-        "voxel51-eta>=0.6.1,<0.7",
-    ],
+    install_requires=get_install_requirements(
+        INSTALL_REQUIRES, CHOOSE_INSTALL_REQUIRES
+    ),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
