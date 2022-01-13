ea991ca9942f20ad4f71485ae3c154b17eef34da
Benjamin Kane <ben@voxel51.com>
2022-01-12 21:47:36 -0700

    include package data

--- setup.py
+++ setup.py
@@ -131,6 +131,7 @@ setup(
     install_requires=get_install_requirements(
         INSTALL_REQUIRES, CHOOSE_INSTALL_REQUIRES
     ),
+    include_package_data=True,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
