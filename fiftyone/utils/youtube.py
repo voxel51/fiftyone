2e653535f1db1ef241c7d6d0e44aa75ae8d256d2
Eric Hofesmann <ehofesmann@gmail.com>
2022-01-21 17:17:46 -0500

    update copyright

--- fiftyone/utils/youtube.py
+++ fiftyone/utils/youtube.py
@@ -2,7 +2,7 @@
 Utilities for working with
 `youtube-dl <https://github.com/ytdl-org/youtube-dl>`
 
-| Copyright 2017-2021, Voxel51, Inc.
+| Copyright 2017-2022, Voxel51, Inc.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
