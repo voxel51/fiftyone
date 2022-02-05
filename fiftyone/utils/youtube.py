bd4e6ea926d4f75ab759fdbd0c436d9014acf9eb
Eric Hofesmann <ehofesmann@gmail.com>
2022-02-05 13:42:02 -0500

    update patch

--- fiftyone/utils/youtube.py
+++ fiftyone/utils/youtube.py
@@ -510,7 +510,8 @@ def _patch_pytube_cypher():
         # a.C && (b = a.get("n")) && (b = Bpa[0](b), a.set("n", b),
         # Bpa.length || iha("")) }};
         # In the above case, `iha` is the relevant function name
-        r'a\.[A-Z]\s*&&\s*\(b\s*=\s*a\.get\("n"\)\)\s*&&\s*\(b\s*=\s*([a-zA-Z0-9$]{3})(\[\d+\])?\(b\)',
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]{3})(\[\d+\])?\([a-z]\)',
     ]
     logger.debug('Finding throttling function name')
     for pattern in function_patterns:
