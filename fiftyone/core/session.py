0c600daa01957e3cd9dec193eccebb1e568405e5
twsl <45483159+twsl@users.noreply.github.com>
2022-02-19 16:02:04 +0000

    Dont update base url

--- fiftyone/core/session.py
+++ fiftyone/core/session.py
@@ -982,9 +982,6 @@ class Session(foc.HasClient):
             return eval_js(
                 "google.colab.kernel.proxyPort(%d)" % self.server_port
             )
-        
-        if self._context == focx._DATABRICKS:
-            return _get_databricks_proxy_url(self.server_port)
 
         address = self.server_address or "localhost"
         return "http://%s:%d/" % (address, self.server_port)
