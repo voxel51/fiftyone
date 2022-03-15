fe4e594b1ce3dbae453994ea75d830dea6ef8f28
brimoor <brimoor@umich.edu>
2022-03-15 12:26:16 -0400

    clarification

--- fiftyone/core/aggregations.py
+++ fiftyone/core/aggregations.py
@@ -1704,12 +1704,16 @@ def _parse_field_and_expr(
     if field_name is None:
         field_name, expr = _extract_prefix_from_expr(expr)
 
-    root = True if not field_name else "." not in field_name
-    found_expr = expr is not None
+    if field_name is None:
+        root = True
+        field_type = None
+    else:
+        root = "." not in field_name
+        field_type = _get_field_type(
+            sample_collection, field_name, unwind=auto_unwind
+        )
 
-    field_type = _get_field_type(
-        sample_collection, field_name, unwind=auto_unwind
-    )
+    found_expr = expr is not None
 
     if safe:
         expr = _to_safe_expr(expr, field_type)
@@ -1952,9 +1956,6 @@ def _remove_prefix(expr, prefix):
 
 
 def _get_field_type(sample_collection, field_name, unwind=True):
-    if field_name is None:
-        return None
-
     # Remove array references
     field_name = "".join(field_name.split("[]"))
 
