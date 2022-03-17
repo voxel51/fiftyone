afc0ef9465bb614916ebc04a1014956490e0c3ed
brimoor <brimoor@umich.edu>
2022-03-16 22:29:12 -0400

    fixing _needs_frames()

--- fiftyone/core/aggregations.py
+++ fiftyone/core/aggregations.py
@@ -144,12 +144,16 @@ class Aggregation(object):
         Returns:
             True/False
         """
-        if self._field_name is not None:
-            return _is_frame_path(sample_collection, self._field_name)
+        if sample_collection.media_type != fom.VIDEO:
+            return False
 
-        if self._expr is not None:
-            field_name, _ = _extract_prefix_from_expr(self._expr)
-            return _is_frame_path(sample_collection, field_name)
+        if self._field_name is not None:
+            expr = F(self._field_name)
+        else:
+            expr = self._expr
+
+        if expr is not None:
+            return foe.is_frames_expr(expr)
 
         return False
 
@@ -1617,16 +1621,6 @@ class Values(Aggregation):
         )
 
         return pipeline
-
-
-def _is_frame_path(sample_collection, field_name):
-    if not field_name:
-        return False
-
-    # Remove array references
-    path = "".join(field_name.split("[]"))
-
-    return sample_collection._is_frame_field(path)
 
 
 def _transform_values(values, fcn, level=1):
