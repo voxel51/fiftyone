63750b54c99b9f5d6a8c54707e9cff2dc717dd5f
Benjamin Kane <ben@voxel51.com>
2022-04-12 14:15:46 -0600

    consistent node version

--- install.bash
+++ install.bash
@@ -97,8 +97,8 @@ if [ ${BUILD_APP} = true ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-    nvm install v17.3.1
-    nvm use v17.3.1
+    nvm install v16.4.2
+    nvm use v16.4.2
     npm -g install yarn
     if [ -f ~/.bashrc ]; then
         source ~/.bashrc
