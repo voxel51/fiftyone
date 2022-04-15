e2b89f0157267739dd66e6dab776b47baec22c73
Benjamin Kane <ben@voxel51.com>
2022-04-15 12:39:42 -0600

    skeleton work for keypoint overlays

--- app/packages/looker/src/overlays/keypoint.ts
+++ app/packages/looker/src/overlays/keypoint.ts
@@ -3,7 +3,7 @@
  */
 
 import { INFO_COLOR, TOLERANCE } from "../constants";
-import { BaseState, Coordinates } from "../state";
+import { BaseState, Coordinates, KeypointSkeleton } from "../state";
 import { distance } from "../util";
 import { CONTAINS, CoordinateOverlay, PointInfo, RegularLabel } from "./base";
 import { t } from "./util";
@@ -31,7 +31,8 @@ export default class KeypointOverlay<
     const selected = this.isSelected(state);
     ctx.lineWidth = 0;
 
-    for (const point of this.label.points) {
+    for (let i = 0; i < this.label.points.length; i++) {
+      const point = this.label.points[i];
       ctx.fillStyle = color;
       ctx.beginPath();
       const [x, y] = t(state, ...point);
@@ -49,6 +50,19 @@ export default class KeypointOverlay<
         ctx.beginPath();
         ctx.arc(x, y, state.pointRadius, 0, Math.PI * 2);
         ctx.fill();
+      }
+    }
+
+    const skeleton = getSkeleton(this.field, state);
+
+    if (!skeleton) return;
+
+    for (let i = 0; i < skeleton.edges.length; i++) {
+      const path = skeleton.edges[i].map((index) => this.label.points[index]);
+      this.strokePath(ctx, state, path, color, selected);
+
+      if (selected) {
+        this.strokePath(ctx, state, path, INFO_COLOR, state.dashLength);
       }
     }
   }
@@ -90,6 +104,25 @@ export default class KeypointOverlay<
 
     return distances.sort((a, b) => a[0] - b[0])[0];
   }
+
+  private strokePath(
+    ctx: CanvasRenderingContext2D,
+    state: Readonly<State>,
+    path: Coordinates[],
+    color: string,
+    dash?: number
+  ) {
+    ctx.beginPath();
+    ctx.lineWidth = state.strokeWidth;
+    ctx.strokeStyle = color;
+    ctx.setLineDash(dash ? [dash] : []);
+    ctx.moveTo(...t(state, path[0][0], path[0][1]));
+    for (const [x, y] of path.slice(1)) {
+      ctx.lineTo(...t(state, x, y));
+    }
+
+    ctx.stroke();
+  }
 }
 
 export const getKeypointPoints = (labels: KeypointLabel[]): Coordinates[] => {
@@ -98,4 +131,15 @@ export const getKeypointPoints = (labels: KeypointLabel[]): Coordinates[] => {
     points = [...points, ...label.points];
   });
   return points;
+};
+
+const getSkeleton = (
+  name: string,
+  state: BaseState
+): KeypointSkeleton | null => {
+  const defaultSkeleton = state.options.defaultSkeleton;
+
+  const namedSkeleton = state.options.skeletons[name];
+
+  return namedSkeleton || defaultSkeleton || null;
 };
