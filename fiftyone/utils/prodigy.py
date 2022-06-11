a74f7df5ed42875ff4d656ff7a509bc79d76b85e
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-06-11 12:12:02 +0200

    add some reminder descriptions of what goes in each class

--- fiftyone/utils/prodigy.py
+++ fiftyone/utils/prodigy.py
@@ -44,18 +44,33 @@ logger = logging.getLogger(__name__)
 
 
 class ProdigyBackend(foua.AnnotationBackend):
-    """Backend for the Prodigy integration."""
+    """Backend for the Prodigy integration.
+
+    This class implements the logic required for your annotation backend to
+    declare the types of labeling tasks that it supports, as well as the core
+    upload_annotations() and download_annotations() methods, which handle
+    uploading and downloading data and labels to your annotation tool.
+    """
 
     pass
 
 
 class ProdigyBackendConfig(foua.AnnotationAPI):
-    """Configuration for the Prodigy backend."""
+    """Configuration for the Prodigy backend.
+
+    This class defines the available parameters that users can pass as keyword
+    arguments to annotate() to customize the behavior of the annotation run.
+    """
 
     pass
 
 
 class ProdigyAnnotationResults(foua.AnnotationResults):
-    """Results from the Prodigy backend."""
+    """Results from the Prodigy backend.
+
+    This class stores any intermediate information necessary to track the
+    progress of an annotation run that has been created and is now waiting for
+    its results to be merged back into the FiftyOne dataset.
+    """
 
     pass
