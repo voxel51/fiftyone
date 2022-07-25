3dae35fce93db140f0aefea21328881b608c3ead
ritch <ritchie@voxel51.com>
2022-07-25 08:06:25 -0700

    move point cloud plugin

---
+++ app/packages/point-clouds/src/PointCloudPlugin.tsx
@@ -0,0 +1,19 @@
+import { PointCloud, getFilepathField } from "./PointCloud";
+import {
+  registerComponent,
+  PluginComponentType,
+  usePluginSettings,
+} from "@fiftyone/plugins";
+
+registerComponent({
+  name: "PointCloud",
+  component: PointCloud,
+  type: PluginComponentType.Visualizer,
+  activator: ({ sample, pinned, isGroupMainView }) => {
+    if (isGroupMainView) return false;
+    const settings = usePluginSettings("point-clouds");
+    const field = getFilepathField(sample, settings.filepathFields);
+    console.log({ field });
+    return field !== null;
+  },
+});
