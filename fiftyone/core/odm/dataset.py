dd8410f4a719023eac8620e9e2ac94c3c8851f99
brimoor <brimoor@umich.edu>
2022-08-23 17:55:46 -0400

    avoid deserializing extra fields

--- fiftyone/core/odm/dataset.py
+++ fiftyone/core/odm/dataset.py
@@ -362,7 +362,8 @@ class KeypointSkeleton(EmbeddedDocument):
 class DatasetDocument(Document):
     """Backing document for datasets."""
 
-    meta = {"collection": "datasets"}
+    # strict=False lets this class ignore unknown fields from other versions
+    meta = {"collection": "datasets", "strict": False}
 
     name = StringField(unique=True, required=True)
     version = StringField(required=True, null=True)
