ce76eff6557467d97b83f1b3a5f42e197c0819f3
Benjamin Kane <ben@voxel51.com>
2022-08-31 20:31:08 -0400

    tweaks

--- app/packages/state/src/recoil/aggregations.ts
+++ app/packages/state/src/recoil/aggregations.ts
@@ -384,7 +384,10 @@ export const count = selectorFamily<
         }
 
         if (split.length < 2) {
-          throw new Error(`invalid path ${path}`);
+          // this will never resolve, which allows for incoming schema changes
+          // this shouldn't be necessary, but there is a mismatch between
+          // aggs and schema when there is a field change
+          return new Promise(() => {});
         }
 
         const parent = split.slice(0, split.length - 1).join(".");
