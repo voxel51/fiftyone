53a1c15bc434472790cc70a1f830dafec9ffc235
brimoor <brimoor@umich.edu>
2022-09-09 02:00:10 -0400

    updating OSS compat version

--- fiftyone/constants.py
+++ fiftyone/constants.py
@@ -48,7 +48,7 @@ COMPATIBLE_VERSIONS = ">=0.16.6,<0.18"
 # Package metadata
 _META = metadata("fiftyone")
 NAME = _META["name"]
-VERSION = "0.16.6"  # open source compatibility version
+VERSION = "0.17.0"  # open source compatibility version
 TEAMS_VERSION = _META["version"]
 DESCRIPTION = _META["summary"]
 AUTHOR = _META["author"]
