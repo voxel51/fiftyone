3e467c67118862fb36e82f0add35f02b69882675
Benjamin Kane <ben@voxel51.com>
2022-09-09 16:13:15 -0400

    merge app

--- app/packages/app/src/components/Actions/ActionsRow.tsx
+++ app/packages/app/src/components/Actions/ActionsRow.tsx
@@ -6,6 +6,7 @@ import React, {
 } from "react";
 import { CircularProgress } from "@material-ui/core";
 import {
+  ArrowDownward,
   Bookmark,
   Check,
   FlipToBack,
--- app/packages/app/src/components/Actions/Export.tsx
+++ app/packages/app/src/components/Actions/Export.tsx
@@ -1,11 +1,7 @@
+import * as fos from "@fiftyone/state";
 import { formatDateTime, getFetchFunction } from "@fiftyone/utilities";
 import React, { useState } from "react";
 import { useRecoilCallback } from "recoil";
-
-import * as atoms from "../../recoil/atoms";
-import * as filterAtoms from "../../recoil/filters";
-import * as selectors from "../../recoil/selectors";
-import * as viewAtoms from "../../recoil/view";
 
 import Checkbox from "../Common/Checkbox";
 import { Button, PopoutSectionTitle } from "../utils";
@@ -14,36 +10,37 @@ import Popout from "./Popout";
 
 const useDownload = () => {
   return useRecoilCallback(
-    ({ snapshot }) => async (tags: boolean) => {
-      const dataset = await snapshot.getPromise(selectors.datasetName);
-      const timeZone = await snapshot.getPromise(selectors.timeZone);
-      const sample_ids = await snapshot.getPromise(atoms.selectedSamples);
-      const blob = await getFetchFunction()(
-        "POST",
-        "/export",
-        {
-          filters: await snapshot.getPromise(filterAtoms.filters),
-          view: await snapshot.getPromise(viewAtoms.view),
-          dataset,
-          sample_ids: [...sample_ids],
-          tags,
-        },
-        "blob"
-      );
-      const url = window.URL.createObjectURL(new Blob([blob as BlobPart]));
-      const link = document.createElement("a");
-      link.style.display = "none";
-      link.href = url;
-      link.setAttribute(
-        "download",
-        `${dataset}-${formatDateTime(Date.now(), timeZone)
-          .replaceAll(":", "-")
-          .replaceAll(", ", "-")}.csv`
-      );
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode && link.parentNode.removeChild(link);
-    },
+    ({ snapshot }) =>
+      async (tags: boolean) => {
+        const dataset = await snapshot.getPromise(fos.datasetName);
+        const timeZone = await snapshot.getPromise(fos.timeZone);
+        const sample_ids = await snapshot.getPromise(fos.selectedSamples);
+        const blob = await getFetchFunction()(
+          "POST",
+          "/export",
+          {
+            filters: await snapshot.getPromise(fos.filters),
+            view: await snapshot.getPromise(fos.view),
+            dataset,
+            sample_ids: [...sample_ids],
+            tags,
+          },
+          "blob"
+        );
+        const url = window.URL.createObjectURL(new Blob([blob as BlobPart]));
+        const link = document.createElement("a");
+        link.style.display = "none";
+        link.href = url;
+        link.setAttribute(
+          "download",
+          `${dataset}-${formatDateTime(Date.now(), timeZone)
+            .replaceAll(":", "-")
+            .replaceAll(", ", "-")}.csv`
+        );
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode && link.parentNode.removeChild(link);
+      },
     []
   );
 };
