20e6821470d30145a8087a1365840f0ae8b0eecb
imanjra <ibrahim@voxel51.com>
2022-10-12 11:31:00 -0400

    cleanup theme provider and tweak dark theme colors

--- app/packages/components/src/components/ThemeProvider/index.tsx
+++ app/packages/components/src/components/ThemeProvider/index.tsx
@@ -7,55 +7,75 @@ import {
 } from "@mui/material/styles";
 import { useRecoilValue } from "recoil";
 import { ThemeContext as LegacyTheme } from "styled-components";
+import hsl from "hsl-to-hex";
 
-const white100 = "hsl(0, 0%, 100%)";
-const grey11 = "hsl(210, 11%, 11%)";
-const grey15 = "hsl(210, 11%, 15%)";
-const grey19 = "hsl(214, 7%, 19%)";
-const grey24 = "hsl(210, 5%, 24%)";
-const grey37 = "hsl(200, 2%, 37%)";
-const grey60 = "hsl(230, 3%, 60%)";
-const grey68 = "hsl(220, 2%, 68%)";
+const colors = {
+  gray0: hsl(200, 0, 0),
+  gray5: hsl(200, 0, 5),
+  gray10: hsl(200, 0, 10),
+  gray12: hsl(200, 0, 12),
+  gray15: hsl(200, 0, 15),
+  gray20: hsl(200, 0, 20),
+  gray30: hsl(200, 0, 30),
+  gray40: hsl(200, 0, 40),
+  gray50: hsl(200, 0, 50),
+  gray60: hsl(200, 0, 60),
+  gray70: hsl(200, 0, 70),
+  gray80: hsl(200, 0, 80),
+  gray85: hsl(200, 0, 85),
+  gray90: hsl(200, 0, 90),
+  gray95: hsl(200, 0, 95),
+  gray98: hsl(200, 0, 98),
+  gray99: hsl(200, 0, 99),
+  gray100: hsl(200, 0, 100),
+  orange51: hsl(25, 100, 51),
+  orange45: hsl(25, 100, 45),
+  red0: hsl(0, 75, 20),
+  red53: hsl(0, 87, 53),
+  blue53: hsl(213, 100, 53),
+};
 
-const orange49 = "hsl(27, 95%, 49%)";
-const orange45 = "hsl(27, 95%, 45%)";
-const orange49a0 = "hsla(27, 95%, 49%, 0)";
-const orange49a10 = "hsla(27, 95%, 49%, 0.1)";
-const orange49a40 = "hsla(27, 95%, 49%, 0.4)";
-
-const blue53 = "hsl(213, 100%, 53%)";
-const blue92 = "hsl(211, 85%, 92%)";
-
-const red = "hsl(0, 87%, 53%)";
-
-export const darkTheme = {
-  background: grey19,
-  backgroundDark: grey15,
-  backgroundDarker: grey11,
-  backgroundLight: grey24,
-  backgroundLightBorder: grey15,
-  backgroundDarkBorder: grey11,
-  backgroundTransparent: "hsl(200, 0%, 15%, 0.7)",
-  border: grey37,
-  borderLight: grey24,
-
-  button: grey37,
-  buttonBorder: grey24,
-
-  brand: orange49,
-  brandDark: orange45,
-  brandTransparent: orange49a40,
-  brandMoreTransparent: orange49a10,
-  brandFullyTransparent: orange49a0,
-
-  font: white100,
-  fontDark: grey68,
-  fontDarkest: grey60,
-
-  secondary: blue53,
-  secondaryLight: blue92,
-
-  error: red,
+const themes = {
+  dark: {
+    primaryBg: colors.gray15,
+    primaryHoverBg: colors.gray12,
+    secondaryBg: colors.gray10,
+    primaryText: colors.gray100,
+    secondaryText: colors.gray70,
+    tertiaryText: colors.gray50,
+    primaryButtonBg: colors.orange45,
+    primaryButtonHoverBg: colors.orange51,
+    linkText: colors.gray100,
+    linkHoverText: colors.gray90,
+    itemBg: colors.gray20,
+    itemText: colors.gray20,
+    dividerPrimary: colors.gray20,
+    dividerSecondary: colors.gray50,
+    shadow: colors.gray5,
+    inputBg: colors.gray5,
+    inputBorder: colors.gray5,
+    inputBorderHover: colors.gray5,
+  },
+  light: {
+    primaryBg: colors.gray100,
+    primaryHoverBg: colors.gray95,
+    secondaryBg: colors.gray98,
+    primaryText: colors.gray0,
+    secondaryText: colors.gray40,
+    tertiaryText: colors.gray60,
+    primaryButtonBg: colors.orange45,
+    primaryButtonHoverBg: colors.orange51,
+    linkText: colors.gray0,
+    linkHoverText: colors.gray10,
+    itemBg: colors.gray90,
+    itemText: colors.gray80,
+    dividerPrimary: colors.gray90,
+    dividerSecondary: colors.gray50,
+    shadow: colors.gray85,
+    inputBg: colors.gray100,
+    inputBorder: colors.gray90,
+    inputBorderHover: colors.gray80,
+  },
 };
 
 const theme = extendJoyTheme({
@@ -71,7 +91,7 @@ const theme = extendJoyTheme({
         neutral: {
           softBg: "rgba(27, 31, 36, 0.15)",
           softBorder: "#F3F4F6",
-          plainColor: blue53,
+          plainColor: colors.blue53,
         },
         primary: {
           plainColor: "rgba(27, 31, 36, 0.15)",
@@ -82,7 +102,7 @@ const theme = extendJoyTheme({
         focusVisible: "rgba(3, 102, 214, 0.3)",
         divider: "#EAECF0",
         danger: {
-          plainColor: red,
+          plainColor: colors.red53,
         },
         text: {
           primary: "#000",
@@ -93,37 +113,57 @@ const theme = extendJoyTheme({
     },
     dark: createTheme({
       palette: {
-        background: {
-          body: "hsl(200, 0%, 15%)",
-          level1: "hsl(200, 0%, 20%)",
-          level2: "hsl(200, 0%, 10%)",
-          level3: "hsl(200, 0%, 5%)",
-          tooltip: "hsl(200, 0%, 5%)",
+        divider: themes.dark.dividerPrimary,
+        primary: {
+          main: themes.dark.primaryText,
+          plainColor: "hsl(25, 100%, 51%)",
+          plainBorder: themes.dark.dividerPrimary,
+          softBg: "hsl(200, 0%, 15%, 0.7)",
+          softBorder: themes.dark.dividerPrimary,
         },
-        divider: "hsl(200, 0%, 20%)",
-        danger: {
-          plainColor: red,
+        background: {
+          primary: themes.dark.primaryBg,
+          primaryHover: themes.dark.primaryHoverBg,
+          secondary: themes.dark.secondaryBg,
+          paper: themes.dark.primaryBg,
+          item: themes.dark.itemBg,
+          input: themes.dark.inputBg,
+          body: colors.gray10,
+          level1: colors.gray15,
+          level2: colors.gray10,
+          level3: "hsl(200, 0%, 5%)",
+          tooltip: themes.dark.primaryBg,
+        },
+        secondary: {
+          main: themes.dark.secondaryText,
+        },
+        text: {
+          primary: themes.dark.primaryText,
+          secondary: themes.dark.secondaryText,
+          tertiary: themes.dark.tertiaryText,
         },
         grey: {
-          400: "#fff",
+          100: "#5c5f60",
         },
-        neutral: {
-          softBg: "hsl(200, 0%, 20%, 0.3)",
-          softBorder: "hsl(200, 0%, 25%)",
-          plainColor: blue53,
+        voxel: {
+          500: "#FF6D04",
+          600: "#D54B00", // Not in the design. Darker shade of 500 of is used
+          dangerAlertBg: "#412729", // Not in the design
+          dangerAlertBorder: "#764a48", // Not in the design
         },
-        primary: {
-          main: "hsl(25, 100%, 51%)",
-          plainColor: "hsl(25, 100%, 51%)",
-          plainBorder: "hsl(200, 0%, 5%)",
-          softBg: "hsl(200, 0%, 15%, 0.7)",
-          softBorder: "hsl(200, 0%, 20%)",
+        custom: {
+          shadow: themes.dark.shadow,
+          inputBorder: themes.dark.inputBorder,
+          inputBorderHover: themes.light.inputBorderHover,
+        },
+        danger: {
+          plainColor: colors.blue53,
         },
         focusVisible: "rgba(3, 102, 214, 0.3)",
-        text: {
-          primary: "hsl(200, 0%, 100%)",
-          secondary: "hsl(200, 0%, 70%)",
-          tertiary: "hsl(200, 0%, 50%)",
+        neutral: {
+          softBg: "hsl(200, 0%, 20%, 0.3)",
+          softBorder: themes.dark.dividerPrimary,
+          plainColor: colors.blue53,
         },
       },
     }),
