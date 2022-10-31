9b667da30a5488a92cb261aaf339ad3bbc424e4e
brimoor <brimoor@umich.edu>
2022-10-30 22:57:15 -0400

    directly working with IDs when tagging/untagging labels

--- fiftyone/core/patches.py
+++ fiftyone/core/patches.py
@@ -5,6 +5,7 @@ Patches views.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
+from collections import defaultdict
 from copy import deepcopy
 
 from bson import ObjectId
@@ -192,32 +193,52 @@ class _PatchesView(fov.DatasetView):
 
         return names
 
-    def tag_labels(self, tags, label_fields=None):
-        for field in self._parse_label_fields(label_fields=label_fields):
-            view = self._get_source_labels_view(field)
-            view.tag_labels(tags, label_fields=[field])
+    def _tag_labels(self, tags, label_field, ids=None, label_ids=None):
+        if label_field in self._label_fields:
+            _ids = self.values("_" + self._id_field)
 
-        super().tag_labels(tags, label_fields=label_fields)
+        _, label_ids = super()._tag_labels(
+            tags, label_field, ids=ids, label_ids=label_ids
+        )
 
-    def untag_labels(self, tags, label_fields=None):
-        for field in self._parse_label_fields(label_fields=label_fields):
-            view = self._get_source_labels_view(field)
-            view.untag_labels(tags, label_fields=[field])
+        if label_field in self._label_fields:
+            ids, label_ids = self._to_source_ids(label_field, _ids, label_ids)
+            self._source_collection._tag_labels(
+                tags, label_field, ids=ids, label_ids=label_ids
+            )
 
-        super().untag_labels(tags, label_fields=label_fields)
+    def _untag_labels(self, tags, label_field, ids=None, label_ids=None):
+        if label_field in self._label_fields:
+            _ids = self.values("_" + self._id_field)
 
-    def _parse_label_fields(self, label_fields=None):
-        if label_fields is None:
-            label_fields = self._label_fields
-        elif etau.is_str(label_fields):
-            label_fields = [label_fields]
+        _, label_ids = super()._untag_labels(
+            tags, label_field, ids=ids, label_ids=label_ids
+        )
 
-        return list(set(label_fields) & set(self._label_fields))
+        if label_field in self._label_fields:
+            ids, label_ids = self._to_source_ids(label_field, _ids, label_ids)
+            self._source_collection._untag_labels(
+                tags, label_field, ids=ids, label_ids=label_ids
+            )
 
-    def _get_source_labels_view(self, field):
-        _, id_path = self._get_label_field_path(field, "id")
-        ids = self.values(id_path, unwind=True)
-        return self._source_collection.select_labels(ids=ids, fields=[field])
+    def _to_source_ids(self, label_field, ids, label_ids):
+        label_type = self._source_collection._get_label_field_type(label_field)
+        is_list_field = issubclass(label_type, fol._LABEL_LIST_FIELDS)
+
+        if not is_list_field:
+            return ids, label_ids
+
+        id_map = defaultdict(list)
+        for _id, _label_id in zip(ids, label_ids):
+            if etau.is_container(_label_id):
+                id_map[_id].extend(_label_id)
+            else:
+                id_map[_id].append(_label_id)
+
+        if not id_map:
+            return [], []
+
+        return zip(*id_map.items())
 
     def set_values(self, field_name, *args, **kwargs):
         field = field_name.split(".", 1)[0]
