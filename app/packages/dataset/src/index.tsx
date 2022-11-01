80ed3ceac122aaf870ac400bc618251e3c51b6ab
Ritchie Martori <ritchie@voxel51.com>
2022-11-01 08:51:25 -0700

    export fos

--- app/packages/dataset/src/index.tsx
+++ app/packages/dataset/src/index.tsx
@@ -1,8 +1,8 @@
 import React, { useRef } from "react";
 import { createRoot } from "react-dom/client";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilState } from "recoil";
 import { RecoilRelayEnvironmentProvider } from "recoil-relay";
-import { Dataset, getEnvProps } from "./";
+import { Dataset, getEnvProps, fos } from "./";
 
 // import "./index.css";
 
@@ -25,14 +25,39 @@ const DatasetWrapper = () => {
   );
 };
 
+const EXAMPLE_VIEW = [
+  {
+    _cls: "fiftyone.core.stages.Limit",
+    kwargs: [["limit", 10]],
+    _uuid: "020b33dd-775a-4b4a-a865-4901e2e6ee43",
+  },
+];
+
 function LoadableDataset() {
   const [settings, setSettings] = React.useState({
     dataset: "quickstart",
     readOnly: false,
   });
+  const [view, setView] = useRecoilState(fos.view);
+
+  function printView() {
+    console.log(JSON.stringify(view, null, 2));
+  }
+
+  function changeView() {
+    setView(EXAMPLE_VIEW);
+  }
+
+  function clearView() {
+    setView([]);
+  }
+
   return (
     <>
       <DatasetSettings current={settings} onChange={setSettings} />
+      <button onClick={() => printView()}>Print View</button>
+      <button onClick={() => changeView()}>Set View</button>
+      <button onClick={() => clearView()}>Clear View</button>
       <div style={{ height: "100vh", overflow: "hidden" }}>
         <Dataset datasetName={settings.dataset} readOnly={settings.readOnly} />
       </div>
