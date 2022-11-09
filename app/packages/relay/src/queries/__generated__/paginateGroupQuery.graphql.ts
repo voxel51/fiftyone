b360e1a8fcef62b1db1b2ae5b2ace687138ab7ab
brimoor <brimoor@umich.edu>
2022-11-08 19:36:52 -0500

    commiting generated files

--- app/packages/relay/src/queries/__generated__/paginateGroupQuery.graphql.ts
+++ app/packages/relay/src/queries/__generated__/paginateGroupQuery.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<982f8d1f19b73ac2d5d3d82b5019176f>>
+ * @generated SignedSource<<1df96bc7e4d60864c3a5f49fc263c010>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -230,6 +230,21 @@ return {
         "name": "paginateGroupPinnedSample_query",
         "selections": [
           (v13/*: any*/)
+        ],
+        "args": null,
+        "argumentDefinitions": [
+          {
+            "kind": "RootArgument",
+            "name": "dataset"
+          },
+          {
+            "kind": "RootArgument",
+            "name": "pinnedSampleFilter"
+          },
+          {
+            "kind": "RootArgument",
+            "name": "view"
+          }
         ]
       }
     ],
