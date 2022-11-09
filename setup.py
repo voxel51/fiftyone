a7e3f5ac610b063fd7ae6784f0af963faf24bdb8
Benjamin Kane <ben@voxel51.com>
2022-11-09 11:49:34 -0600

    bumps

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.14.2,<0.15"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.15.0,<0.16"]}
 
 
 with open("README.md", "r") as fh:
