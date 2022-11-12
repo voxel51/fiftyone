922d45c87fafb06e1639fc108bd1ee6427c6c554
mani askari <mani@voxel51.com>
2022-11-11 20:03:55 -0800

    change view on view selection click

--- app/packages/state/src/hooks/useSetView.ts
+++ app/packages/state/src/hooks/useSetView.ts
@@ -43,9 +43,13 @@ const useSetView = (
         viewOrUpdater:
           | State.Stage[]
           | ((current: State.Stage[]) => State.Stage[]),
-        addStages?: State.Stage[]
+        addStages?: State.Stage[],
+        viewName?: string
       ) => {
         const dataset = snapshot.getLoadable(fos.dataset).contents;
+        console.log("setting view", viewName, dataset);
+        const savedViews = dataset.savedViews || [];
+
         send((session) => {
           const value =
             viewOrUpdater instanceof Function
@@ -53,6 +57,7 @@ const useSetView = (
               : viewOrUpdater;
           commit({
             variables: {
+              viewName,
               subscription,
               session,
               view: value,
@@ -79,9 +84,11 @@ const useSetView = (
               router.history.location.state.state = {
                 ...router.history.location.state,
                 view: value,
+                viewName,
                 viewCls: dataset.viewCls,
                 selected: [],
                 selectedLabels: [],
+                savedViews,
               };
               updateState({
                 dataset: transformDataset(dataset),
@@ -89,8 +96,10 @@ const useSetView = (
                   view: value,
                   viewName: viewName,
                   viewCls: dataset.viewCls,
+                  viewName,
                   selected: [],
                   selectedLabels: [],
+                  savedViews,
                 },
               });
               onComplete && onComplete();
