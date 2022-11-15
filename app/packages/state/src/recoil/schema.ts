14e2267e449c7fcee10b66af7c667f0b0f80896a
Ritchie Martori <ritchie@voxel51.com>
2022-11-15 15:23:29 -0700

    add initial numeric support to embeddings plot

--- app/packages/state/src/recoil/schema.ts
+++ app/packages/state/src/recoil/schema.ts
@@ -155,7 +155,7 @@ export const fieldPaths = selectorFamily<
 >({
   key: "fieldPaths",
   get:
-    ({ path, space, ftype, embeddedDocType }) =>
+    ({ path, space, ftype, embeddedDocType } = {}) =>
     ({ get }) => {
       if (path && space) {
         throw new Error("path and space provided");
@@ -216,7 +216,7 @@ export const fields = selectorFamily<
 >({
   key: "fields",
   get:
-    (params) =>
+    (params = {}) =>
     ({ get }) => {
       if (!params.path && !params.space) {
         throw new Error("invalid parameters");
