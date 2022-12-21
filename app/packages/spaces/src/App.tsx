4f8ba8b271d0e7ec574e0bce283e9e11ba2a5564
imanjra <ibrahim@voxel51.com>
2022-12-21 11:12:06 -0500

    spaces test app enhancements

--- app/packages/spaces/src/App.tsx
+++ app/packages/spaces/src/App.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilState } from "recoil";
 import { SpacesRoot } from "./";
 import "./App.css";
+import { excludedPluginsAtom } from "./AppModules";
 
 function App() {
+  const [state, setState] = useRecoilState(excludedPluginsAtom);
   const defaultState = {
     id: "root",
     children: [
@@ -37,15 +39,55 @@ function App() {
     layout: "horizontal",
   };
 
+  function toggleExclude(name: string) {
+    const nextState = new Set(state);
+    if (nextState.has(name)) nextState.delete(name);
+    else nextState.add(name);
+    setState(nextState);
+  }
+
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <RecoilRoot>
+    <div>
+      <span>Exclude: </span>
+      <input
+        type="checkbox"
+        id="embedding"
+        name="embedding"
+        value="embedding"
+        onClick={() => toggleExclude("Embeddings")}
+      />
+      <label htmlFor="embedding">Embeddings</label>
+      <input
+        type="checkbox"
+        id="form"
+        name="form"
+        value="form"
+        onClick={() => toggleExclude("Form")}
+      />
+      <label htmlFor="form">Form</label>
+      <input
+        type="checkbox"
+        id="map"
+        name="map"
+        value="map"
+        onClick={() => toggleExclude("Map")}
+      />
+      <label htmlFor="map">Map</label>
+      <div style={{ height: "calc(100vh - 25px)", width: "100vw" }}>
         <SpacesRoot id="main" defaultState={defaultState} />
-      </RecoilRoot>
+      </div>
     </div>
   );
 }
 
+function RecoilApp() {
+  return (
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <RecoilApp />
 );
