45cba26251c908de121ba828cfd04b1013a7c5a6
Kacey Wang <kacey@voxel51.com>
2022-12-28 11:08:00 -0800

    fix viewBarMachine

--- app/packages/core/src/components/ViewBar/viewBarMachine.ts
+++ app/packages/core/src/components/ViewBar/viewBarMachine.ts
@@ -1,5 +1,5 @@
 import { v4 as uuid } from "uuid";
-import { actions, assign, createMachine, Machine, send, spawn } from "xstate";
+import { actions, assign, createMachine, send, spawn } from "xstate";
 
 import { viewsAreEqual } from "@fiftyone/state";
 import { getFetchFunction } from "@fiftyone/utilities";
