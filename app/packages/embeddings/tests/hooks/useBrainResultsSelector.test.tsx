2293584b57f8f35e9bbe7eb4366b93ef53327ba5
Ritchie Martori <ritchie@voxel51.com>
2023-01-06 08:07:01 -0700

    add initial embeddings tests

---
+++ app/packages/embeddings/tests/hooks/useBrainResultsSelector.test.tsx
@@ -0,0 +1,44 @@
+import * as fos from "@fiftyone/state";
+import { act, renderHook } from "@testing-library/react-hooks";
+import React, { useEffect } from "react";
+import { RecoilRoot, useRecoilValue } from "recoil";
+import { expect, test } from "vitest";
+import {
+  useBrainResult,
+  useBrainResultsSelector,
+} from "../../src/useBrainResult";
+
+function Initializer({ initialValue }) {
+  const [selected, setSelected] = useBrainResult();
+  useEffect(() => {
+    setSelected(initialValue);
+  }, []);
+  return null;
+}
+
+test("useBrainResultSelector", () => {
+  const { result } = renderHook(() => useBrainResultsSelector(), {
+    wrapper: ({ children }) => {
+      return (
+        <RecoilRoot
+          initializeState={(snapshot) => {
+            snapshot.set(fos.dataset, {
+              brainMethods: [{ key: "hello" }, { key: "world" }],
+            });
+          }}
+        >
+          <Initializer initialValue="test" />
+          {children}
+        </RecoilRoot>
+      );
+    },
+  });
+
+  act(() => {
+    result.current.handlers.onSelect("hello");
+  });
+
+  expect(result.current.brainKey).toBe("hello");
+  expect(result.current.canSelect).toBe(true);
+  expect(result.current.hasSelection).toBe(true);
+});
