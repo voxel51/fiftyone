f7db4d177ba61803aa4d0c54f414389b9d471517
brimoor <brimoor@umich.edu>
2023-01-10 16:59:33 -0500

    fixing imports

--- fiftyone/core/spaces.py
+++ fiftyone/core/spaces.py
@@ -1,8 +1,8 @@
-import fiftyone as fo
-
 from bson import ObjectId
-from fiftyone.core.odm import EmbeddedDocument
 from mongoengine.errors import ValidationError
+
+import fiftyone.core.fields as fof
+from fiftyone.core.odm import EmbeddedDocument
 
 
 def _validate_children(children):
@@ -18,7 +18,7 @@ class Component(EmbeddedDocument):
 
     meta = {"abstract": True, "allow_inheritance": True}
 
-    id = fo.ObjectIdField(
+    id = fof.ObjectIdField(
         default=lambda: str(ObjectId()),
         db_field="_id",
     )
@@ -28,26 +28,25 @@ class Panel(Component):
 
     meta = {"strict": False, "allow_inheritance": True}
 
-    type = fo.StringField()
-    pinned = fo.BooleanField(default=False)
+    type = fof.StringField()
+    pinned = fof.BooleanField(default=False)
 
 
 class Space(Component):
 
     meta = {"strict": False, "allow_inheritance": True}
 
-    children = fo.ListField(
-        fo.EmbeddedDocumentField(Component),
+    children = fof.ListField(
+        fof.EmbeddedDocumentField(Component),
         validation=_validate_children,
     )
-    orientation = fo.StringField(
+    orientation = fof.StringField(
         choices=["horizontal", "vertical"], default=None
     )
-    active_child = fo.StringField(default=None)
+    active_child = fof.StringField(default=None)
 
 
 default_spaces = Space(
-    children=[Panel(type="Samples", pinned=True, id="default-samples-node")],
+    children=[Panel(type="Samples", pinned=True)],
     active_child="default-samples-node",
-    id="root",
 )
