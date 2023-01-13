399c6d7c0c978865aecbce760955e1e18fd392cd
Benjamin Kane <ben@tapes.co>
2023-01-13 16:08:14 -0600

    comment

--- app/packages/relay/src/resolve.ts
+++ app/packages/relay/src/resolve.ts
@@ -1,3 +1,9 @@
+/**
+ * It NextJS environments, or perhaps some webpack environments generally,
+ * graphql calls are resolved as modules and the default must be accessed.
+ *
+ * @benjaminpkane not fully understood
+ */
 export default <T extends unknown>(module: T): T => {
   // @ts-ignore
   if (module.default) {
