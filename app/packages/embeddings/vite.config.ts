988140243f4d1714ea01c59cc186821cafa9b639
imanjra <ibrahim@voxel51.com>
2023-01-13 15:29:06 -0500

    testing build failure

--- app/packages/embeddings/vite.config.ts
+++ app/packages/embeddings/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { viteExternalsPlugin } from "vite-plugin-externals";
@@ -6,32 +6,45 @@ import { viteExternalsPlugin } from "vite-plugin-externals";
 const isPluginBuild = process.env.STANDALONE !== "true";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  mode: "development",
-  plugins: [
-    react(),
-    isPluginBuild
-      ? viteExternalsPlugin({
-          react: "React",
-          "react-dom": "ReactDOM",
-          recoil: "recoil",
-          "@fiftyone/state": "__fos__",
-        })
-      : undefined,
-  ],
-  build: {
-    minify: true,
-    lib: {
-      entry: path.resolve(__dirname, "./src/index.ts"),
-      name: "Map",
-      fileName: (format) => `index.${format}.js`,
-      formats: ["umd"],
+export default defineConfig(({ mode }) => {
+  const config: UserConfig = {
+    mode,
+    plugins: [react()],
+    build: {
+      minify: true,
+      lib: {
+        entry: path.resolve(__dirname, "./src/index.ts"),
+        name: "Embeddings",
+        fileName: (format) => `index.${format}.js`,
+        formats: ["umd"],
+      },
     },
-  },
-  define: {
-    "process.env.NODE_ENV": '"development"',
-  },
-  optimizeDeps: {
-    exclude: ["react", "react-dom"],
-  },
+    define: {
+      "process.env.NODE_ENV": '"development"',
+    },
+    optimizeDeps: {
+      exclude: ["react", "react-dom"],
+    },
+  };
+
+  if ((mode === "production" || isPluginBuild) && config.plugins) {
+    config.plugins.push(
+      viteExternalsPlugin({
+        react: "React",
+        "react-dom": "ReactDOM",
+        recoil: "recoil",
+        "@fiftyone/state": "__fos__",
+        "@fiftyone/components": "__foc__",
+        "@fiftyone/plugins": "__fop__",
+        "@fiftyone/spaces": "__fos__",
+        "@fiftyone/utilities": "__fou__",
+        "@mui/icons-material": "__muiim__",
+        "styled-components": "StyledComponents",
+        typescript: "Typescript",
+        "react-plotly.js": "react-plotly.js",
+      })
+    );
+  }
+
+  return config;
 });
