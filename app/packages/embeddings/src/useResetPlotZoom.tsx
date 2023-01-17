a002a16cfb39856ad9be38400f2231cbd294e56a
imanjra <ibrahim@voxel51.com>
2023-01-16 20:05:42 -0500

    better fix for embeddings zoom and pan reset bug

--- app/packages/embeddings/src/useResetPlotZoom.tsx
+++ app/packages/embeddings/src/useResetPlotZoom.tsx
@@ -1,10 +1,10 @@
 import { usePanelStatePartial } from "@fiftyone/spaces";
 
-export const useZoomRevision = () => usePanelStatePartial("zoomRevision", 1);
+export const useZoomRevision = () => usePanelStatePartial("zoomRevision", 0);
 export function useResetPlotZoom() {
   const [zoomRevision, setZoomRevision] = useZoomRevision();
   const reset = () => {
-    setZoomRevision((rev) => (rev ? rev + 1 : 1));
+    setZoomRevision((rev) => (typeof rev === "number" ? rev + 1 : 1));
   };
 
   return reset;
