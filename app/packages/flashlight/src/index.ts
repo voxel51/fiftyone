4a23f2c7fe2172c1090609ffb853cffc08e4e3d9
brimoor <brimoor@umich.edu>
2023-01-19 15:40:14 -0500

    updating copyrights

--- app/packages/flashlight/src/index.ts
+++ app/packages/flashlight/src/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-2022, Voxel51, Inc.
+ * Copyright 2017-2023, Voxel51, Inc.
  */
 import { MARGIN, NUM_ROWS_PER_SECTION } from "./constants";
 import SectionElement from "./section";
--- fiftyone/zoo/__init__.py
+++ fiftyone/zoo/__init__.py
@@ -1,7 +1,7 @@
 """
 The FiftyOne Zoo.
 
-Copyright 2017-2022, Voxel51, Inc.
+Copyright 2017-2023, Voxel51, Inc.
 voxel51.com
 """
 import types
