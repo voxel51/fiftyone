06296276d5b318a9a36d80a3fa6e03908df053bf
brimoor <brimoor@umich.edu>
2023-01-20 18:12:17 -0500

    some args are optional

--- fiftyone/internal/__init__.py
+++ fiftyone/internal/__init__.py
@@ -1,5 +1,5 @@
 """
-File storage utilities.
+Internal utilities.
 
 | Copyright 2017-2023, Voxel51, Inc.
 | `voxel51.com <https://voxel51.com/>`_
--- fiftyone/internal/credentials.py
+++ fiftyone/internal/credentials.py
@@ -1,7 +1,7 @@
 """
 Internal cloud credential utilities.
 
-| Copyright 2017-2022, Voxel51, Inc.
+| Copyright 2017-2023, Voxel51, Inc.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
@@ -102,20 +102,19 @@ def _parse_credentials(creds_list, creds_dir, fernet, provider):
     if not creds:
         return None
 
-    file_name = ""
     if provider == "AWS":
-        file_name = "aws_creds.ini"
-
-    if provider == "GCP":
-        file_name = "gcp_creds.json"
-
-    if provider == "MINIO":
-        file_name = "minio_creds.ini"
+        filename = "aws_creds.ini"
+    elif provider == "GCP":
+        filename = "gcp_creds.json"
+    elif provider == "MINIO":
+        filename = "minio_creds.ini"
+    else:
+        filename = "creds.ini"
 
     raw_creds = fernet.decrypt(creds["credentials"]).decode()
 
     if "ini-file" in raw_creds:
-        creds_path = os.path.join(creds_dir, file_name)
+        creds_path = os.path.join(creds_dir, filename)
         creds_str = json.loads(raw_creds)["ini-file"]
         with open(creds_path, "w") as f:
             f.write(creds_str)
@@ -124,20 +123,20 @@ def _parse_credentials(creds_list, creds_dir, fernet, provider):
 
     if provider == "AWS":
         raw_creds_dict = json.loads(raw_creds)
-        creds_path = os.path.join(creds_dir, file_name)
+        creds_path = os.path.join(creds_dir, filename)
         _write_default_aws_ini_file(raw_creds_dict, creds_path)
         return creds_path
 
     if provider == "MINIO":
         raw_creds_dict = json.loads(raw_creds)
-        creds_path = os.path.join(creds_dir, file_name)
+        creds_path = os.path.join(creds_dir, filename)
         _write_default_minio_ini_file(raw_creds_dict, creds_path)
         return creds_path
 
     if "service-account-file" in raw_creds:
         raw_creds_json = json.loads(raw_creds)
         creds_dict = json.loads(raw_creds_json["service-account-file"])
-        creds_path = os.path.join(creds_dir, file_name)
+        creds_path = os.path.join(creds_dir, filename)
         with open(creds_path, "w") as f:
             json.dump(creds_dict, f)
 
@@ -151,8 +150,11 @@ def _write_default_aws_ini_file(creds_dict, creds_path):
     config["default"] = {
         "aws_access_key_id": creds_dict["access-key-id"],
         "aws_secret_access_key": creds_dict["secret-access-key"],
-        "region": creds_dict["default-region"],
     }
+
+    region = creds_dict.get("default-region", None)
+    if region is not None:
+        config["default"]["region"] = region
 
     with open(creds_path, "w") as f:
         config.write(f)
@@ -164,9 +166,15 @@ def _write_default_minio_ini_file(creds_dict, creds_path):
         "access_key": creds_dict["access-key-id"],
         "secret_access_key": creds_dict["secret-access-key"],
         "endpoint_url": creds_dict["endpoint-url"],
-        "alias": creds_dict["alias"],
-        "region": creds_dict["default-region"],
     }
 
-    with open(creds_path, "w+") as f:
+    alias = creds_dict.get("alias", None)
+    if alias is not None:
+        config["default"]["alias"] = alias
+
+    region = creds_dict.get("default-region", None)
+    if region is not None:
+        config["default"]["region"] = region
+
+    with open(creds_path, "w") as f:
         config.write(f)
