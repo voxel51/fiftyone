969b326a9e201c205f46ccd96791f811191f9cfd
Lanny W <lanzhenwang9@gmail.com>
2023-02-03 11:44:43 -0600

    update frontend to filter keypoints by dynamic properties

--- app/packages/state/src/recoil/skeletonFilter.ts
+++ app/packages/state/src/recoil/skeletonFilter.ts
@@ -2,7 +2,7 @@ import { Point } from "@fiftyone/looker";
 import { selectorFamily } from "recoil";
 import { filters, modalFilters } from "./filters";
 import { NumericFilter } from "./pathFilters/numeric";
-import { StringFilter } from "./pathFilters/string";
+import { string, StringFilter } from "./pathFilters/string";
 import { expandPath } from "./schema";
 
 export default selectorFamily<(path: string, value: Point) => boolean, boolean>(
@@ -14,56 +14,92 @@ export default selectorFamily<(path: string, value: Point) => boolean, boolean>(
         const f = get(modal ? modalFilters : filters);
         return getCallback(({ snapshot }) => (path: string, value: Point) => {
           path = snapshot.getLoadable(expandPath(path)).contents;
-          const labels = f[`${path}.points`] as StringFilter;
+          let result: boolean = true;
 
+          const stringListFilters: string[] = [];
+          const numberListFilters: string[] = [];
+
+          Object.entries(value).forEach(([k, v]) => {
+            if (typeof v === "string" && !["label"].includes(k)) {
+              stringListFilters.push(k);
+            }
+            if (typeof v === "number") {
+              numberListFilters.push(k);
+            }
+          });
+
+          // skeleton points is a special case:
+          const labels = f[`${path}.points`] as StringFilter;
           if (labels && labels.values.length && value.label) {
             const included = labels.values.includes(value.label);
             if (labels.exclude) {
               if (included) {
-                return false;
+                result = false;
               }
             } else if (!labels.exclude) {
               if (!included) {
-                return false;
+                result = false;
               }
             }
           }
 
-          const confidence = f[`${path}.confidence`] as NumericFilter;
-          if (confidence) {
-            if (typeof value.confidence !== "number") {
-              switch (value.confidence) {
-                case "inf":
-                  return confidence.inf
-                    ? !confidence.exclude
-                    : confidence.exclude;
-                case "ninf":
-                  return confidence.ninf
-                    ? !confidence.exclude
-                    : confidence.exclude;
-                case "nan":
-                  return confidence.nan
-                    ? !confidence.exclude
-                    : confidence.exclude;
-                case null:
-                  return confidence.none
-                    ? !confidence.exclude
-                    : confidence.exclude;
-                case undefined:
-                  return confidence.none
-                    ? !confidence.exclude
-                    : confidence.exclude;
+          stringListFilters.forEach((key) => {
+            const strFilter = f[`${path}.${key}`] as StringFilter;
+            if (strFilter && strFilter.values.length && value[key]) {
+              const included = strFilter.values.includes(value[key]);
+              if (strFilter.exclude) {
+                if (included) {
+                  result = false;
+                }
+              } else if (!strFilter.exclude) {
+                if (!included) {
+                  result = false;
+                }
               }
             }
+          });
 
-            const includes =
-              value.confidence >= confidence.range[0] &&
-              value.confidence <= confidence.range[1];
+          numberListFilters.forEach((key) => {
+            const numFilter = f[`${path}.${key}`] as NumericFilter;
+            if (numFilter) {
+              if (typeof value[key] !== "number") {
+                switch (value[key]) {
+                  case "inf":
+                    return numFilter.inf
+                      ? !numFilter.exclude
+                      : numFilter.exclude;
+                  case "ninf":
+                    return numFilter.ninf
+                      ? !numFilter.exclude
+                      : numFilter.exclude;
+                  case "nan":
+                    return numFilter.nan
+                      ? !numFilter.exclude
+                      : numFilter.exclude;
+                  case null:
+                    return numFilter.none
+                      ? !numFilter.exclude
+                      : numFilter.exclude;
+                  case undefined:
+                    return numFilter.none
+                      ? !numFilter.exclude
+                      : numFilter.exclude;
+                }
+              }
 
-            return includes ? !confidence.exclude : confidence.exclude;
-          }
+              const includes =
+                value[key] >= numFilter.range[0] &&
+                value[key] <= numFilter.range[1];
+              const r = numFilter.exclude ? !includes : includes;
 
-          return true;
+              if (!r) {
+                console.log("5");
+                result = false;
+              }
+            }
+          });
+          console.info("result", result);
+          return result;
         });
       },
   }
