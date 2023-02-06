26961760335882c185deaa3fba06d60af6e651cd
Ritchie Martori <ritchie@voxel51.com>
2023-02-06 16:01:49 -0700

    initial python operators

---
+++ fiftyone/operators/executor.py
@@ -0,0 +1,49 @@
+from .registry import list_operators, operator_exists, load_operator
+
+
+def execute_operator(operator_name, request_params):
+    """Executes the operator with the given name.
+    Args:
+        operator_name: the name of the operator
+        params: a dictionary of parameters for the operator
+    Returns:
+        the result of the operator
+    """
+    if operator_exists(operator_name) is False:
+        raise ValueError("Operator '%s' does not exist" % operator_name)
+
+    operator = load_operator(operator_name)
+    ctx = ExecutionContext(request_params)
+    raw_result = operator.execute(ctx)
+    return ExecutionResult(ctx, raw_result)
+
+
+class ExecutionContext:
+    def __init__(self, execution_request_params):
+        self.request_params = execution_request_params
+        self.params = execution_request_params.get("params", {})
+
+    @property
+    def view(self):
+        stages = self.request_params.get("stages", None)
+        extended = self.request_params.get("extended_stages", None)
+        dataset_name = self.request_params.get("dataset_name", None)
+        return fosv.get_view(
+            dataset_name,
+            stages=stages,
+            extended_stages=extended,
+        )
+
+    @property
+    def dataset(self):
+        dataset_name = self.request_params.get("dataset_name", None)
+        return fo.load_dataset(dataset_name)
+
+
+class ExecutionResult:
+    def __init__(self, ctx, result):
+        self.ctx = ctx
+        self.result = result
+
+    def to_json(self):
+        return foj.stringify(self.result)
---
+++ fiftyone/operators/operator.py
@@ -0,0 +1,44 @@
+"""
+FiftyOne operators.
+
+| Copyright 2017-2023, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+from .definition import OperatorDefinition
+
+
+class Operator:
+    """A FiftyOne operator.
+
+    Operators represent an operation and the details of how to execute it.
+
+    FiftyOne operators contain enough information for a user interface to
+    render a form or button allowing a user to execute the operation.
+    Args:
+        name (None): the name of the operator.
+    """
+
+    def __init__(
+        self,
+        name=None,
+    ):
+        if name is None:
+            raise ValueError("Operator name cannot be None")
+
+        self.name = name
+        self._definition = OperatorDefinition()
+
+    def __eq__(self, other):
+        return type(other) == type(self) and self.name == other.name
+
+    def __copy__(self):
+        return self  # operators are singletons
+
+    def __deepcopy__(self, memo):
+        return self  # operators are singletons
+
+    def execute(self, ctx):
+        """Executes the operator. Subclasses must implement this method."""
+        raise NotImplementedError("subclass must implement execute()")
