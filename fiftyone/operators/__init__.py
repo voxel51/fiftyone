65d39601934e11ad43a67a0ee506373559d12bf7
Ritchie Martori <ritchie@voxel51.com>
2023-02-06 16:01:49 -0700

    initial python operators

---
+++ fiftyone/operators/__init__.py
@@ -0,0 +1,9 @@
+"""
+| Copyright 2017-2023, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+from .operator import Operator
+from .registry import register_operator
+from .executor import execute_operator
