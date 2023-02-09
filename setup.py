b1105ca96995f730917ce6802777057f356671cf
brimoor <brimoor@umich.edu>
2023-02-08 22:47:42 -0500

    bumping brain dependency

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ INSTALL_REQUIRES = [
     "xmltodict",
     "universal-analytics-python3>=1.0.1,<2",
     # internal packages
-    "fiftyone-brain>=0.9.2,<0.10",
+    "fiftyone-brain>=0.10,<0.11",
     "fiftyone-db>=0.4,<0.5",
     "voxel51-eta>=0.8.1,<0.9",
 ]
