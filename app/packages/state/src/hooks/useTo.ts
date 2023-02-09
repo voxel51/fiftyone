2f84d0e6907596660c6b4d9433ad1ec6d163ad89
Benjamin Kane <ben@tapes.co>
2023-02-09 14:19:57 -0600

    rm view

--- app/packages/state/src/hooks/useTo.ts
+++ app/packages/state/src/hooks/useTo.ts
@@ -2,7 +2,9 @@ import { useCallback, useContext, useTransition } from "react";
 import { RouterContext, RoutingContext } from "../routing";
 
 const goTo = (router: RoutingContext, path: string, state: any) => {
-  const search = new URLSearchParams(window.location.search).toString();
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.delete("view");
+  const search = searchParams.toString();
   router.history.push(`${path}${search.length ? "?" : ""}${search}`, state);
 };
 
