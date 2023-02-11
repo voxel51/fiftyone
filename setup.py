9d190108f343c5ea239f2ad14ce1d77c39443cfd
brimoor <brimoor@umich.edu>
2023-02-11 17:54:23 -0500

    bumping ETA version

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ INSTALL_REQUIRES = [
     # internal packages
     "fiftyone-brain>=0.10,<0.11",
     "fiftyone-db>=0.4,<0.5",
-    "voxel51-eta>=0.8.2,<0.9",
+    "voxel51-eta>=0.8.3,<0.9",
 ]
 
 
