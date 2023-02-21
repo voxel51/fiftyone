289c61f0e88b5104fd685dd02d180aada27bfdc8
Sashank Aryal <sashank2012@gmail.com>
2023-02-21 09:14:00 -0600

    uncomment code so that action bar auto-hides

--- app/packages/looker-3d/src/Looker3d.tsx
+++ app/packages/looker-3d/src/Looker3d.tsx
@@ -326,11 +326,10 @@ const Looker3dCore = ({ api: { sample, src, dataset } }: Looker3dProps) => {
         />
         <axesHelper />
       </Canvas>
-      {/* {(hoveringRef.current || hovering) && ( */}
-      {true && (
+      {(hoveringRef.current || hovering) && (
         <ActionBarContainer
           onMouseEnter={() => (hoveringRef.current = true)}
-          // onMouseLeave={() => (hoveringRef.current = false)}
+          onMouseLeave={() => (hoveringRef.current = false)}
         >
           {hasMultiplePcdSlices && <SliceSelector dataset={dataset} />}
           <ActionsBar>
