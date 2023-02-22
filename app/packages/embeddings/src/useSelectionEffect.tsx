3655634f3474ac23561f5e008b475524cdc23ec3
imanjra <ibrahim@voxel51.com>
2023-02-22 15:51:56 -0500

    cleanup unused code

--- app/packages/embeddings/src/useSelectionEffect.tsx
+++ app/packages/embeddings/src/useSelectionEffect.tsx
@@ -1,46 +1,21 @@
 import { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import * as fos from "@fiftyone/state";
 import { usePanelStatePartial } from "@fiftyone/spaces";
 import { useBrainResult } from "./useBrainResult";
-import { useColorByField } from "./useLabelSelector";
-import { useWarnings } from "./useWarnings";
 import { fetchUpdatedSelection } from "./fetch";
 import { usePlotSelection } from "./usePlotSelection";
 
 export function useSelectionEffect() {
   const { setPlotSelection } = usePlotSelection();
   const datasetName = useRecoilValue(fos.datasetName);
-  const [selectedSamples, setSelectedSamples] = useRecoilState(
-    fos.selectedSamples
-  );
+  const selectedSamples = useRecoilValue(fos.selectedSamples);
   const [brainKey] = useBrainResult();
-  const [labelField] = useColorByField();
   const view = useRecoilValue(fos.view);
-  const [loadedPlot, setLoadedPlot] = usePanelStatePartial(
-    "loadedPlot",
-    null,
-    true
-  );
-  const [loadingPlot, setLoadingPlot] = usePanelStatePartial(
-    "loadingPlot",
-    true,
-    true
-  );
-  const [loadingPlotError, setLoadingPlotError] = usePanelStatePartial(
-    "loadingPlotError",
-    null,
-    true
-  );
+  const [loadedPlot] = usePanelStatePartial("loadedPlot", null, true);
   const filters = useRecoilValue(fos.filters);
   const extended = useRecoilValue(fos.extendedStagesUnsorted);
-  const [overrideStage, setOverrideStage] = useRecoilState(
-    fos.extendedSelectionOverrideStage
-  );
-  const [extendedSelection, setExtendedSelection] = useRecoilState(
-    fos.extendedSelection
-  );
-  const warnings = useWarnings();
+  const extendedSelection = useRecoilValue(fos.extendedSelection);
 
   // updated the selection when the extended view updates
   useEffect(() => {
--- app/packages/state/src/hooks/index.ts
+++ app/packages/state/src/hooks/index.ts
@@ -30,7 +30,6 @@ export { default as useSetDataset } from "./useSetDataset";
 export { default as useSetExpandedSample } from "./useSetExpandedSample";
 export { default as useSetGroupSlice } from "./useSetGroupSlice";
 export { default as useSetSelected } from "./useSetSelected";
-export { default as useSetExtendedSelection } from "./useSetExtendedSelection";
 export { default as useSetSelectedLabels } from "./useSetSelectedLabels";
 export { default as useSetView, stateProxy } from "./useSetView";
 export { default as useStateUpdate } from "./useStateUpdate";
