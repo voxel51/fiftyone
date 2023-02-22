e2d455ffaff791b3a3b368bf869cd35545a9a190
brimoor <brimoor@umich.edu>
2023-02-22 09:41:43 -0500

    updating other instances

--- fiftyone/core/dataset.py
+++ fiftyone/core/dataset.py
@@ -3757,7 +3757,7 @@ class Dataset(foc.SampleCollection, metaclass=DatasetSingleton):
                 -   a folder name like ``"data"`` or ``"data/"`` specifying a
                     subfolder of ``dataset_dir`` in which the media lies
                 -   an absolute directory path in which the media lies. In this
-                    case, the ``export_dir`` has no effect on the location of
+                    case, the ``dataset_dir`` has no effect on the location of
                     the data
                 -   a filename like ``"data.json"`` specifying the filename of
                     a JSON manifest file in ``dataset_dir`` that maps UUIDs to
@@ -3918,7 +3918,7 @@ class Dataset(foc.SampleCollection, metaclass=DatasetSingleton):
                 -   a folder name like ``"data"`` or ``"data/"`` specifying a
                     subfolder of ``dataset_dir`` in which the media lies
                 -   an absolute directory path in which the media lies. In this
-                    case, the ``export_dir`` has no effect on the location of
+                    case, the ``dataset_dir`` has no effect on the location of
                     the data
                 -   a filename like ``"data.json"`` specifying the filename of
                     a JSON manifest file in ``dataset_dir`` that maps UUIDs to
