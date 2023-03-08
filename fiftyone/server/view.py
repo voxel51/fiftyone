3b5644a6324ff535096e02ed23fd68b4e7da3f63
Benjamin Kane <ben@tapes.co>
2023-03-08 16:21:30 -0600

    is matching

--- fiftyone/server/view.py
+++ fiftyone/server/view.py
@@ -498,7 +498,7 @@ def _make_filter_stages(
 
             match_exprs.append(expr)
 
-        if match_exprs:
+        if match_exprs and is_matching:
             matcher = F.all if exclude else F.any
             stages.append(fosg.Match(matcher(match_exprs)))
 
