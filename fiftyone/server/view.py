ef48a6601b314340eda07569748a70257a803c1c
Lanny W <lanzhenwang9@gmail.com>
2023-03-08 16:49:13 -0600

    fix filter mode

--- fiftyone/server/view.py
+++ fiftyone/server/view.py
@@ -498,7 +498,7 @@ def _make_filter_stages(
 
             match_exprs.append(expr)
 
-        if match_exprs and is_matching:
+        if match_exprs:
             matcher = F.all if exclude else F.any
             stages.append(fosg.Match(matcher(match_exprs)))
 
