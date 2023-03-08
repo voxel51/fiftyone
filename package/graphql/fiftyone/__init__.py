21e0f5c7f914ea32091d7bf94be8231c704257cf
Benjamin Kane <ben@tapes.co>
2023-03-08 09:14:09 -0600

    __init__

---
+++ package/graphql/fiftyone/__init__.py
@@ -0,0 +1,20 @@
+"""
+FiftyOne V2 GraphQL schema definition package
+
+See https://voxel51.com/fiftyone/package/graphql for more information.
+
+| Copyright 2017-2023, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+from pkgutil import extend_path
+
+#
+# This statement allows multiple `fiftyone.XXX` packages to be installed in the
+# same environment and used simultaneously.
+#
+# https://docs.python.org/3/library/pkgutil.html#pkgutil.extend_path
+#
+__path__ = extend_path(__path__, __name__)
+
+from fiftyone.__public__ import *
---
+++ package/graphql/fiftyone/graphql/__init__.py
@@ -0,0 +1,9 @@
+"""
+FiftyOne V2 GraphQL schema definition package
+
+See https://voxel51.com/fiftyone/package/graphql for more information.
+
+| Copyright 2017-2023, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
