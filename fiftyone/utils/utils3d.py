57075bc0d3372d233f635bd3729c79ddbd9e86c9
brimoor <brimoor@umich.edu>
2023-03-13 15:40:03 -0400

    media field updates

--- fiftyone/utils/utils3d.py
+++ fiftyone/utils/utils3d.py
@@ -16,8 +16,9 @@ import eta.core.image as etai
 import eta.core.numutils as etan
 
 import fiftyone.core.fields as fof
+import fiftyone.core.labels as fol
 import fiftyone.core.media as fom
-import fiftyone.core.sample as fos
+from fiftyone.core.sample import Sample
 import fiftyone.core.utils as fou
 import fiftyone.core.validation as fov
 from fiftyone.core.odm import DynamicEmbeddedDocument
@@ -410,7 +411,7 @@ def _compute_intersection_points(box1, box2):
     return intersection_points
 
 
-class OrthographicProjectionMetadata(DynamicEmbeddedDocument):
+class OrthographicProjectionMetadata(DynamicEmbeddedDocument, fol._HasMedia):
     """Class for storing metadata about orthographic projections.
 
     Args:
@@ -422,6 +423,8 @@ class OrthographicProjectionMetadata(DynamicEmbeddedDocument):
         width: the width of the image, in pixels
         height: the height of the image, in pixels
     """
+
+    _MEDIA_FIELD = "filepath"
 
     filepath = fof.StringField()
     min_bound = fof.ListField(fof.FloatField())
@@ -554,7 +557,7 @@ def compute_orthographic_projection_images(
             metadata.filepath = image_path
 
             if out_group_slice is not None:
-                sample = fos.Sample(filepath=image_path)
+                sample = Sample(filepath=image_path)
                 sample[group_field] = group.element(out_group_slice)
                 sample[metadata_field] = metadata
                 out_samples.append(sample)
