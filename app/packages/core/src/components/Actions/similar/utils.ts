99accf05782269b14d6c5a675deba635bcf70c3f
Lanny W <lanzhenwang9@gmail.com>
2023-03-15 16:35:49 -0500

    reset selected labels after submission

--- app/packages/core/src/components/Actions/similar/utils.ts
+++ app/packages/core/src/components/Actions/similar/utils.ts
@@ -115,7 +115,6 @@ export const useSortBySimilarity = (close) => {
             set(fos.modal, null);
             set(fos.similaritySorting, false);
             set(fos.savedLookerOptions, (cur) => ({ ...cur, showJSON: false }));
-            set(fos.selectedLabels, {});
             set(fos.hiddenLabels, {});
             set(fos.modal, null);
             close();
