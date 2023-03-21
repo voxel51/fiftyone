137b232dc100099d846f6cca9b4b7094f2a079a2
brimoor <brimoor@umich.edu>
2023-03-21 18:13:55 -0400

    updating version numbers

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.17.0rc1,<0.18"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.17.0,<0.18"]}
 
 
 with open("README.md", "r") as fh:
