46762d0e1b665a77652d2f2616d00af4dd2faf72
imanjra <ibrahim@voxel51.com>
2023-03-28 11:40:05 -0400

    initial generated input for operators

---
+++ app/packages/operators/src/OperatorInput.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import validator from "@rjsf/validator-ajv8";
+import Form from "@rjsf/mui";
+import { templates, widgets } from "./RJSFOverrides";
+
+const uiSchema = {
+  "ui:submitButtonOptions": {
+    norender: true,
+  },
+};
+
+export default function OperatorInput(props) {
+  const { schema, onChange, formData, onError } = props;
+  return (
+    <Form
+      schema={schema}
+      uiSchema={uiSchema}
+      validator={validator}
+      onChange={onChange}
+      formData={formData}
+      onError={onError}
+      templates={templates}
+      widgets={widgets}
+    />
+  );
+}
