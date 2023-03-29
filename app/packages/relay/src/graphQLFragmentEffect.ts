bc139d0ca4849ab8a811457ffd41c1c4a7f84822
Benjamin Kane <ben@tapes.co>
2023-03-29 08:25:05 -0600

    rename

---
+++ app/packages/relay/src/graphQLFragmentEffect.ts
@@ -0,0 +1,61 @@
+import { Snapshot } from "react-relay";
+import { AtomEffect } from "recoil";
+import {
+  createOperationDescriptor,
+  GraphQLTaggedNode,
+  readInlineData,
+} from "relay-runtime";
+import { KeyType, KeyTypeData } from "relay-runtime/lib/store/readInlineData";
+import { getPageQuery } from "./PageQuery";
+
+export function graphQLFragmentEffect<T extends KeyType>(
+  fragment: GraphQLTaggedNode
+): AtomEffect<KeyTypeData<T>> {
+  return ({ setSelf, trigger }) => {
+    if (trigger == "set") {
+      throw new Error("fragement not writeable");
+    }
+
+    const setter = (snapshot: Snapshot) => {
+      if (!snapshot.isMissingData && snapshot.data != null) {
+        setSelf(readInlineData(fragment, snapshot.data as T));
+      }
+    };
+    const [{ preloadedQuery, concreteRequest }, subscribe] = getPageQuery();
+    let operation = createOperationDescriptor(
+      concreteRequest,
+      preloadedQuery.variables
+    );
+    let operationDisposable = preloadedQuery.environment.retain(operation);
+    const snapshot = preloadedQuery.environment.lookup(operation.fragment);
+    setter(snapshot);
+
+    const subscriptionDisposable = preloadedQuery.environment.subscribe(
+      snapshot,
+      setter
+    );
+
+    const unsubscribe = () => {
+      operationDisposable?.dispose();
+      subscriptionDisposable?.dispose();
+    };
+
+    subscribe(({ preloadedQuery, concreteRequest }) => {
+      operationDisposable?.dispose();
+      subscriptionDisposable?.dispose();
+
+      operation = createOperationDescriptor(
+        concreteRequest,
+        preloadedQuery.variables
+      );
+      operationDisposable = preloadedQuery.environment.retain(operation);
+      preloadedQuery.environment.subscribe(snapshot, setter);
+
+      return unsubscribe;
+    });
+
+    return unsubscribe;
+  };
+}
+
+export default graphQLFragmentEffect;
