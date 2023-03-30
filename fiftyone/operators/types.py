e98ad3b4421805981effb5cd5cf7b06ce02aef83
Ritchie Martori <ritchie@voxel51.com>
2023-03-30 16:24:02 -0700

    update types

--- fiftyone/operators/types.py
+++ fiftyone/operators/types.py
@@ -9,17 +9,27 @@ class BaseType:
 
 
 class Object(BaseType):
-    def __init__(self):
-        self.properties = []
+    def __init__(self, **kwargs):
+        self.properties = {}
         self._needsResolution = False
 
-    def add_property(self, property):
-        self.properties.append(property)
+    def add_property(self, name, property):
+        self.properties.set(name, property)
         return property
 
+    def get_property(self, name):
+        return self.properties.get(name, None)
+
     def define_property(self, name, type, **kwargs):
-        property = Property(name, type, **kwargs)
-        self.properties.append(property)
+        label = kwargs.get("label", None)
+        description = kwargs.get("description", None)
+        view = kwargs.get("view", types.View())
+        if label is not None:
+            view.label = label
+        if description is not None:
+            view.description = description
+        property = Property(type, **kwargs, view=view)
+        self.add_property(name, property)
         return property
 
     def to_json(self):
@@ -33,9 +43,8 @@ class Object(BaseType):
         self._needsResolution = True
 
 
-class Property:
-    def __init__(self, name, type, **kwargs):
-        self.name = name
+class Property(BaseType):
+    def __init__(self, type, **kwargs):
         self.type = type
         self.default = kwargs.get("default", None)
         self.required = kwargs.get("required", False)
@@ -44,7 +53,6 @@ class Property:
 
     def to_json(self):
         return {
-            "name": self.name,
             "type": self.type.to_json(),
             "default": self.default,
             "required": self.required,
@@ -79,20 +87,23 @@ class Number(BaseType):
 
 
 class List(BaseType):
-    def __init__(self, element_type):
+    def __init__(self, element_type, min_items=None, max_items=None):
         self.element_type = element_type
+        self.min_items = min_items
+        self.max_items = max_items
 
     def to_json(self):
         return {
             "name": self.__class__.__name__,
             "element_type": self.element_type.to_json(),
+            "min_items": self.min_items,
+            "max_items": self.max_items,
         }
 
 
 class SampleID(String):
     def __init__(self):
         pass
-
 
 class Enum(BaseType):
     def __init__(self, values):
@@ -103,15 +114,97 @@ class Enum(BaseType):
 
 
 class Plot(BaseType):
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        self.title = kwargs.get("title", None)
+        self.description = kwargs.get("description", None)
+        self.caption = kwargs.get("caption", None)
+        self.spaces = kwargs.get("spaces", None)
 
+    def to_json(self):
+        return {"name": self.__class__.__name__, "values": self.values}
+
+
+#
+# Trigger
+#
+
+class Trigger(BaseType):
+    def __init__(self, operator, params=None):
+        self.operator = operator
+        self.params = params
+    
+    def to_json(self):
+        return {
+            "name": self.__class__.__name__,
+            "operator": self.operator,
+            "params": self.params,
+        }
+
+#
+# Fiftyone
+#
+
+class SampleField(Enum):
+    def __init__(self, dataset):
+        super().__init__(dataset.get_field_schema().keys())
+
+#
+# Views
+#
 
 class View:
     def __init__(self, **kwargs):
         self.label = kwargs.get("label", None)
+        self.description = kwargs.get("description", None)
+        self.caption = kwargs.get("caption", None)
+        self.spaces = kwargs.get("spaces", None)
 
     def to_json(self):
         return {
             "label": self.label,
         }
+
+class Choice(View) {
+    def __init__(self, value, **kwargs):
+        super().__init__(**kwargs)
+        self.value = value
+}
+
+class Choices(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.choices = kwargs.get("choices", [])
+
+    def values(self):
+        return [choice.value for choice in self.choices]
+
+    def to_json(self):
+        return {
+            **super().to_json(),
+            "name": self.__class__.__name__,
+            "choices": self.choices,
+        }
+
+class RadioGroup(Choices):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Dropdown(Choices):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Notice(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Header(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Warning(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Button(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
