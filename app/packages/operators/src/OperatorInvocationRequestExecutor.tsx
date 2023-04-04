e90858d06623e168a24f77a69fb22357ece7e41b
Ritchie Martori <ritchie@voxel51.com>
2023-04-03 18:25:51 -0700

    trigger support

---
+++ app/packages/operators/src/OperatorInvocationRequestExecutor.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useInvocationRequestExecutor, useInvocationRequestQueue } from "./state";
+
+export default function OperatorInvocationRequestExecutor() {
+  const {requests, onSuccess, onError} = useInvocationRequestQueue();
+
+  return <>
+    {
+      requests.map(queueItem => (
+        <RequestExecutor
+          key={queueItem.id}
+          queueItem={queueItem}
+          onSuccess={onSuccess}
+          onError={onError}
+        />
+      ))
+    }
+  </>
+}
+
+function RequestExecutor({queueItem, onSuccess, onError}) {
+  const executor = useInvocationRequestExecutor({queueItem, onSuccess, onError});
+
+  useEffect(() => {
+    executor.execute(queueItem.request.params);
+  }, []);
+
+  return null
+}
