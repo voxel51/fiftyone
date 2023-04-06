5007286311f632f8f8461c8f11e490581be36152
imanjra <ibrahim@voxel51.com>
2023-04-06 08:02:36 -0400

    cleanup and fix more types

--- app/packages/operators/src/OperatorInput.tsx
+++ app/packages/operators/src/OperatorInput.tsx
@@ -1,36 +1,16 @@
-import validator from "@rjsf/validator-ajv8";
-import Form from "@rjsf/mui";
-import { templates, widgets } from "./RJSFOverrides";
 import { Box } from "@mui/material";
 import { useActivePlugins } from "@fiftyone/plugins";
 import { PluginComponentType } from "@fiftyone/plugins";
-
-const uiSchema = {
-  "ui:submitButtonOptions": {
-    norender: true,
-  },
-};
 
 export default function OperatorInput(props) {
   const componentPlugins = useActivePlugins(PluginComponentType.Component, {});
   const OperatorIO = componentPlugins.find(
     ({ name }) => name === "OperatorIOComponent"
   ).component;
-  const { schema, onChange, formData, onError, inputFields } = props;
-  console.log(inputFields);
+  const { schema, onChange, formData, onError } = props;
   return (
     <Box sx={{ p: 2 }}>
-      <OperatorIO schema={inputFields} onChange={onChange} />
-      {/* <Form
-        schema={schema}
-        uiSchema={uiSchema}
-        validator={validator}
-        onChange={onChange}
-        formData={formData}
-        onError={onError}
-        templates={templates}
-        widgets={widgets}
-      /> */}
+      <OperatorIO schema={schema} onChange={onChange} />
     </Box>
   );
 }
