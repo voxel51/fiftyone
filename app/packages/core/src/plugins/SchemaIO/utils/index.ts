5c4d95e986ff02812a554c1ab6f1a74490c3bb96
imanjra <ibrahim@voxel51.com>
2023-04-06 11:43:32 -0400

    add support for oneof in schemaio

--- app/packages/core/src/plugins/SchemaIO/utils/index.ts
+++ app/packages/core/src/plugins/SchemaIO/utils/index.ts
@@ -1,3 +1,12 @@
+import ObjectView from "../components/ObjectView";
+import RadioView from "../components/RadioView";
+import DropdownView from "../components/DropdownView";
+import CheckboxView from "../components/CheckboxView";
+import FieldView from "../components/FieldView";
+import ListView from "../components/ListView";
+import OneOfView from "../components/OneOfView";
+import UnsupportedView from "../components/UnsupportedView";
+
 export function log(...args) {
   console.log(">>>", ...args);
 }
@@ -7,6 +16,22 @@ export function getPath(basePath, propertyKey) {
   if (computedPath) computedPath += ".";
   computedPath += propertyKey;
   return computedPath;
+}
+
+const componentsMap = {
+  ObjectView: ObjectView,
+  CheckboxView: CheckboxView,
+  FieldView: FieldView,
+  ListView: ListView,
+  RadioView: RadioView,
+  DropdownView: DropdownView,
+  OneOfView: OneOfView,
+  UnsupportedView: UnsupportedView,
+};
+
+export function getComponent(schema) {
+  const { component } = schema?.view || {};
+  return componentsMap[component] || UnsupportedView;
 }
 
 export { operatorToIOSchema } from "./operator-schema";
