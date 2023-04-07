b7faf49991580f493f87160e42e5fa3d3380769c
Sashank Aryal <sashank2012@gmail.com>
2023-04-07 13:45:47 -0500

    increase app wait timeout even more :(

--- e2e/lib/constants.ts
+++ e2e/lib/constants.ts
@@ -1,7 +1,7 @@
 import { Duration } from "../cypress/support/utils";
 
 // time to wait for fiftyone app to load
-export const DEFAULT_APP_LOAD_TIMEOUT = Duration.Minutes(1);
+export const DEFAULT_APP_LOAD_TIMEOUT = Duration.Minutes(2);
 
 export const DEFAULT_APP_PORT = 8787;
 export const DEFAULT_APP_HOSTNAME = "0.0.0.0";
