118197c0863b80e1bfa60a5afaec57e6c1b91e66
imanjra <ibrahim@voxel51.com>
2023-04-10 10:56:04 -0400

    codeeditorview readonly, language, and types

--- app/packages/core/src/plugins/SchemaIO/components/CodeEditorView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/CodeEditorView.tsx
@@ -1,24 +1,34 @@
 import Editor from "@monaco-editor/react";
 import { Box, useColorScheme } from "@mui/material";
 import React from "react";
-import { log } from "../utils";
 import Header from "./Header";
 
 export default function CodeEditorView(props) {
   const { mode } = useColorScheme();
-  log({ mode });
-  const { default: defaultValue, onChange, path, schema } = props;
-  const { view = {} } = schema;
+  const { onChange, path, schema } = props;
+  const { default: defaultValue, view = {} } = schema;
+  const { language, readOnly } = view;
 
-  // todo: add support for readonly and remove codeblock/jsonview components
   return (
-    <Box>
+    <Box
+      sx={{
+        ...(readOnly
+          ? {
+              "& .cursors-layer": {
+                display: "none",
+              },
+            }
+          : {}),
+      }}
+    >
       <Header {...view} />
       <Editor
         height={250}
         theme={mode === "dark" ? "vs-dark" : "light"}
         defaultValue={defaultValue}
         onChange={(value) => onChange(path, value)}
+        language={language}
+        options={{ readOnly }}
       />
     </Box>
   );
