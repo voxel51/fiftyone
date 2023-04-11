7149ab3ed93796c4e764fec99d6f16ab1d34ff53
Aida Alonso Iglesias <aida@voxel51.com>
2023-04-11 16:10:32 -0700

    Change DEFAULT_APP_HOSTNAME in e2e

--- e2e/lib/constants.ts
+++ e2e/lib/constants.ts
@@ -4,7 +4,7 @@ import { Duration } from "../cypress/support/utils";
 export const DEFAULT_APP_LOAD_TIMEOUT = Duration.Minutes(2);
 
 export const DEFAULT_APP_PORT = 8787;
-export const DEFAULT_APP_HOSTNAME = "localhost";
+export const DEFAULT_APP_HOSTNAME = "127.0.0.1";
 export const DEFAULT_APP_ADDRESS = `http://${DEFAULT_APP_HOSTNAME}:${DEFAULT_APP_PORT}`;
 
 export const GHOST_SERVER_TIMEOUT =
