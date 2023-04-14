327b00d235bcb3f130d8435622ec70b92930074c
imanjra <ibrahim@voxel51.com>
2023-04-13 22:32:33 -0400

    initial inferredview support

---
+++ app/packages/core/src/plugins/SchemaIO/components/InferredView.tsx
@@ -0,0 +1,152 @@
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import Header from "./Header";
+import KeyValueView from "./KeyValueView";
+import PrimitiveListView from "./PrimitiveListView";
+import TableView from "./TableView";
+
+export default function InferredView(props) {
+  const { label, data, ...otherProps } = props;
+  const Component = getComponent(data);
+  return <Component label={label} data={data} {...otherProps} />;
+}
+
+function PrimitiveView(props) {
+  const { label, data } = props;
+  return (
+    <Stack direction="row" spacing={1}>
+      <Typography color="text.secondary">{label}</Typography>
+      <Typography>{data.toString()}</Typography>
+    </Stack>
+  );
+}
+
+function ObjectView(props) {
+  const { label, data, nested } = props;
+  const properties = Object.keys(data);
+
+  const keyValue = properties.map((key) => {
+    const value = data[key];
+    const type = getType(value);
+    const prop = {
+      id: key,
+      label: key,
+    };
+    if (type === "array")
+      prop.Component = <ArrayView label={key} data={value} nested />;
+    else if (type === "object")
+      prop.Component = <ObjectView label={key} data={value} nested />;
+    else prop.value = value;
+    return prop;
+  });
+
+  return (
+    <Box>
+      {!nested && <Header label={label} />}
+      <KeyValueView schema={{}} data={keyValue} nested={nested} />
+    </Box>
+  );
+}
+
+function ArrayView(props) {
+  const { label, data, nested } = props;
+
+  const type = getDominantType(data);
+
+  if (primitives.includes(type)) {
+    const formattedData = data.map((value) => ({
+      id: value.toString(),
+      label: value.toString(),
+    }));
+    return (
+      <PrimitiveListView
+        data={formattedData}
+        schema={nested ? {} : { view: { label } }}
+      />
+    );
+  }
+
+  const tableView = isTable(data);
+  if (tableView) {
+    const columns = Object.keys(data[0]).map((key) => ({ key, label: key }));
+    return (
+      <TableView view={{ columns, label: !nested && label }} data={data} />
+    );
+  }
+
+  const keyValueData = data.map((item, i) => ({
+    id: i,
+    label: i,
+    Component: <InferredView key={label} data={item} nested={true} />,
+  }));
+  return (
+    <KeyValueView
+      schema={{ view: nested ? {} : { label } }}
+      data={keyValueData}
+      nested={nested}
+    />
+  );
+}
+
+const primitives = ["string", "number", "boolean"];
+const typeToComponent = {
+  array: ArrayView,
+  object: ObjectView,
+  string: PrimitiveView,
+  number: PrimitiveView,
+  boolean: PrimitiveView,
+};
+
+function getType(value) {
+  let type = typeof value;
+  if (typeof value === "object") {
+    type = Array.isArray(value) ? "array" : "object";
+  }
+  return type;
+}
+
+function getComponent(data) {
+  const dataType = getType(data);
+  return typeToComponent[dataType];
+}
+
+function getDominantType(array) {
+  const typesCount = {};
+  for (const item of array) {
+    const itemType = getType(item);
+    typesCount[itemType] = typesCount[itemType] + 1 || 1;
+  }
+  let dominantType;
+  let dominantTypeCount;
+  for (const type in typesCount) {
+    const count = typesCount[type];
+    if (!dominantType) {
+      dominantType = type;
+      dominantTypeCount = count;
+    } else if (count > dominantTypeCount) {
+      dominantType = type;
+      dominantTypeCount = count;
+    } else if (count === dominantTypeCount && primitives.includes(type)) {
+      dominantType = type;
+    }
+  }
+  return dominantType;
+}
+
+function isTable(array) {
+  let serializedKeys;
+  for (const item of array) {
+    if (getType(item) !== "object") {
+      return false;
+    }
+    const serializedItemKeys = Object.keys(item).join(",");
+    if (!serializedKeys) {
+      serializedKeys = serializedItemKeys;
+      continue;
+    }
+    if (serializedKeys !== serializedItemKeys) {
+      return false;
+    }
+  }
+  return true;
+}
--- app/packages/core/src/plugins/SchemaIO/components/KeyValueView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/KeyValueView.tsx
@@ -11,12 +11,12 @@ import {
 import Header from "./Header";
 
 export default function KeyValueView(props) {
-  const { schema, data } = props;
+  const { schema, data, nested } = props;
   const { view } = schema;
   return (
     <Box>
       <Header {...view} divider />
-      <TableContainer component={Paper}>
+      <TableContainer component={nested ? Box : Paper}>
         <Table sx={{ minWidth: 650 }}>
           <TableBody>
             {data.map((item) => (
@@ -25,11 +25,21 @@ export default function KeyValueView(props) {
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell
-                  sx={{ fontWeight: 600, fontSize: "1rem", width: "25%" }}
+                  sx={{
+                    fontWeight: 600,
+                    fontSize: "1rem",
+                    width: "15%",
+                    minWidth: 32,
+                    maxWidth: 96,
+                    color: (theme) => theme.palette.text.secondary,
+                    textAlign: "right",
+                  }}
                 >
                   {item.label}
                 </TableCell>
-                <TableCell align="left">{item.value}</TableCell>
+                <TableCell align="left">
+                  {item.Component || item.value.toString()}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
