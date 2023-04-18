8e3bb1260f0f0d5f6c5816086a701ce620ce4f7a
imanjra <ibrahim@voxel51.com>
2023-04-18 11:26:51 -0400

    pass down data to view components

--- app/packages/core/src/plugins/SchemaIO/components/CheckboxView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/CheckboxView.tsx
@@ -3,14 +3,14 @@ import { FormControlLabel, Checkbox } from "@mui/material";
 import Header from "./Header";
 
 export default function CheckboxView(props) {
-  const { onChange, path, schema } = props;
+  const { onChange, path, schema, data } = props;
   const { view = {} } = schema;
 
   return (
     <FormControlLabel
       control={
         <Checkbox
-          defaultChecked={schema.default === true}
+          defaultChecked={data === true || schema.default === true}
           onChange={(e, value) => onChange(path, value)}
         />
       }
