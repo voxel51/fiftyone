d1ea422cd8c451881d78feace36691a28b50be69
imanjra <ibrahim@voxel51.com>
2023-04-18 11:28:27 -0400

    operator output views enhancements

--- app/packages/core/src/plugins/SchemaIO/components/TagsView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/TagsView.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Box, Chip } from "@mui/material";
 import Header from "./Header";
 
-export default function PrimitiveListView(props) {
-  const { data, schema } = props;
+export default function TagsView(props) {
+  const { data, path, schema } = props;
   const { view } = schema;
   return (
     <Box>
       <Header {...view} divider />
-      {data.map(({ id, label }) => (
-        <Chip key={id} label={label} sx={{ m: 0.25 }} />
+      {data.map((item, i) => (
+        <Chip key={`${path}-${i}`} label={item.toString()} sx={{ m: 0.25 }} />
       ))}
     </Box>
   );
