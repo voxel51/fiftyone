060729a102db732fe20a6578d08701a9006d1750
imanjra <ibrahim@voxel51.com>
2023-04-19 09:39:15 -0400

    initial validation error for operator prompt

--- app/packages/core/src/plugins/SchemaIO/components/FieldWrapper.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/FieldWrapper.tsx
@@ -3,8 +3,12 @@ import { Box, Typography } from "@mui/material";
 import Header from "./Header";
 
 export default function FieldWrapper(props) {
-  const { schema, children } = props;
+  const { schema, children, errors, path } = props;
   const { view = {} } = schema;
+
+  const fieldErrors = errors?.[path] || [];
+  const [firstError] = fieldErrors; // todo:...
+
   return (
     <Box>
       <Header {...view} sx={{ pb: 1 }} variant="secondary" omitCaption />
@@ -12,6 +16,11 @@ export default function FieldWrapper(props) {
       {view.caption && (
         <Typography variant="body2" color="text.tertiary" sx={{ pt: 0.5 }}>
           {view.caption}
+        </Typography>
+      )}
+      {firstError && (
+        <Typography variant="body2" color="error.main" sx={{ pt: 0.5 }}>
+          {firstError.reason}
         </Typography>
       )}
     </Box>
