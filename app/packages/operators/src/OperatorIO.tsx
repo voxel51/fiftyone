298db9ba489912203955a75afe97549133f2ef9e
imanjra <ibrahim@voxel51.com>
2023-04-19 09:39:15 -0400

    initial validation error for operator prompt

--- app/packages/operators/src/OperatorIO.tsx
+++ app/packages/operators/src/OperatorIO.tsx
@@ -7,15 +7,9 @@ export default function OperatorIO(props) {
   const OperatorIOComponent = componentPlugins.find(
     ({ name }) => name === "OperatorIOComponent"
   ).component;
-  const { schema, onChange, data, onError, type } = props;
   return (
     <Box sx={{ p: 2 }}>
-      <OperatorIOComponent
-        schema={schema}
-        onChange={onChange}
-        type={type}
-        data={data}
-      />
+      <OperatorIOComponent {...props} />
     </Box>
   );
 }
