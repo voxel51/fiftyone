77fab8a3dbaceff4ecad6c7291941880ff65fea3
Ritchie Martori <ritchie@voxel51.com>
2023-04-20 14:53:40 -0700

    refactor plugin listing

--- fiftyone/operators/registry.py
+++ fiftyone/operators/registry.py
@@ -11,17 +11,22 @@ from .loader import load_from_dir
 
 OPERATOR_DICT = {}
 
+
 class OperatorRegistration:
-    def __init__(self, operator_name, operator=None, failed=False, errors=None):
+    def __init__(
+        self, operator_name, operator=None, failed=False, errors=None
+    ):
         self.operator_name = operator_name
         self.operator = operator
         self.failed = failed
         self.errors = errors
 
+
 def list_registrations():
     load_from_dir()
     all_registrations = OPERATOR_DICT.values()
     return [r for r in all_registrations if r.failed is False]
+
 
 def list_operators():
     """Lists the available FiftyOne operators.
@@ -31,6 +36,7 @@ def list_operators():
     """
     registrations = list_registrations()
     return [r.operator for r in registrations]
+
 
 def operator_exists(name):
     """Checks if the operator exists.
@@ -44,16 +50,31 @@ def operator_exists(name):
     registrations = list_registrations()
     return name in [r.operator_name for r in registrations]
 
+
 def register_operator(operator):
-    OPERATOR_DICT[operator.name] = OperatorRegistration(operator.name, operator=operator)
+    OPERATOR_DICT[operator.name] = OperatorRegistration(
+        operator.name, operator=operator
+    )
+
+
+def register(cls):
+    if issubclass(cls, Operator):
+        register_operator(cls())
+        return
+    raise ValueError("Class '%s' is not known subclass" % cls)
+
 
 def unregister_operator(operator):
     name = operator.name if operator is not None else None
     if name:
         remove_from_dict(OPERATOR_DICT, operator.name)
 
+
 def register_failed_operator(name, errors):
-    OPERATOR_DICT[name] = OperatorRegistration(name, failed=True, errors=errors)
+    OPERATOR_DICT[name] = OperatorRegistration(
+        name, failed=True, errors=errors
+    )
+
 
 def get_operator(name):
     load_from_dir()
@@ -61,6 +82,7 @@ def get_operator(name):
     if registration.failed:
         raise ValueError("Operator '%s' failed to load" % name)
     return registration.operator
+
 
 def remove_from_dict(d, key):
     if d is not None and key in d:
