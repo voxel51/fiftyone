1d4a220ffa70ba24f333a642f2a43bf3f0309049
Ritchie Martori <ritchie@voxel51.com>
2023-04-24 09:13:08 -0700

    fixes

--- fiftyone/operators/loader.py
+++ fiftyone/operators/loader.py
@@ -18,16 +18,33 @@ import fiftyone.plugins as fop
 REGISTERED_MODULES = {}
 FAILED_MODULES = {}
 
+class PluginContext:
+    def __init__(self, plugin_definition):
+        self.plugin_definition = plugin_definition
+        self.instances = []
+    
+    def register(self, cls):
+        if self.plugin_definition.can_register(cls):
+            instance = cls()
+            self.instances.append(instance)
+        else:
+            raise ValueError(self.plugin_definition.name + " cannot register " + cls.__name__)
 
-def register_module(filepath, mod, plugin_definition):
+    def unregister_all():
+        for instance in self.instances:
+            unregister(instance)
+
+def register_module(plugin_definition, mod):
     """Registers all operators in the given module."""
-    if REGISTERED_MODULES.get(filepath, None) is not None:
-        unregister_module(filepath)
+    if REGISTERED_MODULES.get(plugin_definition.name, None) is not None:
+        unregister_module(plugin_definition)
     try:
-        mod.register(plugin_definition)
-        REGISTERED_MODULES[filepath] = mod
-        if filepath in FAILED_MODULES:
-            FAILED_MODULES.pop(filepath)
+        pctx = PluginContext(plugin_definition)
+        mod.register(pctx)
+        REGISTERED_MODULES[plugin_definition.name] = pctx
+        if plugin_definition.name in FAILED_MODULES:
+            FAILED_MODULES.pop(plugin_definition.name)
+        return pctx
     except Exception as e:
         errors = [traceback.format_exc()]
         try:
@@ -35,24 +52,24 @@ def register_module(filepath, mod, plugin_definition):
         except Exception as ue:
             errors.append(traceback.format_exc())
             pass
-        FAILED_MODULES[filepath] = errors
+        FAILED_MODULES[plugin_definition.name] = errors
 
 
-def unregister_module(filepath):
+def unregister_module(plugin_name):
     """Unegisters all operators in the given module."""
-    mod = REGISTERED_MODULES.get(filepath, None)
-    if mod is not None:
+    pctx = REGISTERED_MODULES.get(plugin_name, None)
+    if pctx is not None:
         try:
-            mod.unregister()
+            pctx.unregister_all()
         except Exception as e:
             pass
-        del REGISTERED_MODULES[filepath]
+        del REGISTERED_MODULES[plugin_name]
 
 
 def unregister_all():
     """Unregisters all operators."""
-    for filepath, mod in REGISTERED_MODULES:
-        unregister_module(filepath)
+    for name, pctx in REGISTERED_MODULES:
+        unregister_module(plugin_name)
 
 
 def list_module_errors():
@@ -63,9 +80,12 @@ def list_module_errors():
 def load_from_dir():
     """Loads all operators from the default operator directory."""
     plugin_definitions = fop.list_plugins()
+    plugin_contexts = []
     for plugin_definition in plugin_definitions:
         module_dir = plugin_definition.directory
-        exec_module_from_dir(module_dir, plugin_definition)
+        pctx = exec_module_from_dir(module_dir, plugin_definition)
+        plugin_contexts.append(pctx)
+    return plugin_contexts
 
 
 def exec_module_from_dir(module_dir, plugin_definition):
@@ -75,8 +95,7 @@ def exec_module_from_dir(module_dir, plugin_definition):
     mod = importlib.util.module_from_spec(spec)
     sys.modules[mod.__name__] = mod
     spec.loader.exec_module(mod)
-    register_module(mod_filepath, mod, plugin_definition)
-    return mod
+    return register_module(plugin_definition, mod)
 
 
 # BEFORE PR: where should this go?
