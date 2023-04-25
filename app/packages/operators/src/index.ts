cbee6951a293b5c9a38c27a9617f3bcbd7d06338
Ritchie Martori <ritchie@voxel51.com>
2023-04-25 15:43:21 -0700

    operator generators

--- app/packages/operators/src/index.ts
+++ app/packages/operators/src/index.ts
@@ -1,9 +1,8 @@
 export { default as OperatorBrowser } from "./OperatorBrowser";
-export { default as OperatorPrompt } from "./OperatorPrompt";
+export { default as OperatorPrompt, OperatorViewModal } from "./OperatorPrompt";
 export { default as OperatorInvocationRequestExecutor } from "./OperatorInvocationRequestExecutor";
 export { registerOperator, Operator } from "./operators";
 export { useOperatorExecutor, useOperatorPlacements } from "./state";
 export { loadOperators } from "./built-in-operators";
-export { default as OperatorPlacement } from './OperatorPlacement'
-export * as types from "./types"
-
+export { default as OperatorPlacement } from "./OperatorPlacement";
+export * as types from "./types";
