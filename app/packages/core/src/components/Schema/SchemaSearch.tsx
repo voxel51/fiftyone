6e91304aecc96f5ed43cf7eb2b7ce92304521262
manivoxel51 <mani@voxel51@gmail.com>
2023-04-25 17:42:10 -0700

    search box setup

--- app/packages/core/src/components/Schema/SchemaSearch.tsx
+++ app/packages/core/src/components/Schema/SchemaSearch.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Box } from "@mui/material";
 
+import { CodeBlock, useTheme } from "@fiftyone/components";
+import { SchemaSelection } from "./SchemaSelection";
+
 interface Props {
   searchTerm?: string;
   setSearchTerm: (value: string) => void;
@@ -8,15 +11,28 @@ interface Props {
 
 export const SchemaSearch = (props: Props) => {
   const { searchTerm, setSearchTerm } = props;
+  const theme = useTheme();
 
   return (
-    <Box style={{ display: "flex", position: "relative" }}>
-      <input
-        value={searchTerm}
-        placeholder="search by fields and attributes"
-        style={{ color: "#232323", width: "100%" }}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+    <Box
+      style={{ display: "flex", position: "relative", flexDirection: "column" }}
+    >
+      <Box width="100%" paddingTop="0.5rem">
+        <input
+          value={searchTerm}
+          placeholder="search by fields and attributes"
+          style={{
+            color: theme.text.secondary,
+            width: "100%",
+            border: `1px solid ${theme.primary.plainBorder}`,
+            padding: "0.5rem 0.75rem",
+          }}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </Box>
+      <Box width="100%">
+        <SchemaSelection mode="search" />
+      </Box>
     </Box>
   );
 };
