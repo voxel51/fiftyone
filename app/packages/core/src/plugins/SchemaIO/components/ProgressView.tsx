9e2390e2e6b66f8f878edb6b8e2ba30f750c0052
imanjra <ibrahim@voxel51.com>
2023-04-26 18:01:50 -0400

    add progressview

---
+++ app/packages/core/src/plugins/SchemaIO/components/ProgressView.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Box,
+  CircularProgress,
+  LinearProgress,
+  Typography,
+} from "@mui/material";
+
+export default function ProgressView(props) {
+  const { schema, data } = props;
+  const { view = {} } = schema;
+  const { label, variant = "linear" } = view;
+  const progress = parseInt(schema?.default ?? data);
+
+  const progressVariant = isNaN(progress) ? "indeterminate" : "determinate";
+
+  return (
+    <Box sx={{ position: "relative" }}>
+      <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+        <Box width="100%">
+          {variant === "linear" && (
+            <LinearProgress value={progress} variant={progressVariant} />
+          )}
+          {variant === "circular" && (
+            <CircularProgress value={progress} variant={progressVariant} />
+          )}
+        </Box>
+        {!isNaN(progress) && (
+          <Typography
+            color="text.secondary"
+            sx={
+              variant === "circular"
+                ? { position: "absolute", left: 6, top: 10, fontSize: 12 }
+                : {}
+            }
+          >
+            {progress}%
+          </Typography>
+        )}
+      </Box>
+      {label && (
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={variant === "circular" ? {} : { textAlign: "center" }}
+        >
+          {label}
+        </Typography>
+      )}
+    </Box>
+  );
+}
