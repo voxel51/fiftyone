8729224edc2a0489617bb5a8be8a9faa0b19a9fe
kacey <kacey@voxel51.com>
2023-04-26 18:46:19 -0700

    fix api docs main page

--- fiftyone/plugins/__init__.py
+++ fiftyone/plugins/__init__.py
@@ -3,10 +3,27 @@
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
-
+import types
 from .definitions import list_plugins
 from .core import *
+from deprecated import deprecated
 
+
+@deprecated(reason="Use list_plugins() instead")
 def list_all_plugins():
-    """Wrapper for backwards compatibility. Returns a list of all plugins that have not explicitly been disabled."""
+    """Wrapper for backwards compatibility. Returns a list of all plugins that have not explicitly been disabled.
+    .. warning::
+
+            This method is deprecated and will be removed in a future release.
+            Use the drop-in replacement :meth:`list_plugins()` or `list_downloaded_plugins()` instead.
+    """
+
     return list_plugins()
+
+
+# This enables Sphinx refs to directly use paths imported here
+__all__ = [
+    k
+    for k, v in globals().items()
+    if not k.startswith("_") and not isinstance(v, types.ModuleType)
+]
