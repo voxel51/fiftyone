dbb28db5b675d155ef1aec264339df5b8889cd2f
kacey <kacey@voxel51.com>
2023-04-26 19:13:47 -0700

    fix sphinx
    fix comment
    
    fix yaml load

--- fiftyone/plugins/__init__.py
+++ fiftyone/plugins/__init__.py
@@ -4,26 +4,42 @@
 |
 """
 import types
+import deprecated
+import wrapt
+
+from .core import (
+    enable_plugin,
+    disable_plugin,
+    delete_plugin,
+    list_disabled_plugins,
+    list_downloaded_plugins,
+    list_enabled_plugins,
+    find_plugin,
+    download_plugin,
+    create_plugin,
+)
 from .definitions import list_plugins
-from .core import *
-from deprecated import deprecated
 
 
-@deprecated(reason="Use list_plugins() instead")
+@deprecated.deprecated(reason="Use list_plugins() instead")
 def list_all_plugins():
-    """Wrapper for backwards compatibility. Returns a list of all plugins that have not explicitly been disabled.
+    """Wrapper for backwards compatibility. Returns a list of all plugins
+    that have not explicitly been disabled.
+
     .. warning::
 
             This method is deprecated and will be removed in a future release.
-            Use the drop-in replacement :meth:`list_plugins()` or `list_downloaded_plugins()` instead.
+            Use the drop-in replacement :meth:`list_plugins()` instead.
     """
 
     return list_plugins()
 
 
 # This enables Sphinx refs to directly use paths imported here
+# and ignore functions marked as depreciated
 __all__ = [
     k
     for k, v in globals().items()
-    if not k.startswith("_") and not isinstance(v, types.ModuleType)
+    if not k.startswith("_")
+    and not (type(v) in [wrapt.decorators.AdapterWrapper, types.ModuleType])
 ]
