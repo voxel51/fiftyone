ecc7ce2346e3715a80544ade26d89daac1282417
topher <topher@voxel51.com>
2023-04-27 12:52:40 +0000

    1.3.0.dev1

--- package/teams/setup.py
+++ package/teams/setup.py
@@ -12,12 +12,12 @@ import re
 from setuptools import setup
 
 
-VERSION = "1.2.1"
+VERSION = "1.3.0"
 
 
 INSTALL_REQUIRES = [
     "python-jose>=3.3.0,<4",
-    "fiftyone>0.12.0,<0.13",
+    "fiftyone>0.12.0,<0.13.1",
 ]
 
 
