3545032c00b59ca29331734ee9cf586ee872d3c4
imanjra <ibrahim@voxel51.com>
2023-04-28 00:01:26 -0400

    display operator initialization errors in browser

--- app/packages/core/src/plugins/SchemaIO/components/HeaderView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/HeaderView.tsx
@@ -7,10 +7,6 @@ export default function HeaderView(props) {
   const { schema, errors, ...otherProps } = props;
   const { view = {} } = schema;
 
-  const errorsForView = getErrorsForView(props);
-  console.log(props.path);
-  if (errorsForView.length > 0) console.log({ errorsForView });
-
   return (
     <Box>
       <Header errors={getErrorsForView(props)} {...view} {...otherProps} />
