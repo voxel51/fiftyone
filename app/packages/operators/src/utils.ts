edc657168cfe45b265cd086ab5aba6dde11ae28a
imanjra <ibrahim@voxel51.com>
2023-04-28 01:06:44 -0400

    display operator execution errors

---
+++ app/packages/operators/src/utils.ts
@@ -0,0 +1,11 @@
+export function stringifyError(error, fallback?) {
+  if (typeof error === "string") return error;
+  return (
+    error?.stack ||
+    error?.bodyResponse?.stack ||
+    error?.bodyResponse?.toString?.() ||
+    error?.toString?.() ||
+    fallback ||
+    "No details available for the error"
+  );
+}
