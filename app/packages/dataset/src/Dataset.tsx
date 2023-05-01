c91b0640d7bd05d31e746675332dcc3cc9536468
Ritchie Martori <ritchie@voxel51.com>
2023-05-01 12:07:38 -0700

    missing dataset for teams

--- app/packages/dataset/src/Dataset.tsx
+++ app/packages/dataset/src/Dataset.tsx
@@ -22,6 +22,12 @@ import { PreloadedQuery, useQueryLoader, usePreloadedQuery } from "react-relay";
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 import { RecoilRelayEnvironmentProvider } from "recoil-relay";
 import styled from "styled-components";
+import {
+  OperatorBrowser,
+  OperatorInvocationRequestExecutor,
+  OperatorPrompt,
+  OperatorViewModal,
+} from "@fiftyone/operators";
 
 // built-in plugins
 import "@fiftyone/looker-3d";
@@ -131,6 +137,10 @@ export const Dataset: React.FC<DatasetProps> = ({
       </Suspense>
       <div id="modal" />
       <div id="colorModal" />
+      <OperatorBrowser />
+      <OperatorPrompt />
+      <OperatorViewModal />
+      <OperatorInvocationRequestExecutor />
     </Container>
   );
 };
