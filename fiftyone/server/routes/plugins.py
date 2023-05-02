6221246f35f6c316f8c72173f902b0ec8e9377a2
Ritchie Martori <ritchie@voxel51.com>
2023-05-02 13:19:35 -0700

    refactor plugins list api

--- fiftyone/server/routes/plugins.py
+++ fiftyone/server/routes/plugins.py
@@ -15,46 +15,16 @@ import eta.core.serial as etas
 
 import fiftyone as fo
 from fiftyone.server.decorators import route
+from fiftyone.plugins import list_plugins
 
 
 class Plugins(HTTPEndpoint):
     @route
     async def get(self, request: Request, data: dict):
-        plugins_dir = fo.config.plugins_dir
-
-        if not plugins_dir:
-            return {"plugins": [], "settings": None}
-
-        settings_path = os.path.join(plugins_dir, "settings.json")
-        settings = load_json_or_none(settings_path)
-
-        pkgs = glob.glob(os.path.join(plugins_dir, "*", "package.json"))
-        pkgs += glob.glob(
-            os.path.join(plugins_dir, "node_modules", "*", "package.json")
-        )
-        plugin_packages = []
-
-        for filepath in pkgs:
-            pkg = etas.read_json(filepath)
-
-            if "fiftyone" not in pkg or not pkg["fiftyone"].get(
-                "enabled", True
-            ):
-                continue
-
-            plugin_definition = {
-                "name": pkg["name"],
-                "version": pkg["version"],
-            }
-            plugin_definition.update(pkg["fiftyone"])
-            plugin_definition["scriptPath"] = "/" + os.path.join(
-                "plugins",
-                os.path.dirname(os.path.relpath(filepath, plugins_dir)),
-                plugin_definition["script"],
-            )
-            plugin_packages.append(plugin_definition)
-
-        return {"plugins": plugin_packages, "settings": settings}
+        plugin_packages = [
+            plugin_definition.to_json() for plugin_definition in list_plugins()
+        ]
+        return {"plugins": plugin_packages}
 
 
 def load_json_or_none(filepath):
