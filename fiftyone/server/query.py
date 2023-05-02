64803fcc7050d518cf46289439de04f80fbbd35f
Benjamin Kane <ben@tapes.co>
2023-05-02 09:27:02 -0600

    support select fields for saved views

--- fiftyone/server/query.py
+++ fiftyone/server/query.py
@@ -23,6 +23,7 @@ import fiftyone.core.context as focx
 import fiftyone.core.dataset as fod
 import fiftyone.core.media as fom
 from fiftyone.core.odm import SavedViewDocument
+import fiftyone.core.stages as fosg
 from fiftyone.core.state import SampleField, serialize_fields
 import fiftyone.core.uid as fou
 import fiftyone.core.view as fov
@@ -344,7 +345,6 @@ class AppConfig:
 
 @gql.type
 class Query(fosa.AggregateQuery):
-
     aggregations = gql.field(resolver=aggregate_resolver)
 
     @gql.field
@@ -466,6 +466,9 @@ async def serialize_dataset(
             doc = dataset._get_saved_view_doc(saved_view_slug, slug=True)
             view = dataset.load_saved_view(doc.name)
             view_name = view.name
+            if serialized_view:
+                for stage in serialized_view:
+                    view.add_stage(fosg.ViewStage._from_dict(stage))
         except:
             view = fov.DatasetView._build(dataset, serialized_view or [])
 
