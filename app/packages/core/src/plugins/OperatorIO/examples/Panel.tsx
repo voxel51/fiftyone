db60be5dc8043a6bc96320c2b99432bff24a4083
imanjra <ibrahim@voxel51.com>
2023-05-03 12:03:56 -0400

    fix operatorio test panel exception

--- app/packages/core/src/plugins/OperatorIO/examples/Panel.tsx
+++ app/packages/core/src/plugins/OperatorIO/examples/Panel.tsx
@@ -9,7 +9,7 @@ import { errors as inputErrors, schema as inputSchema } from "./input.json";
 import { data, schema as outputSchema } from "./output.json";
 
 // Panel enabled only in development environment for testing and debugging SchemaIO
-if (import.meta.env.MODE === "development") {
+if (import.meta.env?.MODE === "development") {
   registerComponent({
     name: "OperatorIO",
     label: "OperatorIO",
