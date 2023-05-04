e01ba558e42697ed8999a72a32445468e18f4636
Benjamin Kane <ben@tapes.co>
2023-05-04 15:49:45 -0600

    0.0.0.0

--- e2e/lib/constants.ts
+++ e2e/lib/constants.ts
@@ -4,5 +4,5 @@ import { Duration } from "../cypress/support/utils";
 export const DEFAULT_APP_LOAD_TIMEOUT = Duration.Minutes(2);
 
 export const DEFAULT_APP_PORT = 8787;
-export const DEFAULT_APP_HOSTNAME = "localhost";
+export const DEFAULT_APP_HOSTNAME = "0.0.0.0";
 export const DEFAULT_APP_ADDRESS = `http://${DEFAULT_APP_HOSTNAME}:${DEFAULT_APP_PORT}`;
