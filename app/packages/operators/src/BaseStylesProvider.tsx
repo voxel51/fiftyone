9346304b3980bb76feb74d55e46ae0695336f117
imanjra <ibrahim@voxel51.com>
2023-05-04 18:07:57 -0400

    fix operator prompt styles

---
+++ app/packages/operators/src/BaseStylesProvider.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const BaseStylesProvider = styled.div`
+  color: var(--fo-palette-text-primary);
+  font-family: "Palanquin", sans-serif;
+  font-size: 14px;
+
+  input {
+    color: var(--fo-palette-text-primary);
+  }
+`;
+
+export default BaseStylesProvider;
