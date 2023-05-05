d16af44c7257cd3c0b7a972a05f1780e77ad2844
imanjra <ibrahim@voxel51.com>
2023-05-05 00:14:48 -0400

    more operator views bug fixes

--- app/packages/core/src/plugins/OperatorIO/examples/Panel.tsx
+++ app/packages/core/src/plugins/OperatorIO/examples/Panel.tsx
@@ -5,7 +5,11 @@ import React, { useEffect, useState } from "react";
 import { SchemaIOComponent } from "../../SchemaIO";
 import { TabsView } from "../../SchemaIO/components";
 import { log, operatorToIOSchema } from "../utils";
-import { errors as inputErrors, schema as inputSchema } from "./input.json";
+import {
+  errors as inputErrors,
+  schema as inputSchema,
+  simpleSchema,
+} from "./input.json";
 import { data, schema as outputSchema } from "./output.json";
 
 // Panel enabled only in development environment for testing and debugging SchemaIO
@@ -25,18 +29,20 @@ function OperatorIO() {
   const ioSchema = operatorToIOSchema(input);
   const output = types.Property.fromJSON(outputSchema);
   const oSchema = operatorToIOSchema(output, { isOutput: true });
+  const simple = types.Property.fromJSON(simpleSchema);
+  const sSchema = operatorToIOSchema(simple);
   const [state, setState] = useState(data);
 
   useEffect(() => {
     if (mode === "output") {
-      if (state.linear < 100) {
+      if (state.linear < 1) {
         setTimeout(() => {
-          setState((state) => ({ ...state, linear: state.linear + 5 }));
+          setState((state) => ({ ...state, linear: state.linear + 0.05 }));
         }, 500);
       }
-      if (state.circular < 100) {
+      if (state.circular < 1) {
         setTimeout(() => {
-          setState((state) => ({ ...state, circular: state.circular + 5 }));
+          setState((state) => ({ ...state, circular: state.circular + 0.05 }));
         }, 500);
       }
     }
@@ -46,13 +52,17 @@ function OperatorIO() {
     <Box sx={{ p: 4 }}>
       <Box sx={{ pb: 2 }}>
         <TabsView
-          onChange={(path, mode) => setMode(mode)}
+          onChange={(path, mode) => {
+            setMode(mode);
+            setState({ ...state, linear: 0, circular: 0 });
+          }}
           schema={{
             default: "input",
             view: {
               choices: [
                 { value: "input", label: "Input" },
                 { value: "output", label: "Output" },
+                { value: "inferred-input", label: "Inferred Input" },
               ],
             },
           }}
@@ -67,6 +77,9 @@ function OperatorIO() {
       )}
       {mode === "output" && (
         <SchemaIOComponent schema={oSchema} onChange={log} data={state} />
+      )}
+      {mode === "inferred-input" && (
+        <SchemaIOComponent schema={sSchema} onChange={log} />
       )}
     </Box>
   );
