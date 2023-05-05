40601a68cf43bd89a2ba4c824ddbfbcffe21d533
imanjra <ibrahim@voxel51.com>
2023-05-05 00:14:48 -0400

    more operator views bug fixes

--- app/packages/core/src/plugins/SchemaIO/components/InferredView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/InferredView.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { generateSchema, log } from "../utils";
+import { generateSchema } from "../utils";
 import DynamicIO from "./DynamicIO";
 
-// todo: add support for input
 export default function InferredView(props) {
-  const { data, schema, onChange } = props;
-  const generatedSchema = generateSchema(data, { label: schema?.view?.label });
+  const { data, schema } = props;
+  const readOnly = schema?.readOnly || schema?.view?.readOnly;
+  const generatedSchema = generateSchema(data || schema?.default, {
+    label: schema?.view?.label,
+    readOnly,
+  });
+
   return <DynamicIO {...props} schema={generatedSchema} />;
 }
