bb271d5853eb637c393c180b35a3e3be23c49483
brimoor <brimoor@umich.edu>
2023-05-09 22:34:10 -0400

    adding fiftyone-db build for Ubuntu 22.04

--- package/db/setup.py
+++ package/db/setup.py
@@ -40,6 +40,9 @@ MONGODB_DOWNLOAD_URLS = {
     "ubuntu2004": {
         "manylinux1_x86_64": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.4.tgz",
     },
+    "ubuntu2204": {
+        "manylinux1_x86_64": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.5.tgz",
+    },
 }
 
 # mongodb binaries to distribute
