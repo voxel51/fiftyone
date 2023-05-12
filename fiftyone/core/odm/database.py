fe84da13c39d291afa6ce3a32b1c311553bec272
brimoor <brimoor@umich.edu>
2023-05-11 20:56:50 -0400

    restrict patching to correct Run class

--- fiftyone/core/odm/database.py
+++ fiftyone/core/odm/database.py
@@ -34,7 +34,11 @@ import fiftyone.core.utils as fou
 
 from .document import Document
 
+foa = fou.lazy_import("fiftyone.core.annotation")
+fob = fou.lazy_import("fiftyone.core.brain")
 fod = fou.lazy_import("fiftyone.core.dataset")
+foe = fou.lazy_import("fiftyone.core.evaluation")
+
 
 logger = logging.getLogger(__name__)
 
@@ -870,6 +874,7 @@ def patch_annotation_runs(dataset_name, dry_run=False):
     _patch_runs(
         dataset_name,
         "annotation_runs",
+        foa.AnnotationMethod,
         "annotation run",
         dry_run=dry_run,
     )
@@ -887,6 +892,7 @@ def patch_brain_runs(dataset_name, dry_run=False):
     _patch_runs(
         dataset_name,
         "brain_methods",
+        fob.BrainMethod,
         "brain method run",
         dry_run=dry_run,
     )
@@ -901,10 +907,16 @@ def patch_evaluations(dataset_name, dry_run=False):
         dry_run (False): whether to log the actions that would be taken but not
             perform them
     """
-    _patch_runs(dataset_name, "evaluations", "evaluation", dry_run=dry_run)
+    _patch_runs(
+        dataset_name,
+        "evaluations",
+        foe.EvaluationMethod,
+        "evaluation",
+        dry_run=dry_run,
+    )
 
 
-def _patch_runs(dataset_name, runs_field, run_str, dry_run=False):
+def _patch_runs(dataset_name, runs_field, run_cls, run_str, dry_run=False):
     conn = get_db_conn()
     _logger = _get_logger(dry_run=dry_run)
 
@@ -922,7 +934,9 @@ def _patch_runs(dataset_name, runs_field, run_str, dry_run=False):
     rd = {}
     for run_dict in conn.runs.find({"_dataset_id": dataset_id}):
         try:
-            rd[run_dict["key"]] = run_dict["_id"]
+            cls = etau.get_class(run_dict["config"]["cls"][: -len("Config")])
+            if issubclass(cls, run_cls):
+                rd[run_dict["key"]] = run_dict["_id"]
         except:
             pass
 
