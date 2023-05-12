87295d1336c85c3b661e2ff6c402b1eb5b7eb76f
Lanny W <lanzhenwang9@gmail.com>
2023-05-12 09:56:20 -0500

    video frames sidebar filter color inconsistency

--- app/packages/state/src/recoil/color.ts
+++ app/packages/state/src/recoil/color.ts
@@ -82,9 +82,15 @@ export const pathColor = selectorFamily<
     ({ modal, path }) =>
     ({ get }) => {
       // video path tweak
-      const adjustedPath = path.startsWith("frames.")
-        ? path.slice("frames.".length)
-        : path;
+      const video = get(selectors.mediaTypeSelector) !== "image";
+      const parentPath =
+        video && path.startsWith("frames.")
+          ? path.split(".").slice(0, 2).join(".")
+          : path.split(".")[0];
+      const adjustedPath = parentPath.startsWith("frames.")
+        ? parentPath.slice("frames.".length)
+        : parentPath;
+
       const setting = get(
         atoms.sessionColorScheme
       )?.customizedColorSettings?.find((x) => x.field === adjustedPath);
@@ -94,12 +100,6 @@ export const pathColor = selectorFamily<
       }
 
       const map = get(colorMap(modal));
-      const video = get(selectors.mediaTypeSelector) !== "image";
-
-      const parentPath =
-        video && path.startsWith("frames.")
-          ? path.split(".").slice(0, 2).join(".")
-          : path.split(".")[0];
 
       if (get(schemaAtoms.labelFields({})).includes(parentPath)) {
         return map(parentPath);
