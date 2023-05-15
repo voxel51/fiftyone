0028545f3085c1c75036bc5823a673ade541f2de
Ritchie Martori <ritchie@voxel51.com>
2023-05-15 13:24:37 -0700

    removed unused imports

--- fiftyone/operators/__init__.py
+++ fiftyone/operators/__init__.py
@@ -4,12 +4,9 @@
 |
 """
 
-import types
-import wrapt
 from .operator import Operator, OperatorConfig
 from .registry import register_operator
 from .executor import execute_operator
 
 # This enables Sphinx refs to directly use paths imported here
-# and ignore functions marked as depreciated
 __all__ = [k for k, v in globals().items() if not k.startswith("_")]
