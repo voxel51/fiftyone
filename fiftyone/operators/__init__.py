e4078ebf351d36b748d2f10c03119c423afcc762
brimoor <brimoor@umich.edu>
2023-05-18 10:54:31 -0400

    list operators

--- fiftyone/operators/__init__.py
+++ fiftyone/operators/__init__.py
@@ -5,15 +5,9 @@ FiftyOne operators.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
-import types
-
 from .operator import Operator, OperatorConfig
 from .registry import OperatorRegistry, register_operator
 from .executor import execute_operator
 
 # This enables Sphinx refs to directly use paths imported here
-__all__ = [
-    k
-    for k, v in globals().items()
-    if not k.startswith("_") and not isinstance(v, types.ModuleType)
-]
+__all__ = [k for k, v in globals().items() if not k.startswith("_")]
