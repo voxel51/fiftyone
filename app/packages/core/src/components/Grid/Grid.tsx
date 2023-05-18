447d4b89dfb1cdbae52294b7130f1cd69e6ef198
Lanny W <lanzhenwang9@gmail.com>
2023-05-18 13:47:08 -0500

    tried to update on grid and modal

--- app/packages/core/src/components/Grid/Grid.tsx
+++ app/packages/core/src/components/Grid/Grid.tsx
@@ -1,10 +1,4 @@
-import React, {
-  Suspense,
-  useEffect,
-  useLayoutEffect,
-  useMemo,
-  useRef,
-} from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { useRecoilCallback, useRecoilValue } from "recoil";
 import { v4 as uuid } from "uuid";
 
@@ -20,7 +14,6 @@ import useResize from "./useResize";
 
 import * as fos from "@fiftyone/state";
 import { deferrer, stringifyObj } from "@fiftyone/state";
-import EmptySamples from "../EmptySamples";
 
 const Grid: React.FC<{}> = () => {
   const [id] = React.useState(() => uuid());
@@ -141,6 +134,19 @@ const Grid: React.FC<{}> = () => {
     }),
     [lookerOptions, selected]
   );
+
+  // useEffect(
+  //   deferred(() => {
+  //     // flashlight.updateItems((sampleId) => {
+  //     //   const sample = store.samples.get(sampleId);
+  //     //   store.lookers.get(sampleId)?.updateSample(sample);
+  //     // });
+  //     store.samples.forEach((sample, sampleId) => {
+  //       store.lookers.get(sampleId)?.updateSample(sample);
+  //     });
+  //   }),
+  //   [useRecoilValue(fos.sessionColorScheme)]
+  // );
 
   useLayoutEffect(() => {
     flashlight.attach(id);
