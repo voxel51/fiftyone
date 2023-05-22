9edcd95ebf52f6a3e0d454d657e58dfff5042d0b
Ritchie Martori <ritchie@voxel51.com>
2023-05-22 12:38:28 -0700

    improved markdown

--- app/packages/core/src/plugins/SchemaIO/components/MarkdownView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/MarkdownView.tsx
@@ -1,10 +1,23 @@
-import React from "react";
-import { Typography, Box, Link } from "@mui/material";
+import React, { useCallback } from "react";
+import { Typography, Box, Link, Button } from "@mui/material";
 import ReactMarkdown from "react-markdown";
 import CodeView from "./CodeView";
 import { HeaderView } from ".";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import { getComponentProps } from "../utils";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
+import ts from "react-syntax-highlighter/dist/esm/languages/hljs/typescript";
+import python from "react-syntax-highlighter/dist/esm/languages/hljs/python";
+import tomorrow from "react-syntax-highlighter/dist/esm/styles/hljs/tomorrow";
+import vs2015 from "react-syntax-highlighter/dist/esm/styles/hljs/vs2015";
+import CopyIcon from "@mui/icons-material/ContentCopy";
+import CheckIcon from "@mui/icons-material/DoneOutlined";
+import copyToClipboard from "copy-to-clipboard";
+
+SyntaxHighlighter.registerLanguage("javascript", js);
+SyntaxHighlighter.registerLanguage("typescript", ts);
+SyntaxHighlighter.registerLanguage("python", python);
 
 const InlineCode = styled.span`
   background: ${({ theme }) => theme.background.level1};
@@ -15,23 +28,72 @@ const InlineCode = styled.span`
   font-family: Roboto Mono, monospace;
 `;
 
+const CodeContainer = styled(Box)`
+  border: 1px solid ${({ theme }) => theme.background.level1};
+  pre {
+    margin: 0;
+    padding: 1rem !important;
+  }
+  border-radius: 3px;
+`;
+
+const CodeHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0rem 1rem;
+  border-bottom: 1px solid ${({ theme }) => theme.background.level1};
+  background: ${({ theme }) => theme.background.level2};
+`;
+
+const CopyButton = ({ content, children }) => {
+  const [copied, setCopied] = React.useState(false);
+  const handleClick = useCallback(() => {
+    copyToClipboard(content);
+    setCopied(true);
+  }, [children, copyToClipboard]);
+
+  if (copied) {
+    return <Button startIcon={<CheckIcon />}>Copied!</Button>;
+  }
+
+  return (
+    <Button startIcon={<CopyIcon />} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+};
+
 const componenntMap = {
   a({ children, ...props }) {
     return <Link {...props}>{children}</Link>;
   },
   code({ node, inline, className, children, ...props }) {
+    const theme = useTheme();
+    const isDarkMode = theme.mode === "dark";
+    const highlightTheme = isDarkMode ? vs2015 : tomorrow;
     const match = /language-(\w+)/.exec(className || "");
-    const language = match ? match[1] : "text";
+    let language = match ? match[1] : "text";
+    if (language === "js") {
+      language = "javascript";
+    }
+    if (language === "ts") {
+      language = "typescript";
+    }
+    if (language === "py") {
+      language = "python";
+    }
     return !inline && match ? (
-      <CodeView
-        schema={{
-          view: {
-            language,
-            height: "auto",
-          },
-        }}
-        data={String(children).replace(/\n$/, "")}
-      />
+      <CodeContainer>
+        <CodeHeader>
+          <Typography variant="caption">{language}</Typography>
+          <CopyButton content={children}>Copy Code</CopyButton>
+        </CodeHeader>
+
+        <SyntaxHighlighter language={language} style={highlightTheme}>
+          {children}
+        </SyntaxHighlighter>
+      </CodeContainer>
     ) : (
       <InlineCode className={className} {...props}>
         {children}
