7dd8afd116cf2444cc51c5861f41d0fb9cb9bd90
Sashank Aryal <sashank2012@gmail.com>
2023-05-24 10:15:26 -0500

    add command for removing view stages

--- e2e/cypress/support/commands.ts
+++ e2e/cypress/support/commands.ts
@@ -15,12 +15,28 @@ Cypress.Commands.add("executePythonCode", (sourceCode) =>
 );
 
 Cypress.Commands.add("waitForGridToBeVisible", (datasetName?: string) => {
-  if (datasetName) {
-    cy.visit(`/datasets/${datasetName}`);
-  } else {
+  const forceDatasetFromSelector = () => {
     cy.visit("/");
     cy.get(`[data-cy="selector-Select dataset"]`).click();
-    cy.get("[data-cy=selector-result]").first().click();
+
+    if (datasetName) {
+      cy.get(`[data-cy=selector-result-${datasetName}]`).click();
+    } else {
+      cy.get(`[data-cy^="selector-result"]`).first().click();
+    }
+  };
+
+  if (!datasetName) {
+    forceDatasetFromSelector();
+  } else {
+    cy.visit(`/datasets/${datasetName}`).then(() => {
+      const location = window.location.href;
+
+      // behavior of directly visiting the dataset page is sometimes flaky
+      if (!location.includes("datasets")) {
+        forceDatasetFromSelector();
+      }
+    });
   }
 
   cy.get("[data-cy=fo-grid]").should("be.visible");
@@ -32,6 +48,11 @@ Cypress.Commands.add(
     cy.wait(timeout ?? Duration.LOOKER_RENDER_MAX_TIME_MS);
   }
 );
+
+Cypress.Commands.add("clearViewStages", () => {
+  // chaining with root in case modal is open and ctx is within modal
+  cy.root().find("[data-cy=btn-clear-view-bar]").click();
+});
 
 Cypress.Commands.add("consoleLog", (message) => {
   cy.task("logTask", message);
