bbbc449834f22a1b2765484d03d1b2c7a27b3e27
Benjamin Kane <ben@tapes.co>
2023-05-30 17:02:09 -0400

    null

--- app/packages/looker/src/elements/common/tags.ts
+++ app/packages/looker/src/elements/common/tags.ts
@@ -435,8 +435,8 @@ const getFieldAndValue = (
       field &&
       field.embeddedDocType === withPath(LABELS_PATH, CLASSIFICATIONS)
     ) {
-      value = value["classifications"] || [];
-      field = field.fields["classifications"];
+      value = value?.["classifications"] || [];
+      field = field.fields?.["classifications"];
       break;
     }
 
