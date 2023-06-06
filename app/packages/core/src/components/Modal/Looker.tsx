43509c195bd83de138eb4ca8bdcc7532f3549b2f
Sashank Aryal <sashank2012@gmail.com>
2023-06-06 15:25:31 -0500

    fix urls bug

--- app/packages/core/src/components/Modal/Looker.tsx
+++ app/packages/core/src/components/Modal/Looker.tsx
@@ -88,12 +88,13 @@ const Looker = ({
     if (propsSampleData) {
       return {
         ...modalSampleData,
+        urls,
         sample: propsSampleData,
       };
     }
 
     return modalSampleData;
-  }, [propsSampleData, modalSampleData]);
+  }, [propsSampleData, modalSampleData, urls]);
 
   const { sample } = sampleData;
 
