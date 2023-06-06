d65261183d36477e9d02de6636faa64c7d85e697
Sashank Aryal <sashank2012@gmail.com>
2023-06-06 15:32:22 -0500

    account for query param in src

--- app/packages/looker/src/elements/pcd.ts
+++ app/packages/looker/src/elements/pcd.ts
@@ -34,7 +34,8 @@ export class PcdElement extends BaseElement<PcdState, HTMLImageElement> {
 
   renderSelf({ config: { src } }: Readonly<PcdState>) {
     if (this.src !== src) {
-      if (src.endsWith(".pcd")) {
+      // split("?")[0] is to remove query params, if any, from signed urls
+      if (src.split("?")[0].endsWith(".pcd")) {
         // this means orthographic projections have not been computed, replace with a 1x1 base64 blank image
         const base64Blank =
           "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4AWNgYGD4DwABDQF/w6a5YwAAAABJRU5ErkJggg==";
