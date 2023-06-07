58f79e18d319c17173a9a3f8be59e279fd6c9dc0
Sashank Aryal <sashank2012@gmail.com>
2023-06-07 18:28:18 -0500

    catch known errors

--- e2e/cypress/support/e2e.ts
+++ e2e/cypress/support/e2e.ts
@@ -1,6 +1,31 @@
 // this file (default support file) runs before every test
 import compareSnapshotCommand from "cypress-visual-regression/dist/command";
 
+Cypress.on("uncaught:exception", (err, runnable, promise) => {
+  // returning false here prevents Cypress from failing the test
+
+  // occasionally get this error when clearing the view stages
+  if (err.message.includes("Unable to evaluate guard")) {
+    console.log(`caught known error: ${err.message}`);
+    cy.clearViewStages();
+    return false;
+  }
+
+  // occasionally get this error from react-three-fiber library
+  if (
+    err.message.includes(
+      "Cannot read properties of null (reading 'addEventListener')"
+    )
+  ) {
+    console.log(`caught known error: ${err.message}`);
+    return false;
+  }
+
+  console.log(`unknown error: ${err.message}`);
+
+  return true;
+});
+
 import "./commands";
 
 compareSnapshotCommand({
