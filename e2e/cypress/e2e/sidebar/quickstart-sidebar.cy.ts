ad39529b7681f9a2d4c535d48460c661806b55aa
Lanny W <lanzhenwang9@gmail.com>
2023-06-08 22:55:00 -0500

    primitive field filter test

---
+++ e2e/cypress/e2e/sidebar/quickstart-sidebar.cy.ts
@@ -0,0 +1,168 @@
+/**
+ * This test suite validates that sidebar filtering functionality works in the fiftyone app with quickstart dataset.
+ */
+
+describe("Sidebar filter", () => {
+  context("quickstart dataset filter", () => {
+    before(() => {
+      cy.executePythonCode(
+        `
+            import fiftyone as fo
+            import fiftyone.zoo as foz
+            quickstart_dataset = foz.load_zoo_dataset("quickstart", max_samples=50, dataset_name="quickstart-test")
+            quickstart_dataset.persistent = True
+          `
+      );
+    });
+
+    beforeEach(() => {
+      cy.waitForGridToBeVisible("quickstart-test");
+    });
+
+    it("should show all default sidebar groups by names", () => {
+      const defaultGroups = ["TAGS", "METADATA", "LABELS", "PRIMITIVES"];
+      defaultGroups.forEach((g) => {
+        cy.get(`[data-cy=sidebar-group-${g}]`).should("exist");
+      });
+    });
+
+    it("should be able to filter by ground_truth label", () => {
+      // ground_truth selector should be visible and expandable
+      cy.get("[data-cy=sidebar-field-ground_truth]").should("be.visible");
+      cy.get("[data-cy=sidebar-field-arrow-ground_truth]").should("be.visible");
+      cy.get("[data-cy=sidebar-field-arrow-ground_truth]").click();
+
+      // entry count should be 199 before filtering
+      cy.contains("199", { timeout: 5000 }).should("be.visible");
+      cy.get("[data-cy=sidebar-field-container-ground_truth").within(() => {
+        cy.get("[data-cy=entry-count-all]").should("have.text", 199);
+
+        // select "cat" label, expect 7 samples out 50 samples
+        // cy.get('[data-cy=selector-+ filter by label]').first().should('exist');
+      });
+    });
+
+    it("should be able to filter by primitive field", () => {
+      cy.get("[data-cy=sidebar-field-uniqueness]").should("be.visible");
+      cy.get("[data-cy=sidebar-field-arrow-uniqueness]").should("be.visible");
+      cy.get("[data-cy=sidebar-field-arrow-uniqueness]").click();
+      // turn on the uniqueness tags on looker
+      cy.get("[data-cy=checkbox-uniqueness]").click();
+      // wait for the filter to expand
+      cy.contains("0.54", { timeout: 5000 }).should("be.visible");
+
+      // move slider's min to 0.90
+      cy.get(".MuiSlider-root")
+        .first()
+        .within(() => {
+          cy.get("input").first().should("have.value", "0.540524359851793");
+          cy.get("input").last().should("have.value", "1");
+          cy.get("input")
+            .first()
+            .invoke("val", 0.9)
+            .trigger("input", { force: true });
+        });
+      // grid aggregation summary
+      cy.get("[data-cy=entry-counts]").should("have.text", "2 of 50 samples");
+      // field aggregation summary
+      cy.get("[data-cy=entry-count-part]")
+        .last()
+        .should("have.text", "2 of 50");
+
+      // check the tags on looker of filtered samples
+      cy.get("[data-cy=tag-uniqueness]").each(($el) => {
+        // get the text of the element and convert it into a number
+        const value = Number($el.text());
+        // assert that the value is greater than 1
+        expect(value).to.be.gt(0.9);
+      });
+
+      // primitive fields should have match/omit two options
+
+      // verify omit option results
+    });
+
+    it("should be able to use tagger with no samples selected", () => {
+      // verify sample tags number and label tags number on the tagger popout
+      // tag all labels as "<0.90"
+      // after grids refresh, verify the new tag and its count on label tags
+      // tag all samples in the dataset as "all"
+      // after grids refresh, verify the new sample tags are created
+    });
+  });
+
+  context("quickstart-video dataset filter", () => {
+    before(() => {
+      cy.executePythonCode(
+        `
+              import fiftyone as fo
+              import fiftyone.zoo as foz
+              quickstart_dataset = foz.load_zoo_dataset("quickstart-video", max_samples=5, dataset_name="quickstart-video-test")
+              quickstart_dataset.persistent = True
+            `
+      );
+    });
+
+    beforeEach(() => {
+      cy.waitForGridToBeVisible("quickstart-video");
+    });
+
+    it("should show all default sidebar groups by names", () => {
+      const defaultGroups = ["TAGS", "METADATA", "LABELS", "PRIMITIVES"];
+      defaultGroups.forEach((g) => {
+        cy.get(`[data-cy=sidebar-group-${g}]`).should("exist");
+      });
+    });
+
+    // it("should be able to filter by ground_truth label", () => {
+    //   // ground_truth selector should be visible and expandable
+    //   cy.get("[data-cy=sidebar-field-ground_truth]").should("be.visible");
+    //   cy.get("[data-cy=sidebar-field-arrow-ground_truth]").should("be.visible");
+    //   cy.get("[data-cy=sidebar-field-arrow-ground_truth]").click();
+
+    //   // entry count should be 199 before filtering
+    //   cy.contains('199', {timeout: 5000}).should('be.visible');
+    //   cy.get("[data-cy=sidebar-field-container-ground_truth").within(() => {
+    //   cy.get("[data-cy=entry-count-all]").should("have.text", 199);
+
+    //   // select "cat" label, expect 7 samples out 50 samples
+    //   cy.get('[data-cy=selector-+ filter by label]').first().should('exist');
+    //   })
+    // })
+
+    // it("should be able to filter by primitive field", () => {
+    //   cy.get("[data-cy=sidebar-field-uniqueness]").should("be.visible");
+    //   cy.get("[data-cy=sidebar-field-arrow-uniqueness]").should("be.visible");
+    //   cy.get("[data-cy=sidebar-field-arrow-uniqueness]").click();
+    //   // turn on the uniqueness tags on looker
+    //   cy.get("[data-cy=checkbox-uniqueness]").click()
+    //   // wait for the filter to expand
+    //   cy.contains('0.54', {timeout: 5000}).should('be.visible');
+
+    //   // move slider's min to 0.90
+    //   cy.get(".MuiSlider-root").first().within(() => {
+    //       cy.get("input").first().should("have.value", "0.540524359851793");
+    //       cy.get("input").last().should("have.value", "1");
+    //       cy.get("input").first().invoke('val', 0.90).trigger("input", {force: true})
+    //   })
+    //   // grid aggregation summary
+    //   cy.get("[data-cy=entry-counts]").should("have.text", "2 of 50 samples");
+    //   // field aggregation summary
+    //   cy.get("[data-cy=entry-count-part]").last().should("have.text", "2 of 50")
+
+    //   // check the tags on looker of filtered samples
+    //   cy.get("[data-cy=tag-uniqueness]").each(($el) => {
+    //       // get the text of the element and convert it into a number
+    //       const value = Number($el.text());
+    //       // assert that the value is greater than 1
+    //       expect(value).to.be.gt(0.90);
+    //   });
+
+    //   // check filter options
+
+    //   // omit option
+
+    //   // reset
+    // })
+  });
+});
