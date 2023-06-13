eb7b80b6a255a09fe9c2b5a4be8d97b6ce3e5c79
manivoxel51 <mani@voxel51@gmail.com>
2023-06-13 12:05:19 -0700

    disable field unit testing

--- app/packages/utilities/src/index.ts
+++ app/packages/utilities/src/index.ts
@@ -380,6 +380,14 @@ export const CLIPS_FRAME_FIELDS = withPath(LABELS_PATH, [
   "Polylines",
 ]);
 
+export const DISABLED_PATHS = ["id", "filepath", "tags", "metadata"];
+export const DISABLED_FIELD_TYPES = [
+  OBJECT_ID_FIELD,
+  FRAME_NUMBER_FIELD,
+  FRAME_SUPPORT_FIELD,
+  VECTOR_FIELD,
+];
+
 export function withPath(path: string, types: string): string;
 export function withPath(path: string, types: string[]): string[];
 export function withPath(
