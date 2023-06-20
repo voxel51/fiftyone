1a413997d5b5f2e81cd46e4278cc5eb5242a3f13
manivoxel51 <mani@voxel51@gmail.com>
2023-06-20 16:52:16 -0700

    schemaSearchResultList test complete

--- app/packages/state/src/recoil/schemaSettings.atoms.test.ts
+++ app/packages/state/src/recoil/schemaSettings.atoms.test.ts
@@ -3,6 +3,8 @@ import { snapshot_UNSTABLE } from "../../../../__mocks__/recoil";
 import * as ss from "./schemaSettings.atoms";
 import { FIELDS } from "../hooks/useSchemaSettings.utils.test";
 
+const NON_EXISTENT_PATH = "non-existent-path";
+
 describe("schema search", () => {
   afterEach(() => {
     vi.restoreAllMocks();
@@ -27,5 +29,24 @@ describe("schema search", () => {
     ).contents;
 
     expect(contents).toEqual([FIELDS.ID_FIELD.path]);
+  });
+
+  it("should return an emoty array when searching for non-existent field path", () => {
+    const schemaFields = {
+      [FIELDS.ID_FIELD.path]: FIELDS.ID_FIELD,
+      [FIELDS.FILEPATH_FIELD.path]: FIELDS.FILEPATH_FIELD,
+    };
+
+    const testSnapshot = snapshot_UNSTABLE(({ set }) => {
+      set(ss.viewSchemaState, {});
+      set(ss.fieldSchemaState, schemaFields);
+      set(ss.schemaSearchResultList([NON_EXISTENT_PATH]), [NON_EXISTENT_PATH]);
+    });
+
+    const contents = testSnapshot.getLoadable(
+      ss.schemaSearchResultList([])
+    ).contents;
+
+    expect(contents).toEqual([]);
   });
 });
