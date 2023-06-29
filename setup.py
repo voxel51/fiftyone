2c9c8c40f232c4b0f9031d7c64269aed1d326514
Benjamin Kane <ben@tapes.co>
2023-06-29 14:22:23 -0600

    tweak desktop req

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.18.0,<0.20"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.18.0,<0.19"]}
 
 
 with open("README.md", "r") as fh:
