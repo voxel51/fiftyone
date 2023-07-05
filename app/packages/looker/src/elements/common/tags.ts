9ce3c597d379cbf5935331d3b3fa26e1cb98f73a
Lanny W <lanzhenwang9@gmail.com>
2023-07-05 11:35:04 -0500

    fix list_string and sample tags

--- app/packages/looker/src/elements/common/tags.ts
+++ app/packages/looker/src/elements/common/tags.ts
@@ -272,31 +272,35 @@ export class TagsElement<State extends BaseState> extends BaseElement<State> {
       if (path === "tags") {
         if (Array.isArray(sample.tags)) {
           sample.tags.forEach((tag) => {
-            const v = coloring.by === "value" ? tag : "tags";
-            elements.push({
-              color: getColorFromOptions({
-                coloring,
-                path,
-                param: tag,
-                customizeColorSetting,
-                labelDefault: false,
-              }),
-              title: tag,
-              value: tag,
-              path: v,
-            });
+            if (filter(path, tag)) {
+              const v = coloring.by === "value" ? tag : "tags";
+              elements.push({
+                color: getColorFromOptions({
+                  coloring,
+                  path,
+                  param: tag,
+                  customizeColorSetting,
+                  labelDefault: false,
+                }),
+                title: tag,
+                value: tag,
+                path: v,
+              });
+            }
           });
         }
       } else if (path === "_label_tags") {
         Object.entries(sample._label_tags ?? {}).forEach(([tag, count]) => {
           const value = `${tag}: ${count}`;
           const v = coloring.by === "value" ? tag : path;
-          elements.push({
-            color: getColor(coloring.pool, coloring.seed, v),
-            title: value,
-            value: value,
-            path: v,
-          });
+          if (filter(path, tag)) {
+            elements.push({
+              color: getColor(coloring.pool, coloring.seed, v),
+              title: value,
+              value: value,
+              path: v,
+            });
+          }
         });
       } else {
         const [field, value] = getFieldAndValue(sample, fieldSchema, path);
@@ -335,8 +339,8 @@ export class TagsElement<State extends BaseState> extends BaseElement<State> {
         if (value === undefined) continue;
         if (field && LABEL_RENDERERS[field.embeddedDocType]) {
           Array.isArray(value)
-            ? pushList(LABEL_RENDERERS[field.embeddedDocType], value)
-            : elements.push(
+            ? filter(path, value) && pushList(LABEL_RENDERERS[field.embeddedDocType], value)
+            : filter(path, value) && elements.push(
                 LABEL_RENDERERS[field.embeddedDocType](path, value)
               );
 
@@ -355,7 +359,15 @@ export class TagsElement<State extends BaseState> extends BaseElement<State> {
           field.ftype === LIST_FIELD &&
           PRIMITIVE_RENDERERS[field.subfield]
         ) {
-          pushList(PRIMITIVE_RENDERERS[field.subfield], value);
+          // there may be visibility settings
+          const visibleValue = [];
+          value?.forEach((v) => {
+            if (filter(path, v)) {
+              visibleValue.push(v);
+            }
+          });
+
+          pushList(PRIMITIVE_RENDERERS[field.subfield], visibleValue);
           continue;
         }
       }
@@ -380,9 +392,7 @@ export class TagsElement<State extends BaseState> extends BaseElement<State> {
         div.title = title;
         div.style.backgroundColor = color;
         div.setAttribute("data-cy", `tag-${path}`);
-      if (filter(path, value)) {
-          this.element.appendChild(div);
-      }
+        this.element.appendChild(div);
       });
 
     return this.element;
