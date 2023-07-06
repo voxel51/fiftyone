f0427d5e27c1f458d79d2a469d47b9d9819ad601
Lanny W <lanzhenwang9@gmail.com>
2023-07-05 23:41:32 -0500

    make sidebar search filter sticky

--- app/packages/core/src/components/Sidebar/Entries/utils.tsx
+++ app/packages/core/src/components/Sidebar/Entries/utils.tsx
@@ -8,6 +8,33 @@ export const InputDiv = styled.div`
   font-weight: bold;
   user-select: none;
   padding-top: 2px;
+  margin: 0 0.25rem 0 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  & > input {
+    color: ${({ theme }) => theme.text.secondary};
+    font-size: 14px !important;
+    font-size: 1rem;
+    width: 100%;
+    background: transparent;
+    box-shadow: none;
+    border: none;
+    outline: none;
+    border-bottom: 2px solid ${({ theme }) => theme.background.level1};
+    text-transform: uppercase;
+    font-weight: bold;
+    padding: 3px;
+  }
+`;
+
+export const FilterInputDiv = styled.div`
+  box-sizing: border-box;
+  cursor: pointer;
+  font-weight: bold;
+  user-select: none;
+  padding-top: 2px;
+  margin: 0 1rem 0.25rem 1rem;
 
   display: flex;
   justify-content: space-between;
