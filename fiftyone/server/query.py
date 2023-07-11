ae6bcfc9b9492fd01948f11bf29986d2229948fa
Kacey <kacey@voxel51.com>
2023-07-11 09:23:55 -0700

    Suppress errors due to none fields (#3275)
    * don't throw error
    
    * add debug msg

--- fiftyone/server/query.py
+++ fiftyone/server/query.py
@@ -5,6 +5,7 @@ FiftyOne Server queries.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
+import logging
 from dataclasses import asdict
 from datetime import date, datetime
 from enum import Enum
@@ -524,7 +525,13 @@ def _flatten_fields(
 ) -> t.List[t.Dict]:
     result = []
     for field in fields:
-        key = field.pop("name")
+        key = field.pop("name", None)
+        if key is None:
+            # Issues with concurrency can cause this to happen.
+            # Until it's fixed, just ignore these fields to avoid throwing hard
+            # errors when loading in the app.
+            logging.debug("Skipping field with no name: %s", field)
+            continue
         field_path = path + [key]
         field["path"] = ".".join(field_path)
         result.append(field)
