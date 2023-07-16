da2427b4b39723d4ca919a526611d8bb4ebf0b45
Lanny W <lanzhenwang9@gmail.com>
2023-07-16 17:04:05 -0500

    test useclearvisibility and useclearfiltered

--- app/packages/state/tests/hooks/useGroupEntries.test.tsx
+++ app/packages/state/tests/hooks/useGroupEntries.test.tsx
@@ -2,8 +2,16 @@ import { act, renderHook } from "@testing-library/react-hooks";
 import React from "react";
 import { RecoilRoot, useRecoilValue } from "recoil";
 import { expect, it, test } from "vitest";
+import {
+  attributeVisibility,
+  modalAttributeVisibility,
+  sidebarGroupsDefinition,
+} from "../../src";
+import {
+  useClearFiltered,
+  useClearVisibility,
+} from "../../src/hooks/useGroupEntries";
 import { filters, modalFilters } from "../../src/recoil/filters";
-import { useClearFiltered } from "../../src/hooks/useGroupEntries";
 
 const Root1: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   return (
@@ -28,6 +36,104 @@ const Root1: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
             isMatching: true,
           },
         });
+        snapshot.set(sidebarGroupsDefinition(false), [
+          {
+            name: "ground_truth",
+            paths: ["ground_truth.detection"],
+            expanded: true,
+          },
+          {
+            name: "predictions",
+            paths: ["predictions.detection"],
+            expanded: true,
+          },
+          {
+            name: "primitives",
+            paths: ["list_bools", "id", "filepath"],
+            expanded: true,
+          },
+        ]);
+        snapshot.set(sidebarGroupsDefinition(true), [
+          {
+            name: "ground_truth",
+            paths: ["ground_truth.detection"],
+            expanded: true,
+          },
+          {
+            name: "predictions",
+            paths: ["predictions.detection"],
+            expanded: true,
+          },
+          {
+            name: "primitives",
+            paths: ["list_bools", "id", "filepath"],
+            expanded: true,
+          },
+        ]);
+      }}
+    >
+      {children}
+    </RecoilRoot>
+  );
+};
+
+const Root2: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  return (
+    <RecoilRoot
+      initializeState={(snapshot) => {
+        snapshot.set(attributeVisibility, {
+          "ground_truth.detections.camera": {
+            values: ["cannon"],
+            exclude: false,
+            isMatching: true,
+          },
+          "ground_truth.detections.label": {
+            values: ["cat", "dog"],
+            exclude: false,
+            isMatching: true,
+          },
+        });
+        snapshot.set(modalAttributeVisibility, {
+          "ground_truth.detections.camera": {
+            values: ["cannon"],
+            exclude: false,
+            isMatching: true,
+          },
+        });
+        snapshot.set(sidebarGroupsDefinition(false), [
+          {
+            name: "ground_truth",
+            paths: ["ground_truth.detection"],
+            expanded: true,
+          },
+          {
+            name: "predictions",
+            paths: ["predictions.detection"],
+            expanded: true,
+          },
+          {
+            name: "primitives",
+            paths: ["list_bools", "id", "filepath"],
+            expanded: true,
+          },
+        ]);
+        snapshot.set(sidebarGroupsDefinition(true), [
+          {
+            name: "ground_truth",
+            paths: ["ground_truth.detection"],
+            expanded: true,
+          },
+          {
+            name: "predictions",
+            paths: ["predictions.detection"],
+            expanded: true,
+          },
+          {
+            name: "primitives",
+            paths: ["list_bools", "id", "filepath"],
+            expanded: true,
+          },
+        ]);
       }}
     >
       {children}
@@ -38,8 +144,8 @@ const Root1: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
 test("Test useClearFiltered", () => {
   const { result } = renderHook(
     () => ({
-      clearModalFilters: useClearFiltered(true, ""),
-      clearGridFilters: useClearFiltered(false, ""),
+      clearModalFilters: useClearFiltered(true, "ground_truth"),
+      clearGridFilters: useClearFiltered(false, "ground_truth"),
       filters: useRecoilValue(filters),
       modalFilters: useRecoilValue(modalFilters),
     }),
@@ -63,5 +169,36 @@ test("Test useClearFiltered", () => {
     });
     expect(result.current.filters).toStrictEqual({});
     expect(Object.keys(result.current.modalFilters)).toHaveLength(1);
+  });
+});
+
+test("Test useClearVisibility", () => {
+  const { result } = renderHook(
+    () => ({
+      clearModalVisibility: useClearVisibility(true, "ground_truth"),
+      clearGridVisibility: useClearVisibility(false, "ground_truth"),
+      visibility: useRecoilValue(attributeVisibility),
+      modalVisibility: useRecoilValue(modalAttributeVisibility),
+    }),
+    {
+      wrapper: Root2,
+    }
+  );
+
+  it("should clear modal attribute visibility", () => {
+    act(() => {
+      result.current.clearModalVisibility();
+    });
+
+    expect(result.current.modalVisibility).toStrictEqual({});
+    expect(Object.keys(result.current.visibility)).toHaveLength(2);
+  });
+
+  it("should clear grid attribute visibility", () => {
+    act(() => {
+      result.current.clearGridVisibility();
+    });
+    expect(result.current.visibility).toStrictEqual({});
+    expect(Object.keys(result.current.modalVisibility)).toHaveLength(1);
   });
 });
