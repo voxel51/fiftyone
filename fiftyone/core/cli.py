5e51d51323b3cf7f7b54112d24fdead4b1942785
brimoor <brimoor@umich.edu>
2023-07-27 12:04:47 -0400

    better printing

--- fiftyone/core/cli.py
+++ fiftyone/core/cli.py
@@ -3073,12 +3073,7 @@ class DelegatedInfoCommand(Command):
     def execute(parser, args):
         dos = food.DelegatedOperation()
         op = dos.get(ObjectId(args.id))
-
-        _print_delegated_info(op)
-
-
-def _print_delegated_info(op):
-    fo.pprint(op.to_pymongo())
+        fo.pprint(op._doc)
 
 
 class DelegatedCleanupCommand(Command):
