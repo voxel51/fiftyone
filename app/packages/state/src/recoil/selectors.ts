e4f6b9be3bf09e16ce803146406944356f52a7a1
imanjra <ibrahim@voxel51.com>
2023-08-22 23:04:30 -0400

    add snapshot support for local app

--- app/packages/state/src/recoil/selectors.ts
+++ app/packages/state/src/recoil/selectors.ts
@@ -26,6 +26,20 @@ export const datasetName = selector<string>({
   },
 });
 
+export const datasetHeadName = selector<string>({
+  key: "datasetHeadName",
+  get: ({ get }) => {
+    return get(atoms.dataset)?.headName;
+  },
+});
+
+export const datasetSnapshotName = selector<string>({
+  key: "datasetSnapshotName",
+  get: ({ get }) => {
+    return get(atoms.dataset)?.snapshotName;
+  },
+});
+
 export const isNotebook = selector<boolean>({
   key: "isNotebook",
   get: () => {
