5d382480fc431668236094c34bdb946476d6c121
manivoxel51 <mani@voxel51@gmail.com>
2023-08-24 16:07:34 -0700

    disable drag and e2e test

--- e2e-pw/src/oss/specs/smoke-tests/field-visibility.spec.ts
+++ e2e-pw/src/oss/specs/smoke-tests/field-visibility.spec.ts
@@ -1,12 +1,19 @@
-import { test as base, expect } from "src/oss/fixtures";
+import { test as base } from "src/oss/fixtures";
 import { FieldVisibilityPom } from "src/oss/poms/field-visibility/field-visibility";
 import { getUniqueDatasetNameWithPrefix } from "src/oss/utils";
+import { SidebarPom } from "src/oss/poms/sidebar";
 
 const datasetName = getUniqueDatasetNameWithPrefix("smoke-quickstart");
 
-const test = base.extend<{ fieldVisibility: FieldVisibilityPom }>({
+const test = base.extend<{
+  fieldVisibility: FieldVisibilityPom;
+  sidebar: SidebarPom;
+}>({
   fieldVisibility: async ({ page }, use) => {
     await use(new FieldVisibilityPom(page));
+  },
+  sidebar: async ({ page }, use) => {
+    await use(new SidebarPom(page));
   },
 });
 
@@ -27,24 +34,25 @@ test.describe("field visibility", () => {
 
   test("sidebar group is hidden if all its fields are hidden using field visibility", async ({
     fieldVisibility,
+    sidebar,
   }) => {
-    await fieldVisibility.assert.assertSidebarGroupIsVisibile("labels");
-    await fieldVisibility.assert.assertFieldsInSidebar([
+    await sidebar.asserter.assertSidebarGroupIsVisibile("labels");
+    await sidebar.asserter.assertFieldsInSidebar([
       "predictions",
       "ground_truth",
     ]);
 
     await fieldVisibility.hideFields(["predictions", "ground_truth"]);
-    await fieldVisibility.assert.assertFieldsNotInSidebar([
+    await sidebar.asserter.assertFieldsNotInSidebar([
       "predictions",
       "ground_truth",
     ]);
 
-    await fieldVisibility.assert.assertSidebarGroupIsHidden("labels");
+    await sidebar.asserter.assertSidebarGroupIsHidden("labels");
 
     await fieldVisibility.clearFieldVisibilityChanges();
-    await fieldVisibility.assert.assertSidebarGroupIsVisibile("labels");
-    await fieldVisibility.assert.assertFieldsInSidebar([
+    await sidebar.asserter.assertSidebarGroupIsVisibile("labels");
+    await sidebar.asserter.assertFieldsInSidebar([
       "predictions",
       "ground_truth",
     ]);
@@ -52,15 +60,15 @@ test.describe("field visibility", () => {
 
   test("sidebar entries are not draggable when field visibility is active", async ({
     fieldVisibility,
+    sidebar,
   }) => {
-    // TODO: assert draggable
+    // drag predictions to metadata group succeeds
+    await sidebar.asserter.assertCanDragFieldToGroup("predictions", "metadata");
 
-    await fieldVisibility.hideFields(["predictions", "ground_truth"]);
-    await fieldVisibility.assert.assertFieldsNotInSidebar([
-      "predictions",
-      "ground_truth",
-    ]);
+    await fieldVisibility.hideFields(["ground_truth"]);
+    await sidebar.asserter.assertFieldsNotInSidebar(["ground_truth"]);
 
-    // TODO: assert not draggable
+    // drag predictions back to labels group fails
+    await sidebar.asserter.assertCannotDragField("predictions");
   });
 });
