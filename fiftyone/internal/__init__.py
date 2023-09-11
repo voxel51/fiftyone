a52a1626f2844c97f0e6bdd1251f91891ae2467f
Ritchie Martori <ritchie@voxel51.com>
2023-09-11 11:22:46 -0700

    fix merge

--- fiftyone/internal/__init__.py
+++ fiftyone/internal/__init__.py
@@ -7,3 +7,14 @@ Internal classes and utilities.
 """
 
 from .secrets import *
+import os
+
+
+def is_internal_service():
+    """Whether the SDK is running in an internal service context.
+
+    Returns:
+        True/False
+    """
+    val = os.environ.get("FIFTYONE_INTERNAL_SERVICE", "")
+    return val.lower() in ("true", "1")
