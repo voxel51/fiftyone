b47449cfb62848fc6b673948f2d7202acc793894
Yang <yangyuphd@gmail.com>
2023-09-30 17:42:25 -0700

    Skip loading zoo sample dataset for fiftyone

--- start.py
+++ start.py
@@ -7,9 +7,7 @@ Installs FiftyOne.
 |
 """
 import fiftyone as fo
-import fiftyone.zoo as foz
 if __name__ == "__main__":
-    dataset = foz.load_zoo_dataset("quickstart")
     fo.app_config.theme="light"
-    session = fo.launch_app(dataset, port=5151, remote=True)
+    session = fo.launch_app(port=5151, remote=True)
     session.wait()
