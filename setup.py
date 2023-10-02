124b814681128ced157587a0b65dd01b21680b56
voxelbot <voxelbot@voxel51.com>
2023-10-02 17:41:01 +0000

    [skip ci] rc bump `fiftyone` to 0.14.2rc4 and `fiftyone-app` to 1.4.2rc4

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="0.14.2rc3",
+    version="0.14.2rc4",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
