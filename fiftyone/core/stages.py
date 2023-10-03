f71d6229c9b51aaff59bc6a7ee6aae1eeffae0dc
manivoxel51 <mani@voxel51@gmail.com>
2023-10-03 08:27:58 +0300

    Allow only field type for SortBy stage field type instead of str|json|field

--- fiftyone/core/stages.py
+++ fiftyone/core/stages.py
@@ -6937,7 +6937,7 @@ class SortBy(ViewStage):
         return [
             {
                 "name": "field_or_expr",
-                "type": "field|str|json",
+                "type": "field",
                 "placeholder": "field or expression",
             },
             {
