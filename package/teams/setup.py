ce42d17039aaebf265363126ec861a7737504aa2
voxelbot <voxelbot@voxel51.com>
2023-10-06 20:45:51 +0000

    [skip ci] rc bump `fiftyone` to 0.14.3rc1 and `fiftyone-app` to 1.4.3rc1

--- package/teams/setup.py
+++ package/teams/setup.py
@@ -47,7 +47,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone-teams-app",
-    version="1.4.2",
+    version="1.4.3rc1",
     description=("FiftyOne Teams"),
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
