bcc5dab55b743e7fbed8b413c34c14be5569e254
brimoor <brimoor@umich.edu>
2023-10-10 09:04:26 -0400

    bumping versions

--- package/desktop/setup.py
+++ package/desktop/setup.py
@@ -16,7 +16,7 @@ import os
 import shutil
 
 
-VERSION = "0.30.2"
+VERSION = "0.30.3"
 
 
 def get_version():
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import re
 from setuptools import setup, find_packages
 
 
-VERSION = "0.22.1"
+VERSION = "0.22.2"
 
 
 def get_version():
@@ -113,7 +113,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.29,<0.30"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.30,<0.31"]}
 
 
 with open("README.md", "r") as fh:
