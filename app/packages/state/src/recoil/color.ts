ba5de7c6f2278892e3ee687a817e8274fb0941bf
Lanny W <lanzhenwang9@gmail.com>
2023-10-11 15:55:04 -0500

    fix type errors

--- app/packages/state/src/recoil/color.ts
+++ app/packages/state/src/recoil/color.ts
@@ -117,17 +117,20 @@ export const ensureColorScheme = (
   appConfig?: datasetQuery$data["config"]
 ): ColorSchemeInput => {
   colorScheme = toCamelCase(colorScheme);
+  debugger;
   return {
-    colorPool: colorScheme.colorPool || appConfig.colorPool,
-    colorBy: colorScheme.colorBy || appConfig.colorBy,
+    colorPool: colorScheme.colorPool || appConfig?.colorPool,
+    colorBy: colorScheme.colorBy || appConfig?.colorBy,
     fields: colorScheme.fields as ColorSchemeInput["fields"],
     multicolorKeypoints:
-      colorScheme.multicolorKeypoints || appConfig.multicolorKeypoints,
+      typeof colorScheme.multicolorKeypoints == "boolean"
+        ? colorScheme.multicolorKeypoints
+        : appConfig?.multicolorKeypoints,
     opacity:
       typeof colorScheme.opacity === "number" ? colorScheme.opacity : 0.7,
     showSkeletons:
       typeof colorScheme.showSkeletons == "boolean"
         ? colorScheme.showSkeletons
-        : appConfig.showSkeletons,
+        : appConfig?.showSkeletons,
   };
 };
