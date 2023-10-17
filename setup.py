224fa4e1d444974d3e3c8c4bda4c6d2cebcc48b2
Benjamin Kane <ben@voxel51.com>
2023-10-17 07:56:44 -0600

    bump desktop (#3674)
    * bump desktop
    
    * syntax change

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop>=0.30,<0.31"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop~=0.31"]}
 
 
 with open("README.md", "r") as fh:
