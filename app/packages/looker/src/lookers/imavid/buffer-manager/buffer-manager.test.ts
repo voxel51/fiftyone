bc6d2620f6025f850586c3ae86754e3b2317816c
Sashank Aryal <sashank2012@gmail.com>
2023-10-19 20:08:03 +0545

    add buffer manager

---
+++ app/packages/looker/src/lookers/imavid/buffer-manager/buffer-manager.test.ts
@@ -0,0 +1,61 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { BufferManager } from ".";
+
+describe("BufferManager class tests", () => {
+  const bufferManager = new BufferManager();
+
+  beforeEach(() => {
+    bufferManager.reset();
+  });
+
+  test("addBufferRangeToBuffer method - no overlap", async () => {
+    bufferManager.addBufferRangeToBuffer([1, 4]);
+    bufferManager.addBufferRangeToBuffer([5, 7]);
+
+    const mergedBuffers = bufferManager.buffers;
+
+    // no overlap, so there should be two merged buffers
+    expect(mergedBuffers.length).toBe(2);
+    expect(mergedBuffers[0][0]).toBe(1);
+    expect(mergedBuffers[0][1]).toBe(4);
+    expect(mergedBuffers[1][0]).toBe(5);
+    expect(mergedBuffers[1][1]).toBe(7);
+  });
+
+  test("addBufferRangeToBuffer method - complete encapsulation", async () => {
+    bufferManager.addBufferRangeToBuffer([1, 5]);
+    bufferManager.addBufferRangeToBuffer([2, 4]);
+
+    const mergedBuffers = bufferManager.buffers;
+
+    // buffer [2, 4] should be encapsulated by [1, 5], so only one merged range should exist
+    expect(mergedBuffers.length).toBe(1);
+    expect(mergedBuffers[0][0]).toBe(1);
+    expect(mergedBuffers[0][1]).toBe(5);
+  });
+
+  test("addBufferRangeToBuffer method - completely encapsulated by existing range", async () => {
+    bufferManager.addBufferRangeToBuffer([2, 4]);
+    bufferManager.addBufferRangeToBuffer([1, 5]);
+
+    const mergedBuffers = bufferManager.buffers;
+
+    // buffer [2, 4] should be encapsulated by [1, 5], so only one merged range should exist
+    expect(mergedBuffers.length).toBe(1);
+    expect(mergedBuffers[0][0]).toBe(1);
+    expect(mergedBuffers[0][1]).toBe(5);
+  });
+
+  test("addBufferRangeToBuffer method - multiple merges", async () => {
+    bufferManager.addBufferRangeToBuffer([1, 4]);
+    bufferManager.addBufferRangeToBuffer([5, 7]);
+    bufferManager.addBufferRangeToBuffer([2, 6]);
+
+    const mergedBuffers = bufferManager.buffers;
+
+    // all buffer ranges overlap, so there should be only one merged buffer
+    expect(mergedBuffers.length).toBe(1);
+    expect(mergedBuffers[0][0]).toBe(1);
+    expect(mergedBuffers[0][1]).toBe(7);
+  });
+});
---
+++ app/packages/looker/src/lookers/imavid/buffer-manager/index.ts
@@ -0,0 +1,54 @@
+import { BufferRange, Buffers } from "../../../state";
+
+/**
+ * Manages buffer ranges for ImaVid.
+ */
+export class BufferManager {
+  public buffers: Buffers;
+
+  constructor() {
+    this.buffers = [];
+  }
+
+  /**
+   * Adds a buffer range to the buffer.
+   * If the new range overlaps with an existing range, the two ranges are merged.
+   * Time complexity: O(nlogn)
+   */
+  addBufferRangeToBuffer(range: BufferRange): void {
+    // add the new range to the buffer
+    this.buffers.push(range);
+
+    // sort the buffers based on their start value
+    this.buffers.sort((a, b) => a[0] - b[0]);
+
+    // initialize a stack to store the merged ranges
+    const stack: BufferRange[] = [];
+
+    // push the first range to stack
+    stack.push(this.buffers[0]);
+
+    // merge overlapping buffers
+    for (let i = 1; i < this.buffers.length; i++) {
+      // get top element of stack
+      const top = stack[stack.length - 1];
+
+      // if current interval is not overlapping with stack top,
+      // push it to the stack
+      if (top[1] < this.buffers[i][0]) stack.push(this.buffers[i]);
+      // else if end of current interval is more than the
+      // end of stack top interval, update the stack top
+      else if (top[1] < this.buffers[i][1]) {
+        top[1] = this.buffers[i][1];
+        stack.pop();
+        stack.push(top);
+      }
+    }
+
+    this.buffers = stack;
+  }
+
+  public reset() {
+    this.buffers = [];
+  }
+}
