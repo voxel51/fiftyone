9f44449a043d451fae8e97c8ff93ec6e682bf592
topher <topher@voxel51.com>
2023-10-20 16:42:49 +0000

    bump desktop version for new release

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop~=0.21"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop~=0.22"]}
 
 
 with open("README.md", "r") as fh:
