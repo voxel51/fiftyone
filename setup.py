37513f6aad77450b5311e363d1565d20b422bf70
manivoxel51 <mani@voxel51@gmail.com>
2023-10-30 16:48:17 +0200

    revert desktop version changes in teams

--- setup.py
+++ setup.py
@@ -16,22 +16,6 @@ import re
 from setuptools import setup, find_packages
 
 
-VERSION = "0.23.0"
-
-
-def get_version():
-    if "RELEASE_VERSION" in os.environ:
-        version = os.environ["RELEASE_VERSION"]
-        if not version.startswith(VERSION):
-            raise ValueError(
-                "Release version does not match version: %s and %s"
-                % (version, VERSION)
-            )
-        return version
-
-    return VERSION
-
-
 INSTALL_REQUIRES = [
     # third-party packages
     "aiofiles",
@@ -128,7 +112,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop~=0.31"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop~=0.21"]}
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
