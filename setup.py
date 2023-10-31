782bc8dbea332bb6d979695e24e035c953a41358
Benjamin Kane <ben@voxel51.com>
2023-10-31 08:54:34 -0600

    bump db

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ INSTALL_REQUIRES = [
     "universal-analytics-python3>=1.0.1,<2",
     # internal packages
     "fiftyone-brain>=0.13.1,<0.14",
-    "fiftyone-db>=0.4,<0.5",
+    "fiftyone-db>=0.4,<0.6",
     "voxel51-eta>=0.12,<0.13",
 ]
 
