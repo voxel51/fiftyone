f24534c05fba793f804f5986267c9cf9999d992a
Benjamin Kane <ben@voxel51.com>
2023-10-31 11:44:37 -0600

    onSetFieldVisibilityStage update

--- app/packages/app/src/useSetters/onSetFieldVisibilityStage.ts
+++ app/packages/app/src/useSetters/onSetFieldVisibilityStage.ts
@@ -3,15 +3,21 @@ import {
   setFieldVisibilityStageMutation,
   subscribeBefore,
 } from "@fiftyone/relay";
-import { State, stateSubscription } from "@fiftyone/state";
+import { State, datasetName, stateSubscription } from "@fiftyone/state";
 import { DefaultValue } from "recoil";
 import { commitMutation } from "relay-runtime";
 import { pendingEntry } from "../Renderer";
+import { resolveURL } from "../utils";
 import { RegisteredSetter } from "./registerSetter";
 
 const onSetFieldVisibilityStage: RegisteredSetter =
   ({ environment, router, sessionRef }) =>
   ({ get, set }, input?: DefaultValue | State.FieldVisibilityStage) => {
+    const dataset = get(datasetName);
+    if (!dataset) {
+      throw new Error("no dataset");
+    }
+
     set(pendingEntry, true);
     const newValue = input instanceof DefaultValue ? undefined : input;
     const stage = newValue
@@ -30,13 +36,10 @@ const onSetFieldVisibilityStage: RegisteredSetter =
     });
 
     // reload page query with new extended view
-    router.history.replace(
-      `${router.history.location.pathname}${router.history.location.search}`,
-      {
-        ...router.get().state,
-        fieldVisibility: stage,
-      }
-    );
+    router.history.replace(resolveURL(router), {
+      ...router.get().state,
+      fieldVisibility: stage,
+    });
 
     // send event as side effect
     commitMutation<setFieldVisibilityStageMutation>(environment, {
