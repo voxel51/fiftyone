4ded37f231ab057bfb4bd1247ac1d9576b2fd88e
topher <topher@voxel51.com>
2023-11-03 13:05:09 +0000

    bump OSS compatibility version

--- fiftyone/constants.py
+++ fiftyone/constants.py
@@ -49,7 +49,7 @@ COMPATIBLE_VERSIONS = ">=0.19,<0.23"
 # Package metadata
 _META = metadata("fiftyone")
 NAME = _META["name"]
-VERSION = "0.22.2"  # open source compatibility version
+VERSION = "0.22.3"  # open source compatibility version
 TEAMS_VERSION = _META["version"]
 DESCRIPTION = _META["summary"]
 AUTHOR = _META["author"]
