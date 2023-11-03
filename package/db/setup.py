29b3dcbb61502cce4ede1879d6e4c16f0dd7a307
Benjamin Kane <ben@voxel51.com>
2023-11-03 13:23:02 -0600

    use ubuntu 22.04 for debian 12

--- package/db/setup.py
+++ package/db/setup.py
@@ -36,6 +36,10 @@ LINUX_DOWNLOADS = {
         "11": {
             "x86_64": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian11-5.0.4.tgz"
         },
+        "12": {
+            "aarch64": "https://fastdl.mongodb.org/linux/mongodb-linux-aarch64-ubuntu2204-6.0.5.tgz",
+            "x86_64": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.5.tgz",
+        },
     },
     "rhel": {
         "7": {
