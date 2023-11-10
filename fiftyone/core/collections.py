273a3058f0eb7e36e0f3a576e55d826b1a0c9989
brimoor <brimoor@umich.edu>
2023-11-10 10:03:53 -0500

    optimize metadata computation

--- fiftyone/core/collections.py
+++ fiftyone/core/collections.py
@@ -2702,7 +2702,11 @@ class SampleCollection(object):
         self._dataset.delete_labels(ids=ids, fields=fields)
 
     def compute_metadata(
-        self, overwrite=False, num_workers=None, skip_failures=True
+        self,
+        overwrite=False,
+        num_workers=None,
+        skip_failures=True,
+        warn_failures=False,
     ):
         """Populates the ``metadata`` field of all samples in the collection.
 
@@ -2711,15 +2715,18 @@ class SampleCollection(object):
 
         Args:
             overwrite (False): whether to overwrite existing metadata
-            num_workers (None): a suggested number of processes to use
+            num_workers (None): a suggested number of threads to use
             skip_failures (True): whether to gracefully continue without
                 raising an error if metadata cannot be computed for a sample
+            warn_failures (False): whether to log a warning if metadata cannot
+                be computed for a sample
         """
         fomt.compute_metadata(
             self,
             overwrite=overwrite,
             num_workers=num_workers,
             skip_failures=skip_failures,
+            warn_failures=warn_failures,
         )
 
     def apply_model(
