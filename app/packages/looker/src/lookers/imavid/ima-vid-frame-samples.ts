b9da6c558df549c577619bee8845ae271bbc12d6
Sashank Aryal <sashank2012@gmail.com>
2023-11-13 21:53:33 +0545

    add dispose logic

--- app/packages/looker/src/lookers/imavid/ima-vid-frame-samples.ts
+++ app/packages/looker/src/lookers/imavid/ima-vid-frame-samples.ts
@@ -1,15 +1,32 @@
 import LRUCache from "lru-cache";
 import { MAX_FRAME_SAMPLES_CACHE_SIZE } from "./constants";
 import { SampleId, SampleResponse } from "./types";
+import { BufferManager } from "./buffer-manager";
 
 export class ImaVidFrameSamples {
   public readonly samples: LRUCache<SampleId, SampleResponse>;
-  public readonly frameIndex: Map<number, string>;
-  public readonly reverseFrameIndex: Map<string, number>;
+  public readonly frameIndex: Map<number, SampleId>;
+  public readonly reverseFrameIndex: Map<SampleId, number>;
 
-  constructor() {
+  private readonly storeBufferManager: BufferManager;
+
+  constructor(storeBufferManager: BufferManager) {
+    this.storeBufferManager = storeBufferManager;
+
     this.samples = new LRUCache<SampleId, SampleResponse>({
       max: MAX_FRAME_SAMPLES_CACHE_SIZE,
+      dispose: (sampleId) => {
+        // remove it from the frame index
+        const frameNumber = this.reverseFrameIndex.get(sampleId);
+        if (frameNumber !== undefined) {
+          this.frameIndex.delete(frameNumber);
+        }
+        // remove from reverse frame index
+        this.reverseFrameIndex.delete(sampleId);
+
+        // remove from store buffer manager
+        this.storeBufferManager.removeBufferValue(frameNumber);
+      },
     });
 
     this.frameIndex = new Map<number, string>();
