9d331ef6f3dea9dec4c2ad0bab5486cc76d63590
Benjamin Kane <ben@voxel51.com>
2023-12-01 13:25:51 -0600

    fix id

--- app/packages/state/src/recoil/lightning.ts
+++ app/packages/state/src/recoil/lightning.ts
@@ -168,7 +168,7 @@ export const lightningPaths = selectorFamily<Set<string>, string>({
             })
           )
             .map((p) => `${expanded}.${p}`)
-            .filter((p) => indexes.has(get(schemaAtoms.dbPath(p))))
+            .filter((p) => indexes.has(p))
         );
       }
 
