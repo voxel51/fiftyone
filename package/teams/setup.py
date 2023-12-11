99f3f9b08265f9cfc87ddfbd8a7aff0b65094ef0
topher <topher@voxel51.com>
2023-12-11 20:11:26 +0000

    release v1.5.2

--- package/teams/setup.py
+++ package/teams/setup.py
@@ -47,7 +47,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone-teams-app",
-    version="1.5.1",
+    version="1.5.2",
     description=("FiftyOne Teams"),
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
