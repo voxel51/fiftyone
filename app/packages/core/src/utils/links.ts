4470ad886f89a44bf04cc73f2abd9a1f530adff7
manivoxel51 <mani@voxel51.com>
2023-12-14 13:23:06 -0800

    Improves field visibility's show metadata toggle text and adds a link to the docs

--- app/packages/core/src/utils/links.ts
+++ app/packages/core/src/utils/links.ts
@@ -7,6 +7,9 @@ export const COLOR_SCHEME =
 export const EVALUATION_PATCHES =
   "https://docs.voxel51.com/user_guide/app.html#app-evaluation-patches";
 
+export const FIELD_METADATA =
+  "https://docs.voxel51.com/user_guide/using_datasets.html#storing-field-metadata";
+
 export const LIGHTNING_MODE =
   "https://docs.voxel51.com/user_guide/app.html#lightning-mode";
 
