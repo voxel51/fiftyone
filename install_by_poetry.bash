9a7ae2c02370e019de8fcbd399355c9c640f7a19
bigstones <davidkim1994@hanmail.net>
2023-12-19 06:23:50 +0900

    create install_by_poetry.bash

---
+++ install_by_poetry.bash
@@ -0,0 +1,162 @@
+#!/bin/bash
+# Installs the `fiftyone` package and its dependencies.
+#
+# Usage:
+#   bash install.bash
+#
+# Copyright 2017-2023, Voxel51, Inc.
+# voxel51.com
+#
+
+# Show usage information
+usage() {
+    echo "Usage:  bash $0 [-h] [-d] [-e] [-m] [-p] [-v]
+
+Getting help:
+-h      Display this help message.
+
+Custom installations:
+-d      Install developer dependencies.
+-e      Source install of voxel51-eta.
+-m      Install MongoDB from scratch, rather than installing fiftyone-db.
+-p      Install only the core python package, not the App.
+-v      Voxel51 developer install (don't install fiftyone-brain).
+"
+}
+
+# Parse flags
+SHOW_HELP=false
+DEV_INSTALL=false
+SOURCE_ETA_INSTALL=false
+SCRATCH_MONGODB_INSTALL=false
+BUILD_APP=true
+VOXEL51_INSTALL=false
+GITHUB_INSTALL=false
+E2E_INSTALL=false
+while getopts "hdempv" FLAG; do
+    case "${FLAG}" in
+        h) SHOW_HELP=true ;;
+        d) DEV_INSTALL=true ;;
+        e) SOURCE_ETA_INSTALL=true ;;
+        m) SCRATCH_MONGODB_INSTALL=true ;;
+        v) VOXEL51_INSTALL=true ;;
+        p) BUILD_APP=false ;;
+        *) usage ;;
+    esac
+done
+[ ${SHOW_HELP} = true ] && usage && exit 0
+
+set -e
+NODE_VERSION=17.9.0
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [ ${SCRATCH_MONGODB_INSTALL} = true ]; then
+    echo "***** INSTALLING MONGODB FROM SCRATCH *****"
+    MONGODB_VERSION=6.0.5
+    INSTALL_MONGODB=true
+
+    mkdir -p ~/.fiftyone/bin
+    cd ~/.fiftyone
+    mkdir -p var/lib/mongo
+    if [ -x bin/mongod ]; then
+        VERSION_FULL=$(bin/mongod --version | grep 'db version')
+        CURRENT_VERSION="${VERSION_FULL:12}"
+        if [ ${CURRENT_VERSION} != ${MONGODB_VERSION} ]; then
+            echo "Upgrading MongoDB v${CURRENT_VERSION} to v${MONGODB_VERSION}"
+        else
+            echo "MongoDB v${MONGODB_VERSION} already installed"
+            INSTALL_MONGODB=false
+        fi
+    fi
+
+    if [ ${INSTALL_MONGODB} = true ]; then
+        echo "Installing MongoDB v${MONGODB_VERSION}"
+        if [ "${OS}" == "Darwin" ]; then
+            MONGODB_BUILD=mongodb-macos-x86_64-${MONGODB_VERSION}
+            curl https://fastdl.mongodb.org/osx/${MONGODB_BUILD}.tgz --output mongodb.tgz
+            tar -zxvf mongodb.tgz
+            mv ${MONGODB_BUILD}/bin/* ./bin/
+            rm mongodb.tgz
+            rm -rf ${MONGODB_BUILD}
+        elif [ "${OS}" == "Linux" ]; then
+            MONGODB_BUILD=mongodb-linux-x86_64-ubuntu2204-${MONGODB_VERSION}
+            curl https://fastdl.mongodb.org/linux/${MONGODB_BUILD}.tgz --output mongodb.tgz
+            tar -zxvf mongodb.tgz
+            mv ${MONGODB_BUILD}/bin/* ./bin/
+            rm mongodb.tgz
+            rm -rf ${MONGODB_BUILD}
+        else
+            echo "WARNING: unsupported OS, skipping MongoDB installation"
+        fi
+    fi
+    cd -
+else
+    echo "***** INSTALLING FIFTYONE-DB *****"
+    pip install fiftyone-db
+fi
+
+if [ ${VOXEL51_INSTALL} = false ]; then
+    echo "***** INSTALLING FIFTYONE-BRAIN *****"
+    pip install --upgrade fiftyone-brain
+fi
+
+pip install poetry
+
+echo "***** INSTALLING FIFTYONE *****"
+if [ ${DEV_INSTALL} = true ] || [ ${VOXEL51_INSTALL} = true ]; then
+    poetry install --with docs,extras,test,dev
+elif [ ${GITHUB_INSTALL} = true ]; then
+    echo "Performing github install"
+    poetry install --with test,github
+elif [ ${E2E_INSTALL} = true ]; then
+    echo "Performing e2e install"
+    poetry install --with test,github,e2e
+else
+    poetry build
+    pip install dist/fiftyone-*.whl
+fi
+
+if [ ${SOURCE_ETA_INSTALL} = true ]; then
+    echo "***** INSTALLING ETA *****"
+    if [[ ! -d "eta" ]]; then
+        echo "Cloning ETA repository"
+        git clone https://github.com/voxel51/eta
+    fi
+    cd eta
+    if [ ${DEV_INSTALL} = true ] || [ ${VOXEL51_INSTALL} = true ]; then
+        pip install -e .
+    else
+        pip install .
+    fi
+    if [[ ! -f eta/config.json ]]; then
+        echo "Installing default ETA config"
+        cp config-example.json eta/config.json
+    fi
+    cd ..
+fi
+
+# Do this last since `source` can exit Python virtual environments
+if [ ${BUILD_APP} = true ]; then
+    echo "***** INSTALLING FIFTYONE-APP *****"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    nvm install ${NODE_VERSION}
+    nvm use ${NODE_VERSION}
+    npm -g install yarn
+    if [ -f ~/.bashrc ]; then
+        source ~/.bashrc
+    elif [ -f ~/.bash_profile ]; then
+        source ~/.bash_profile
+    else
+        echo "WARNING: unable to locate a bash profile to 'source'; you may need to start a new shell"
+    fi
+    cd app
+    echo "Building the App. This will take a minute or two..."
+    yarn install > /dev/null 2>&1
+    yarn build
+    cd ..
+fi
+
+echo "***** INSTALLATION COMPLETE *****"
