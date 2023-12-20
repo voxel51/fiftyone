2fc8c118d050e6bfddad14fe79ea286fe00980d0
Stuart Wheaton <stuart@voxel51.com>
2023-12-20 11:30:48 -0500

    minor

--- fiftyone/core/dataset.py
+++ fiftyone/core/dataset.py
@@ -2581,11 +2581,6 @@ class Dataset(foc.SampleCollection, metaclass=DatasetSingleton):
             except:
                 pass
 
-        # Get default batcher:
-        # dynamic - Dynamically size batches so that they are as large as
-        #   possible while still achieving a nice frame rate on the progress bar
-        # size - Dynamically size batches to reach a target bson content size
-        # static - fixed size batches
         batcher = fou.get_default_batcher(samples, progress=True)
 
         with batcher:
