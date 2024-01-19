bfbca95d5ade672627e28273fffcf95609835790
kacey <kacey@voxel51.com>
2024-01-19 15:49:01 -0800

    rename file not plugin dir

--- tests/unittests/operators/decorators_tests.py
+++ tests/unittests/operators/decorators_tests.py
@@ -52,7 +52,10 @@ class DirStateTests(unittest.TestCase):
             self.assertNotEqual(dir_state(tmpdirname), 0)
 
     def test_rgrs_dir_state_change_with_delete(self):
-        plugin_paths = ["@org1/plugin1/file1.txt", "@org2/plugin2/file2.txt"]
+        plugin_paths = [
+            "@org1/plugin1/fiftyone.yml",
+            "@org2/plugin2/fiftyone.yml",
+        ]
         with tempfile.TemporaryDirectory() as tmpdirname:
             initial_dir_state = dir_state(tmpdirname)
             for p in plugin_paths:
@@ -74,21 +77,24 @@ class DirStateTests(unittest.TestCase):
             shutil.rmtree(
                 os.path.join(tmpdirname, plugin_paths[1].rsplit("/", 1)[0])
             )
+
+            time.sleep(0.1)
             dir_state3 = dir_state(tmpdirname)
             self.assertNotEqual(dir_state3, dir_state2)
 
     def test_rgrs_dir_state_change_with_rename(self):
         plugin_paths = ["@org1/plugin1", "@org2/plugin2"]
-        plugin_file = "fiftyone.yml"
+        plugin_files = ["fiftyone.yml", "plugin-code.py"]
         with tempfile.TemporaryDirectory() as tmpdirname:
             initial_dir_state = dir_state(tmpdirname)
             for p in plugin_paths:
-                time.sleep(0.1)
-                plugin_dir = os.path.join(tmpdirname, p)
-                os.makedirs(plugin_dir)
-                fname = os.path.join(plugin_dir, plugin_file)
-                with open(fname, "a") as f:
-                    f.write("test")
+                for plugin_file in plugin_files:
+                    time.sleep(0.1)
+                    plugin_dir = os.path.join(tmpdirname, p)
+                    os.makedirs(plugin_dir)
+                    fname = os.path.join(plugin_dir, plugin_file)
+                    with open(fname, "a") as f:
+                        f.write("test")
 
             # add wait for test to pass on older systems/python versions
             time.sleep(0.1)
@@ -99,8 +105,10 @@ class DirStateTests(unittest.TestCase):
 
             # verify that dir_state is different after renaming plugin dir
             os.rename(
-                os.path.join(tmpdirname, plugin_paths[0]),
-                os.path.join(tmpdirname, plugin_paths[0] + "renamed"),
+                os.path.join(tmpdirname, plugin_paths[0], plugin_files[1]),
+                os.path.join(
+                    tmpdirname, plugin_paths[0], plugin_files[1] + "renamed"
+                ),
             )
 
             # add wait for test to pass on older systems/python versions
