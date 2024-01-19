95601ae7b95df735e4c4e2bf9704b388b0ada75d
Sashank Aryal <sashank2012@gmail.com>
2024-01-18 22:00:50 -0600

    use plugin looker for fo3d

--- app/packages/relay/src/queries/__generated__/mainSampleQuery.graphql.ts
+++ app/packages/relay/src/queries/__generated__/mainSampleQuery.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<d1abd1addd7913e1193336100095b405>>
+ * @generated SignedSource<<d2915b41290da7b904ed7d7becf917d8>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -36,6 +36,15 @@ export type mainSampleQuery$data = {
     }>;
   } | {
     readonly __typename: "PointCloudSample";
+    readonly aspectRatio: number;
+    readonly id: string;
+    readonly sample: object;
+    readonly urls: ReadonlyArray<{
+      readonly field: string;
+      readonly url: string | null;
+    }>;
+  } | {
+    readonly __typename: "ThreeDSample";
     readonly aspectRatio: number;
     readonly id: string;
     readonly sample: object;
@@ -86,57 +95,28 @@ v3 = {
   "kind": "LocalArgument",
   "name": "view"
 },
-v4 = [
-  {
-    "kind": "Variable",
-    "name": "dataset",
-    "variableName": "dataset"
-  },
-  {
-    "kind": "Variable",
-    "name": "filter",
-    "variableName": "filter"
-  },
-  {
-    "kind": "Variable",
-    "name": "filters",
-    "variableName": "filters"
-  },
-  {
-    "kind": "Variable",
-    "name": "view",
-    "variableName": "view"
-  }
-],
-v5 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "__typename",
-  "storageKey": null
-},
-v6 = {
+v4 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
   "name": "aspectRatio",
   "storageKey": null
 },
-v7 = {
+v5 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
   "name": "id",
   "storageKey": null
 },
-v8 = {
+v6 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
   "name": "sample",
   "storageKey": null
 },
-v9 = {
+v7 = {
   "alias": null,
   "args": null,
   "concreteType": "MediaURL",
@@ -161,49 +141,96 @@ v9 = {
   ],
   "storageKey": null
 },
-v10 = [
+v8 = [
+  (v4/*: any*/),
+  (v5/*: any*/),
   (v6/*: any*/),
-  (v7/*: any*/),
-  (v8/*: any*/),
-  (v9/*: any*/)
+  (v7/*: any*/)
 ],
-v11 = {
-  "kind": "InlineFragment",
-  "selections": (v10/*: any*/),
-  "type": "ImageSample",
-  "abstractKey": null
-},
-v12 = {
-  "kind": "InlineFragment",
-  "selections": (v10/*: any*/),
-  "type": "PointCloudSample",
-  "abstractKey": null
-},
-v13 = {
-  "kind": "InlineFragment",
-  "selections": [
-    (v6/*: any*/),
-    (v7/*: any*/),
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "frameRate",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "frameNumber",
-      "storageKey": null
-    },
-    (v8/*: any*/),
-    (v9/*: any*/)
-  ],
-  "type": "VideoSample",
-  "abstractKey": null
-};
+v9 = [
+  {
+    "alias": null,
+    "args": [
+      {
+        "kind": "Variable",
+        "name": "dataset",
+        "variableName": "dataset"
+      },
+      {
+        "kind": "Variable",
+        "name": "filter",
+        "variableName": "filter"
+      },
+      {
+        "kind": "Variable",
+        "name": "filters",
+        "variableName": "filters"
+      },
+      {
+        "kind": "Variable",
+        "name": "view",
+        "variableName": "view"
+      }
+    ],
+    "concreteType": null,
+    "kind": "LinkedField",
+    "name": "sample",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "__typename",
+        "storageKey": null
+      },
+      {
+        "kind": "InlineFragment",
+        "selections": (v8/*: any*/),
+        "type": "ImageSample",
+        "abstractKey": null
+      },
+      {
+        "kind": "InlineFragment",
+        "selections": (v8/*: any*/),
+        "type": "PointCloudSample",
+        "abstractKey": null
+      },
+      {
+        "kind": "InlineFragment",
+        "selections": [
+          (v4/*: any*/),
+          (v5/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "frameRate",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "frameNumber",
+            "storageKey": null
+          },
+          (v6/*: any*/),
+          (v7/*: any*/)
+        ],
+        "type": "VideoSample",
+        "abstractKey": null
+      },
+      {
+        "kind": "InlineFragment",
+        "selections": (v8/*: any*/),
+        "type": "ThreeDSample",
+        "abstractKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
 return {
   "fragment": {
     "argumentDefinitions": [
@@ -215,23 +242,7 @@ return {
     "kind": "Fragment",
     "metadata": null,
     "name": "mainSampleQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v4/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "sample",
-        "plural": false,
-        "selections": [
-          (v5/*: any*/),
-          (v11/*: any*/),
-          (v12/*: any*/),
-          (v13/*: any*/)
-        ],
-        "storageKey": null
-      }
-    ],
+    "selections": (v9/*: any*/),
     "type": "Query",
     "abstractKey": null
   },
@@ -245,43 +256,19 @@ return {
     ],
     "kind": "Operation",
     "name": "mainSampleQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v4/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "sample",
-        "plural": false,
-        "selections": [
-          (v5/*: any*/),
-          (v11/*: any*/),
-          (v12/*: any*/),
-          (v13/*: any*/),
-          {
-            "kind": "InlineFragment",
-            "selections": [
-              (v7/*: any*/)
-            ],
-            "type": "ThreeDSample",
-            "abstractKey": null
-          }
-        ],
-        "storageKey": null
-      }
-    ]
+    "selections": (v9/*: any*/)
   },
   "params": {
-    "cacheID": "5b046d25589fd5c93c6beb35f4330cf8",
+    "cacheID": "3620bb08051185b996237076ef49a2f1",
     "id": null,
     "metadata": {},
     "name": "mainSampleQuery",
     "operationKind": "query",
-    "text": "query mainSampleQuery(\n  $dataset: String!\n  $view: BSONArray!\n  $filter: SampleFilter!\n  $filters: JSON\n) {\n  sample(dataset: $dataset, view: $view, filters: $filters, filter: $filter) {\n    __typename\n    ... on ImageSample {\n      aspectRatio\n      id\n      sample\n      urls {\n        field\n        url\n      }\n    }\n    ... on PointCloudSample {\n      aspectRatio\n      id\n      sample\n      urls {\n        field\n        url\n      }\n    }\n    ... on VideoSample {\n      aspectRatio\n      id\n      frameRate\n      frameNumber\n      sample\n      urls {\n        field\n        url\n      }\n    }\n    ... on ThreeDSample {\n      id\n    }\n  }\n}\n"
+    "text": "query mainSampleQuery(\n  $dataset: String!\n  $view: BSONArray!\n  $filter: SampleFilter!\n  $filters: JSON\n) {\n  sample(dataset: $dataset, view: $view, filters: $filters, filter: $filter) {\n    __typename\n    ... on ImageSample {\n      aspectRatio\n      id\n      sample\n      urls {\n        field\n        url\n      }\n    }\n    ... on PointCloudSample {\n      aspectRatio\n      id\n      sample\n      urls {\n        field\n        url\n      }\n    }\n    ... on VideoSample {\n      aspectRatio\n      id\n      frameRate\n      frameNumber\n      sample\n      urls {\n        field\n        url\n      }\n    }\n    ... on ThreeDSample {\n      aspectRatio\n      id\n      sample\n      urls {\n        field\n        url\n      }\n    }\n  }\n}\n"
   }
 };
 })();
 
-(node as any).hash = "2b2a2cfd1b68e8fd6d01258f45d2f99a";
+(node as any).hash = "e886574436e86028aafc7255b6088901";
 
 export default node;
