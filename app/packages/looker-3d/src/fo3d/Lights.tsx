a6254a62f7c1443988f1cad7e270e2186487ce0c
Sashank Aryal <sashank2012@gmail.com>
2024-01-26 17:54:19 -0600

    fix mtl loading

---
+++ app/packages/looker-3d/src/fo3d/Lights.tsx
@@ -0,0 +1,46 @@
+import { useControls } from "leva";
+
+export const Lights = () => {
+  const {
+    ambientLightIntensity,
+    directionalLightPosition,
+    directionalLightIntensity,
+  } = useControls("Lights", {
+    ambientLightIntensity: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+      label: "Ambient light intensity",
+    },
+    directionalLightPosition: {
+      value: [100, 1000, 100],
+      label: "Directional light position",
+    },
+    directionalLightIntensity: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+      label: "Directional light intensity",
+    },
+  });
+
+  return (
+    <>
+      <ambientLight intensity={ambientLightIntensity} />
+      <directionalLight
+        position={directionalLightPosition}
+        intensity={directionalLightIntensity}
+      />
+      <pointLight position={[1000, 1000, 100]} />
+      <pointLight position={[-100, -100, -100]} />
+      <pointLight position={[100, 100, -100]} />
+      <pointLight position={[-100, 100, 100]} />
+      <pointLight position={[100, -100, 100]} />
+      <pointLight position={[100, -100, -100]} />
+      <pointLight position={[-100, 100, -100]} />
+      <pointLight position={[-100, -100, 100]} />
+    </>
+  );
+};
--- app/packages/looker-3d/src/fo3d/Objs.tsx
+++ app/packages/looker-3d/src/fo3d/Objs.tsx
@@ -1,22 +1,60 @@
 import { useLoader } from "@react-three/fiber";
-import React, { useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Mesh, MeshPhongMaterial } from "three";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { ObjReturnType } from "../hooks";
 import { getIdentifierForAsset, getVisibilityMapFromFo3dParsed } from "./utils";
+import { COLOR_POOL } from "../constants";
 
 type ObjsProps = {
   objs: ObjReturnType[];
   visibilityMap: ReturnType<typeof getVisibilityMapFromFo3dParsed>;
 };
 
-const ObjMesh = ({ obj }: { obj: ObjReturnType }) => {
-  const { objUrl, mtlUrl, position, quaternion, scale } = obj;
-  const materials = useLoader(MTLLoader, mtlUrl);
+const ObjMeshDefaultMaterial = ({ obj }: { obj: ObjReturnType }) => {
+  const { objUrl, position, quaternion, scale } = obj;
+  const mesh = useLoader(OBJLoader, objUrl);
 
+  useEffect(() => {
+    if (!mesh) {
+      return;
+    }
+
+    // create hash from obj url to generate a unique color from color pool
+    const hash = objUrl.split("").reduce((acc, char, idx) => {
+      const charCode = char.charCodeAt(0);
+      return acc + charCode * idx;
+    }, 0);
+    const color = COLOR_POOL[hash % COLOR_POOL.length];
+    mesh.traverse((child) => {
+      if (child instanceof Mesh) {
+        child.material = new MeshPhongMaterial({
+          color,
+        });
+      }
+    });
+  }, [mesh]);
+
+  return (
+    <primitive
+      object={mesh}
+      position={position}
+      quaternion={quaternion}
+      scale={scale}
+    />
+  );
+};
+
+const ObjMeshWithCustomMaterial = ({ obj }: { obj: ObjReturnType }) => {
+  const { objUrl, mtlUrl, position, quaternion, scale } = obj;
+
+  const materials = useLoader(MTLLoader, mtlUrl);
   const mesh = useLoader(OBJLoader, objUrl, (loader) => {
-    materials.preload();
-    loader.setMaterials(materials);
+    if (mtlUrl) {
+      materials.preload();
+      loader.setMaterials(materials);
+    }
   });
 
   return (
@@ -37,7 +75,11 @@ export const Objs = ({ objs, visibilityMap }: ObjsProps) => {
         return (
           <group key={obj.objUrl}>
             <ObjErrorBoundary>
-              <ObjMesh obj={obj} />
+              {obj.mtlUrl ? (
+                <ObjMeshWithCustomMaterial obj={obj} />
+              ) : (
+                <ObjMeshDefaultMaterial obj={obj} />
+              )}
             </ObjErrorBoundary>
           </group>
         );
