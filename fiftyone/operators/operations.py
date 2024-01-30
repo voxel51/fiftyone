aa0056b85c597f274f2c131834eae9e41d0fa74a
Ritchie Martori <ritchie@voxel51.com>
2024-01-30 16:14:35 -0700

    add operations object to ctx

---
+++ fiftyone/operators/operations.py
@@ -0,0 +1,27 @@
+import json
+from bson import json_util
+
+
+class Operations(object):
+    def __init__(self, ctx):
+        self._ctx = ctx
+
+    """
+    Call the "set_view" operator. This operator sets the view of the FiftyOne App.
+
+    Args:
+        ctx: the current context
+        view: the view to set
+    Returns:
+        The :class:`fiftyone.operators.message.GeneratedMessage` object
+    """
+
+    def set_view(self, view):
+        return self._ctx.trigger(
+            "set_view",
+            params=dict(view=_serialize_view(view)),
+        )
+
+
+def _serialize_view(view):
+    return json.loads(json_util.dumps(view._serialize()))
