7aa9388e8872395893d7df78f9cfde34a091c7e3
voxelbot <voxelbot@voxel51.com>
2024-02-08 21:37:58 +0000

    [skip ci] automated bump `fiftyone` to 0.16.0+54c905f and `fiftyone-app` to 1.6.0+54c905f

--- package/teams/setup.py
+++ package/teams/setup.py
@@ -47,7 +47,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone-teams-app",
-    version="1.6.0+6bd22b6",
+    version="1.6.0+54c905f",
     description=("FiftyOne Teams"),
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
