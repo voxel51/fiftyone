263cd7af1d983b34310f8b34651bc05f26d0ce3d
topher <topher@voxel51.com>
2024-02-08 11:22:05 -0500

    bump to reflect 1.6.0 package versions

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="0.15.4+6ac7eb8",
+    version="0.16.0",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
