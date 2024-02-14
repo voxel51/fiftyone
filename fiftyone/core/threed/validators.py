882f3ef78eb8140ac99561912bfc753ebe3c8898
Sashank Aryal <sashank2012@gmail.com>
2024-02-14 15:36:58 -0600

    add camera and serialization

--- fiftyone/core/threed/validators.py
+++ fiftyone/core/threed/validators.py
@@ -12,18 +12,22 @@ from pydantic import validator
 from .transformation import Vec3UnionType, Vector3
 
 
-def convert_list_to_vec3(v: Optional[Vec3UnionType]) -> Vector3 | None:
+def normalize_to_vec3(v: Optional[Vec3UnionType]) -> Vector3 | None:
     if v is None:
         return None
 
-    if isinstance(v, (list, tuple)) and len(v) == 3:
+    if isinstance(v, (int, float)):
+        v = Vector3(v, v, v)
+    elif isinstance(v, (list, tuple)) and len(v) == 3:
         v = Vector3(*v)
+
     if not isinstance(v, Vector3):
         raise ValueError("Expected a list / tuple of length 3 or a Vector3")
+
     return v
 
 
 def vec3_normalizing_validator(field: str) -> classmethod:
     decorator = validator(field, allow_reuse=True)
-    validator_class_method = decorator(convert_list_to_vec3)
+    validator_class_method = decorator(normalize_to_vec3)
     return validator_class_method
