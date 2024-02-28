fc64d6c35f9d316931d4d97b8f8aaa147f4b5a44
Sashank Aryal <sashank2012@gmail.com>
2024-02-27 20:16:26 -0600

    add light helper

--- app/packages/looker-3d/src/fo3d/lights/Lights.tsx
+++ app/packages/looker-3d/src/fo3d/lights/Lights.tsx
@@ -1,7 +1,14 @@
+import { useHelper } from "@react-three/drei";
 import { folder, useControls } from "leva";
 import { partition } from "lodash";
-import { useMemo } from "react";
-import { PANEL_ORDER_LIGHTS } from "../../constants";
+import { useCallback, useMemo, useRef, useState } from "react";
+import {
+  DirectionalLightHelper,
+  Object3D,
+  PointLightHelper,
+  SpotLightHelper,
+} from "three";
+import { PANEL_ORDER_LIGHTS, VOXEL51_THEME_COLOR } from "../../constants";
 import { FoScene } from "../../hooks";
 import { DefaultLights } from "./DefaultLights";
 
@@ -9,9 +16,19 @@ export interface FoLightProps {
   lights: FoScene["lights"];
 }
 
+type LightHelperConfig = {
+  type: Omit<FoScene["lights"][number]["_type"], "AmbientLight">;
+  lightIndex: number;
+};
+
 const getIntensityKey = (lightName: string) => `${lightName}Intensity`;
+const getColorKey = (lightName: string) => `${lightName}Color`;
+const getPositionKey = (lightName: string) => `${lightName}Position`;
 
 const CustomLights = ({ lights }: Pick<FoLightProps, "lights">) => {
+  const [lightHelperConfig, setLightHelperConfig] =
+    useState<LightHelperConfig | null>(null);
+
   const filteredLights = useMemo(() => {
     const [ambientLights, otherLights] = partition(lights, (light) => {
       return light._type === "AmbientLight";
@@ -25,11 +42,26 @@ const CustomLights = ({ lights }: Pick<FoLightProps, "lights">) => {
     return lights;
   }, [lights]);
 
-  const lightIntensityConfig = useMemo(() => {
+  const onLightDragStart = useCallback((type: string, lightIndex: number) => {
+    setLightHelperConfig({
+      type,
+      lightIndex,
+    });
+  }, []);
+
+  const onLightDragEnd = useCallback(() => {
+    setLightHelperConfig(null);
+  }, []);
+
+  const lightLevaControls = useMemo(() => {
     const config = {};
 
-    for (const light of filteredLights) {
+    for (let i = 0; i < filteredLights.length; i++) {
+      const light = filteredLights[i];
       const intensityKey = getIntensityKey(light.name);
+      const colorKey = getColorKey(light.name);
+      const positionKey = getPositionKey(light.name);
+
       config[intensityKey] = {
         value: light.intensity,
         min: 0,
@@ -37,25 +69,153 @@ const CustomLights = ({ lights }: Pick<FoLightProps, "lights">) => {
         step: 0.01,
         label: `${light.name} intensity`,
       };
+
+      config[colorKey] = {
+        value: light.color,
+        label: `${light.name} color`,
+      };
+
+      if (light._type !== "AmbientLight") {
+        config[positionKey] = {
+          value: light.position,
+          label: `${light.name} position`,
+          onEditStart: () => {
+            onLightDragStart(light._type, i);
+          },
+          onEditEnd: onLightDragEnd,
+        };
+      }
     }
 
     return config;
+  }, [filteredLights, onLightDragEnd, onLightDragStart]);
+
+  const [lightConfig] = useControls(
+    () => ({
+      Lights: folder(lightLevaControls, {
+        order: PANEL_ORDER_LIGHTS,
+        collapsed: true,
+      }),
+    }),
+    [lightLevaControls]
+  );
+
+  const lightRef = useRef();
+
+  const target = useMemo(() => {
+    for (const light of filteredLights) {
+      if (
+        (light._type === "DirectionalLight" || light._type === "SpotLight") &&
+        light.target
+      ) {
+        const targetObject = new Object3D();
+        targetObject.position.set(...light.target);
+        return targetObject;
+      }
+    }
+    return null;
   }, [filteredLights]);
 
-  const intensities = useControls(() => ({
-    Lights: folder(lightIntensityConfig, {
-      order: PANEL_ORDER_LIGHTS,
-      collapsed: true,
-    }),
-  }));
-
-  return filteredLights.map((light) => {
-    const intensityKey = getIntensityKey(light.name);
-    switch (light._type) {
-      case "AmbientLight":
-        return <ambientLight intensity={intensities[intensityKey]} />;
+  const lightElements = useMemo(() => {
+    if (!filteredLights || !lightConfig) {
+      return null;
     }
-  });
+
+    return filteredLights.map((light, index) => {
+      const intensityKey = getIntensityKey(light.name);
+      const colorKey = getColorKey(light.name);
+      const positionKey = getPositionKey(light.name);
+
+      const intensity = lightConfig[intensityKey];
+      const color = lightConfig[colorKey];
+      const position = lightConfig[positionKey];
+
+      const refProps =
+        lightHelperConfig?.lightIndex === index ? { ref: lightRef } : {};
+
+      switch (light._type) {
+        case "AmbientLight":
+          return (
+            <ambientLight
+              key={light.name}
+              color={color}
+              intensity={intensity}
+              {...refProps}
+            />
+          );
+        case "DirectionalLight":
+          return (
+            <directionalLight
+              key={light.name}
+              color={color}
+              intensity={intensity}
+              position={position}
+              quaternion={light.quaternion}
+              target={target}
+              {...refProps}
+            />
+          );
+        case "SpotLight":
+          return (
+            <spotLight
+              key={light.name}
+              color={color}
+              intensity={intensity}
+              position={position}
+              quaternion={light.quaternion}
+              angle={light.angle}
+              penumbra={light.penumbra}
+              decay={light.decay}
+              distance={light.distance}
+              target={target}
+              {...refProps}
+            />
+          );
+        case "PointLight":
+          return (
+            <pointLight
+              key={light.name}
+              color={color}
+              intensity={intensity}
+              position={position}
+              distance={light.distance}
+              decay={light.decay}
+              {...refProps}
+            />
+          );
+        default:
+          return null;
+      }
+    });
+  }, [filteredLights, lightConfig, target, lightHelperConfig]);
+
+  const helperClass = useMemo(() => {
+    if (!lightHelperConfig) {
+      return null;
+    }
+
+    const { type } = lightHelperConfig;
+
+    switch (type) {
+      case "DirectionalLight":
+        return DirectionalLightHelper;
+      case "SpotLight":
+        return SpotLightHelper;
+      case "PointLight":
+        return PointLightHelper;
+      default:
+        return null;
+    }
+  }, [lightHelperConfig]);
+
+  useHelper(
+    lightHelperConfig ? lightRef : false,
+    helperClass,
+    0.1,
+    VOXEL51_THEME_COLOR
+  );
+
+  return <group>{lightElements}</group>;
 };
 
 export const Lights = ({ lights: customLights }: FoLightProps) => {
