04b08bdaa83f68dc3895ab933748686999c5209b
topher <topher@voxel51.com>
2024-03-01 12:45:43 +0000

    release v1.6.0-beta.1

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="0.16.0rc2+c8939b3",
+    version="0.16.0b1",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
