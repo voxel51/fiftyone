4cec5719bff502ecc8804fab00b7389eb96c592e
topher <topher@voxel51.com>
2024-03-01 19:34:10 +0000

    preparing for release of v1.5.7

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ def get_install_requirements(install_requires, choose_install_requires):
     return install_requires
 
 
-EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop~=0.23.4"]}
+EXTRAS_REQUIREMENTS = {"desktop": ["fiftyone-desktop~=0.23.5"]}
 
 
 with open("README.md", "r") as fh:
