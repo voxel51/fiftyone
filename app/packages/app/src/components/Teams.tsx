20dc75cb65d63e442724b71fe9e1e382e1566dfc
Benjamin Kane <ben@voxel51.com>
2024-03-05 15:20:27 -0600

    update link

--- app/packages/app/src/components/Teams.tsx
+++ app/packages/app/src/components/Teams.tsx
@@ -3,7 +3,9 @@ import React from "react";
 
 export default function Teams() {
   return (
-    <ExternalLink href={"https://voxel51.com/book-a-demo/"}>
+    <ExternalLink
+      href={"https://voxel51.com/book-a-demo/?utm_source=FiftyOneApp"}
+    >
       <Button>Have a Team?</Button>
     </ExternalLink>
   );
