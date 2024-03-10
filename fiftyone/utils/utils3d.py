05021239667484ad1849792a7a91efc880339c59
brimoor <brimoor@umich.edu>
2024-03-10 13:34:56 -0400

    use point cloud

--- fiftyone/utils/utils3d.py
+++ fiftyone/utils/utils3d.py
@@ -1046,7 +1046,7 @@ def _make_scene(
             pcd_path = rel_path
 
     scene = Scene()
-    scene.add(Pointcloud("pcd", pcd_path))
+    scene.add(Pointcloud("point cloud", pcd_path))
     scene.write(scene_path)
 
     return scene_path
