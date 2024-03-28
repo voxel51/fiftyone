a539d767f78332afda4254b49b3ceae3a741a58e
Jacob Marks <jamarks13@gmail.com>
2024-03-28 13:12:58 -0700

    adds standard module docstring

--- fiftyone/utils/huggingface.py
+++ fiftyone/utils/huggingface.py
@@ -1,3 +1,10 @@
+"""
+Utilities for working with `Hugging Face <https://huggingface.co>`_.
+
+| Copyright 2017-2024, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
 from concurrent.futures import ThreadPoolExecutor
 import json
 import logging
