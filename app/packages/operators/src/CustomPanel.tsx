d99055d511ce2f239429df411dbbff44bcc9e00a
Ritchie Martori <ritchie@voxel51.com>
2024-04-01 17:32:25 -0700

    panel state refactor

--- app/packages/operators/src/CustomPanel.tsx
+++ app/packages/operators/src/CustomPanel.tsx
@@ -1,9 +1,15 @@
 import { usePanelState } from "@fiftyone/spaces";
 import { useEffect } from "react";
 import { executeOperator } from "./operators";
+import OperatorIO from "./OperatorIO";
 
 export function CustomPanel({ panelId, onLoad, onUnLoad }) {
   const [panelState, setPanelState] = usePanelState(panelId);
+  const renderableSchema = panelState?.schema;
+  const data = panelState?.state;
+  const handlePanelStateChange = (newState) => {
+    console.log(newState);
+  };
 
   useEffect(() => {
     if (onLoad) executeOperator(onLoad, { panel_id: panelId });
@@ -13,7 +19,13 @@ export function CustomPanel({ panelId, onLoad, onUnLoad }) {
     };
   }, []);
 
-  return <pre>{JSON.stringify(panelState, null, 2)}</pre>;
+  return (
+    <OperatorIO
+      schema={renderableSchema}
+      onChange={handlePanelStateChange}
+      data={data}
+    />
+  );
 }
 
 export function defineCustomPanel(onLoad, onUnLoad) {
