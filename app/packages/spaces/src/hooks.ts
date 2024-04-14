33a5fcb2711f9610073e7c6f92120e2147a2603f
imanjra <ibrahim@voxel51.com>
2024-04-14 15:05:03 -0400

    persist python panel on app reload

--- app/packages/spaces/src/hooks.ts
+++ app/packages/spaces/src/hooks.ts
@@ -1,6 +1,10 @@
-import { PluginComponentType, useActivePlugins } from "@fiftyone/plugins";
+import {
+  PluginComponentType,
+  subscribeToRegistry,
+  useActivePlugins,
+} from "@fiftyone/plugins";
 import * as fos from "@fiftyone/state";
-import { useContext, useEffect, useMemo, useRef } from "react";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { SortableEvent } from "react-sortablejs";
 import {
   useRecoilCallback,
@@ -96,6 +100,17 @@ export function usePanels() {
 }
 
 export function usePanel(name: SpaceNodeType) {
+  const panels = usePanels();
+  return panels.find((panel) => panel.name === name);
+}
+
+export function useReactivePanel(name: SpaceNodeType) {
+  const [_, setCount] = useState(0);
+  useEffect(() => {
+    return subscribeToRegistry(() => {
+      setCount((count) => count + 1); // trigger re-resolution of panels
+    });
+  }, []);
   const panels = usePanels();
   return panels.find((panel) => panel.name === name);
 }
