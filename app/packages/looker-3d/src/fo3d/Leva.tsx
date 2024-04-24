fec45c39e5385166aa8448a5d7f20a6facc9ee4e
Sashank Aryal <sashank2012@gmail.com>
2024-04-24 17:10:12 -0500

    persist leva panel position in browser storage

--- app/packages/looker-3d/src/fo3d/Leva.tsx
+++ app/packages/looker-3d/src/fo3d/Leva.tsx
@@ -1,19 +1,124 @@
 import { useFont, useTheme } from "@fiftyone/components";
 import * as fos from "@fiftyone/state";
 import { Leva as LevaOptions } from "leva";
+import { useCallback, useLayoutEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { useRecoilValue } from "recoil";
 import { LevaContainer } from "../containers";
+
+const LEVA_CONTAINER_ID = "fo-leva-container";
+const LEVA_POSITION_KEY = "fo-leva-container-position";
 
 function Leva() {
   const theme = useTheme();
   const font = useFont();
   const isSidebarVisible = useRecoilValue(fos.sidebarVisible(true));
+  const [isDragging, setIsDragging] = useState(false);
+
+  const updateLevaContainerPosition = useCallback(
+    (container: HTMLDivElement) => {
+      const levaContainerRect = container.getBoundingClientRect();
+      const levaContainerPosition = {
+        x: levaContainerRect.x,
+        y: levaContainerRect.y,
+      };
+
+      localStorage.setItem(
+        LEVA_POSITION_KEY,
+        JSON.stringify(levaContainerPosition)
+      );
+    },
+    []
+  );
+
+  const mouseDownEventHandler = useCallback(() => {
+    setIsDragging(true);
+
+    const levaContainer = document.getElementById(LEVA_CONTAINER_ID)
+      .firstElementChild as HTMLDivElement;
+
+    updateLevaContainerPosition(levaContainer);
+  }, [updateLevaContainerPosition]);
+
+  const mouseMoveEventHandler = useCallback(() => {
+    if (!isDragging) {
+      return;
+    }
+
+    const levaContainer = document.getElementById(LEVA_CONTAINER_ID)
+      .firstElementChild as HTMLDivElement;
+
+    updateLevaContainerPosition(levaContainer);
+  }, [isDragging, updateLevaContainerPosition]);
+
+  const mouseUpEventHandler = useCallback(() => {
+    if (!isDragging) {
+      return;
+    }
+
+    setIsDragging(false);
+  }, [isDragging]);
+
+  // this effect adds event listeners to the leva container header to handle dragging
+  useLayoutEffect(() => {
+    const levaParentContainer = document.getElementById(LEVA_CONTAINER_ID);
+
+    if (!levaParentContainer) {
+      return;
+    }
+
+    const levaContainer =
+      levaParentContainer.firstElementChild as HTMLDivElement;
+    const levaContainerHeader =
+      levaContainer.firstElementChild as HTMLDivElement;
+
+    levaContainerHeader.addEventListener("mousedown", mouseDownEventHandler);
+    levaContainerHeader.addEventListener("mouseup", mouseUpEventHandler);
+    levaContainerHeader.addEventListener("mousemove", mouseMoveEventHandler);
+
+    return () => {
+      levaContainerHeader.removeEventListener(
+        "mousedown",
+        mouseDownEventHandler
+      );
+      levaContainerHeader.removeEventListener("mouseup", mouseUpEventHandler);
+      levaContainerHeader.removeEventListener(
+        "mousemove",
+        mouseMoveEventHandler
+      );
+    };
+  }, [mouseMoveEventHandler, mouseUpEventHandler, mouseDownEventHandler]);
+
+  // this effect syncs the position of the leva container with the local storage on component mount
+  useLayoutEffect(() => {
+    const levaParentContainer = document.getElementById(LEVA_CONTAINER_ID);
+
+    if (!levaParentContainer) {
+      return;
+    }
+
+    // restore from local storage
+    const levaPosition = localStorage.getItem(LEVA_POSITION_KEY);
+    if (levaPosition) {
+      const { x, y } = JSON.parse(levaPosition);
+      levaParentContainer.style.left = `${x}px`;
+      levaParentContainer.style.right = "unset";
+      levaParentContainer.style.top = `${y}px`;
+    }
+  }, []);
+
+  const [isLevaCollapsed, setIsLevaCollapsed] = fos.useBrowserStorage(
+    "fo-is-leva-collapsed",
+    false
+  );
 
   return (
     <>
       {createPortal(
-        <LevaContainer isSidebarVisible={isSidebarVisible}>
+        <LevaContainer
+          isSidebarVisible={isSidebarVisible}
+          id={LEVA_CONTAINER_ID}
+        >
           <LevaOptions
             theme={{
               colors: {
@@ -40,6 +145,10 @@ function Leva() {
             fill
             hideCopyButton
             flat
+            collapsed={{
+              collapsed: isLevaCollapsed,
+              onChange: setIsLevaCollapsed,
+            }}
           />
         </LevaContainer>,
         document.getElementById("modal")
