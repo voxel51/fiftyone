d75fef20c9a959cdefcd6e8bad1ea6283d5528ae
Benjamin Kane <ben@voxel51.com>
2024-04-24 10:08:04 -0400

    fix e2e

--- e2e-pw/src/oss/specs/smoke-tests/quickstart-groups.spec.ts
+++ e2e-pw/src/oss/specs/smoke-tests/quickstart-groups.spec.ts
@@ -27,59 +27,38 @@ const test = base.extend<{
 
 test.describe("quickstart-groups", () => {
   test.beforeAll(async ({ fiftyoneLoader }) => {
-    await fiftyoneLoader.executePythonCode(
-      `
-        import fiftyone as fo
-        import fiftyone.zoo as foz
+    await fiftyoneLoader.loadZooDataset("quickstart-groups", datasetName, {
+      max_samples: 12,
+    });
+  });
 
-        dataset = foz.load_zoo_dataset("quickstart-groups")
-        dataset.persistent = True
-
-        step = 25
-        for slice in dataset.group_slices:
-            dataset.group_slice = slice
-            scene_id = 0
-            order_by = 0
-            for sample in dataset:
-                sample.set_field("scene_id", scene_id // step)
-                sample.set_field("timestamp", order_by % step)
-                sample.save()
-                scene_id += 1
-                order_by += 1
-
-        view = dataset.group_by("scene_id", order_by="timestamp")
-        dataset.save_view("dynamic", view)
-        `
-    );
+  test.beforeEach(async ({ page, fiftyoneLoader }) => {
+    await fiftyoneLoader.waitUntilGridVisible(page, datasetName);
   });
 
   test("should have four lookers with 'left' as default slice", async ({
-    fiftyoneLoader,
     grid,
     page,
   }) => {
-    await fiftyoneLoader.waitUntilGridVisible(page, datasetName);
     await grid.assert.isLookerCountEqualTo(4);
     const selectorSlice = page.getByTestId("selector-slice");
     await expect(selectorSlice).toHaveValue("left");
   });
 
-  test("entry counts works", async ({ fiftyoneLoader, grid, page }) => {
-    await fiftyoneLoader.waitUntilGridVisible(page, datasetName);
-    expect(await grid.getEntryCountText()).toEqual("200 groups with slice");
+  test("entry counts works", async ({ grid }) => {
+    expect(await grid.getEntryCountText()).toEqual("4 groups with slice");
 
     await grid.actionsRow.toggleDisplayOptions();
     await grid.actionsRow.displayActions.setSidebarStatisticsMode("group");
 
     // note: entry-counts might take a while to change, which is why we're asserting using polling
     await grid.assert.isEntryCountTextEqualTo(
-      "(600 samples) 200 groups with slice"
+      "(12 samples) 4 groups with slice"
     );
   });
 
   test.describe("modal", () => {
-    test.beforeEach(async ({ fiftyoneLoader, grid, modal, page }) => {
-      await fiftyoneLoader.waitUntilGridVisible(page, datasetName);
+    test.beforeEach(async ({ modal, grid }) => {
       await grid.openFirstSample();
       await modal.waitForSampleLoadDomAttribute();
     });
@@ -172,24 +151,5 @@ test.describe("quickstart-groups", () => {
 
     await modal.navigateSlice("group.name", "right");
     await modal.sidebar.assert.verifySidebarEntryText("group.name", "right");
-  });
-
-  test("dynamic groups", async ({ fiftyoneLoader, grid, modal, page }) => {
-    await fiftyoneLoader.waitUntilGridVisible(page, datasetName, {
-      savedView: "dynamic",
-    });
-    await grid.assert.isEntryCountTextEqualTo("8 groups with slice");
-    await grid.assert.isLookerCountEqualTo(8);
-
-    await grid.openFirstSample();
-    await modal.group.assert.assertGroupPinnedText("left is pinned");
-    await modal.sidebar.assert.verifySidebarEntryText("group.name", "left");
-    await modal.group.assert.assertIsCarouselVisible();
-    await modal.navigateSlice("group.name", "right");
-    await modal.sidebar.assert.verifySidebarEntryText("group.name", "left");
-
-    await modal.clickOnLooker3d();
-    await modal.group.assert.assertGroupPinnedText("pcd is pinned");
-    await modal.sidebar.assert.verifySidebarEntryText("group.name", "pcd");
   });
 });
