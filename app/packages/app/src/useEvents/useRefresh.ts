033f4b6acc34c291307a6647c09e0f67c86bddc6
Benjamin Kane <ben@voxel51.com>
2024-05-02 17:07:02 -0400

    handle state payload in useRefresh

--- app/packages/app/src/useEvents/useRefresh.ts
+++ app/packages/app/src/useEvents/useRefresh.ts
@@ -1,8 +1,37 @@
-import { useRefresh as useRefreshState } from "@fiftyone/state";
+import { subscribe } from "@fiftyone/relay";
+import * as fos from "@fiftyone/state";
+import { env } from "@fiftyone/utilities";
+import { useCallback } from "react";
+import { getDatasetName, getParam, resolveURL } from "../utils";
 import { EventHandlerHook } from "./registerEvent";
+import { processState } from "./utils";
 
-const useRefresh: EventHandlerHook = () => {
-  return useRefreshState();
+const useRefresh: EventHandlerHook = ({ router, session }) => {
+  return useCallback(
+    (payload: any) => {
+      const state = processState(session.current, payload.state);
+      const stateless = env().VITE_NO_STATE;
+      const path = resolveURL({
+        currentPathname: router.history.location.pathname,
+        currentSearch: router.history.location.search,
+        nextDataset: stateless
+          ? getDatasetName()
+          : payload.state.dataset ?? null,
+        nextView: stateless ? getParam("view") : payload.state.saved_view_slug,
+        extra: {
+          workspace: state.workspace?._name || null,
+        },
+      });
+
+      const unsubscribe = subscribe((_, { set }) => {
+        set(fos.refresher, (cur) => cur + 1);
+        unsubscribe();
+      });
+
+      router.history.replace(path, state);
+    },
+    [router, session]
+  );
 };
 
 export default useRefresh;
