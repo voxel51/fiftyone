5c49b5cdf8992dcae0e2a6bb441483ad633ac9ff
imanjra <ibrahim@voxel51.com>
2024-05-06 16:32:55 -0400

    invoke python panel on load on external state changes

--- app/packages/operators/src/constants.ts
+++ app/packages/operators/src/constants.ts
@@ -7,3 +7,4 @@ export enum OPERATOR_PROMPT_AREAS {
   DRAWER_LEFT = "operator_prompt_area_drawer_left",
   DRAWER_RIGHT = "operator_prompt_area_drawer_right",
 }
+export const PANEL_LOAD_TIMEOUT = 5000;
