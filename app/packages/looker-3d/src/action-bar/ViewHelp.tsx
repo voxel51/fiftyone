2753171da5125c03a31dc7b33e18d5edebc2cfaf
Sashank Aryal <sashank2012@gmail.com>
2024-05-07 15:25:28 -0500

    more clarity in help

--- app/packages/looker-3d/src/action-bar/ViewHelp.tsx
+++ app/packages/looker-3d/src/action-bar/ViewHelp.tsx
@@ -4,15 +4,19 @@ import { ActionItem } from "../containers";
 import { ACTION_VIEW_HELP } from "../constants";
 
 export const LOOKER3D_HELP_ITEMS = [
-  { shortcut: "Left mouse drag", title: "Rotate", detail: "Rotate the camera" },
-  { shortcut: "Wheel", title: "Zoom", detail: "Zoom in and out" },
-  {
-    shortcut: "Shift + drag",
-    title: "Translate",
-    detail: "Translate the camera",
-  },
+  { shortcut: "Left drag", title: "Rotate", detail: "Rotate the camera" },
   { shortcut: "T", title: "Top-down", detail: "Reset camera to top-down view" },
+  {
+    shortcut: "Ctrl + left drag / Wheel",
+    title: "Zoom",
+    detail: "Zoom in and out (dolly)",
+  },
   { shortcut: "E", title: "Ego-view", detail: "Reset the camera to ego view" },
+  {
+    shortcut: "Shift + left drag / Right drag",
+    title: "Translate",
+    detail: "Translate the camera (truck)",
+  },
   { shortcut: "B", title: "Background", detail: "Toggle background" },
   { shortcut: "C", title: "Controls", detail: "Toggle controls" },
   { shortcut: "G", title: "Grid", detail: "Toggle grid" },
