23d91af18b7bec1208328fc8e6c46c6f19c62164
Sashank Aryal <sashank2012@gmail.com>
2024-05-07 20:55:37 -0500

    make view fo3d an icon button

--- app/packages/looker-3d/src/action-bar/ViewHelp.tsx
+++ app/packages/looker-3d/src/action-bar/ViewHelp.tsx
@@ -1,7 +1,7 @@
 import { HelpIcon } from "@fiftyone/components";
 import { useHelpPanel } from "@fiftyone/state";
-import { ActionItem } from "../containers";
 import { ACTION_VIEW_HELP } from "../constants";
+import { ActionItem } from "../containers";
 
 export const LOOKER3D_HELP_ITEMS = [
   { shortcut: "Left drag", title: "Rotate", detail: "Rotate the camera" },
@@ -9,19 +9,20 @@ export const LOOKER3D_HELP_ITEMS = [
   {
     shortcut: "Ctrl + left drag / Wheel",
     title: "Zoom",
-    detail: "Zoom in and out (dolly)",
+    detail: "Zoom in and out",
   },
   { shortcut: "E", title: "Ego-view", detail: "Reset the camera to ego view" },
   {
     shortcut: "Shift + left drag / Right drag",
     title: "Translate",
-    detail: "Translate the camera (truck)",
+    detail: "Translate the camera",
   },
   { shortcut: "B", title: "Background", detail: "Toggle background" },
   { shortcut: "C", title: "Controls", detail: "Toggle controls" },
   { shortcut: "G", title: "Grid", detail: "Toggle grid" },
   { shortcut: "F", title: "Full-screen", detail: "Toggle full-screen" },
   { shortcut: "J", title: "Json ", detail: "Toggle JSON view" },
+  { shortcut: "I", title: "FO3D ", detail: "Toggle FO3D JSON view" },
   { shortcut: "?", title: "Display help", detail: "Display this help window" },
   { shortcut: "ESC", title: "Escape ", detail: "Escape the current context" },
 ];
