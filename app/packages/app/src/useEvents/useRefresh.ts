efde9ff8111346f23620d5bd600a27c6febb909d
Benjamin Kane <ben@voxel51.com>
2024-05-08 15:45:23 -0400

    skip e2e refresh

--- app/packages/app/src/useEvents/useRefresh.ts
+++ app/packages/app/src/useEvents/useRefresh.ts
@@ -1,5 +1,6 @@
 import { subscribe } from "@fiftyone/relay";
 import * as fos from "@fiftyone/state";
+import { env } from "@fiftyone/utilities";
 import { useCallback } from "react";
 import { resolveURL } from "../utils";
 import { EventHandlerHook } from "./registerEvent";
@@ -8,6 +9,12 @@ import { processState } from "./utils";
 const useRefresh: EventHandlerHook = ({ router, session }) => {
   return useCallback(
     (payload: any) => {
+      const stateless = env().VITE_NO_STATE;
+      if (stateless) {
+        // skip in e2e
+        return;
+      }
+
       const state = processState(session.current, payload.state);
       const path = resolveURL({
         currentPathname: router.history.location.pathname,
