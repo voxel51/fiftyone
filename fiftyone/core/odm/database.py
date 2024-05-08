842cbda992cbd18374c68cce5efd966a3b9b8822
Stuart Wheaton <stuart@voxel51.com>
2024-05-08 11:03:36 -0400

    utc

--- fiftyone/core/odm/database.py
+++ fiftyone/core/odm/database.py
@@ -423,7 +423,7 @@ def _apply_options(db):
     timezone = fo.config.timezone
 
     if not timezone:
-        return db
+        timezone = "utc"
 
     if timezone.lower() == "local":
         tzinfo = datetime.now().astimezone().tzinfo
