d0c3c942f8c909de083eeb339cfc016566cf7eb7
Benjamin Kane <ben@voxel51.com>
2024-05-09 11:40:18 -0400

    cleanup, comment

--- app/packages/app/src/useEvents/useRefresh.ts
+++ app/packages/app/src/useEvents/useRefresh.ts
@@ -1,20 +1,17 @@
 import { subscribe } from "@fiftyone/relay";
 import * as fos from "@fiftyone/state";
-import { env } from "@fiftyone/utilities";
 import { useCallback } from "react";
 import { resolveURL } from "../utils";
 import { EventHandlerHook } from "./registerEvent";
 import { processState } from "./utils";
 
+/**
+ * Handles a session state refresh event from the server and reloads the page
+ * query and aggregation requests
+ */
 const useRefresh: EventHandlerHook = ({ router, session }) => {
   return useCallback(
     (payload: any) => {
-      const stateless = env().VITE_NO_STATE;
-      if (stateless) {
-        // skip in e2e
-        return;
-      }
-
       const state = processState(session.current, payload.state);
       const path = resolveURL({
         currentPathname: router.history.location.pathname,
