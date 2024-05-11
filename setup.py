bb7c8a9b2afd7ec3321f73761a0020848467def0
voxelbot <voxelbot@voxel51.com>
2024-05-11 11:26:19 +0000

    [skip ci] automated bump `fiftyone` to 0.18.0.dev5 and `fiftyone-app` to 1.8.0.dev5

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="0.18.0.dev4",
+    version="0.18.0.dev5",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
