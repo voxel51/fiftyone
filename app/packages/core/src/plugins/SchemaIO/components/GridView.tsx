95f70d542634c8d5e0b6808e511e3ef47bb4c076
imanjra <ibrahim@voxel51.com>
2024-05-13 11:37:30 -0400

    css grid based GridView

--- app/packages/core/src/plugins/SchemaIO/components/GridView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/GridView.tsx
@@ -1,19 +1,15 @@
-import { Box, Grid } from "@mui/material";
+import { Box, BoxProps } from "@mui/material";
 import React from "react";
 import { HeaderView } from ".";
-import {
-  getComponentProps,
-  getLayoutProps,
-  getPath,
-  spaceToHeight,
-} from "../utils";
+import { getComponentProps, getPath, getProps, spaceToHeight } from "../utils";
+import { ObjectSchemaType, ViewPropsType } from "../utils/types";
 import DynamicIO from "./DynamicIO";
 
-export default function GridView(props) {
+export default function GridView(props: ViewPropsType) {
   const { schema, path, data } = props;
-  const { properties, view = {} } = schema;
+  const { properties, view = {} } = schema as ObjectSchemaType;
   const { alignX, alignY, align_x, align_y, gap = 1, orientation } = view;
-  const direction = orientation === "vertical" ? "column" : "row";
+  const direction = orientation === "vertical" ? "row" : "column";
 
   const propertiesAsArray = [];
 
@@ -23,46 +19,56 @@ export default function GridView(props) {
 
   const layoutHeight = props?.layout?.height;
 
+  const baseGridProps: BoxProps = {
+    sx: {
+      display: "grid",
+      gap,
+      justifyContent: alignX || align_x || "start",
+      alignItems: alignY || align_y || "start",
+      gridAutoFlow: direction,
+    },
+  };
+
   return (
     <Box {...getComponentProps(props, "container")}>
       <HeaderView {...props} divider nested />
-      <Grid
-        container
-        spacing={gap}
-        direction={direction}
-        justifyContent={alignX || align_x}
-        alignItems={alignY || align_y}
-        {...getLayoutProps(props)}
-        {...getComponentProps(props, "gridContainer")}
-      >
+      <Box {...getProps(props, "grid", baseGridProps)}>
         {propertiesAsArray.map((property) => {
-          const space = property?.view?.space || "auto";
-          const { id } = property;
-          return (
-            <Grid
-              key={id}
-              item
-              xs={space}
-              {...getLayoutProps({ ...props, schema: property })}
-              minHeight={
+          const { id, view = {} } = property;
+          const { alignX, alignY, align_x, align_y, space } = view;
+          const itemPath = getPath(path, id);
+          const baseItemProps: BoxProps = {
+            sx: {
+              justifySelf: alignX || align_x || "start",
+              alignSelf: alignY || align_y || "start",
+              maxHeight:
                 orientation === "vertical"
                   ? spaceToHeight(space, layoutHeight)
-                  : undefined
-              }
-              {...getComponentProps(props, "gridItem")}
+                  : undefined,
+            },
+            key: id,
+          };
+          return (
+            <Box
+              key={id}
+              {...getProps(
+                { ...props, schema: property },
+                "item",
+                baseItemProps
+              )}
             >
               <DynamicIO
                 {...props}
                 schema={property}
-                path={getPath(path, id)}
+                path={itemPath}
                 data={data?.[id]}
                 parentSchema={schema}
                 relativePath={id}
               />
-            </Grid>
+            </Box>
           );
         })}
-      </Grid>
+      </Box>
     </Box>
   );
 }
--- app/packages/core/src/plugins/SchemaIO/utils/index.ts
+++ app/packages/core/src/plugins/SchemaIO/utils/index.ts
@@ -1,33 +1,58 @@
+import { get, merge } from "lodash";
 import * as components from "../components";
 import { UnsupportedView } from "../components";
-import { get } from "lodash";
+import {
+  ArraySchemaType,
+  CustomComponentsType,
+  SchemaType,
+  ViewPropsType,
+} from "./types";
+import { getLayoutProps } from "./layout";
 
-export function log(...args) {
+export function log(...args: string[]) {
   console.groupCollapsed(">>>", ...args);
   console.trace();
   console.groupEnd();
 }
 
-export function getPath(basePath, propertyKey) {
+export function getPath(basePath: string, propertyKey: string) {
   let computedPath = basePath;
   if (computedPath) computedPath += ".";
   computedPath += propertyKey;
   return computedPath;
 }
 
-export function getComponent(schema, customComponents?: object) {
+export function getComponent(
+  schema: SchemaType,
+  customComponents?: CustomComponentsType
+) {
   const { component } = schema?.view || {};
   return (
     customComponents?.[component] || components[component] || UnsupportedView
   );
 }
 
-export function getComponentProps(props, id) {
-  return get(props, `schema.view.componentsProps.${id}`, {});
+export function getComponentProps<P>(
+  props: ViewPropsType,
+  id: string,
+  baseProps?: P
+) {
+  return merge(
+    baseProps || {},
+    get(props, `schema.view.componentsProps.${id}`, {})
+  );
+}
+
+export function getProps<P>(
+  props: ViewPropsType,
+  id: string,
+  baseProps?: P
+): P {
+  return merge(getLayoutProps(props), getComponentProps(props, id, baseProps));
 }
 
 // add map,tuple,oneof support
-export function getEmptyValue(schema) {
+export function getEmptyValue(schema: ArraySchemaType) {
   const itemsType = schema?.items?.type || "string";
   const emptyValuesByType = {
     string: "",
@@ -35,19 +60,21 @@ export function getEmptyValue(schema) {
     object: {},
     array: [],
   };
-  return emptyValuesByType[itemsType];
+  return emptyValuesByType[
+    itemsType as unknown as keyof typeof emptyValuesByType
+  ];
 }
 
-export function getErrorsForView(props) {
+export function getErrorsForView(props: ViewPropsType) {
   const { errors, path } = props;
 
   const errorsForView = errors?.[path];
   return Array.isArray(errorsForView) ? errorsForView : [];
 }
 
+export { default as autoFocus } from "./auto-focus";
 export * from "./generate-schema";
 export * from "./layout";
-export { default as autoFocus } from "./auto-focus";
 
 // Views that renders DynamicIO as a child component
 const COMPOSITE_VIEWS = [
@@ -58,6 +85,6 @@ const COMPOSITE_VIEWS = [
   "TupleView",
 ];
 
-export function isCompositeView(schema) {
+export function isCompositeView(schema: SchemaType) {
   return COMPOSITE_VIEWS.includes(schema?.view?.component);
 }
