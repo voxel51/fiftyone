d2ca5f7137729d88be9cce0f53701018c06a6c29
imanjra <ibrahim@voxel51.com>
2024-06-04 14:12:55 -0400

    fix operator prompt hidden for executing, output, and error (#4445)
    * fix operator prompt hidden for executing, output, and error
    
    * add e2e tests for operators prompt
    
    * operator drawer prompt tweaks
    
    * mv to asserter
    
    * lint
    
    ---------
    
    Co-authored-by: Benjamin Kane <ben@voxel51.com>

--- app/packages/operators/src/OperatorPrompt/OperatorModalPrompt.tsx
+++ app/packages/operators/src/OperatorPrompt/OperatorModalPrompt.tsx
@@ -6,28 +6,12 @@ import { getOperatorPromptConfigs } from "../utils";
 
 export default function OperatorModalPrompt(props: OperatorPromptPropsType) {
   const { prompt } = props;
-  const {
-    title,
-    hasValidationErrors,
-    resolving,
-    pendingResolve,
-    validationErrorsStr,
-    ...otherConfigs
-  } = getOperatorPromptConfigs(prompt);
+  const promptConfig = getOperatorPromptConfigs(prompt);
   return (
     <OperatorPalette
-      {...otherConfigs}
-      title={title}
-      onClose={otherConfigs.onCancel || prompt.close}
+      {...promptConfig}
       submitOnControlEnter
-      disableSubmit={hasValidationErrors || resolving || pendingResolve}
-      disabledReason={
-        hasValidationErrors
-          ? "Cannot execute operator with validation errors\n\n" +
-            validationErrorsStr
-          : "Cannot execute operator while validating form"
-      }
-      loading={resolving || pendingResolve}
+      dialogProps={{ PaperProps: { "data-cy": "operators-prompt-modal" } }}
     >
       <PaletteContentContainer>
         <OperatorPromptBody operatorPrompt={prompt} />
