106aad2cfd4efda04c79109c093d44d775ab3553
brimoor <brimoor@umich.edu>
2024-06-05 10:40:13 -0400

    abspath vs realpath

--- fiftyone/utils/data/exporters.py
+++ fiftyone/utils/data/exporters.py
@@ -1205,8 +1205,8 @@ class MediaExporter(object):
         input_to_output_paths = {}
         for asset_path in asset_paths:
             if not fos.isabs(asset_path):
-                absolute_asset_path = fos.join(
-                    os.path.dirname(fo3d_path), asset_path
+                absolute_asset_path = fos.abspath(
+                    os.path.join(os.path.dirname(fo3d_path), asset_path)
                 )
             else:
                 absolute_asset_path = asset_path
@@ -1216,6 +1216,7 @@ class MediaExporter(object):
             asset_output_path = self._filename_maker.get_output_path(
                 absolute_asset_path
             )
+            # By convention, we always write *relative* asset paths
             input_to_output_paths[asset_path] = os.path.relpath(
                 asset_output_path, os.path.dirname(fo3d_output_path)
             )
