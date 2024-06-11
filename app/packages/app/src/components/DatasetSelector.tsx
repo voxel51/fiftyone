3d17bb4acf48fa0b7cd55c852797aa223b37ac5d
Benjamin Kane <ben@voxel51.com>
2024-06-11 12:49:53 -0400

    fix return to index page, add index-page e2e spec (#4478)

--- app/packages/app/src/components/DatasetSelector.tsx
+++ app/packages/app/src/components/DatasetSelector.tsx
@@ -35,6 +35,7 @@ const DatasetSelector: React.FC<{
 
   return (
     <Selector<string>
+      cy={"dataset"}
       component={DatasetLink}
       placeholder={"Select dataset"}
       inputStyle={{ height: 40, maxWidth: 300 }}
--- app/packages/app/src/pages/IndexPage.tsx
+++ app/packages/app/src/pages/IndexPage.tsx
@@ -5,6 +5,7 @@ import { graphql } from "relay-runtime";
 import Nav from "../components/Nav";
 import { Route } from "../routing";
 import { IndexPageQuery } from "./__generated__/IndexPageQuery.graphql";
+import style from "./index.module.css";
 
 const IndexPageQueryNode = graphql`
   query IndexPageQuery($search: String = "", $count: Int, $cursor: String) {
@@ -28,7 +29,11 @@ const IndexPage: Route<IndexPageQuery> = ({ prepared }) => {
   return (
     <>
       <Nav fragment={queryRef} hasDataset={false} />
-      <Starter mode={totalDatasets === 0 ? "ADD_DATASET" : "SELECT_DATASET"} />
+      <div className={style.page} data-cy={"index-page"}>
+        <Starter
+          mode={totalDatasets === 0 ? "ADD_DATASET" : "SELECT_DATASET"}
+        />
+      </div>
       <Snackbar />
     </>
   );
