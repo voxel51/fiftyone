ea71a7c5773da2fee7cdee3b2a2bb7c8fddcc305
Benjamin Kane <ben@voxel51.com>
2024-06-11 14:02:57 -0400

    add slice test for dynamic groups paging

--- app/packages/state/src/recoil/dynamicGroups.test.ts
+++ app/packages/state/src/recoil/dynamicGroups.test.ts
@@ -2,10 +2,14 @@ import { describe, expect, it, vi } from "vitest";
 vi.mock("recoil");
 vi.mock("recoil-relay");
 
-import { setMockAtoms, TestSelector } from "../../../../__mocks__/recoil";
+import {
+  setMockAtoms,
+  TestSelector,
+  TestSelectorFamily,
+} from "../../../../__mocks__/recoil";
 import * as dynamicGroups from "./dynamicGroups";
 
-describe("handles non-nested dynamic groups", () => {
+describe("handles dynamic groups", () => {
   const testNesting = <
     TestSelector<typeof dynamicGroups.isNonNestedDynamicGroup>
   >(<unknown>dynamicGroups.isNonNestedDynamicGroup);
@@ -24,5 +28,32 @@ describe("handles non-nested dynamic groups", () => {
       parentMediaTypeSelector: "group",
     });
     expect(testNesting()).toBe(false);
+  });
+
+  const modalPageSelector = <
+    TestSelectorFamily<typeof dynamicGroups.dynamicGroupPageSelector>
+  >(<unknown>(
+    dynamicGroups.dynamicGroupPageSelector({ value: "test", modal: true })
+  ));
+  const pageSelector = <
+    TestSelectorFamily<typeof dynamicGroups.dynamicGroupPageSelector>
+  >(<unknown>(
+    dynamicGroups.dynamicGroupPageSelector({ value: "test", modal: false })
+  ));
+
+  it("uses correct slice", () => {
+    setMockAtoms({
+      datasetName: "dataset",
+      dynamicGroupViewQuery: () => [],
+      groupSlice: "main",
+      modalGroupSlice: "modal",
+    });
+
+    expect(pageSelector()(0, 1).filter).toStrictEqual({
+      group: { slice: "main" },
+    });
+    expect(modalPageSelector()(0, 1).filter).toStrictEqual({
+      group: { slice: "modal" },
+    });
   });
 });
