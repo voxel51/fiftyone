9dcd204960336fa6f58a36d66e338d33f7fce6ee
imanjra <ibrahim@voxel51.com>
2024-07-11 15:21:37 -0400

    enhancement for grid layout

--- app/packages/state/src/hooks/useDimensions.ts
+++ app/packages/state/src/hooks/useDimensions.ts
@@ -1,28 +1,48 @@
-import { useState, useRef, useLayoutEffect, DOMElement } from "react";
+import { throttle } from "lodash";
+import { useLayoutEffect, useMemo, useRef, useState } from "react";
 
-type Dimensions = {
-  width: number;
-  height: number;
-};
+const DIMENSION_REFRESH_THROTTLE = 250;
+
+type Dimensions = { width: number; height: number };
 
 export default function useDimensions() {
   const [bounds, update] = useState<Dimensions>(null);
   const ref = useRef<HTMLElement>();
+  const widthRef = useRef<HTMLElement>();
+  const heightRef = useRef<HTMLElement>();
 
   function refresh() {
-    if (!ref.current) return;
-    const { width, height } = ref.current.getBoundingClientRect();
+    if (!ref.current && !widthRef.current && !heightRef.current) return;
+    let height, width;
+    if ((!widthRef.current || !heightRef.current) && ref.current) {
+      height = ref.current.getBoundingClientRect().height;
+      width = ref.current.getBoundingClientRect().width;
+    }
+    if (widthRef.current) {
+      width = widthRef.current.getBoundingClientRect().width;
+    }
+    if (heightRef.current) {
+      height = heightRef.current.getBoundingClientRect().height;
+    }
     update({ width, height });
   }
 
-  const ro = new ResizeObserver(refresh);
+  const throttledRefresh = useMemo(() => {
+    return throttle(() => {
+      refresh();
+    }, DIMENSION_REFRESH_THROTTLE);
+  }, []);
+
+  const ro = useMemo(() => {
+    return new ResizeObserver(throttledRefresh);
+  }, [throttledRefresh]);
 
   useLayoutEffect(() => {
     if (ref.current) {
       refresh();
       ro.observe(ref.current);
     }
-  }, [ref.current]);
+  }, [ro, ref.current]); // eslint-disable-line
 
-  return { bounds, ref, update, refresh };
+  return { bounds, ref, heightRef, widthRef, update, refresh };
 }
