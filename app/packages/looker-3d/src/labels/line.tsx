0b7be86303077c8576094fbeefd7b9017f21a804
Camron Staley <camronstaley@camrons-mbp.lan>
2024-07-23 16:30:19 -0500

    Adding polyline support for resizing

--- app/packages/looker-3d/src/labels/line.tsx
+++ app/packages/looker-3d/src/labels/line.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import * as THREE from "three";
 import { OverlayProps } from "./shared";
+import { useRecoilState } from "recoil";
+import { polylineLabelLineWidthAtom } from "../state";
+import { Line2 } from "three/examples/jsm/lines/Line2";
+import { LineMaterial } from "three/examples/jsm/lines/LineMaterial";
+import { LineGeometry } from "three/examples/jsm/lines/LineGeometry";
+import { extend, ReactThreeFiber } from "@react-three/fiber";
+
+extend({ Line2, LineMaterial, LineGeometry });
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      line2: ReactThreeFiber.Node<Line2, typeof Line2>;
+      lineGeometry: ReactThreeFiber.Node<LineGeometry, typeof LineGeometry>;
+    }
+  }
+}
 
 interface LineProps extends OverlayProps {
   points: THREE.Vector3Tuple[];
@@ -15,6 +32,7 @@ export const Line = ({
   tooltip,
   label,
 }: LineProps) => {
+  const [lineWidth] = useRecoilState(polylineLabelLineWidthAtom);
   const geo = React.useMemo(() => {
     const g = new THREE.BufferGeometry().setFromPoints(
       points.map((p) => new THREE.Vector3(...p))
@@ -29,10 +47,21 @@ export const Line = ({
     return tooltip.getMeshProps(label);
   }, [tooltip, label]);
 
+  const geometry = React.useMemo(() => {
+    return new LineGeometry().setPositions(
+      new Float32Array(geo.attributes.position.array)
+    );
+  }, [geo]);
+  const material = React.useMemo(() => {
+    return new LineMaterial({
+      color: color,
+      linewidth: lineWidth,
+    });
+  }, [color, lineWidth]);
+
   return (
-    <line onClick={onClick} {...tooltipProps} opacity={opacity}>
-      <primitive object={geo} attach="geometry" rotation={rotation} />
-      <lineBasicMaterial attach="material" color={color} />
-    </line>
+    <mesh onClick={onClick}>
+      <line2 geometry={geometry} material={material} />
+    </mesh>
   );
 };
