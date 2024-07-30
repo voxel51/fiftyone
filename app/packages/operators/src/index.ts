820b91f8f65a7867fed0eeb2f1342b8f68cd278c
Benjamin Kane <ben@voxel51.com>
2024-07-30 16:19:08 -0400

    operator type fix

--- app/packages/operators/src/index.ts
+++ app/packages/operators/src/index.ts
@@ -1,25 +1,28 @@
+export { OPERATOR_PROMPT_AREAS } from "./constants";
+export { useOperators } from "./loader";
 export { default as OperatorBrowser } from "./OperatorBrowser";
-export { default as OperatorPrompt } from "./OperatorPrompt";
+export { default as OperatorCore } from "./OperatorCore";
 export { default as OperatorInvocationRequestExecutor } from "./OperatorInvocationRequestExecutor";
+export { default as OperatorIO } from "./OperatorIO";
+export { default as OperatorPlacements } from "./OperatorPlacements";
+export { default as OperatorPrompt } from "./OperatorPrompt";
+export { default as OperatorPromptArea } from "./OperatorPrompt/OperatorPromptArea";
 export {
-  registerOperator,
+  ExecutionContext,
   Operator,
   OperatorConfig,
-  executeOperator,
   abortOperationsByExpression,
   abortOperationsByURI,
+  executeOperator,
+  registerOperator,
 } from "./operators";
 export {
+  useGlobalExecutionContext,
   useOperatorExecutor,
   useOperatorPlacements,
-  useGlobalExecutionContext,
 } from "./state";
-export { useOperators } from "./loader";
-export { default as OperatorPlacements } from "./OperatorPlacements";
 export * as types from "./types";
-export { default as OperatorCore } from "./OperatorCore";
-export { default as OperatorIO } from "./OperatorIO";
-export { default as useOperatorEvent } from "./usePanelEvent";
-export { default as OperatorPromptArea } from "./OperatorPrompt/OperatorPromptArea";
-export { OPERATOR_PROMPT_AREAS } from "./constants";
-export { default as usePanelEvent } from "./usePanelEvent";
+export {
+  default as useOperatorEvent,
+  default as usePanelEvent,
+} from "./usePanelEvent";
