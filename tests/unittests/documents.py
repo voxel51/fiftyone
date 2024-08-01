757c61b3b4dab3509eda7db5743d3f173aa88c29
minhtuevo <minhtuev@voxel51.com>
2024-07-31 20:18:34 -0700

    Added unit tests

---
+++ tests/unittests/documents.py
@@ -0,0 +1,38 @@
+import unittest
+from mongoengine import EmbeddedDocument
+from mongoengine.errors import ValidationError
+from fiftyone.core.fields import EmbeddedDocumentListField
+from fiftyone.core.odm.dataset import SampleFieldDocument
+
+
+class MockEmbeddedDocument(EmbeddedDocument):
+    pass
+
+
+class TestEmbeddedDocumentListField(unittest.TestCase):
+    def test_validate(self):
+        list_field = EmbeddedDocumentListField(SampleFieldDocument)
+
+        # Test with valid input
+        valid_input = [
+            SampleFieldDocument(
+                name="id", ftype="fiftyone.core.fields.ObjectIdField"
+            ),
+            SampleFieldDocument(
+                name="filepath", ftype="fiftyone.core.fields.StringField"
+            ),
+        ]
+        from pdb import set_trace
+
+        set_trace()
+        list_field.validate(valid_input)
+
+        # Test with invalid input
+        invalid_input = valid_input + [None]
+        with self.assertRaises(ValueError):
+            list_field.validate(invalid_input)
+
+        # Test with invalid input type
+        invalid_input = valid_input + [MockEmbeddedDocument()]
+        with self.assertRaises(ValidationError):
+            list_field.validate(invalid_input)
