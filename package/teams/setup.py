ddf72effe0b2137175b0ff058f1794e71e225c49
voxelbot <voxelbot@voxel51.com>
2024-08-06 11:27:01 +0000

    [skip ci] automated bump `fiftyone` to 2.0.0.dev7 and `fiftyone-app` to 2.0.0.dev9

--- package/teams/setup.py
+++ package/teams/setup.py
@@ -47,7 +47,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone-teams-app",
-    version="2.0.0.dev8",
+    version="2.0.0.dev9",
     description=("FiftyOne Teams"),
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="2.0.0.dev6",
+    version="2.0.0.dev7",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
