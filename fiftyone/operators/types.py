725f78cfce3904895833412673062a104191e2d3
Ritchie Martori <ritchie@voxel51.com>
2024-08-08 11:13:00 -0700

    dashboard fixes

--- fiftyone/operators/types.py
+++ fiftyone/operators/types.py
@@ -331,11 +331,10 @@ class Object(BaseType):
     def dashboard(self, name, **kwargs):
         """Defines a dashboard view as a :class:`View`.
 
+        See :class:`DashboardView` for more information.
+
         Args:
             name: the name of the property
-            layout (None): the layout of the dashboard
-            on_layout_change (None): event handler for layout change
-            on_close_item (None): event handler for item close
 
         Returns:
             an :class:`Object`
@@ -2096,13 +2095,21 @@ class DashboardView(View):
 
     Args:
         layout (None): the layout of the dashboard.
-        on_layout_change (None): event triggered when the layout changes
+        on_save_layout (None): event triggered when the layout changes
         on_add_item (None): event triggered when an item is added
         on_remove_item (None): event triggered when an item is closed
         on_edit_item (None): event triggered when an item is edited
         allow_addition (True): whether to allow adding items
         allow_deletion (True): whether to allow deleting items
         allow_edit (True): whether to allow editing items
+        layout (None): the layout of the dashboard
+        cta_title (None): the title of the call to action
+        cta_body (None): the body of the call to action
+        cta_button_label (None): the label of the call to action button
+        rows (None): the number of rows in the dashboard
+        cols (None): the number of columns in the dashboard
+        items (None): the custom layout of the dashboard
+        auto_layout (True): whether to automatically layout the dashboard
     """
 
     def __init__(self, **kwargs):
@@ -2110,6 +2117,14 @@ class DashboardView(View):
         self.allow_addition = kwargs.get("allow_addition", True)
         self.allow_deletion = kwargs.get("allow_deletion", True)
         self.allow_edit = kwargs.get("allow_edit", True)
+        self.layout = kwargs.get("layout", None)
+        self.cta_title = kwargs.get("cta_title", None)
+        self.cta_body = kwargs.get("cta_body", None)
+        self.cta_button_label = kwargs.get("cta_button_label", None)
+        self.rows = kwargs.get("rows", None)
+        self.cols = kwargs.get("cols", None)
+        self.items = kwargs.get("items", None)
+        self.auto_layout = kwargs.get("auto_layout", None)
 
     def to_json(self):
         return {
@@ -2117,6 +2132,14 @@ class DashboardView(View):
             "allow_addition": self.allow_addition,
             "allow_deletion": self.allow_deletion,
             "allow_edit": self.allow_edit,
+            "layout": self.layout,
+            "cta_title": self.cta_title,
+            "cta_body": self.cta_body,
+            "cta_button_label": self.cta_button_label,
+            "rows": self.rows,
+            "cols": self.cols,
+            "items": self.items,
+            "auto_layout": self.auto_layout,
         }
 
 
