f4de23197f93faea397ab8c69484fefb61d8d49a
Stuart Wheaton <stuart@voxel51.com>
2024-08-26 11:57:04 -0400

    Fields have created_at attribute

--- tests/unittests/import_export_tests.py
+++ tests/unittests/import_export_tests.py
@@ -3294,6 +3294,14 @@ class MultitaskImageDatasetTests(ImageDatasetTests):
         created_at2 = dataset2.values("created_at")
         last_modified_at2 = dataset2.values("last_modified_at")
 
+        print(field_created_at1)
+        print(field_created_at2)
+        assert 0
+        self.assertTrue(
+            all(
+                f1 < f2 for f1, f2 in zip(field_created_at1, field_created_at2)
+            )
+        )
         self.assertTrue(
             all(
                 f1 < f2 for f1, f2 in zip(field_created_at1, field_created_at2)
