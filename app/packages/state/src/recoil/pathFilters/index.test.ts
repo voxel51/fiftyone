2a53acef6d6144f8105db31f799ea1aab0dddb0c
Benjamin Kane <ben@voxel51.com>
2024-08-27 10:17:40 -0400

    other tests

--- app/packages/state/src/recoil/pathFilters/index.test.ts
+++ app/packages/state/src/recoil/pathFilters/index.test.ts
@@ -1,13 +1,34 @@
-import { KEYPOINT_FIELD, KEYPOINTS_FIELD } from "@fiftyone/utilities";
+import {
+  DETECTION_FIELD,
+  DETECTIONS_FIELD,
+  KEYPOINT_FIELD,
+  KEYPOINTS_FIELD,
+} from "@fiftyone/utilities";
 import { describe, expect, it } from "vitest";
 import { keypointFilter } from ".";
 
 describe("path filter handling", () => {
-  it("overrides keypoint object filters", () => {
+  it("overrides keypoint array filters", () => {
     const keypoint = keypointFilter("test", KEYPOINT_FIELD, () => false);
     expect(keypoint({ test: [] })).toBe(true);
 
     const keypoints = keypointFilter("test", KEYPOINTS_FIELD, () => false);
     expect(keypoints({ test: [] })).toBe(true);
+  });
+
+  it("overrides keypoint array filters", () => {
+    const keypoint = keypointFilter("test", KEYPOINT_FIELD, () => false);
+    expect(keypoint({ test: [] })).toBe(true);
+
+    const keypoints = keypointFilter("test", KEYPOINTS_FIELD, () => false);
+    expect(keypoints({ test: [] })).toBe(true);
+  });
+
+  it("does not override other label fields", () => {
+    const keypoint = keypointFilter("test", DETECTION_FIELD, () => false);
+    expect(keypoint({ test: [] })).toBe(false);
+
+    const keypoints = keypointFilter("test", DETECTIONS_FIELD, () => false);
+    expect(keypoints({ test: [] })).toBe(false);
   });
 });
