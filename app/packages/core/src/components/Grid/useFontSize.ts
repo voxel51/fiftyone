60c7c76cb7e65c29862d7f1f8fbe095bf107a32d
Benjamin Kane <ben@voxel51.com>
2024-08-28 12:34:11 -0400

    scale grid text bubbles

---
+++ app/packages/core/src/components/Grid/useFontSize.ts
@@ -0,0 +1,22 @@
+import { useCallback } from "react";
+import useThreshold from "./useThreshold";
+
+const MAX = 36;
+const MIN = 8;
+const SCALE_FACTOR = 0.09;
+
+export default (id: string) => {
+  const threshold = useThreshold();
+
+  return useCallback(() => {
+    const width = document.getElementById(id)?.getBoundingClientRect().width;
+    if (!width) {
+      throw new Error("unexpected");
+    }
+
+    return Math.max(
+      Math.min((width / threshold(width)) * SCALE_FACTOR, MAX),
+      MIN
+    );
+  }, [id, threshold]);
+};
