f76f7f3ed9ec087a0788d4f4e0b78cc73f95c983
minhtuevo <minhtuev@voxel51.com>
2024-08-30 11:50:36 -0700

    Updated with field distribution

--- dbscript.py
+++ dbscript.py
@@ -16,13 +16,20 @@ print("Storage size:", etau.to_human_bytes_str(db_info["storageSize"]))
 sample_counts = []
 sizes = []
 datasets = fo.list_datasets()
+field_types = {}
+
+
 for dataset_name in datasets:
 	dataset = fo.load_dataset(dataset_name)
 	dataset_info = dataset.stats(include_indexes=True)
 	sample_counts.append(dataset_info["samples_count"])
 	sizes.append(dataset_info["total_bytes"])
+	for field in dataset.get_field_schema().values():
+		field_type = field.__class__.__name__
+		field_types[field_type] = field_types.get(field_type, 0) + 1
 
 print("Min sample count:", min(sample_counts))
 print("Max sample count:", max(sample_counts))
 print("Min dataset size:", etau.to_human_bytes_str(min(sizes)))
 print("Max dataset size:", etau.to_human_bytes_str(max(sizes)))
+print("Field distribution:", field_types)
