c428471ee75574b49ce15c7f64bcbb1a99fdefc2
Ritchie Martori <ritchie@voxel51.com>
2024-09-16 14:22:39 -0700

    wip

--- app/packages/core/src/plugins/SchemaIO/components/FrameLoaderView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/FrameLoaderView.tsx
@@ -17,9 +17,9 @@ import {
   usePanelState,
   useSetPanelStateById,
 } from "@fiftyone/spaces";
-import { useCreateTimeline } from "@fiftyone/playback/src/lib/use-create-timeline";
-import { Timeline } from "@mui/icons-material";
+import { useTimeline } from "@fiftyone/playback/src/lib/use-timeline";
 import _ from "lodash";
+import { useDefaultTimelineName } from "@fiftyone/playback/src/lib/use-default-timeline-name";
 
 export default function FrameLoaderView(props: ViewPropsType) {
   const { schema, path, data } = props;
@@ -31,57 +31,54 @@ export default function FrameLoaderView(props: ViewPropsType) {
     React.useState(DEFAULT_FRAME_NUMBER);
   const triggerEvent = usePanelEvent();
   const setPanelState = useSetPanelStateById(true);
-  const panelState = usePanelState(null, panelId, true);
+  const { getName } = useDefaultTimelineName();
+  const timelineName = React.useMemo(() => getName(), [getName]);
 
   const loadRange = React.useCallback(async (range: BufferRange) => {
+    console.log("loadRange", range);
     if (on_load_range) {
-      triggerEvent(panelId, {
+      return triggerEvent(panelId, {
         params: { range },
         operator: on_load_range,
       });
     }
+  }, [triggerEvent, on_load_range]);
+
+  useEffect(() => {
+    loadRange([0, 50]);
   }, []);
 
   const [currentFrame, setCurrentFrame] = useState(DEFAULT_FRAME_NUMBER);
 
   const myRenderFrame = React.useCallback((frameNumber: number) => {
     setMyLocalFrameNumber(frameNumber);
-    console.log("rendering frame", frameNumber, props);
+    // console.log("rendering frame", frameNumber, props);
     setPanelState(panelId, (current) => {
       const currentFrameData = data?.frames[frameNumber] || {};
-      const currentData = current.data || {};
-      const updatedData = { ...currentData };
-      console.log("currentData", currentData);
-      _.set(updatedData, target, currentFrameData);
+      const currentData = current.data ? _.cloneDeep(current.data) : {}; // Clone the object
+      let updatedData = { ...currentData };
+    
+      console.log("data?.frames", data?.frames);
+      console.log("target", target);
+      _.set(updatedData, target, currentFrameData); // Use lodash set to update safely
       console.log("updatedData", updatedData);
+    
       return { ...current, data: updatedData };
     });
     setCurrentFrame(frameNumber)
-  }, []);
+  }, [data, setPanelState, panelId, target]);
 
-  const { isTimelineInitialized, subscribe } = useCreateTimeline({
-    config: {
-      totalFrames: 50,
-      loop: true,
-    },
-  });
-
-  useEffect(() => {
-    console.log("data", data);
-  }, [data]);
+  const { isTimelineInitialized, subscribe } = useTimeline();
 
   React.useEffect(() => {
     if (isTimelineInitialized) {
       subscribe({
-        name: timeline_id || GLOBAL_TIMELINE_ID,
-        subscription: {
-          id: "sub1", // hmmm
-          loadRange,
-          renderFrame: myRenderFrame,
-        },
+        id: `sub1`,
+        loadRange,
+        renderFrame: myRenderFrame,
       });
     }
-  }, [isTimelineInitialized, loadRange, myRenderFrame]);
+  }, [isTimelineInitialized, loadRange, myRenderFrame, subscribe]);
 
   return (
     <h1>{currentFrame}</h1>
