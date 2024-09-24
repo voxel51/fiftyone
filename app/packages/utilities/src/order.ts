584a5c213ef50a9dfadd8aafd946e3547ccafb15
imanjra <ibrahim@voxel51.com>
2024-09-24 16:46:46 -0400

    fix operator placements not loading issue

--- app/packages/utilities/src/order.ts
+++ app/packages/utilities/src/order.ts
@@ -1,28 +1,33 @@
+import { useState, useMemo } from "react";
+
 export function useItemsWithOrderPersistence(
   items: SortableItemsType,
   key: string
 ) {
-  const order = localStorage.getItem(key);
-  let orderedItems = items;
-  if (order) {
-    try {
-      const idToIndex: IdToIndexType = JSON.parse(order);
-      orderedItems = sortItems(items, idToIndex);
-    } catch (e) {
-      console.error(e);
+  const initialOrder = localStorage.getItem(key);
+  const [order, updateOrder] = useState(initialOrder);
+
+  const orderedItems = useMemo(() => {
+    if (order) {
+      try {
+        const idToIndex: IdToIndexType = JSON.parse(order);
+        return sortItems(items, idToIndex);
+      } catch (e) {
+        console.error(e);
+      }
     }
-  }
+    return items;
+  }, [items, order]);
 
   const setOrder = (items: SortableItemsType) => {
-    localStorage.setItem(
-      key,
-      JSON.stringify(
-        items.reduce((acc, item, index) => {
-          acc[item.id] = index;
-          return acc;
-        }, {} as IdToIndexType)
-      )
+    const orderCache = JSON.stringify(
+      items.reduce((acc, item, index) => {
+        acc[item.id] = index;
+        return acc;
+      }, {} as IdToIndexType)
     );
+    localStorage.setItem(key, orderCache);
+    updateOrder(orderCache);
   };
   return { orderedItems, setOrder };
 }
