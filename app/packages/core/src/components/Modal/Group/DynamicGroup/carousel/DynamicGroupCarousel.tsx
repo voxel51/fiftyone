e74fe84dd31e6fe176477334e266a84d8c3e4d66
Sashank Aryal <sashank2012@gmail.com>
2024-09-27 18:01:19 -0500

    fix flashlight rerendering bug in dynamic groups

--- app/packages/core/src/components/Modal/Group/DynamicGroup/carousel/DynamicGroupCarousel.tsx
+++ app/packages/core/src/components/Modal/Group/DynamicGroup/carousel/DynamicGroupCarousel.tsx
@@ -2,13 +2,13 @@ import { useTheme } from "@fiftyone/components";
 import * as fos from "@fiftyone/state";
 import { useBrowserStorage } from "@fiftyone/state";
 import { Resizable } from "re-resizable";
-import React from "react";
-import { useRecoilValue } from "recoil";
+import React, { useEffect, useState } from "react";
+import { useRecoilCallback, useRecoilValue } from "recoil";
 import { DynamicGroupsFlashlightWrapper } from "./DynamicGroupsFlashlightWrapper";
 
 const MAX_CAROUSEL_HEIGHT = 600;
 
-export const DynamicGroupCarousel = () => {
+export const DynamicGroupCarousel = React.memo(() => {
   const [height, setHeight] = useBrowserStorage(
     "dynamic-group-carousel-height",
     150
@@ -17,6 +17,37 @@ export const DynamicGroupCarousel = () => {
   const theme = useTheme();
   const isMainVisible = useRecoilValue(fos.groupMediaIsMainVisibleSetting);
 
+  /**
+   * BIG HACK: TODO: FIX ME
+   *
+   * Problem = flashlight is not re-rendering when group by field changes.
+   * Solution was to key it by groupByValue, but when the component
+   * subscribes to the groupByFieldValue using useRecoilValue(fos.groupByFieldValue),
+   * while it solves the problem,it causes flashlight to behave weirdly.
+   * (try scrolling carousel and selecting samples, flashlight will reset to the front)
+   *
+   */
+  const getGroupByFieldValue = useRecoilCallback(({ snapshot }) => () => {
+    const groupByField = snapshot.getLoadable(fos.groupByFieldValue).getValue();
+    return groupByField;
+  });
+
+  const [groupByValue, setGroupByValue] = useState(getGroupByFieldValue());
+  const groupByValueRef = React.useRef(groupByValue);
+  groupByValueRef.current = groupByValue;
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      const groupByFieldValue = getGroupByFieldValue();
+      if (groupByFieldValue !== groupByValueRef.current) {
+        setGroupByValue(groupByFieldValue);
+      }
+    }, 50);
+
+    return () => window.clearInterval(intervalId);
+  }, []);
+
+  console.log("Group by value is ", groupByValueRef.current);
   return (
     <Resizable
       size={{ height, width: "100%" }}
@@ -41,7 +72,7 @@ export const DynamicGroupCarousel = () => {
       }}
       data-cy={"group-carousel"}
     >
-      <DynamicGroupsFlashlightWrapper />
+      <DynamicGroupsFlashlightWrapper key={groupByValue} />
     </Resizable>
   );
-};
+});
