2a1185b678aa8cbaac5f260fa4d2cb197db7a77f
Ritchie Martori <ritchie@voxel51.com>
2024-10-03 06:41:37 -0700

    execution store cleanup

--- tests/unittests/execution_store_tests.py
+++ tests/unittests/execution_store_tests.py
@@ -8,13 +8,11 @@ FiftyOne execution store related unit tests.
 
 import time
 import unittest
-from unittest import mock
 from unittest.mock import patch, MagicMock, ANY, Mock
 import datetime
 
-import fiftyone.operators as foo
 from fiftyone.operators.store import ExecutionStoreService
-from fiftyone.operators.store.models import StoreDocument, KeyDocument
+from fiftyone.operators.store.models import KeyDocument
 from fiftyone.factory.repo_factory import ExecutionStoreRepo
 from fiftyone.operators.store import ExecutionStore
 from fiftyone.operators.store import ExecutionStoreService
