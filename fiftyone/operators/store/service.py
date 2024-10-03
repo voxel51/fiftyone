99084602d4103501e5ba0cec07da22fd4c650d74
Ritchie Martori <ritchie@voxel51.com>
2024-10-03 06:29:41 -0700

    refactor: add types to all fiftyone.operator.store modules

--- fiftyone/operators/store/service.py
+++ fiftyone/operators/store/service.py
@@ -7,21 +7,24 @@ FiftyOne execution store service.
 """
 
 import logging
+from typing import Optional, List
 from fiftyone.factory.repo_factory import RepositoryFactory
+from fiftyone.operators.store.models import StoreDocument, KeyDocument
+from fiftyone.factory.repos.execution_store import ExecutionStoreRepo
 
 logger = logging.getLogger(__name__)
 
 
-class ExecutionStoreService(object):
+class ExecutionStoreService:
     """Service for managing execution store operations."""
 
-    def __init__(self, repo=None):
+    def __init__(self, repo: Optional[ExecutionStoreRepo] = None):
         if repo is None:
             repo = RepositoryFactory.execution_store_repo()
 
-        self._repo = repo
+        self._repo: ExecutionStoreRepo = repo
 
-    def create_store(self, store_name):
+    def create_store(self, store_name: str) -> StoreDocument:
         """Creates a new store with the specified name.
 
         Args:
@@ -32,7 +35,9 @@ class ExecutionStoreService(object):
         """
         return self._repo.create_store(store_name=store_name)
 
-    def set_key(self, store_name, key, value, ttl=None):
+    def set_key(
+        self, store_name: str, key: str, value: str, ttl: Optional[int] = None
+    ) -> KeyDocument:
         """Sets the value of a key in the specified store.
 
         Args:
@@ -48,7 +53,7 @@ class ExecutionStoreService(object):
             store_name=store_name, key=key, value=value, ttl=ttl
         )
 
-    def get_key(self, store_name, key):
+    def get_key(self, store_name: str, key: str) -> KeyDocument:
         """Retrieves the value of a key from the specified store.
 
         Args:
@@ -60,7 +65,7 @@ class ExecutionStoreService(object):
         """
         return self._repo.get_key(store_name=store_name, key=key)
 
-    def delete_key(self, store_name, key):
+    def delete_key(self, store_name: str, key: str) -> bool:
         """Deletes the specified key from the store.
 
         Args:
@@ -72,7 +77,9 @@ class ExecutionStoreService(object):
         """
         return self._repo.delete_key(store_name=store_name, key=key)
 
-    def update_ttl(self, store_name, key, new_ttl):
+    def update_ttl(
+        self, store_name: str, key: str, new_ttl: int
+    ) -> KeyDocument:
         """Updates the TTL of the specified key in the store.
 
         Args:
@@ -87,7 +94,7 @@ class ExecutionStoreService(object):
             store_name=store_name, key=key, ttl=new_ttl
         )
 
-    def list_stores(self):
+    def list_stores(self) -> List[StoreDocument]:
         """Lists all stores matching the given criteria.
 
         Returns:
@@ -95,7 +102,7 @@ class ExecutionStoreService(object):
         """
         return self._repo.list_stores()
 
-    def delete_store(self, store_name):
+    def delete_store(self, store_name: str) -> StoreDocument:
         """Deletes the specified store.
 
         Args:
@@ -106,10 +113,13 @@ class ExecutionStoreService(object):
         """
         return self._repo.delete_store(store_name=store_name)
 
-    def list_keys(self, store_name):
+    def list_keys(self, store_name: str) -> List[str]:
         """Lists all keys in the specified store.
 
         Args:
             store_name: the name of the store
+
+        Returns:
+            a list of keys in the store
         """
         return self._repo.list_keys(store_name)
