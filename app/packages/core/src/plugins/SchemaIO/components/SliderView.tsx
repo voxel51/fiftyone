ea3fc53f045b04c1af399c2f4b022ecd91000882
manivoxel51 <mani@voxel51.com>
2024-10-06 19:59:33 -0700

    complete slider

--- app/packages/core/src/plugins/SchemaIO/components/SliderView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/SliderView.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   Box,
   FormControl,
   Grid,
-  InputLabel,
   MenuItem,
   Select,
   Slider,
@@ -15,40 +14,62 @@ import { autoFocus, getComponentProps } from "../utils";
 import { useKey } from "../hooks";
 import { isNumber } from "lodash";
 
-type ValueFormat = "float" | "percentage";
-type LabelPosition = "top" | "left";
+type ValueFormat = "flt" | "%";
 
-function valueLabelFormat(
+const valueLabelFormat = (
   value: number,
   min: number,
   max: number,
   valueFormat: ValueFormat,
-  valuePrecision = 0
-) {
-  if (valueFormat === "percentage") {
-    const finalValue = (((value - min) / (max - min)) * 100).toFixed();
-    return `${finalValue}${"%"}`;
-  }
-  return `${value.toFixed(valuePrecision)}`;
+  valuePrecision = 0,
+  skipUnit = true
+) => {
+  const formattedValue =
+    valueFormat === "%"
+      ? (((value - min) / (max - min)) * 100).toFixed()
+      : value.toFixed(valuePrecision);
+
+  return skipUnit ? formattedValue : `${formattedValue} ${valueFormat}`;
+};
+
+interface SliderInputFieldProps {
+  label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  UnitSelection: React.ReactNode;
 }
 
-const formatValue = (
-  value: number,
-  min: number,
-  max: number,
-  valueFormat: ValueFormat,
-  valuePrecision = 0
-): string => {
-  if (valueFormat === "percentage") {
-    return (((value - min) / (max - min)) * 100).toFixed();
-  } else {
-    return value.toFixed(valuePrecision);
-  }
-};
+const SliderInputField: React.FC<SliderInputFieldProps> = ({
+  label,
+  value,
+  onChange,
+  onKeyDown,
+  UnitSelection,
+}) => (
+  <Grid item>
+    <TextField
+      label={label}
+      size="small"
+      variant="outlined"
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      sx={{
+        width: "70px",
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+      }}
+      InputProps={{
+        sx: { borderTopRightRadius: 0, borderBottomRightRadius: 0 },
+      }}
+    />
+    {UnitSelection}
+  </Grid>
+);
 
 export default function SliderView(props) {
   const { data, onChange, path, schema } = props;
-
   const sliderRef = useRef<HTMLInputElement>(null);
   const focus = autoFocus(props);
 
@@ -62,106 +83,148 @@ export default function SliderView(props) {
   const {
     min: viewMin,
     max: viewMax,
-    defaultValue = 0,
-    valueFormat = "percentage",
-    valuePrecision = 2,
+    value_format: valueFormat = "%",
+    value_label_display: valueLabelDisplay = "on",
+    value_precision: valuePrecision = 2,
     variant = null,
-    valueLabelDisplay = "on",
-    labelPosition = "left",
+    label_position: labelPosition = "left",
     label = "Threshold",
     viewMultipleOf = null,
   } = view;
-  const [title, _] = useState(label);
-  console.log("label", title);
 
   const multipleOf = viewMultipleOf || schemaMultipleOf;
-
   const [min, max] = [
-    isNumber(viewMin) ? viewMin : schemaMin,
-    isNumber(viewMax) ? viewMax : schemaMax,
+    isNumber(viewMin) ? viewMin : isNumber(schemaMin) ? schemaMin : 0,
+    isNumber(viewMax) ? viewMax : isNumber(schemaMax) ? schemaMax : 100,
   ];
-  console.log("view", min, max);
-
-  const computedMin = isNumber(min) ? min : 0;
-  const computedMax = isNumber(max) ? max : 100;
 
   const computedMultipleOf = isNumber(multipleOf)
     ? multipleOf
-    : (computedMax - computedMin) / 100;
-  console.log("computedMultipleOf", computedMultipleOf);
+    : (max - min) / 100;
+
+  const [key, setUserChanged] = useKey(path, schema, data, true);
+  const [unit, setUnit] = useState<ValueFormat>(valueFormat);
+  const [minText, setMinText] = useState(
+    valueLabelFormat(data?.[0] || min, min, max, unit, valuePrecision)
+  );
+  const [maxText, setMaxText] = useState(
+    valueLabelFormat(data?.[1] || max, min, max, unit, valuePrecision)
+  );
 
   useEffect(() => {
     if (sliderRef.current && focus) {
       sliderRef.current.querySelector("input")?.focus();
     }
-  }, [sliderRef, focus]);
+  }, [focus]);
 
-  const [key, setUserChanged] = useKey(path, schema, data, true);
-  const [unit, setUnit] = useState<string>(valueFormat);
+  const handleInputChange = (e, isMin: boolean) => {
+    const value = e.target.value;
 
-  console.log("data", data);
+    if (!value) {
+      isMin ? setMinText("") : setMaxText("");
+    } else if (unit === "%") {
+      const percentageValue = parseInt(value);
+      if (percentageValue >= 0 && percentageValue <= 100) {
+        isMin
+          ? setMinText(percentageValue.toFixed())
+          : setMaxText(percentageValue.toFixed());
+      }
+    } else {
+      const floatValue = parseFloat(value);
+      if (!isNaN(floatValue)) {
+        isMin ? setMinText(value) : setMaxText(value);
+      }
+    }
+  };
 
-  const [minText, setMinText] = useState(
-    formatValue(data?.[0] || min, min, max, unit as ValueFormat, valuePrecision)
-  );
-  const [maxText, setMaxText] = useState(
-    formatValue(data?.[1] || max, min, max, unit as ValueFormat, valuePrecision)
+  const handleKeyDown = (e, isMin: boolean) => {
+    if (e.key === "Enter") {
+      let finalValue = e.target.value;
+      if (!finalValue) return;
+
+      if (unit === "%") {
+        finalValue = ((max - min) / 100) * finalValue;
+      }
+
+      onChange(
+        path,
+        isMin
+          ? [parseFloat(finalValue), parseFloat(data?.[1] || max)]
+          : [parseFloat(data?.[0] || min), parseFloat(finalValue)],
+        schema
+      );
+    }
+  };
+
+  const UnitSelection = useMemo(
+    () => (
+      <FormControl variant="outlined">
+        <Select
+          size="small"
+          value={unit}
+          sx={{
+            borderTopLeftRadius: 0,
+            borderBottomLeftRadius: 0,
+          }}
+          onChange={(e) => {
+            const newUnit = e.target.value as ValueFormat;
+            setUnit(newUnit);
+            setMinText(
+              valueLabelFormat(
+                data?.[0] || min,
+                min,
+                max,
+                newUnit,
+                valuePrecision
+              )
+            );
+            setMaxText(
+              valueLabelFormat(
+                data?.[1] || max,
+                min,
+                max,
+                newUnit,
+                valuePrecision
+              )
+            );
+          }}
+        >
+          <MenuItem value="%">%</MenuItem>
+          <MenuItem value="flt">flt</MenuItem>
+        </Select>
+      </FormControl>
+    ),
+    [data, max, min, unit, valuePrecision]
   );
 
   if (labelPosition === "left") {
     delete props?.schema?.view?.label;
   }
 
-  console.log("props", props);
-
   return (
     <Box padding={2} py={4}>
       <FieldWrapper {...props}>
         <Grid container spacing={2} alignItems="center">
-          {labelPosition === "left" && title && (
+          {labelPosition === "left" && label && (
             <Grid item sx={{ width: "100px", overflow: "hidden" }}>
-              <Typography
-                noWrap
-                sx={{
-                  textOverflow: "ellipsis",
-                  overflow: "hidden",
-                  whiteSpace: "nowrap",
-                  width: "100%",
-                }}
-              >
-                {title}
+              <Typography noWrap sx={{ textOverflow: "ellipsis" }}>
+                {label}
               </Typography>
             </Grid>
           )}
-
           <Grid item xs>
             <Slider
-              min={computedMin}
-              max={computedMax}
+              min={min}
+              max={max}
               step={computedMultipleOf}
               key={key}
               disabled={schema.view?.readOnly}
               valueLabelDisplay={valueLabelDisplay}
-              value={data || defaultValue}
-              valueLabelFormat={(value: number) =>
-                valueLabelFormat(
-                  value,
-                  min,
-                  max,
-                  unit as ValueFormat,
-                  valuePrecision
-                )
+              value={data || [min, max]}
+              valueLabelFormat={(value) =>
+                valueLabelFormat(value, min, max, unit, valuePrecision, false)
               }
-              getAriaValueText={(value: number) =>
-                valueLabelFormat(
-                  value,
-                  min,
-                  max,
-                  unit as ValueFormat,
-                  valuePrecision
-                )
-              }
-              onChange={(_, value: string) => {
+              onChange={(_, value: number) => {
                 onChange(
                   path,
                   type === "number" ? parseFloat(value) : value,
@@ -170,24 +233,11 @@ export default function SliderView(props) {
                 setUserChanged();
 
                 if (variant === "withInputs") {
-                  isNumber(min) &&
-                    setMinText(
-                      formatValue(
-                        parseFloat(value[0]),
-                        min,
-                        max,
-                        unit as ValueFormat,
-                        valuePrecision
-                      )
-                    );
+                  setMinText(
+                    valueLabelFormat(value[0], min, max, unit, valuePrecision)
+                  );
                   setMaxText(
-                    formatValue(
-                      parseFloat(value[1]),
-                      min,
-                      max,
-                      unit as ValueFormat,
-                      valuePrecision
-                    )
+                    valueLabelFormat(value[1], min, max, unit, valuePrecision)
                   );
                 }
               }}
@@ -197,170 +247,23 @@ export default function SliderView(props) {
           </Grid>
           {variant === "withInputs" && (
             <Grid container justifyContent="space-between" pl={1}>
-              <Grid item pl="100px">
-                <TextField
+              <Grid item pl={labelPosition === "left" ? "100px" : "0"}>
+                <SliderInputField
                   label="Min"
-                  size="small"
-                  variant="outlined"
                   value={minText}
-                  onChange={(e) => {
-                    const value = e.target.value;
-                    console.log("asdasd", value, minText);
-                    if (!value && !!minText) {
-                      setMinText(null);
-                    } else if (unit === "percentage") {
-                      const percentageValue = parseInt(value);
-                      console.log("percentageValue", percentageValue);
-                      if (
-                        !isNaN(percentageValue) &&
-                        percentageValue >= 0 &&
-                        percentageValue <= 100
-                      ) {
-                        setMinText(percentageValue.toFixed());
-                      }
-                    } else if (unit === "flt") {
-                      const floatValue = parseFloat(value);
-                      if (!isNaN(floatValue)) {
-                        setMinText(value);
-                      }
-                    }
-                  }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      let finalValue = e.target.value;
-
-                      if (!finalValue) {
-                        return;
-                      }
-
-                      if (unit === "percentage") {
-                        finalValue =
-                          ((computedMax - computedMin) / 100) * finalValue;
-                      }
-
-                      onChange(
-                        path,
-                        [parseFloat(finalValue), parseFloat(data?.[1] || max)],
-                        schema
-                      );
-                    }
-                  }}
-                  sx={{ width: "70px" }}
+                  onChange={(e) => handleInputChange(e, true)}
+                  onKeyDown={(e) => handleKeyDown(e, true)}
+                  UnitSelection={UnitSelection}
                 />
-                <FormControl variant="outlined">
-                  <Select
-                    size="small"
-                    labelId="dropdown-label"
-                    value={unit}
-                    onChange={(event: React.ChangeEvent<{ value: string }>) => {
-                      setUnit(event.target.value);
-                      setMinText(
-                        formatValue(
-                          data?.[0] || min,
-                          min,
-                          max,
-                          event.target.value as ValueFormat,
-                          valuePrecision
-                        )
-                      );
-                      setMaxText(
-                        formatValue(
-                          data?.[1] || max,
-                          min,
-                          max,
-                          event.target.value as ValueFormat,
-                          valuePrecision
-                        )
-                      );
-                    }}
-                    label="Select Option"
-                  >
-                    <MenuItem value="percentage">%</MenuItem>
-                    <MenuItem value="flt">flt</MenuItem>
-                  </Select>
-                </FormControl>
               </Grid>
               <Grid item>
-                <TextField
+                <SliderInputField
                   label="Max"
-                  size="small"
-                  variant="outlined"
                   value={maxText}
-                  onChange={(e) => {
-                    const value = e.target.value;
-                    console.log(value, maxText);
-                    if (!value && !!maxText) {
-                      setMaxText(null);
-                    } else if (unit === "percentage") {
-                      const percentageValue = parseInt(value);
-                      if (
-                        !isNaN(percentageValue) &&
-                        percentageValue >= 0 &&
-                        percentageValue <= 100
-                      ) {
-                        setMaxText(percentageValue.toFixed());
-                      }
-                    } else if (unit === "flt") {
-                      const floatValue = parseFloat(value);
-                      if (!isNaN(floatValue)) {
-                        setMaxText(value);
-                      }
-                    }
-                  }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      let finalValue = e.target.value;
-
-                      if (!finalValue) {
-                        return;
-                      }
-
-                      if (unit === "percentage") {
-                        finalValue =
-                          ((computedMax - computedMin) / 100) * finalValue;
-                      }
-
-                      onChange(
-                        path,
-                        [parseFloat(data?.[0] || min), parseFloat(finalValue)],
-                        schema
-                      );
-                    }
-                  }}
-                  sx={{ width: "70px" }}
+                  onChange={(e) => handleInputChange(e, false)}
+                  onKeyDown={(e) => handleKeyDown(e, false)}
+                  UnitSelection={UnitSelection}
                 />
-                <FormControl variant="outlined">
-                  <Select
-                    size="small"
-                    labelId="dropdown-label"
-                    value={unit}
-                    onChange={(event: React.ChangeEvent<{ value: string }>) => {
-                      setUnit(event.target.value);
-                      setMinText(
-                        formatValue(
-                          data?.[0] || min,
-                          min,
-                          max,
-                          event.target.value as ValueFormat,
-                          valuePrecision
-                        )
-                      );
-                      setMaxText(
-                        formatValue(
-                          data?.[1] || max,
-                          min,
-                          max,
-                          event.target.value as ValueFormat,
-                          valuePrecision
-                        )
-                      );
-                    }}
-                    label="Select Option"
-                  >
-                    <MenuItem value="percentage">%</MenuItem>
-                    <MenuItem value="flt">flt</MenuItem>
-                  </Select>
-                </FormControl>
               </Grid>
             </Grid>
           )}
