9b02e46326bbf657498f30f435a0fb5a4f22d7b2
Br2850 <bdbristol@gmail.com>
2024-10-08 12:35:44 -0400

    coloring

--- app/packages/components/src/components/PillBadge/PillBadge.tsx
+++ app/packages/components/src/components/PillBadge/PillBadge.tsx
@@ -1,26 +1,16 @@
 import React from "react";
 import { Badge, Chip } from "@mui/material";
 
-const PillBadge = ({
-  text,
-  style,
-}: {
-  text: string;
-  color?: string;
-  style?: React.CSSProperties;
-}) => {
-  COLORS = {
-    default: "grey",
-    success: "green",
-    warning: "orange",
-    error: "red",
+const PillBadge = ({ text, color }: { text: string; color?: string }) => {
+  const COLORS: { [key: string]: string } = {
+    default: "#777777",
+    primary: "#FFB682",
+    error: "error",
+    warning: "warning",
+    info: "info",
+    success: "#8BC18D",
   };
-  return (
-    <span style={style ?? {}}>
-      <Chip icon={<Badge />} label={text} variant={} />
-      {text}
-    </span>
-  );
+  return <Chip icon={<Badge />} label={text} variant={} />;
 };
 
 export default PillBadge;
