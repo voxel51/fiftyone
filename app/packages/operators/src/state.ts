46e32293291d0c3fd37b5245134e44fb75338779
Ritchie Martori <ritchie@voxel51.com>
2024-10-21 12:12:43 -0700

    fix reference to delegation target

--- app/packages/operators/src/state.ts
+++ app/packages/operators/src/state.ts
@@ -483,8 +483,6 @@ export const useOperatorPrompt = () => {
       if (invalid) {
         return;
       }
-      console.log("execute()")
-      console.log('options', options)
       executor.execute(promptingOperator.params, {
         ...options,
         ...promptingOperator.options,
