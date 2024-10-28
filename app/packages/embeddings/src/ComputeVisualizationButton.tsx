e7f6fdd2d91336228413e33f9ca09077e3173bcc
Ritchie Martori <ritchie@voxel51.com>
2024-10-28 15:30:29 -0700

    new embeddings cta

--- app/packages/embeddings/src/ComputeVisualizationButton.tsx
+++ app/packages/embeddings/src/ComputeVisualizationButton.tsx
@@ -1,12 +1,21 @@
-import { Button } from "@fiftyone/components";
+import { Button } from "@mui/material";
 import { usePanelEvent } from "@fiftyone/operators";
 import { usePanelId } from "@fiftyone/spaces";
+import AddIcon from "@mui/icons-material/Add";
+import { useTheme } from "@fiftyone/components";
 
 export default function ComputeVisualizationButton() {
   const triggerEvent = usePanelEvent();
+  const theme = useTheme();
   const panelId = usePanelId();
   return (
     <Button
+      variant="contained"
+      sx={{
+        backgroundColor: theme.primary.main,
+        color: theme.text.primary,
+      }}
+      startIcon={<AddIcon />}
       onClick={() => {
         triggerEvent(panelId, {
           params: { delegate: true },
--- app/packages/embeddings/src/EmptyEmbeddings.tsx
+++ app/packages/embeddings/src/EmptyEmbeddings.tsx
@@ -1,24 +1,100 @@
-import { Link } from "@mui/material";
-import styled, { useTheme } from "styled-components";
+import { Box, Button, Link, Typography, Grid } from "@mui/material";
+import { useTheme } from "@fiftyone/components";
+import ScatterPlotIcon from "@mui/icons-material/ScatterPlot";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import ComputeVisualizationButton from "./ComputeVisualizationButton";
+
 export default function EmptyEmbeddings() {
   const theme = useTheme();
+  const secondaryBodyColor = theme.text.secondary;
+  const isTeams = false;
+
   return (
-    <NotFound style={{ textAlign: "center" }}>
-      <h3>No embeddings visualizations found.</h3>
-      <ComputeVisualizationButton />
-      <p>
-        <Link
-          style={{ color: theme.text.primary }}
-          href="https://docs.voxel51.com/user_guide/app.html#embeddings-panel"
-        >
-          Learn more
-        </Link>{" "}
-        about using this feature.
-      </p>
-    </NotFound>
+    <Box
+      sx={{
+        height: "calc(100% - 32px)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#262626",
+        margin: 2,
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "510px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+          padding: 4,
+          borderRadius: 2,
+        }}
+      >
+        <Grid container direction="column" alignItems="center" spacing={2}>
+          <Grid item>
+            <ScatterPlotIcon
+              sx={{ fontSize: 64, color: "#ffba85", marginBottom: 2 }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="h6" size="14px">
+              Embeddings help you explore and understand your dataset
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography color={secondaryBodyColor}>
+              Compute and visualize embeddings for your dataset using a
+              selection of pre-trained models or your own embeddings
+            </Typography>
+          </Grid>
+          {!isTeams && <OSSContent />}
+          {isTeams && <TeamsContent />}
+        </Grid>
+      </Box>
+    </Box>
   );
 }
-const NotFound = styled.div`
-  text-align: center;
-`;
+
+function OSSContent() {
+  const theme = useTheme();
+  const secondaryBodyColor = theme.text.secondary;
+  return (
+    <>
+      <Grid item>
+        <Typography color={secondaryBodyColor}>
+          Learn more about using embeddings in the open source:
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="warning"
+          href="https://docs.voxel51.com/user_guide/app.html#embeddings-panel"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            backgroundColor: theme.primary.main,
+            color: theme.text.primary,
+          }}
+          endIcon={<OpenInNewIcon />}
+        >
+          View Documentation
+        </Button>
+      </Grid>
+    </>
+  );
+}
+
+function TeamsContent() {
+  const theme = useTheme();
+  const secondaryBodyColor = theme.text.secondary;
+  return (
+    <>
+      <Grid item>
+        <ComputeVisualizationButton />
+      </Grid>
+    </>
+  );
+}
