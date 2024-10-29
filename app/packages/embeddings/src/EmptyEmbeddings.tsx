d11203a53aa0cda29fc8189556b1606a4df3e2c2
Ritchie Martori <ritchie@voxel51.com>
2024-10-29 13:50:06 -0700

    improve embeddings cta

--- app/packages/embeddings/src/EmptyEmbeddings.tsx
+++ app/packages/embeddings/src/EmptyEmbeddings.tsx
@@ -1,13 +1,30 @@
 import { Box, Button, Link, Typography, Grid } from "@mui/material";
 import { useTheme } from "@fiftyone/components";
-import ScatterPlotIcon from "@mui/icons-material/ScatterPlot";
+import WorkspacesIcon from "@mui/icons-material/Workspaces";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import ComputeVisualizationButton from "./ComputeVisualizationButton";
+import { useMemo } from "react";
+import { listLocalAndRemoteOperators } from "@fiftyone/operators/src/operators";
+
+const useFirstExistingUri = (uris: string[]) => {
+  const availableOperators = useMemo(() => listLocalAndRemoteOperators(), []);
+  return useMemo(() => {
+    const existingUri = uris.find((uri) =>
+      availableOperators.allOperators.some((op) => op.uri === uri)
+    );
+    const exists = Boolean(existingUri);
+    return { firstExistingUri: existingUri, exists };
+  }, [availableOperators, uris]);
+};
 
 export default function EmptyEmbeddings() {
   const theme = useTheme();
   const secondaryBodyColor = theme.text.secondary;
-  const isTeams = false;
+  const { firstExistingUri: computeVisUri, exists: hasComputeVisualization } =
+    useFirstExistingUri([
+      "@voxel51/brain/compute_visualization",
+      "@voxel51/operators/compute_visualization",
+    ]);
 
   return (
     <Box
@@ -34,7 +51,7 @@ export default function EmptyEmbeddings() {
       >
         <Grid container direction="column" alignItems="center" spacing={2}>
           <Grid item>
-            <ScatterPlotIcon
+            <WorkspacesIcon
               sx={{ fontSize: 64, color: "#ffba85", marginBottom: 2 }}
             />
           </Grid>
@@ -45,45 +62,61 @@ export default function EmptyEmbeddings() {
           </Grid>
           <Grid item>
             <Typography color={secondaryBodyColor}>
-              Compute and visualize embeddings for your dataset using a
+              You can compute and visualize embeddings for your dataset using a
               selection of pre-trained models or your own embeddings
             </Typography>
           </Grid>
-          {!isTeams && <OSSContent />}
-          {isTeams && <TeamsContent />}
+          <Grid item />
+          {!hasComputeVisualization && <OSSContent />}
+          {hasComputeVisualization && <TeamsContent />}
         </Grid>
       </Box>
+      {hasComputeVisualization && (
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "100px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <ViewDocsButton variant="secondary" />
+        </Box>
+      )}
     </Box>
   );
 }
 
-function OSSContent() {
+const ViewDocsButton = ({ variant }) => {
   const theme = useTheme();
   const secondaryBodyColor = theme.text.secondary;
+  const backgroundColor = variant === "secondary" ? "none" : theme.primary.main;
+  const isSecondary = variant === "secondary";
+
   return (
-    <>
-      <Grid item>
-        <Typography color={secondaryBodyColor}>
-          Learn more about using embeddings in the open source:
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Button
-          variant="contained"
-          color="warning"
-          href="https://docs.voxel51.com/user_guide/app.html#embeddings-panel"
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{
-            backgroundColor: theme.primary.main,
-            color: theme.text.primary,
-          }}
-          endIcon={<OpenInNewIcon />}
-        >
-          View Documentation
-        </Button>
-      </Grid>
-    </>
+    <Button
+      variant="outlined"
+      href="https://docs.voxel51.com/user_guide/app.html#embeddings-panel"
+      target="_blank"
+      rel="noopener noreferrer"
+      sx={{
+        backgroundColor,
+        color: isSecondary ? secondaryBodyColor : theme.text.primary,
+        borderColor: isSecondary ? secondaryBodyColor : undefined,
+      }}
+      endIcon={<OpenInNewIcon />}
+    >
+      View Documentation
+    </Button>
+  );
+};
+
+function OSSContent() {
+  return (
+    <Grid item>
+      <ViewDocsButton />
+    </Grid>
   );
 }
 
