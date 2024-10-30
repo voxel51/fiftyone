fddd7523fdd8eed0ef2f541d2c854cd1d1bba751
Minh-Tue Vo <minhtuev@gmail.com>
2024-10-29 22:30:40 -0700

    Added QueryPerformance as a builtin panel (#885)
    * Added QueryPerformance as a builtin panel
    * Refactored to avoid circular dep and addressed feedback
    
    ---------
    
    Co-authored-by: minhtuevo <minhtuev@voxel51.com>

---
+++ fiftyone/operators/panels/__init__.py
@@ -0,0 +1,13 @@
+"""
+FiftyOne panels.
+
+| Copyright 2017-2024, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+from .query_performance import QueryPerformancePanel
+
+
+# This enables Sphinx refs to directly use paths imported here
+__all__ = [k for k, v in globals().items() if not k.startswith("_")]
