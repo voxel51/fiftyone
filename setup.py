9a27dc553c05d831d956e545d724ff512351fd6e
topher <topher@voxel51.com>
2024-10-30 12:52:37 +0000

    back to 2.1.2 RCs

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="2.1.3rc1",
+    version="2.1.2rc3",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
