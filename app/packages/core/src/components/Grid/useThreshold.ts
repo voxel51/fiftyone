dff521bacedb290fb10cce6f48b392dad980d3f1
Benjamin Kane <ben@voxel51.com>
2024-11-03 18:11:22 -0500

    better aspect ratio thresholds

--- app/packages/core/src/components/Grid/useThreshold.ts
+++ app/packages/core/src/components/Grid/useThreshold.ts
@@ -2,18 +2,22 @@ import { useCallback } from "react";
 import { useRecoilValue } from "recoil";
 import { gridZoom } from "./recoil";
 
+const WIDEST = 1200;
+const WIDE = 1000;
+const NORMAL = 800;
+
 export default () => {
   const zoom = useRecoilValue(gridZoom);
   return useCallback(
     (width: number) => {
-      let min = 1;
+      let min = 6;
 
-      if (width >= 1200) {
-        min = -5;
-      } else if (width >= 1000) {
-        min = -3;
-      } else if (width >= 800) {
-        min = -1;
+      if (width >= WIDEST) {
+        min = -4;
+      } else if (width >= WIDE) {
+        min = -2;
+      } else if (width >= NORMAL) {
+        min = 2;
       }
 
       return 11 - Math.max(min, zoom);
