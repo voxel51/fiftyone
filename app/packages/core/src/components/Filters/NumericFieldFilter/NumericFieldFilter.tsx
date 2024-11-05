9a99161c3dda0530b101ff93014b36d130f54669
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2024-11-05 17:02:16 -0600

    Add button for range within groups when query performance is enabled (#5044)

--- app/packages/core/src/components/Filters/NumericFieldFilter/NumericFieldFilter.tsx
+++ app/packages/core/src/components/Filters/NumericFieldFilter/NumericFieldFilter.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import FieldLabelAndInfo from "../../FieldLabelAndInfo";
-import Boxes from "./Boxes";
 import RangeSlider from "./RangeSlider";
+import { Button } from "@mui/material";
+import { useTheme } from "@fiftyone/components";
 import * as state from "./state";
+import * as schemaAtoms from "@fiftyone/state/src/recoil/schema";
 
 const Container = styled.div`
   margin: 3px;
@@ -15,6 +17,18 @@ const Container = styled.div`
 const Header = styled.div`
   display: flex;
   justify-content: space-between;
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  column-gap: 1rem;
+  background: ${({ theme }) => theme.background.level2};
+  border: 1px solid var(--fo-palette-divider);
+  border-radius: 2px;
+  color: ${({ theme }) => theme.text.secondary};
+  margin-top: 0.25rem;
+  padding: 0.25rem 0.5rem;
 `;
 
 type Props = {
@@ -28,16 +42,23 @@ type Props = {
 
 const NumericFieldFilter = ({ color, modal, named = true, path }: Props) => {
   const name = path.split(".").slice(-1)[0];
+  const fieldType = useRecoilValue(schemaAtoms.filterFields(path));
+  const isGroup = fieldType.length > 1;
+  const theme = useTheme();
+  const [showRange, setShowRange] = React.useState(!isGroup);
   const field = fos.useAssertedRecoilValue(fos.field(path));
-  const hasBounds = useRecoilValue(state.hasBounds({ path, modal }));
-  const indexed = useRecoilValue(fos.pathHasIndexes(path));
   const queryPerformance = useRecoilValue(fos.queryPerformance);
+  const hasBounds = useRecoilValue(state.hasBounds({ path, modal }));
 
   if (!queryPerformance && named && !hasBounds) {
     return null;
   }
 
-  const boxes = queryPerformance && !indexed;
+  const handleShowRange = () => {
+    setShowRange(true);
+  };
+
+  const showButton = isGroup && queryPerformance && !showRange;
 
   return (
     <Container onClick={(e) => e.stopPropagation()}>
@@ -53,8 +74,24 @@ const NumericFieldFilter = ({ color, modal, named = true, path }: Props) => {
           )}
         />
       )}
-      {boxes ? (
-        <Boxes path={path} />
+      {showButton ? (
+        <Box>
+          <Button
+            onClick={handleShowRange}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+              padding: "10px",
+              color: theme.text.secondary,
+              borderRadius: "8px",
+              border: "1px solid " + theme.secondary.main,
+            }}
+          >
+            Filter by {name}
+          </Button>
+        </Box>
       ) : (
         <RangeSlider color={color} modal={modal} path={path} />
       )}
