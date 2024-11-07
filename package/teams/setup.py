967098961a5fe8e03267d9d84ad6f53998082adb
Benjamin Kane <ben@voxel51.com>
2024-11-07 15:37:43 -0500

    Teams App Performance updates (#934)
    Co-authored-by: Br2850 <bdbristol@gmail.com>
    Co-authored-by: Sashank Aryal <sashank2012@gmail.com>
    Co-authored-by: voxelbot <voxelbot@voxel51.com>

--- package/teams/setup.py
+++ package/teams/setup.py
@@ -48,7 +48,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone-teams-app",
-    version="2.1.3rc1",
+    version="2.1.3rc9",
     description=("FiftyOne Teams"),
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
--- teams-app/packages/app/pages/datasets/[slug]/samples/dynamicRouting/useSetters/onSetGroupSlice.ts
+++ teams-app/packages/app/pages/datasets/[slug]/samples/dynamicRouting/useSetters/onSetGroupSlice.ts
@@ -1,17 +1,19 @@
 import {
   type HistoryState,
+  SLICE_EVENT,
   getHistoryState,
-  replaceHistoryState
-} from '../state';
-import { writeSession } from '../useLocalSession';
-import type { RegisteredSetter } from './registerSetter';
+  replaceHistoryState,
+} from "../state";
+import { writeSession } from "../useLocalSession";
+import type { RegisteredSetter } from "./registerSetter";
 
 const onSetGroupSlice: RegisteredSetter = () => async (_, slice: string) => {
   // create new state
   const { slice: __, ...state } = getHistoryState();
   const newState: HistoryState = {
     ...state,
-    slice
+    event: SLICE_EVENT,
+    slice,
   };
 
   // write the slice to browser storage
