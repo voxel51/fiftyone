e7aa5c7a1e09750342e7e284c273425dfcfdf134
Ritchie Martori <ritchie@voxel51.com>
2024-11-12 11:40:18 -0700

    add get_store method

--- tests/unittests/execution_store_int_tests.py
+++ tests/unittests/execution_store_int_tests.py
@@ -11,7 +11,7 @@ import pytest
 import fiftyone as fo
 from fiftyone.operators.store import ExecutionStoreService
 
-from decorators import drop_stores
+from decorators import drop_stores, drop_datasets
 
 
 @pytest.fixture
@@ -24,10 +24,10 @@ def svc_with_dataset():
     dataset = fo.Dataset(name="test_dataset")
     dataset.save()
     yield ExecutionStoreService(dataset_id=dataset._doc.id)
-    dataset.delete()
 
 
 @drop_stores
+@drop_datasets
 def test_store_creation(svc):
     NAME = "test_store"
     created_store = svc.create_store(NAME)
@@ -42,6 +42,7 @@ def test_store_creation(svc):
 
 
 @drop_stores
+@drop_datasets
 def test_store_creation_with_dataset(svc_with_dataset):
     NAME = "test_store"
     created_store = svc_with_dataset.create_store(NAME)
@@ -56,6 +57,24 @@ def test_store_creation_with_dataset(svc_with_dataset):
 
 
 @drop_stores
+@drop_datasets
+def test_store_creation_with_metadata(svc):
+    NAME = "test_store"
+    METADATA = {"test": "value"}
+    svc.create_store(NAME, metadata=METADATA)
+    created_store = svc.get_store(NAME)
+
+    assert (
+        created_store.store_name == NAME
+    ), "Store name should match the given name"
+    assert (
+        created_store.metadata == METADATA
+    ), "Metadata should match the provided metadata"
+    assert svc.count_stores() == 1, "Store count should be 1"
+
+
+@drop_stores
+@drop_datasets
 def test_set_get_key(svc):
     NAME = "test_store"
     KEY = "test_key"
@@ -71,6 +90,7 @@ def test_set_get_key(svc):
 
 
 @drop_stores
+@drop_datasets
 def test_scoping(svc, svc_with_dataset):
     NAME = "test_store"
     KEY = "test_key"
@@ -93,3 +113,26 @@ def test_scoping(svc, svc_with_dataset):
     assert svc.count_keys(NAME) == 0, "Global store should have 0 keys"
     global_list = svc.list_stores_global()
     assert NAME not in global_list, "Global store should not be listed"
+
+
+@drop_stores
+@drop_datasets
+def test_list_global_stores(svc, svc_with_dataset):
+    NO_DATASET_STORE_NAME = "dataset_less_store"
+    DATASET_STORE_NAME = "dataset_store"
+    KEY_ONLY_STORE_NAME = "key_only_store"
+
+    svc.create_store(NO_DATASET_STORE_NAME)
+    svc_with_dataset.create_store(DATASET_STORE_NAME)
+    svc_with_dataset.set_key(DATASET_STORE_NAME, "key", "value")
+    svc_with_dataset.set_key(KEY_ONLY_STORE_NAME, "key", "value")
+
+    global_list = svc.list_stores_global()
+    store_names = [store.store_name for store in global_list]
+    dataset_ids = [store.dataset_id for store in global_list]
+    assert len(global_list) == 3
+    assert NO_DATASET_STORE_NAME in store_names
+    assert DATASET_STORE_NAME in store_names
+    assert KEY_ONLY_STORE_NAME in store_names
+    assert None in dataset_ids
+    assert svc_with_dataset._dataset_id in dataset_ids
