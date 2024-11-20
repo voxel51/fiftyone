5b67fce32809751090c9cbddce918b3286092af2
voxelbot <voxelbot@voxel51.com>
2024-11-20 22:47:32 +0000

    [skip ci] automated bump `fiftyone` to 2.2.0rc25, `fiftyone-app` to 2.2.0rc25, and `teams-app` to v2.2.0-rc.24

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="2.2.0rc24",
+    version="2.2.0rc25",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
