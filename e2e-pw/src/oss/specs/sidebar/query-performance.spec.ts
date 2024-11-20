5c50e6c79e9dddcb651af2726c53c8ebf9c2aaf6
Benjamin Kane <ben@voxel51.com>
2024-11-19 20:13:57 -0500

    initial assertions

--- e2e-pw/src/oss/specs/sidebar/query-performance.spec.ts
+++ e2e-pw/src/oss/specs/sidebar/query-performance.spec.ts
@@ -17,143 +17,161 @@ const test = base.extend<{ sidebar: SidebarPom; grid: GridPom }>({
 test.describe("query performance sidebar", () => {
   test.beforeAll(async ({ fiftyoneLoader }) => {
     await fiftyoneLoader.executePythonCode(`
-import fiftyone as fo
+      import fiftyone as fo
 
-dataset = fo.Dataset("${datasetName}")
-dataset.add_samples([fo.Sample(filepath=f"{i}.png") for i in range(0, 4)])
-dataset.persistent = True
+      dataset = fo.Dataset("${datasetName}")
+      dataset.add_samples([fo.Sample(filepath=f"{i}.png") for i in range(0, 4)])
+      dataset.persistent = True
 
-# NOT REAL VALUES
+      # NOT REAL VALUES
 
-first = dataset.first()
+      first = dataset.first()
 
-first["bool"] = False
-first["bool_list"] = False
+      first["bool"] = False
+      first["bool_list"] = False
 
-first["inf"] = float("inf")
-first["inf_list"] = [float("inf")]
-first["inf_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=float("inf"))]
-)
+      first["inf"] = float("inf")
+      first["inf_list"] = [float("inf")]
+      first["inf_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=float("inf"))]
+      )
 
-first["nan"] = float("nan")
-first["nan_list"] = [float("nan")]
-first["nan_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=float("nan"))]
-)
+      first["nan"] = float("nan")
+      first["nan_list"] = [float("nan")]
+      first["nan_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=float("nan"))]
+      )
 
-first["ninf"] = float("-inf")
-first["ninf_list"] = [float("-inf")]
-first["ninf_label_list"] = fo.Classifications(
-    classifications=[
-        fo.Classification(label="label", confidence=float("-inf"))
-    ]
-)
+      first["ninf"] = float("-inf")
+      first["ninf_list"] = [float("-inf")]
+      first["ninf_label_list"] = fo.Classifications(
+          classifications=[
+              fo.Classification(label="label", confidence=float("-inf"))
+          ]
+      )
 
 
-first["str"] = "0"
-first["str_list"] = ["0"]
+      first["str"] = "0"
+      first["str_list"] = ["0"]
 
-first.save()
+      first.save()
 
-# REAL VALUES MAX
+      # REAL VALUES MAX
 
-second = dataset.skip(1).first()
+      second = dataset.skip(1).first()
 
-second["bool"] = False
-second["bool_list"] = False
+      second["bool"] = False
+      second["bool_list"] = False
 
-second["inf"] = 1.0
-second["inf_list"] = [1.0]
-second["inf_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=1.0)]
-)
+      second["inf"] = 1.0
+      second["inf_list"] = [1.0]
+      second["inf_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=1.0)]
+      )
 
-second["nan"] = 1.0
-second["nan_list"] = [1.0]
-second["nan_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=1.0)]
-)
+      second["nan"] = 1.0
+      second["nan_list"] = [1.0]
+      second["nan_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=1.0)]
+      )
 
-second["ninf"] = 1.0
-second["ninf_list"] = [1.0]
-second["ninf_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=float(1.0))]
-)
+      second["ninf"] = 1.0
+      second["ninf_list"] = [1.0]
+      second["ninf_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=float(1.0))]
+      )
 
-second["str"] = "1"
-second["str_list"] = ["1"]
+      second["str"] = "1"
+      second["str_list"] = ["1"]
 
-second.save()
+      second.save()
 
-# REAL VALUES MIN
+      # REAL VALUES MIN
 
-third = dataset.skip(2).first()
+      third = dataset.skip(2).first()
 
-third["bool"] = True
-third["bool_list"] = True
+      third["bool"] = True
+      third["bool_list"] = True
 
-third["inf"] = -1.0
-third["inf_list"] = [-1.0]
-third["inf_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=-1.0)]
-)
+      third["inf"] = -1.0
+      third["inf_list"] = [-1.0]
+      third["inf_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=-1.0)]
+      )
 
-third["nan"] = -1.0
-third["nan_list"] = [-1.0]
-third["nan_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=-1.0)]
-)
+      third["nan"] = -1.0
+      third["nan_list"] = [-1.0]
+      third["nan_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=-1.0)]
+      )
 
-third["ninf"] = -1.0
-third["ninf_list"] = [-1.0]
-third["ninf_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=-1.0)]
-)
+      third["ninf"] = -1.0
+      third["ninf_list"] = [-1.0]
+      third["ninf_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=-1.0)]
+      )
 
-third["str"] = "2"
-third["str_list"] = ["2"]
+      third["str"] = "2"
+      third["str_list"] = ["2"]
 
-third.save()
+      third.save()
 
-# NONE VALUES
+      # NONE VALUES
 
-fourth = dataset.skip(3).first()
+      fourth = dataset.skip(3).first()
 
-fourth["bool"] = None
-fourth["bool_list"] = None
+      fourth["bool"] = None
+      fourth["bool_list"] = None
 
-fourth["inf"] = None
-fourth["inf_list"] = None
-fourth["inf_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=None)]
-)
+      fourth["inf"] = None
+      fourth["inf_list"] = None
+      fourth["inf_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=None)]
+      )
 
-fourth["nan"] = None
-fourth["nan_list"] = None
-fourth["nan_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=None)]
-)
+      fourth["nan"] = None
+      fourth["nan_list"] = None
+      fourth["nan_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=None)]
+      )
 
-fourth["ninf"] = None
-fourth["ninf_list"] = None
-fourth["ninf_label_list"] = fo.Classifications(
-    classifications=[fo.Classification(label="label", confidence=None)]
-)
+      fourth["ninf"] = None
+      fourth["ninf_list"] = None
+      fourth["ninf_label_list"] = fo.Classifications(
+          classifications=[fo.Classification(label="label", confidence=None)]
+      )
 
-fourth["str"] = None
-fourth["str_list"] = None
+      fourth["str"] = None
+      fourth["str_list"] = None
 
-dataset.create_index("$**")
+      dataset.create_index("$**")
 
-fourth.save()
-        `);
+      fourth.save()`);
+  });
 
-    test.beforeEach(async ({ page, fiftyoneLoader }) => {
-      await fiftyoneLoader.waitUntilGridVisible(page, datasetName);
-    });
-    test("assert query performance icons", async ({ sidebar }) => {
-      sidebar.asserter.assertFieldHasQueryPerformance("sample tags");
-    });
+  test.beforeEach(async ({ page, fiftyoneLoader }) => {
+    await fiftyoneLoader.waitUntilGridVisible(page, datasetName);
+  });
+
+  test("assert query performance icons", async ({ sidebar, grid }) => {
+    await grid.actionsRow.toggleDisplayOptions();
+    await grid.actionsRow.displayActions.setQueryPerformance("enabled");
+    for (const i of [
+      "tags",
+      "metadata.mime_type",
+      "inf_label_list",
+      "nan_label_list",
+      "ninf_label_list",
+      "id",
+      "filepath",
+      "created_at",
+      "last_modified_at",
+      "bool",
+      "bool_list",
+      "str",
+      "str_list",
+    ]) {
+      await sidebar.asserter.assertFieldHasQueryPerformance(i);
+    }
   });
 });
