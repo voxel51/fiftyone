014f24a4a0a56f3b2c2a9370b76eb8adea9ddde3
brimoor <brimoor@umich.edu>
2024-12-03 11:28:37 -0500

    use download and save contexts

--- fiftyone/operators/builtins/operators/data_quality.py
+++ fiftyone/operators/builtins/operators/data_quality.py
@@ -6,15 +6,21 @@ Data quality operators.
 |
 """
 
+import contextlib
+import logging
+
 import cv2
 import numpy as np
 from PIL import Image
 
 import fiftyone as fo
+import fiftyone.core.collections as foc
 import fiftyone.core.patches as fop
+import fiftyone.core.utils as fou
 import fiftyone.operators as foo
 import fiftyone.operators.types as types
 import fiftyone.zoo.models as fozm
+
 
 # pylint:disable=no-name-in-module
 from fiftyone.operators.builtins.operators.utils import (
@@ -29,6 +35,9 @@ from fiftyone.operators.builtins.operators.utils import (
 
 # pylint:disable=import-error,no-name-in-module
 import fiftyone.brain as fob
+
+
+logger = logging.getLogger(__name__)
 
 
 ###############################################################################
@@ -261,7 +270,7 @@ class ComputeExactDuplicates(foo.Operator):
         return types.Property(inputs)
 
     def execute(self, ctx):
-        compute_filehashes(ctx.dataset)
+        apply_fcn(ctx.dataset, _compute_filehash, "filehash")
 
 
 def compute_dataset_property(property, dataset, view=None, patches_field=None):
@@ -328,6 +337,39 @@ def prop_compute_function(property, patch_or_sample, args):
 ###############################################################################
 
 
+def apply_fcn(samples, fcn, field, skip_failures=True, progress=None):
+    with contextlib.ExitStack() as context:
+        context.enter_context(
+            samples.download_context(
+                media_fields="filepath",
+                skip_failures=skip_failures,
+            )
+        )
+
+        pb = context.enter_context(fou.ProgressBar(progress=progress))
+        ctx = context.enter_context(foc.SaveContext(samples))
+
+        for sample in pb(samples):
+            try:
+                sample[field] = fcn(sample.local_path)
+                ctx.save(sample)
+            except Exception as e:
+                if not skip_failures:
+                    raise e
+
+                logger.warning("Sample: %s\nError: %s\n", sample.id, e)
+
+
+###############################################################################
+# Computation Functions
+###############################################################################
+
+
+def _compute_filehash(filepath):
+    with Image.open(filepath) as image:
+        return dhash(_convert_pillow_to_opencv(image))
+
+
 def dhash(image, hash_size=8):
     """
     Compute the dHash for the input image.
@@ -336,6 +378,7 @@ def dhash(image, hash_size=8):
     :param hash_size: Size of the hash (default 8x8).
     :return: The dHash value of the image as a 64-bit integer.
     """
+
     # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -355,38 +398,6 @@ def dhash(image, hash_size=8):
     hex_hash = f"{int(binary_string, 2):0{hash_size * hash_size // 4}x}"
 
     return hex_hash
-
-
-def compute_filehashes(sample_collection):
-    for sample in sample_collection.iter_samples(autosave=True, progress=True):
-        filepath = get_filepath(sample)
-        with Image.open(filepath) as image:
-            sample["filehash"] = dhash(_convert_pillow_to_opencv(image))
-
-
-def gen_approx_duplicate_groups_view(ctx, index):
-    """
-    This function is used to generate the approximate duplicate groups view.
-    """
-
-    near_dup_view = index.duplicates_view()
-    dup_ids = near_dup_view.values("id")
-    view = ctx.dataset.select(dup_ids)
-
-    for rep_id, dups in index.neighbors_map.items():
-        ids = [rep_id] + [d[0] for d in dups]
-        subview = view.select(ids)
-        for sample in subview:
-            sample["approx_dup_group_id"] = rep_id
-            sample.save()
-
-    approx_dup_groups_view = view.group_by("approx_dup_group_id")
-    return near_dup_view, approx_dup_groups_view
-
-
-###############################################################################
-# Computation Functions
-###############################################################################
 
 
 def _compute_brightness(pillow_img):
