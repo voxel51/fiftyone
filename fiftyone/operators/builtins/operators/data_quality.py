c1d916a2c8da8a648e6e6a4650ffb71bbc4645df
brimoor <brimoor@umich.edu>
2024-12-03 20:51:40 -0500

    optimize quality scans

--- fiftyone/operators/builtins/operators/data_quality.py
+++ fiftyone/operators/builtins/operators/data_quality.py
@@ -15,78 +15,18 @@ from PIL import Image
 
 import fiftyone as fo
 import fiftyone.core.collections as foc
+import fiftyone.core.media as fom
 import fiftyone.core.patches as fop
 import fiftyone.core.utils as fou
 import fiftyone.operators as foo
 import fiftyone.operators.types as types
 import fiftyone.zoo.models as fozm
 
-
-# pylint:disable=no-name-in-module
-from fiftyone.operators.builtins.operators.utils import (
-    get_filepath,
-    _convert_opencv_to_pillow,
-    _convert_pillow_to_opencv,
-    _crop_pillow_image,
-    _get_opencv_grayscale_image,
-    _get_pillow_patch,
-    _handle_patch_inputs,
-)
-
 # pylint:disable=import-error,no-name-in-module
 import fiftyone.brain as fob
 
 
 logger = logging.getLogger(__name__)
-
-
-###############################################################################
-# Operator Setup
-###############################################################################
-
-
-def _handle_inputs(ctx, property_name):
-    inputs = types.Object()
-
-    label = f'Compute {property_name.replace("_", " ").title()}'
-    inputs.message("computation_label", label=label)
-
-    inputs.view_target(ctx)
-    _handle_patch_inputs(ctx, inputs)
-    return types.Property(inputs)
-
-
-def _handle_execution(ctx, property_name):
-    # UNCOMMENT below & delete line 51 if we ever want to overwrite existing samples w/ property_name
-    #
-    # new_sample_exist = (
-    #     ctx.params.get("panel_state", {})
-    #     .get("new_samples", {})
-    #     .get(property_name, [0, False])[0]
-    #     > 0
-    # )
-    #
-    # view = (
-    #     ctx.dataset.exists(property_name, bool=False)
-    #     if new_sample_exist
-    #     else ctx.target_view()
-    # )
-
-    view = ctx.dataset.exists(
-        property_name, bool=False
-    )  # default to always scanning samples w/o property_name
-
-    patches_field = ctx.params.get("patches_field", None)
-    compute_dataset_property(
-        property_name, ctx.dataset, view=view, patches_field=patches_field
-    )
-    # update the flag that computation has been done
-    print(f"Computation done for {property_name}")
-
-
-###############################################################################
-# Operators
-###############################################################################
 
 
 class ComputeBrightness(foo.Operator):
@@ -106,7 +46,7 @@ class ComputeBrightness(foo.Operator):
         return _handle_inputs(ctx, "brightness")
 
     def execute(self, ctx):
-        _handle_execution(ctx, "brightness")
+        _handle_execution(ctx, "brightness", _compute_sample_brightness)
 
 
 class ComputeEntropy(foo.Operator):
@@ -126,7 +66,7 @@ class ComputeEntropy(foo.Operator):
         return _handle_inputs(ctx, "entropy")
 
     def execute(self, ctx):
-        _handle_execution(ctx, "entropy")
+        _handle_execution(ctx, "entropy", _compute_sample_entropy)
 
 
 class ComputeAspectRatio(foo.Operator):
@@ -146,27 +86,7 @@ class ComputeAspectRatio(foo.Operator):
         return _handle_inputs(ctx, "aspect_ratio")
 
     def execute(self, ctx):
-        _handle_execution(ctx, "aspect_ratio")
-
-
-class ComputeExposure(foo.Operator):
-    @property
-    def config(self):
-        return foo.OperatorConfig(
-            name="compute_exposure",
-            label="Compute exposure",
-            allow_delegated_execution=True,
-            allow_immediate_execution=True,
-            default_choice_to_delegated=True,
-            dynamic=True,
-            unlisted=True,
-        )
-
-    def resolve_input(self, ctx):
-        return _handle_inputs(ctx, "exposure")
-
-    def execute(self, ctx):
-        _handle_execution(ctx, "exposure")
+        _handle_execution(ctx, "aspect_ratio", _compute_sample_aspect_ratio)
 
 
 class ComputeBlurriness(foo.Operator):
@@ -186,7 +106,7 @@ class ComputeBlurriness(foo.Operator):
         return _handle_inputs(ctx, "blurriness")
 
     def execute(self, ctx):
-        _handle_execution(ctx, "blurriness")
+        _handle_execution(ctx, "blurriness", _compute_sample_blurriness)
 
 
 class ComputeNearDuplicates(foo.Operator):
@@ -203,53 +123,10 @@ class ComputeNearDuplicates(foo.Operator):
         )
 
     def resolve_input(self, ctx):
-        inputs = types.Object()
-
-        target_view = get_target_view(ctx, inputs)
-        get_embeddings(ctx, inputs, target_view, None)
-        get_metric(ctx, inputs)
-
-        view = types.View(label="Compute near duplicates")
-        return types.Property(inputs, view=view)
+        return _handle_near_duplicates_inputs(ctx)
 
     def execute(self, ctx):
-        target = ctx.params.get("target", None)
-        embeddings = ctx.params.get("embeddings", None) or None
-        model = ctx.params.get("model", None) or None
-        batch_size = 8
-        metric = ctx.params.get("metric", "cosine")
-
-        # No multiprocessing allowed when running synchronously
-        if not ctx.delegated:
-            num_workers = 0
-        else:
-            num_workers = None
-
-        target_view = _get_target_view(ctx, target)
-
-        index = fob.compute_similarity(
-            target_view,
-            brain_key=None,
-            embeddings=embeddings,
-            model=model,
-            batch_size=batch_size,
-            num_workers=num_workers,
-            skip_failures=True,
-            backend="sklearn",
-            metric=metric,
-        )
-
-        nearest_inds, dists = index._kneighbors(k=1, return_dists=True)
-
-        index_ids = index.current_sample_ids
-        nearest_ids = np.array([index_ids[i[0]] for i in nearest_inds])
-        dists = np.array([d[0] for d in dists])
-
-        values = dict(zip(index_ids, dists))
-        ctx.dataset.set_values("nearest_neighbor", values, key_field="id")
-
-        values = dict(zip(index_ids, nearest_ids))
-        ctx.dataset.set_values("nearest_id", values, key_field="id")
+        _handle_near_duplicates_execution(ctx)
 
 
 class ComputeExactDuplicates(foo.Operator):
@@ -270,74 +147,26 @@ class ComputeExactDuplicates(foo.Operator):
         return types.Property(inputs)
 
     def execute(self, ctx):
-        apply_fcn(ctx.dataset, _compute_filehash, "filehash")
+        _handle_execution(ctx, "filehash", _compute_sample_filehash)
 
 
-def compute_dataset_property(property, dataset, view=None, patches_field=None):
-    if view is None:
-        view = dataset
-    if patches_field is None:
-        dataset.add_sample_field(property, fo.FloatField)
-        for sample in view.iter_samples(autosave=True, progress=True):
-            if sample is None:
-                continue
-            prop_value = prop_compute_function(
-                property, "sample", {"sample": sample}
-            )
-            if property == "exposure":
-                sample["min_exposure"] = prop_value[0]
-                sample["max_exposure"] = prop_value[1]
-            else:
-                sample[property] = prop_value
+def _handle_inputs(ctx, field):
+    inputs = types.Object()
+
+    label = f'Compute {field.replace("_", " ").title()}'
+    inputs.message("computation_label", label=label)
+
+    inputs.view_target(ctx)
+
+    return types.Property(inputs)
+
+
+def _handle_execution(ctx, field, fcn, skip_failures=True, progress=None):
+    if ctx.dataset.has_field(field):
+        samples = ctx.dataset.exists(field, bool=False)
     else:
-        for sample in view.iter_samples(autosave=True, progress=True):
-            if sample[patches_field] is None:
-                continue
-            for detection in sample[patches_field].detections:
-                prop_value = prop_compute_function(
-                    property,
-                    "patch",
-                    {"sample": sample, "detection": detection},
-                )
-                if property == "exposure":
-                    detection["min_exposure"] = prop_value[0]
-                    detection["max_exposure"] = prop_value[1]
-                else:
-                    detection[property] = prop_value
-        dataset.add_dynamic_sample_fields()
+        samples = ctx.dataset
 
-
-def prop_compute_function(property, patch_or_sample, args):
-    if patch_or_sample == "sample":
-        if property == "brightness":
-            return compute_sample_brightness(**args)
-        elif property == "blurriness":
-            return compute_sample_blurriness(**args)
-        elif property == "entropy":
-            return compute_sample_entropy(**args)
-        elif property == "aspect_ratio":
-            return compute_sample_aspect_ratio(**args)
-        elif property == "exposure":
-            return compute_sample_exposure(**args)
-    elif patch_or_sample == "patch":
-        if property == "brightness":
-            return compute_patch_brightness(**args)
-        elif property == "blurriness":
-            return compute_patch_blurriness(**args)
-        elif property == "entropy":
-            return compute_patch_entropy(**args)
-        elif property == "aspect_ratio":
-            return compute_patch_aspect_ratio(**args)
-        elif property == "exposure":
-            return compute_patch_exposure(**args)
-
-
-###############################################################################
-# Operator Helper Functions
-###############################################################################
-
-
-def apply_fcn(samples, fcn, field, skip_failures=True, progress=None):
     with contextlib.ExitStack() as context:
         context.enter_context(
             samples.download_context(
@@ -351,7 +180,7 @@ def apply_fcn(samples, fcn, field, skip_failures=True, progress=None):
 
         for sample in pb(samples):
             try:
-                sample[field] = fcn(sample.local_path)
+                sample[field] = fcn(sample)
                 ctx.save(sample)
             except Exception as e:
                 if not skip_failures:
@@ -360,44 +189,73 @@ def apply_fcn(samples, fcn, field, skip_failures=True, progress=None):
                 logger.warning("Sample: %s\nError: %s\n", sample.id, e)
 
 
-###############################################################################
-# Computation Functions
-###############################################################################
+def _handle_near_duplicates_inputs(ctx):
+    inputs = types.Object()
+
+    target_view = _get_target_view_inputs(ctx, inputs)
+    _get_embeddings_inputs(ctx, inputs, target_view)
+    _get_metric_inputs(ctx, inputs)
+
+    # @todo add warning that this method only supports <100k samples
+    view = types.View(label="Compute near duplicates")
+    return types.Property(inputs, view=view)
 
 
-def _compute_filehash(filepath):
-    with Image.open(filepath) as image:
-        return dhash(_convert_pillow_to_opencv(image))
+def _handle_near_duplicates_execution(ctx):
+    target = ctx.params.get("target", None)
+    embeddings = ctx.params.get("embeddings", None) or None
+    model = ctx.params.get("model", None) or None
+    batch_size = 8
+    metric = ctx.params.get("metric", "cosine")
+
+    # No multiprocessing allowed when running synchronously
+    if not ctx.delegated:
+        num_workers = 0
+    else:
+        num_workers = None
+
+    target_view = _get_target_view(ctx, target)
+
+    # @todo only compute embeddings for samples that don't have them
+    index = fob.compute_similarity(
+        target_view,
+        brain_key=None,
+        embeddings=embeddings,
+        model=model,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        skip_failures=True,
+        backend="sklearn",
+        metric=metric,
+    )
+
+    nearest_inds, dists = index._kneighbors(k=1, return_dists=True)
+
+    index_ids = index.current_sample_ids
+    nearest_ids = np.array([index_ids[i[0]] for i in nearest_inds])
+    dists = np.array([d[0] for d in dists])
+
+    values = dict(zip(index_ids, dists))
+    ctx.dataset.set_values("nearest_neighbor", values, key_field="id")
+
+    values = dict(zip(index_ids, nearest_ids))
+    ctx.dataset.set_values("nearest_id", values, key_field="id")
 
 
-def dhash(image, hash_size=8):
-    """
-    Compute the dHash for the input image.
+def _compute_sample_filehash(sample):
+    filepath = sample.local_path
+    method = "md5" if sample.media_type == fom.VIDEO else None
+    return _compute_filehash(filepath, method=method)
 
-    :param image: Input image to hash (as a NumPy array).
-    :param hash_size: Size of the hash (default 8x8).
-    :return: The dHash value of the image as a 64-bit integer.
-    """
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def _compute_filehash(filepath, method=None):
+    return fou.compute_filehash(filepath, method=method)
 
-    # Resize the image to (hash_size + 1, hash_size)
-    resized = cv2.resize(gray, (hash_size + 1, hash_size))
 
-    # Compute the differences between adjacent pixels
-    diff = resized[:, 1:] > resized[:, :-1]
-
-    # Convert the difference image to a binary hash
-    # hash_value = sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
-
-    # Convert the difference image to a binary hash
-    binary_string = "".join(["1" if v else "0" for v in diff.flatten()])
-
-    # Convert the binary string to a hexadecimal string
-    hex_hash = f"{int(binary_string, 2):0{hash_size * hash_size // 4}x}"
-
-    return hex_hash
+def _compute_sample_brightness(sample):
+    filepath = sample.local_path
+    with Image.open(filepath) as pillow_img:
+        return _compute_brightness(pillow_img)
 
 
 def _compute_brightness(pillow_img):
@@ -422,22 +280,35 @@ def _compute_brightness(pillow_img):
     return brightness
 
 
-def _compute_exposure(opencv_gray_img):
-    # pylint: disable=no-member
-    histogram = cv2.calcHist([opencv_gray_img], [0], None, [256], [0, 256])
-    normalized_histogram = histogram.ravel() / histogram.max()
-    min_exposure = normalized_histogram[0]
-    max_exposure = normalized_histogram[-1]
-    return min_exposure, max_exposure
+def _compute_sample_entropy(sample):
+    filepath = sample.local_path
+    with Image.open(filepath) as pillow_img:
+        return _compute_entropy(pillow_img)
 
 
 def _compute_entropy(pillow_img):
     return pillow_img.entropy()
 
 
+def _compute_sample_aspect_ratio(sample):
+    if sample.metadata is None:
+        sample.compute_metadata()
+
+    width = sample.metadata.width
+    height = sample.metadata.height
+    return _compute_aspect_ratio(width, height)
+
+
 def _compute_aspect_ratio(width, height):
     ratio = width / height
     return min(ratio, 1 / ratio)
+
+
+def _compute_sample_blurriness(sample):
+    filepath = sample.local_path
+    # pylint: disable=no-member
+    cv2_img = cv2.imread(filepath)
+    return _compute_blurriness(cv2_img)
 
 
 def _compute_blurriness(cv2_img):
@@ -449,75 +320,20 @@ def _compute_blurriness(cv2_img):
     return variance
 
 
-def compute_sample_brightness(sample):
-    filepath = get_filepath(sample)
-    with Image.open(filepath) as image:
-        return _compute_brightness(image)
+def _get_target_view(ctx, target):
+    if target == "SELECTED_SAMPLES":
+        return ctx.view.select(ctx.selected)
+
+    if target == "BASE_VIEW":
+        return ctx.view._base_view
+
+    if target == "DATASET":
+        return ctx.dataset
+
+    return ctx.view
 
 
-def compute_patch_brightness(sample, detection):
-    patch = _get_pillow_patch(sample, detection)
-    return _compute_brightness(patch)
-
-
-def compute_sample_exposure(sample):
-    gray_img = _get_opencv_grayscale_image(sample)
-    return _compute_exposure(gray_img)
-
-
-def compute_patch_exposure(sample, detection):
-    gray_img = _get_opencv_grayscale_image(sample)
-    pillow_image = _convert_opencv_to_pillow(gray_img)
-    patch = _crop_pillow_image(pillow_image, detection)
-    patch = _convert_pillow_to_opencv(patch)
-    return _compute_exposure(patch)
-
-
-def compute_sample_entropy(sample):
-    filepath = get_filepath(sample)
-    with Image.open(filepath) as image:
-        return _compute_entropy(image)
-
-
-def compute_patch_entropy(sample, detection):
-    patch = _get_pillow_patch(sample, detection)
-    return _compute_entropy(patch)
-
-
-def compute_sample_aspect_ratio(sample):
-    if sample.metadata is None:
-        sample.compute_metadata()
-    width, height = sample.metadata.width, sample.metadata.height
-    return _compute_aspect_ratio(width, height)
-
-
-def compute_patch_aspect_ratio(sample, detection):
-    if sample.metadata is None:
-        sample.compute_metadata()
-    img_width, img_height = sample.metadata.width, sample.metadata.height
-    bbox_width, bbox_height = detection.bounding_box[2:]
-    width, height = bbox_width * img_width, bbox_height * img_height
-    return _compute_aspect_ratio(width, height)
-
-
-def compute_sample_blurriness(sample):
-    # pylint: disable=no-member
-    image = cv2.imread(get_filepath(sample))
-    return _compute_blurriness(image)
-
-
-def compute_patch_blurriness(sample, detection):
-    patch = _get_pillow_patch(sample, detection)
-    patch = _convert_pillow_to_opencv(patch)
-    return _compute_blurriness(patch)
-
-
-###############################################################################
-# Near Duplicates Functions
-###############################################################################
-
-
-def get_target_view(ctx, inputs, allow_selected=True):
+def _get_target_view_inputs(ctx, inputs, allow_selected=True):
     has_base_view = isinstance(ctx.view, fop.PatchesView)
     if has_base_view:
         has_view = ctx.view != ctx.view._base_view
@@ -572,28 +388,9 @@ def get_target_view(ctx, inputs, allow_selected=True):
     return _get_target_view(ctx, target)
 
 
-def _get_target_view(ctx, target):
-    if target == "SELECTED_SAMPLES":
-        return ctx.view.select(ctx.selected)
-
-    if target == "BASE_VIEW":
-        return ctx.view._base_view
-
-    if target == "DATASET":
-        return ctx.dataset
-
-    return ctx.view
-
-
-def get_embeddings(ctx, inputs, view, patches_field):
-    if patches_field is not None:
-        root, _ = view._get_label_field_root(patches_field)
-        field = view.get_field(root, leaf=True)
-        schema = field.get_field_schema(ftype=fo.VectorField)
-        embeddings_fields = set(root + "." + k for k in schema.keys())
-    else:
-        schema = view.get_field_schema(ftype=fo.VectorField)
-        embeddings_fields = set(schema.keys())
+def _get_embeddings_inputs(ctx, inputs, view):
+    schema = view.get_field_schema(ftype=fo.VectorField)
+    embeddings_fields = set(schema.keys())
 
     embeddings_choices = types.AutocompleteView()
     for field_name in sorted(embeddings_fields):
@@ -649,7 +446,7 @@ def _get_zoo_models():
     return available_models
 
 
-def get_metric(ctx, inputs):
+def _get_metric_inputs(ctx, inputs):
     metric_choices = types.DropdownView()
     metric_choices.add_choice("cosine", label="cosine")
     metric_choices.add_choice("euclidean", label="euclidean")
