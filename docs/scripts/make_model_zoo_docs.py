61f900a9892231456ce9e9bee912b220f979c090
Daniel Bogdoll <dbogdoll@umich.edu>
2024-12-05 12:45:16 -0500

    Fixed example for zero-shot-detection-transformer-torch

--- docs/scripts/make_model_zoo_docs.py
+++ docs/scripts/make_model_zoo_docs.py
@@ -173,6 +173,17 @@ _MODEL_TEMPLATE = """
     model = foz.load_zoo_model("{{ name }}")
 
     embeddings = dataset.compute_embeddings(model)
+
+{% elif 'zero-shot-detection-transformer-torch' in name %}
+    model = foz.load_zoo_model(
+    "{{ name }}",
+    classes=["person", "dog", "cat", "bird", "car", "tree", "chair"]
+    )
+
+    dataset.apply_model(model, label_field="predictions")
+
+    session = fo.launch_app(dataset)
+    
 {% else %}
     model = foz.load_zoo_model("{{ name }}")
 
