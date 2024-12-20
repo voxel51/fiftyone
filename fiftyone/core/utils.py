4953b31c4d0f8a42c1c9b3a7474434532434b7eb
brimoor <brimoor@umich.edu>
2024-12-20 07:04:46 -0500

    bump to ETA v0.13.1

--- fiftyone/core/utils.py
+++ fiftyone/core/utils.py
@@ -973,19 +973,6 @@ class ProgressBar(etau.ProgressBar):
         self._progress = progress
         self._callback = callback
 
-    def __call__(self, iterable):
-        # Ensure that `len(iterable)` is not computed unnecessarily
-        no_len = self._quiet and self._total is None
-        if no_len:
-            self._total = -1
-
-        super().__call__(iterable)
-
-        if no_len:
-            self._total = None
-
-        return self
-
     def set_iteration(self, *args, **kwargs):
         super().set_iteration(*args, **kwargs)
 
