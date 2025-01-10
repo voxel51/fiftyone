a87595411454549cb5e64d1710423eddac6ff150
topher <topher@voxel51.com>
2025-01-10 13:30:44 +0000

    bump versions for v2.4.0 release

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="2.4.0rc3",
+    version="2.4.0",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
