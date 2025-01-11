216a3743ad759e2fea9ed7aea93dfed1dea317cd
Sashank Aryal <sashank2012@gmail.com>
2025-01-10 20:17:54 -0600

    move sync and check logic to another file and add unit tests

---
+++ app/packages/core/src/components/Sidebar/syncAndCheckRefreshNeeded.ts
@@ -0,0 +1,51 @@
+import { CachedLabels, LookerId } from "./useShouldReloadSample";
+
+/**
+ * Checks if a given looker needs to be refreshed based on active label fields.
+ *
+ * @param lookerId - unique Looker ID
+ * @param lut - look-up table tracking which fields each looker has already rendered at least once (and therefore cached)
+ * @param currentActiveLabelFields - set of active label fields for the modal/looker
+ *
+ * @returns whether or not looker should be refreshed (true) or not (false)
+ */
+export function syncAndCheckRefreshNeeded(
+  lookerId: string,
+  lut: Map<LookerId, CachedLabels>,
+  currentActiveLabelFields: Set<string>
+): boolean {
+  // if no active fields, no refresh is needed
+  if (currentActiveLabelFields.size === 0) {
+    return false;
+  }
+
+  const thisLookerActiveFields = lut.get(lookerId);
+
+  // we only care about net-new fields.
+  let hasNewFields = false;
+
+  if (thisLookerActiveFields) {
+    for (const field of currentActiveLabelFields) {
+      if (!thisLookerActiveFields.has(field)) {
+        hasNewFields = true;
+        break;
+      }
+    }
+  } else {
+    // if `thisLookerActiveFields` is undefined, it means this looker has not been considered yet
+    hasNewFields = true;
+  }
+
+  // if no new fields, then no refresh is needed
+  if (!hasNewFields) {
+    return false;
+  }
+
+  // update cached labels set for this looker
+  lut.set(
+    lookerId,
+    new Set([...(thisLookerActiveFields ?? []), ...currentActiveLabelFields])
+  );
+
+  return true;
+}
--- app/packages/core/src/components/Sidebar/useShouldReloadSample.ts
+++ app/packages/core/src/components/Sidebar/useShouldReloadSample.ts
@@ -1,9 +1,10 @@
 import { activeLabelFields } from "@fiftyone/state";
 import { useCallback } from "react";
 import { useRecoilValue } from "recoil";
+import { syncAndCheckRefreshNeeded } from "./syncAndCheckRefreshNeeded";
 
-type LookerId = string;
-type CachedLabels = Set<string>;
+export type LookerId = string;
+export type CachedLabels = Set<string>;
 
 export const gridActivePathsLUT = new Map<LookerId, CachedLabels>();
 
@@ -16,41 +17,11 @@ export const useShouldReloadSampleOnActiveFieldsChange = ({
 
   const shouldRefresh = useCallback(
     (id: string) => {
-      const thisLookerActiveFields = gridActivePathsLUT.get(id);
-      const currentActiveLabelFields = new Set(activeLabelFieldsValue);
-
-      if (currentActiveLabelFields.size === 0) {
-        return false;
-      }
-
-      // diff the two sets, we only care about net new fields
-      // if there are no new fields, we don't need to update the tracker
-      let hasNewFields = false;
-
-      if (thisLookerActiveFields) {
-        for (const field of currentActiveLabelFields) {
-          if (!thisLookerActiveFields.has(field)) {
-            hasNewFields = true;
-            break;
-          }
-        }
-      } else {
-        hasNewFields = true;
-      }
-
-      if (!hasNewFields) {
-        return false;
-      }
-
-      gridActivePathsLUT.set(
+      return syncAndCheckRefreshNeeded(
         id,
-        new Set([
-          ...(thisLookerActiveFields ?? []),
-          ...currentActiveLabelFields,
-        ])
+        gridActivePathsLUT,
+        new Set(activeLabelFieldsValue)
       );
-
-      return true;
     },
     [activeLabelFieldsValue]
   );
