27aa36abc7ded1609390a9dec6e7cbafbc92534d
Ritchie Martori <ritchie@voxel51.com>
2025-01-13 13:21:05 -0700

    cleanup auto complete and dropdowns

--- app/packages/core/src/plugins/SchemaIO/components/AutocompleteView.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/AutocompleteView.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Autocomplete, MenuItem, TextField } from "@mui/material";
+import { Autocomplete, MenuItem, Select, TextField } from "@mui/material";
 import FieldWrapper from "./FieldWrapper";
 import autoFocus from "../utils/auto-focus";
 import { getComponentProps } from "../utils";
@@ -14,24 +14,32 @@ export default function AutocompleteView(props) {
   const multiple = schema.type === "array";
   const allowDups = view.allow_duplicates !== false;
   const [key, setUserChanged] = useKey(path, schema, data, true);
-
+  const valuesOnly = getValuesOnlySettingFromSchema(schema);
+  const allowUserInput = view.allow_user_input !== false;
+  const allowClearing = view.allow_clearing !== false;
   return (
     <FieldWrapper {...props}>
       <Autocomplete
+        disableClearable={!allowClearing}
         key={key}
         disabled={readOnly}
         autoHighlight
         clearOnBlur={multiple}
         defaultValue={getDefaultValue(data, choices)}
-        freeSolo={view.allow_user_input !== false}
+        freeSolo={allowUserInput}
         size="small"
         onChange={(e, choice) => {
-          const changedValue = multiple
-            ? choice.map((c) => {
-                if (view.value_only) return c.value || c;
-                return c;
-              })
-            : choice?.value || choice;
+          if (choice === null) {
+            onChange(path, null);
+            setUserChanged();
+            return;
+          }
+          const changedValue = resolveChangedValues(
+            schema,
+            choice,
+            valuesOnly,
+            multiple
+          );
           onChange(path, changedValue);
           setUserChanged();
         }}
@@ -42,7 +50,6 @@ export default function AutocompleteView(props) {
         }))}
         renderInput={(params) => (
           <TextField
-            autoFocus={autoFocus(props)}
             {...params}
             placeholder={
               multiple
@@ -52,18 +59,24 @@ export default function AutocompleteView(props) {
           />
         )}
         onInputChange={(e) => {
-          if (!multiple && e) {
+          if (!e) return;
+          if (!e.target.value && !multiple) {
+            onChange(path, null);
+            setUserChanged();
+          }
+          if (!multiple && e && allowUserInput) {
             onChange(path, e.target.value);
             setUserChanged();
           }
         }}
         isOptionEqualToValue={(option, value) => {
           if (allowDups) return false;
-          return option.id == value.id;
-        }} // allow duplicates
+          option = resolveChangedValue(schema, option, true);
+          value = resolveChangedValue(schema, value, true);
+          return option == value;
+        }}
         multiple={multiple}
         renderOption={(props, option) => {
-          console.log(props, option);
           return (
             <MenuItem
               {...props}
@@ -79,7 +92,53 @@ export default function AutocompleteView(props) {
   );
 }
 
+// TODO: move these functions to a utils file
+
 function getDefaultValue(defaultValue, choices = []) {
   const choice = choices.find(({ value }) => value === defaultValue);
   return choice || defaultValue;
+}
+
+function getValuesOnlySettingFromSchema(schema) {
+  const { view = {} } = schema;
+  const isObject = schema.type === "object";
+  const providedSetting = view.value_only;
+  const isDefined = providedSetting !== undefined && providedSetting !== null;
+  if (isDefined) return providedSetting;
+  if (isObject) return false;
+  return true;
+}
+
+function resolveChangedValues(schema, choice, valuesOnly, multiple) {
+  if (multiple) {
+    if (Array.isArray(choice))
+      return choice.map((c) => resolveChangedValue(schema, c, valuesOnly));
+    return [];
+  } else {
+    return resolveChangedValue(schema, choice, valuesOnly);
+  }
+}
+
+function resolveChangedValue(schema, choice, valuesOnly) {
+  let resolvedValue;
+  const isObjectSchema = schema.type === "object";
+  const isStringSchema = schema.type === "string";
+  const isChoiceObject =
+    typeof choice === "object" &&
+    choice.value !== undefined &&
+    choice.value !== null;
+  const valuesOnlyIsDefault = valuesOnly == undefined || valuesOnly == null;
+  if (!isChoiceObject) {
+    choice = { id: choice, value: choice };
+  }
+  if (isStringSchema && valuesOnlyIsDefault) {
+    valuesOnly = true;
+  }
+  if (isObjectSchema) {
+    resolvedValue = choice;
+  }
+  if (valuesOnly) {
+    resolvedValue = choice.value;
+  }
+  return resolvedValue;
 }
