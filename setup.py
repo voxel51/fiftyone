8807153808e2ccf190b8c8722172f40517bc46f1
topher <topher@voxel51.com>
2025-01-14 03:52:03 +0000

    chore: bumping version with release branch creation

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from importlib import metadata
 
 from setuptools import find_packages, setup
 
-VERSION = "2.5.0"
+VERSION = "2.6.0"
 
 
 def get_version():
@@ -134,7 +134,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fiftyone",
-    version="2.5.0.dev7",
+    version="2.6.0.dev0",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
