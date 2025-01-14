6800029a37f7b470b6113b9a11697d9a0ebc4953
brimoor <brimoor@umich.edu>
2025-01-14 09:46:01 -0500

    adopt finalized #5375 syntax

--- plugins/operators/__init__.py
+++ plugins/operators/__init__.py
@@ -643,7 +643,7 @@ def _clear_sample_field_inputs(ctx, inputs):
 
     field_names = ctx.params.get("field_names", None) or []
 
-    field_choices = types.AutocompleteView(multiple=True)
+    field_choices = types.AutocompleteView(allow_duplicates=False)
     for key in schema.keys():
         if not any(key.startswith(f + ".") for f in field_names):
             field_choices.add_choice(key, label=key)
@@ -751,7 +751,7 @@ def _clear_frame_field_inputs(ctx, inputs):
 
     field_names = ctx.params.get("field_names", None) or []
 
-    field_choices = types.AutocompleteView(multiple=True)
+    field_choices = types.AutocompleteView(allow_duplicates=False)
     for key in schema.keys():
         if not any(key.startswith(f + ".") for f in field_names):
             field_choices.add_choice(key, label=key)
@@ -903,7 +903,7 @@ def _delete_sample_field_inputs(ctx, inputs):
 
     field_names = ctx.params.get("field_names", None) or []
 
-    field_choices = types.AutocompleteView(multiple=True)
+    field_choices = types.AutocompleteView(allow_duplicates=False)
     for key in schema.keys():
         if not any(key.startswith(f + ".") for f in field_names):
             field_choices.add_choice(key, label=key)
@@ -978,7 +978,7 @@ def _delete_frame_field_inputs(ctx, inputs):
 
     field_names = ctx.params.get("field_names", None) or []
 
-    field_choices = types.AutocompleteView(multiple=True)
+    field_choices = types.AutocompleteView(allow_duplicates=False)
     for key in schema.keys():
         if not any(key.startswith(f + ".") for f in field_names):
             field_choices.add_choice(key, label=key)
@@ -1153,7 +1153,7 @@ def _drop_index_inputs(ctx, inputs):
 
     indexes = [i for i in indexes if i not in default_indexes]
 
-    index_selector = types.DropdownView(multiple=True)
+    index_selector = types.DropdownView()
     for key in indexes:
         index_selector.add_choice(key, label=key)
 
@@ -1465,7 +1465,7 @@ def _delete_summary_field_inputs(ctx, inputs):
         prop.invalid = True
         return
 
-    field_selector = types.DropdownView(multiple=True)
+    field_selector = types.DropdownView()
     for key in summary_fields:
         field_selector.add_choice(key, label=key)
 
@@ -1646,7 +1646,7 @@ def _delete_group_slice_inputs(ctx, inputs):
         prop.invalid = True
         return
 
-    slice_selector = types.DropdownView(multiple=True)
+    slice_selector = types.DropdownView()
     for key in ctx.dataset.group_slices:
         slice_selector.add_choice(key, label=key)
 
@@ -1920,7 +1920,7 @@ def _delete_saved_view_inputs(ctx, inputs):
         prop.invalid = True
         return
 
-    saved_view_selector = types.DropdownView(multiple=True)
+    saved_view_selector = types.DropdownView()
     for key in saved_views:
         saved_view_selector.add_choice(key, label=key)
 
@@ -2211,7 +2211,7 @@ def _delete_workspace_inputs(ctx, inputs):
         prop.invalid = True
         return
 
-    workspace_selector = types.DropdownView(multiple=True)
+    workspace_selector = types.DropdownView()
     for key in workspaces:
         workspace_selector.add_choice(key, label=key)
 
