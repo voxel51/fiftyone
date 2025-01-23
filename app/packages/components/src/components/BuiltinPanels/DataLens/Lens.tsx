266ab165b223075e02ea731e09fc85f6cfb63160
Sashank Aryal <sashank2012@gmail.com>
2025-01-23 13:12:45 -0600

    improve json viewer

--- app/packages/components/src/components/BuiltinPanels/DataLens/Lens.tsx
+++ app/packages/components/src/components/BuiltinPanels/DataLens/Lens.tsx
@@ -1,9 +1,10 @@
 import { Schema } from "@fiftyone/utilities";
-import { Box } from "@mui/material";
+import { Box, useColorScheme } from "@mui/material";
+import { JsonViewer } from "@textea/json-viewer";
 import { useAtomValue } from "jotai";
 import React, { useLayoutEffect, useMemo, useState } from "react";
+import "react-json-view-lite/dist/index.css";
 import { v4 as uuid } from "uuid";
-import JSONViewer from "../../JSONViewer";
 import { MAX_ZOOM_LEVEL, MIN_ZOOM_LEVEL } from "./constants";
 import { useSpotlight } from "./hooks";
 import type { LensSample } from "./models";
@@ -80,9 +81,56 @@ const SpotLightRenderer = ({
 };
 
 const JsonRenderer = ({ samples }: { samples: LensSample[] }) => {
+  const { mode } = useColorScheme();
+
+  const samplesWithClsRemoved = useMemo(() => {
+    function removeClsFields(obj: any): any {
+      if (typeof obj !== "object" || obj === null) {
+        // base case: return primitives or null
+        return obj;
+      }
+
+      if (Array.isArray(obj)) {
+        // if it's an array, recursively process each element
+        return obj.map(removeClsFields);
+      }
+
+      // if it's an object, create a new object without the '_cls' field
+      const result: any = {};
+      for (const key in obj) {
+        if (key !== "_cls") {
+          // recursively process nested objects
+          result[key] = removeClsFields(obj[key]);
+        }
+      }
+
+      return result;
+    }
+
+    return samples.map((sample) =>
+      // note: consider not removing _cls altogether since deep traversal can be expensive...
+      // for large number of samples or samples with deep labels
+      // or, do so with a max-depth strategy
+      removeClsFields(sample)
+    );
+  }, [samples]);
+
+  // note 1: consider just using <pre>{JSON.stringify(samplesWithClsRemoved, null, 2)}</pre>
+  // rendering is significantly faster that way, although we lose on syntax highlighting and
+  // collapsible objects
+
+  // note 2: it's intentional that we didn't use custom fo's custom JsonViewer, which adds
+  // support for searching and filtering. This is because the added searching functionality
+  // has a performance cost, and we want to keep the JSON viewer as fast as possible.
   return (
     <Box>
-      <JSONViewer value={samples as ReturnType<typeof JSON.parse>} />
+      <JsonViewer
+        theme={mode === "dark" ? "dark" : "light"}
+        rootName={false}
+        // value of more than 2 expands labels, which is not desirable
+        defaultInspectDepth={2}
+        value={samplesWithClsRemoved as ReturnType<typeof JSON.parse>}
+      />
     </Box>
   );
 };
