d87fae5e01a96780facce5e5d4679cc9482eaed1
Benjamin Kane <ben@voxel51.com>
2025-01-27 14:11:12 -0500

    Allow SelectGroupSlices for QP (#5428)
    * is query performant
    
    * is qp view selector
    
    * linting

--- app/packages/state/src/recoil/queryPerformance.ts
+++ app/packages/state/src/recoil/queryPerformance.ts
@@ -19,6 +19,9 @@ import { datasetId, datasetName } from "./selectors";
 import { State } from "./types";
 import { view } from "./view";
 
+const SELECT_GROUP_SLICES = "fiftyone.core.stages.SelectGroupSlices";
+const VALID_QP_STAGES = new Set([SELECT_GROUP_SLICES]);
+
 export const lightningQuery = graphQLSelectorFamily<
   foq.lightningQuery$variables,
   foq.LightningInput["paths"],
@@ -188,6 +191,22 @@ export const indexedPaths = selectorFamily<Set<string>, string>({
     },
 });
 
+export const isQueryPerformantView = selector({
+  key: "isQueryPerformantView",
+  get: ({ get }) => {
+    const stages = get(view);
+    if (!stages?.length) {
+      return true;
+    }
+
+    if (stages.length === 1) {
+      return VALID_QP_STAGES.has(stages[0]._cls);
+    }
+
+    return false;
+  },
+});
+
 export const enableQueryPerformanceConfig = selector({
   key: "enableQueryPerformanceConfig",
   get: ({ get }) => get(config).enableQueryPerformance,
@@ -198,24 +217,15 @@ export const defaultQueryPerformanceConfig = selector({
   get: ({ get }) => get(config).defaultQueryPerformance,
 });
 
-const queryPerformanceStore = atomFamily<boolean, string>({
-  key: "queryPerformanceStore",
-  default: undefined,
-  effects: (datasetId) => [
-    getBrowserStorageEffectForKey(`queryPerformance-${datasetId}`, {
-      sessionStorage: true,
-      valueClass: "boolean",
-    }),
-  ],
-});
-
 export const queryPerformance = selector<boolean>({
   key: "queryPerformance",
-  get: ({ get }) => {
-    if (get(view).length) {
-      return false;
-    }
+  get: ({ get }) => get(queryPerformanceSetting) && get(isQueryPerformantView),
+  set: ({ set }, value) => set(queryPerformanceSetting, value),
+});
 
+export const queryPerformanceSetting = selector<boolean>({
+  key: "queryPerformanceSetting",
+  get: ({ get }) => {
     if (!get(enableQueryPerformanceConfig)) {
       return false;
     }
@@ -233,4 +243,15 @@ export const queryPerformance = selector<boolean>({
       value instanceof DefaultValue ? undefined : value
     );
   },
+});
+
+const queryPerformanceStore = atomFamily<boolean, string>({
+  key: "queryPerformanceStore",
+  default: undefined,
+  effects: (datasetId) => [
+    getBrowserStorageEffectForKey(`queryPerformance-${datasetId}`, {
+      sessionStorage: true,
+      valueClass: "boolean",
+    }),
+  ],
 });
