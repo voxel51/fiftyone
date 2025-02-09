58af3b18df5a117d13a5a9b4a2b040c873b9c346
brimoor <brimoor@umich.edu>
2025-02-08 20:10:17 -0500

    add map_samples() implementation

---
+++ fiftyone/utils/multiprocessing.py
@@ -0,0 +1,294 @@
+"""
+Multiprocessing utilities.
+
+| Copyright 2017-2025, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+import itertools
+
+from bson import ObjectId
+import numpy as np
+from tqdm.auto import tqdm
+
+import fiftyone as fo
+import fiftyone.core.utils as fou
+import fiftyone.core.view as fov
+
+
+def map_samples(
+    sample_collection,
+    map_fcn,
+    reduce_fcn=None,
+    save=None,
+    num_workers=None,
+    shard_size=None,
+    shard_method="id",
+    progress=None,
+):
+    """Applies the given function to each sample in the collection via a
+    multiprocessing pool.
+
+    When only a ``map_fcn`` is provided, this function effectively performs
+    the following map operation with the outer loop in parallel::
+
+        for batch_view in fou.iter_batches(sample_collection, shard_size):
+            for sample in batch_view.iter_samples(autosave=True):
+                map_fcn(sample)
+
+    When a ``reduce_fcn`` is provided, this function effectively performs
+    the following map-reduce operation with the outer loop in parallel::
+
+        values = {}
+        for batch_view in fou.iter_batches(sample_collection, shard_size):
+            for sample in batch_view.iter_samples(autosave=save):
+                values[sample.id] = map_fcn(sample)
+
+        output = reduce_fcn(sample_collection, values)
+
+    Example::
+
+        import fiftyone as fo
+        import fiftyone.utils.multiprocessing as foum
+        import fiftyone.zoo as foz
+
+        dataset = foz.load_zoo_dataset("cifar10", split="train")
+        view = dataset.select_fields("ground_truth")
+
+        #
+        # Example 1: map
+        #
+
+        def map_fcn(sample):
+            sample.ground_truth.label = sample.ground_truth.label.upper()
+
+        foum.map_samples(view, map_fcn)
+
+        print(dataset.count_values("ground_truth.label"))
+
+        #
+        # Example 2: map-reduce
+        #
+
+        def map_fcn(sample):
+            return sample.ground_truth.label.lower()
+
+        def reduce_fcn(sample_collection, values):
+            from collections import Counter
+            return dict(Counter(values.values()))
+
+        counts = foum.map_samples(view, map_fcn, reduce_fcn=reduce_fcn)
+        print(counts)
+
+    Args:
+        sample_collection: a
+            :class:`fiftyone.core.collections.SampleCollection`
+        map_fcn: a function to apply to each sample
+        reduce_fcn (None): an optional function to reduce the map outputs. See
+            the docstring above for usage information
+        save (None): whether to save any sample edits applied by ``map_fcn``.
+            By default this is True when no ``reduce_fcn`` is provided and
+            False when a ``reduce_fcn`` is provided
+        num_workers (None): the number of workers to use. The default is
+            :meth:`fiftyone.core.utils.recommend_process_pool_workers`
+        shard_size (None): an optional number of samples to distribute to each
+            worker at a time. By default, samples are evenly distributed to
+            workers with one shard per worker
+        shard_method ("id"): whether to use IDs (``"id"``) or slices
+            (``"slice"``) to assign samples to workers
+        progress (None): whether to render a progress bar for each worker
+            (True/False), use the default value
+            ``fiftyone.config.show_progress_bars`` (None), or "global" to
+            render a single global progress bar, or a progress callback
+            function to invoke instead
+
+    Returns:
+        the output of ``reduce_fcn``, if provided, else None
+    """
+    if isinstance(sample_collection, fov.DatasetView):
+        dataset_name = sample_collection._root_dataset.name
+        view_stages = sample_collection._serialize()
+    else:
+        dataset_name = sample_collection.name
+        view_stages = None
+
+    batches, num_workers = _init_batches(
+        sample_collection,
+        shard_size=shard_size,
+        shard_method=shard_method,
+        num_workers=num_workers,
+    )
+
+    if save is None:
+        save = reduce_fcn is None
+
+    if progress is None:
+        progress = fo.config.show_progress_bars
+
+    return_outputs = reduce_fcn is not None
+    global_progress = False
+    lock = None
+
+    ctx = fou.get_multiprocessing_context()
+
+    if progress is True:
+        lock = ctx.RLock()
+        tqdm.set_lock(lock)
+    elif progress == "global":
+        global_progress = True
+        progress = False
+    elif callable(progress):
+        global_progress = progress
+        progress = False
+
+    pool = ctx.Pool(
+        processes=num_workers,
+        initializer=_init_worker,
+        initargs=(
+            dataset_name,
+            view_stages,
+            map_fcn,
+            save,
+            return_outputs,
+            progress,
+            lock,
+        ),
+    )
+
+    outputs = {}
+
+    pb = fou.ProgressBar(
+        total=len(batches),
+        progress=global_progress,
+        iters_str="batches",
+    )
+
+    with pb, pool:
+        for _outputs in pb(pool.imap_unordered(_map_batch, batches)):
+            if _outputs is not None:
+                outputs.update(_outputs)
+
+    if reduce_fcn is not None:
+        return reduce_fcn(sample_collection, outputs)
+
+
+def _init_batches(
+    sample_collection,
+    shard_size=None,
+    shard_method="id",
+    num_workers=None,
+):
+    if shard_method == "slice":
+        n = len(sample_collection)
+    else:
+        ids = sample_collection.values("id")
+        n = len(ids)
+
+    if num_workers is None:
+        num_workers = fou.recommend_process_pool_workers()
+
+    # Must cap size of select(ids) stages
+    if shard_method == "id":
+        max_shard_size = fou.recommend_batch_size_for_value(
+            ObjectId(), max_size=100000
+        )
+
+        if shard_size is not None:
+            shard_size = min(shard_size, max_shard_size)
+        elif n > num_workers * max_shard_size:
+            shard_size = max_shard_size
+
+    if shard_size is not None:
+        # Fixed size shards
+        edges = list(range(0, n + 1, shard_size))
+        if edges[-1] < n:
+            edges.append(n)
+    else:
+        # Split collection into exactly `num_workers` shards
+        edges = [int(round(b)) for b in np.linspace(0, n, num_workers + 1)]
+
+    if shard_method == "slice":
+        # Slice batches
+        slices = list(zip(edges[:-1], edges[1:]))
+    else:
+        # ID batches
+        slices = [ids[i:j] for i, j in zip(edges[:-1], edges[1:])]
+
+    num_shards = len(slices)
+    batches = list(
+        zip(
+            range(num_shards),
+            itertools.repeat(num_shards),
+            slices,
+        )
+    )
+
+    num_workers = min(num_workers, num_shards)
+
+    return batches, num_workers
+
+
+def _init_worker(dataset_name, view_stages, m, s, r, p, l):
+    from tqdm.auto import tqdm
+
+    import fiftyone as fo
+    import fiftyone.core.odm.database as food
+    import fiftyone.core.view as fov
+
+    global sample_collection, map_fcn, save, return_outputs, progress
+
+    # Ensure that each process creates its own MongoDB clients
+    # https://pymongo.readthedocs.io/en/stable/faq.html#using-pymongo-with-multiprocessing
+    food._disconnect()
+
+    dataset = fo.load_dataset(dataset_name)
+    if view_stages:
+        sample_collection = fov.DatasetView._build(dataset, view_stages)
+    else:
+        sample_collection = dataset
+
+    map_fcn = m
+    save = s
+    return_outputs = r
+    progress = p
+
+    if l is not None:
+        tqdm.set_lock(l)
+
+
+def _map_batch(input):
+    i, num_batches, batch = input
+
+    if isinstance(batch, tuple):
+        # Slice batches
+        start, stop = batch
+        total = stop - start
+        batch_view = sample_collection[start:stop]
+    else:
+        # ID batches
+        sample_ids = batch
+        total = len(sample_ids)
+        batch_view = fov.make_optimized_select_view(
+            sample_collection, sample_ids
+        )
+
+    if return_outputs:
+        values = {}
+
+    if progress:
+        desc = f"Batch {i + 1:0{len(str(num_batches))}}/{num_batches}"
+        with tqdm(total=total, desc=desc, position=i) as pb:
+            for sample in batch_view.iter_samples(autosave=save):
+                result = map_fcn(sample)
+                if return_outputs and result is not None:
+                    values[sample.id] = result
+
+                pb.update()
+    else:
+        for sample in batch_view.iter_samples(autosave=save):
+            result = map_fcn(sample)
+            if return_outputs and result is not None:
+                values[sample.id] = result
+
+    if return_outputs:
+        return values
