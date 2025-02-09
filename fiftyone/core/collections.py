cb22c5e1e69c5271c22ee1f4661fb8027a9585df
brimoor <brimoor@umich.edu>
2025-02-09 00:53:40 -0500

    adding documentation

--- fiftyone/core/collections.py
+++ fiftyone/core/collections.py
@@ -3265,7 +3265,7 @@ class SampleCollection(object):
         When only a ``map_fcn`` is provided, this function effectively performs
         the following map operation with the outer loop in parallel::
 
-            for batch_view in fou.iter_batches(sample_collection, shard_size):
+            for batch_view in fou.iter_slices(sample_collection, shard_size):
                 for sample in batch_view.iter_samples(autosave=True):
                     map_fcn(sample)
 
@@ -3273,8 +3273,8 @@ class SampleCollection(object):
         the following map-reduce operation with the outer loop in parallel::
 
             values = {}
-            for batch_view in fou.iter_batches(sample_collection, shard_size):
-                for sample in batch_view.iter_samples(autosave=save):
+            for batch_view in fou.iter_slices(sample_collection, shard_size):
+                for sample in batch_view.iter_samples(autosave=True):
                     values[sample.id] = map_fcn(sample)
 
             output = reduce_fcn(sample_collection, values)
