2f12ce04ba13afb19fb2a98165d5c7f28e1275c0
Benjamin Kane <ben@voxel51.com>
2025-03-03 17:36:46 -0500

    cleanup types on looker for worker processing

--- app/packages/looker/src/worker/async-labels-rendering-manager.ts
+++ app/packages/looker/src/worker/async-labels-rendering-manager.ts
@@ -1,31 +1,32 @@
-import { Lookers } from "@fiftyone/state";
 import {
   jotaiStore,
   numConcurrentRenderingLabels,
 } from "@fiftyone/state/src/jotai";
+import { Schema } from "@fiftyone/utilities";
 import { v4 as uuid } from "uuid";
-import { ProcessSample } from ".";
-import { Coloring, Sample } from "..";
+import type { ProcessSample, ProcessSampleOptions } from ".";
+import type { Coloring, Sample } from "..";
 import { LookerUtils } from "../lookers/shared";
 import { retrieveTransferables } from "../lookers/utils";
 import { accumulateOverlays } from "../overlays";
 import { createWorker } from "../util";
 
-export type AsyncLabelsRenderingJob = {
-  sample: Sample;
+export type AsyncLabelsRenderingJob<S extends Sample = Sample> = {
   labels: string[];
-  lookerRef: Lookers;
-  resolve: (data: Omit<WorkerResponse, "uuid">) => void;
+  options: ProcessSampleOptions;
+  resolve: (data: Omit<WorkerResponse<S>, "uuid">) => void;
   reject: (error: Error) => void;
+  schema: Schema;
+  sample: S;
 };
 
-export type AsyncJobResolutionResult = {
-  sample: Sample;
+export type AsyncJobResolutionResult<S extends Sample = Sample> = {
+  sample: S;
   coloring: Coloring;
 };
 
-export type WorkerResponse = {
-  sample: Sample;
+export type WorkerResponse<S extends Sample = Sample> = {
+  sample: S;
   coloring: Coloring;
   uuid: string;
 };
@@ -35,7 +36,7 @@ const MAX_WORKERS =
 
 // global job queue and indexes
 const jobQueue: AsyncLabelsRenderingJob[] = [];
-const pendingJobs = new Map<Sample, AsyncLabelsRenderingJob>();
+const pendingJobs = new Map();
 const processingSamples = new Set<Sample>();
 
 const workerPool: Worker[] = Array.from({ length: MAX_WORKERS }, () =>
@@ -124,14 +125,14 @@ const assignJobToFreeWorker = (job: AsyncLabelsRenderingJob) => {
   // filter sample to only include keys in job.labels
   const pluckRelevant = (sample: Sample, frames = false) => {
     const filtered = { ...sample };
-    Object.keys(filtered).forEach((key) => {
+    for (const key of Object.keys(filtered)) {
       if (!job.labels.includes(frames ? `frames.${key}` : key)) {
         if (!frames && key === "frames") {
-          return;
+          continue;
         }
         delete filtered[key];
       }
-    });
+    }
 
     if (filtered.frames?.length) {
       filtered.frames = filtered.frames.map((frame) => {
@@ -147,19 +148,12 @@ const assignJobToFreeWorker = (job: AsyncLabelsRenderingJob) => {
     method: "processSample",
     sample: filteredSample as ProcessSample["sample"],
     uuid: messageUuid,
-    coloring: job.lookerRef.state.options.coloring,
-    customizeColorSetting: job.lookerRef.state.options.customizeColorSetting,
-    colorscale: job.lookerRef.state.options.colorscale,
-    labelTagColors: job.lookerRef.state.options.labelTagColors,
-    selectedLabelTags: job.lookerRef.state.options.selectedLabelTags,
-    sources: job.lookerRef.state.config.sources,
-    schema: job.lookerRef.state.config.fieldSchema,
-    activePaths: job.lookerRef.state.options.activePaths,
+    options: job.options,
   };
 
   const { overlays: filteredOverlays } = accumulateOverlays(
     filteredSample,
-    job.lookerRef.state.config.fieldSchema
+    job.schema
   );
   const transfer = retrieveTransferables(filteredOverlays);
 
@@ -168,41 +162,37 @@ const assignJobToFreeWorker = (job: AsyncLabelsRenderingJob) => {
   updateRenderingCount(1);
 };
 
-export class AsyncLabelsRenderingManager {
-  #lookerRef: Lookers;
-
-  constructor(lookerRef: Lookers) {
-    this.#lookerRef = lookerRef;
-  }
-
+export class AsyncLabelsRenderingManager<S extends Sample = Sample> {
   /**
    * Enqueue a new overlay rendering job.
    * If a pending job exists for the same sample, update it.
    */
   enqueueLabelPaintingJob(
-    item: Omit<AsyncLabelsRenderingJob, "resolve" | "reject" | "lookerRef">
-  ): Promise<AsyncJobResolutionResult> {
+    item: Omit<AsyncLabelsRenderingJob<S>, "resolve" | "reject">
+  ): Promise<AsyncJobResolutionResult<S>> {
     const { sample, labels } = item;
 
-    return new Promise<AsyncJobResolutionResult>((resolve, reject) => {
+    return new Promise<AsyncJobResolutionResult<S>>((resolve, reject) => {
       const pendingJob = pendingJobs.get(sample);
       if (pendingJob) {
         // merge / replace pending job for the same sample
         pendingJob.labels = [...new Set([...pendingJob.labels, ...labels])];
         pendingJob.resolve = resolve;
         pendingJob.reject = reject;
-      } else {
-        const job: AsyncLabelsRenderingJob = {
-          sample,
-          labels,
-          lookerRef: this.#lookerRef,
-          resolve,
-          reject,
-        };
-        pendingJobs.set(sample, job);
-        jobQueue.push(job);
-        processQueue();
+        return;
       }
+
+      const job: AsyncLabelsRenderingJob<S> = {
+        labels,
+        options,
+        resolve,
+        reject,
+        schema,
+        sample,
+      };
+      pendingJobs.set(sample, job);
+      jobQueue.push(job);
+      processQueue();
     });
   }
 
