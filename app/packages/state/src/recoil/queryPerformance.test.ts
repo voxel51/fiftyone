9cd8610d354bcbb846d22ea74ea8aad4e97b6530
Benjamin Kane <ben@voxel51.com>
2025-03-05 16:41:44 -0500

    fix query performance

--- app/packages/state/src/recoil/queryPerformance.test.ts
+++ app/packages/state/src/recoil/queryPerformance.test.ts
@@ -40,6 +40,12 @@ describe("tests query performance selectors", () => {
     setMockAtoms({
       _view__setter: [
         {
+          _cls: "fiftyone.core.stages.ExcludeFields",
+        },
+        {
+          _cls: "fiftyone.core.stages.SelectFields",
+        },
+        {
           _cls: "fiftyone.core.stages.SelectGroupSlices",
         },
       ],
