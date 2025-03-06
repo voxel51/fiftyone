4ae4aef2efd447767f6f93dbe873fd9b0c6c8774
CamronStaley <camron_staley@live.com>
2025-03-06 10:10:00 -0600

    cleanup backpressure

--- fiftyone/core/odm/database.py
+++ fiftyone/core/odm/database.py
@@ -959,8 +959,6 @@ def insert_documents(docs, coll, ordered=False, progress=None, num_docs=None):
                 batch = list(batch)
                 coll.insert_many(batch, ordered=ordered)
                 ids.extend(b["_id"] for b in batch)
-                if batcher.manual_backpressure:
-                    batcher.apply_backpressure(batch)
 
     except BulkWriteError as bwe:
         msg = bwe.details["writeErrors"][0]["errmsg"]
@@ -987,9 +985,6 @@ def bulk_write(ops, coll, ordered=False, progress=False):
             for batch in batcher:
                 batch = list(batch)
                 coll.bulk_write(batch, ordered=ordered)
-                if batcher.manual_backpressure:
-                    content_size = sum(len(str(b)) for b in batch)
-                    batcher.apply_backpressure(content_size)
 
     except BulkWriteError as bwe:
         msg = bwe.details["writeErrors"][0]["errmsg"]
