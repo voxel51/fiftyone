4c0899e656ef9437882ce9f7d8fc8ed2d3d31334
Benjamin Kane <ben@voxel51.com>
2025-03-17 22:01:10 -0400

    update classifiers

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setup(
     ),
     include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
@@ -154,6 +154,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     entry_points={"console_scripts": ["fiftyone=fiftyone.core.cli:main"]},
     python_requires=">=3.9",
