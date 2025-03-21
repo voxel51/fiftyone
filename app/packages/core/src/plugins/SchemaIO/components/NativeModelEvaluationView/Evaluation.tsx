848605ab4f7a4f0fc241213779f5a6d597b2bfbd
manivoxel51 <mani@voxel51.com>
2025-03-21 13:47:53 -0700

    Adds back the missing import

--- app/packages/core/src/plugins/SchemaIO/components/NativeModelEvaluationView/Evaluation.tsx
+++ app/packages/core/src/plugins/SchemaIO/components/NativeModelEvaluationView/Evaluation.tsx
@@ -58,6 +58,7 @@ import {
   getNumericDifference,
   useTriggerEvent,
 } from "./utils";
+import { usePanelId } from "@fiftyone/spaces";
 
 const KEY_COLOR = "#ff6d04";
 const COMPARE_KEY_COLOR = "#03a9f4";
@@ -88,23 +89,15 @@ export default function Evaluation(props: EvaluationProps) {
   const [expanded, setExpanded] = React.useState("summary");
   const [mode, setMode] = useState("chart");
   const [editNoteState, setEditNoteState] = useState({ open: false, note: "" });
-  const [
-    classPerformanceConfig,
-    setClassPerformanceConfig,
-  ] = useState<PLOT_CONFIG_TYPE>({});
+  const [classPerformanceConfig, setClassPerformanceConfig] =
+    useState<PLOT_CONFIG_TYPE>({});
   const panelId = usePanelId();
-  const [
-    classPerformanceDialogConfig,
-    setClassPerformanceDialogConfig,
-  ] = useState<PLOT_CONFIG_DIALOG_TYPE>(DEFAULT_BAR_CONFIG);
-  const [
-    confusionMatrixConfig,
-    setConfusionMatrixConfig,
-  ] = useState<PLOT_CONFIG_TYPE>({ log: true });
-  const [
-    confusionMatrixDialogConfig,
-    setConfusionMatrixDialogConfig,
-  ] = useState<PLOT_CONFIG_DIALOG_TYPE>(DEFAULT_BAR_CONFIG);
+  const [classPerformanceDialogConfig, setClassPerformanceDialogConfig] =
+    useState<PLOT_CONFIG_DIALOG_TYPE>(DEFAULT_BAR_CONFIG);
+  const [confusionMatrixConfig, setConfusionMatrixConfig] =
+    useState<PLOT_CONFIG_TYPE>({ log: true });
+  const [confusionMatrixDialogConfig, setConfusionMatrixDialogConfig] =
+    useState<PLOT_CONFIG_DIALOG_TYPE>(DEFAULT_BAR_CONFIG);
   const [metricMode, setMetricMode] = useState("chart");
   const [classMode, setClassMode] = useState("chart");
   const [performanceClass, setPerformanceClass] = useState("precision");
@@ -1805,8 +1798,9 @@ function getConfigLabel({ config, type, dashed }) {
     type === "classPerformance"
       ? CLASS_PERFORMANCE_SORT_OPTIONS
       : CONFUSION_MATRIX_SORT_OPTIONS;
-  const sortByLabel = sortByLabels.find((option) => option.value === sortBy)
-    ?.label;
+  const sortByLabel = sortByLabels.find(
+    (option) => option.value === sortBy
+  )?.label;
   return dashed ? ` - ${sortByLabel}` : sortByLabel;
 }
 
