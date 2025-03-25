ea792d444146381ced53c68468e3a143e5c9da51
Benjamin Kane <ben@voxel51.com>
2025-03-25 14:49:41 -0400

    rm unused index component

--- app/packages/core/src/components/Grid/index.tsx
+++ app/packages/core/src/components/Grid/index.tsx
@@ -1,12 +1,3 @@
 export { default } from "./Grid";
 export { gridZoom } from "./recoil";
 export { ZOOM_RANGE } from "./useZoomSetting";
-
-export default function Grid() {
-  return (
-    <Container>
-      <Grid key={"grid"} />
-      <ContainerHeader key={"header"} />
-    </Container>
-  );
-}
