bb1520f82a0e033bd7c2221c0ed69fe3aa3ec258
manivoxel51 <mani@voxel51.com>
2025-03-27 11:42:03 -0700

    Adding custom code to float type and too many categories with independent scenario_type custom code memory for better user experience

--- plugins/operators/model_evaluation/utils.py
+++ plugins/operators/model_evaluation/utils.py
@@ -10,106 +10,78 @@ ALLOWED_BY_TYPES = (
     fof.StringField,  # TODO: needs a dropdown - call distinct on the field - if too many, show code view
     fof.BooleanField,  # TODO: needs two checkboxes
     fof.IntField,
-    fof.FloatField,  # TODO: always defaults to code view
+    fof.FloatField,
 )
 
 SCENARIO_BUILDING_CHOICES = [
     {
         "type": "sample_field",
         "label": "Select sample fields",
-        "icon": "add_card_outlined",
+        "icon": "edit_attributes_outlined",
     },
     {
         "type": "label_attribute",
         "label": "Select label attributes",
-        "icon": "add_card_outlined",
+        "icon": "edit_attributes_outlined",
     },
     {
         "type": "view",
         "label": "Select saved views",
-        "icon": "create_new_folder_outlined",
+        "icon": "folder_special_rounded",
     },
     {
         "type": "custom_code",
         "label": "Custom code",
-        "icon": "code_outlined",
+        "icon": "code_rounded",
     },
 ]
 
 
-def get_scenario_example(scenario_name="brightness"):
+def get_scenario_example(reason="float"):
     examples = {
-        "brightness": dedent(
+        "NORMAL": dedent(
+            """
+            from fiftyone import ViewField as F
+
+            subsets = {
+                "Sunny unique objects": [
+                    dict(type="field", field="tags", value="test"),
+                    dict(type="field", expr=F("brightness") > 0.75),
+                ],
+                "Rainy common objects": [
+                    dict(type="field", field="tags", value="test"),
+                    dict(type="field", expr=F("brightness") < 0.25),
+                ]
+            }
+        """
+        ).strip(),
+        "FLOAT_TYPE": dedent(
             """
             from fiftyone import ViewField as F
             subsets = {
                 "Bright objects": [
-                    dict(type="field", field="tags", value="test"),
                     dict(type="field", expr=F("brightness") > 0.5),
                 ],
                 "Dark objects": [
-                    dict(type="field", field="tags", value="test"),
                     dict(type="field", expr=F("brightness") < 0.5),
                 ]
             }
-        """
+            """
         ).strip(),
-        "fashion_10000": dedent(
+        "TOO_MANY_CATEGORIES": dedent(
             """
             from fiftyone import ViewField as F
-
-            subsets = {
-                "throne": dict(type="field", field="predictions.label", value="throne"),
-                "chain": dict(type="field", field="predictions.label", value="chain"),
-            }
-        """
-        ).strip(),
-        "scenario_1": dedent(
-            """
-            from fiftyone import ViewField as F
-
-            subsets = {
-                "sunny": dict(type="field", field="tags", value="sunny"),
-                "cloudy": dict(type="field", field="tags", value="cloudy"),
-                "rainy": dict(type="field", field="tags", value="rainy"),
-                "Sunny unique objects": [
-                    dict(type="field", field="tags", value="sunny"),
-                    dict(type="field", expr=F("uniqueness") > 0.75),
-                ],
-                "Rainy common objects": [
-                    dict(type="field", field="tags", value="rainy"),
-                    dict(type="field", expr=F("uniqueness") < 0.25),
-                ]
-            }
-        """
-        ).strip(),
-        "scenario_2": dedent(
-            """
-            from fiftyone import ViewField as F
-
-            subsets = {
-                "Low": {"field": F($FIELD) < 0.25},
-                "middle": {"field": F($FIELD) >= 0.25 & F($FIELD) < 0.75},
-                "high": {"field": F($FIELD) > 0.75},
-            }
-        """
-        ).strip(),
-        "scenario_3": dedent(
-            """
-            from fiftyone import ViewField as F
-
             bbox_area = F("bounding_box")[2] * F("bounding_box")[3]
             subsets = {
                 "Small objects": dict(type="attribute", expr=bbox_area <= 0.05),
                 "Medium objects": dict(type="attribute", expr=(0.05 <= bbox_area) & (bbox_area <= 0.5)),
                 "Large objects": dict(type="attribute", expr=bbox_area > 0.5),
             }
-        """
+            """
         ).strip(),
-        "scenario_4": dedent(
+        "OTHER": dedent(
             """
             from fiftyone import ViewField as F
-
             subsets = {
                 "Sunny unique objects": [
                     dict(type="field", field="tags", value="sunny"),
@@ -123,4 +95,4 @@ def get_scenario_example(scenario_name="brightness"):
         """
         ).strip(),
     }
-    return examples.get(scenario_name, "")
+    return examples.get(reason, "")
