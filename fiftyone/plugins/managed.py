6294b3e3a7c95b0e9285b97c62683be6436792db
Ritchie Martori <ritchie@voxel51.com>
2025-04-07 10:01:56 -0700

    managed plugins tests

--- fiftyone/plugins/managed.py
+++ fiftyone/plugins/managed.py
@@ -8,7 +8,7 @@ class ManagedPluginContextCache:
 
     def clear(self):
         self.fingerprint = None
-        self.contexts = {}
+        self.contexts = None
 
 
 cache = ManagedPluginContextCache()
@@ -18,7 +18,11 @@ def build_managed_plugin_contexts(
     enabled, managed_plugins: ManagedPlugins
 ) -> PluginContext:
     fingerprint = managed_plugins.fingerprint()
-    if fingerprint != cache.fingerprint or cache.contexts is None:
+    if fingerprint != cache.fingerprint:
+        # TODO: pass managed_plugins to build_plugin_contexts
+        # and use it to avoid searching for plugins
+        # this will require a change to the plugins API to return
+        # plugin.filepath
         plugin_contexts = build_plugin_contexts(enabled=enabled)
         cache.contexts = plugin_contexts
         cache.fingerprint = fingerprint
