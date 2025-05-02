d807728d8d5c06ed278c5905d27083b3013bceb7
voxelbot <voxelbot@voxel51.com>
2025-05-02 11:29:31 +0000

    [skip ci] automated bump `fiftyone` to v2.9.0.dev7, `fiftyone-app` to v2.9.0.dev7, and `teams-app` to v2.9.0-dev.7

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fiftyone",
-    version="2.9.0.dev6",
+    version="2.9.0.dev7",
     description=(
         "FiftyOne Teams: the tool for teams building high-quality datasets "
         "and computer vision models"
