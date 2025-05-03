6b253e711f0ef5c6bc965a28fdbd5bbd7bf7ba0b
brimoor <brimoor@umich.edu>
2025-05-03 12:48:28 -0400

    bumping brain version

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ INSTALL_REQUIRES = [
     "universal-analytics-python3>=1.0.1,<2",
     "pydash",
     # internal packages
-    "fiftyone-brain>=0.21.1,<0.22",
+    "fiftyone-brain>=0.21.2,<0.22",
     "fiftyone-db>=0.4,<2.0",
     "voxel51-eta>=0.14.0,<0.15",
 ]
