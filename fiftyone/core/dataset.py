0bdf1cb5e8794ec9b775456b95ca4f0d2920f8cb
kacey <kacey@voxel51.com>
2025-05-05 15:35:58 -0700

    use strict to ignore fields outside schema

--- fiftyone/core/dataset.py
+++ fiftyone/core/dataset.py
@@ -8533,6 +8533,7 @@ def _create_sample_document_cls(
 ):
     cls = type(sample_collection_name, (foo.DatasetSampleDocument,), {})
     cls._dataset = dataset
+    cls.STRICT = True
 
     _declare_fields(dataset, cls, field_docs=field_docs)
     return cls
