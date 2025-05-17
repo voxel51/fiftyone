a96fc4a0dc6a5193dbea49f0056a49f6c30cdda1
kacey <kacey@voxel51.com>
2025-05-16 17:48:34 -0700

    rm extra line

--- fiftyone/core/map/mapper.py
+++ fiftyone/core/map/mapper.py
@@ -25,7 +25,6 @@ import bson
 
 import fiftyone.core.config as focc
 import fiftyone.core.map.batcher as fomb
-
 import fiftyone.core.utils as fou
 from fiftyone.core.map.typing import SampleCollection
 
