c329ea855eaecb718bbdd0a1a5edcb62e2a9c66e
Sashank Aryal <sashank2012@gmail.com>
2025-06-25 18:32:45 -0500

    add React integration with hooks-based API

---
+++ app/packages/lighter/src/react/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2017-2025, Voxel51, Inc.
+ */
+
+// React components
+export { HookBasedViewer } from "./HookBasedViewer";
+export type { HookBasedViewerProps } from "./HookBasedViewer";
+
+// React hooks
+export { useLighter } from "./useLighter";
