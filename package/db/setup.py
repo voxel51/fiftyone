3b43c3d1b193fd3722628cefb414a28a24744b14
Alexander Foley <alex@voxel51.com>
2025-07-10 12:07:44 -0400

    chore(db): Bump fiftoyne-db

--- package/db/setup.py
+++ package/db/setup.py
@@ -46,7 +46,7 @@ MONGODB_DOWNLOAD_URLS = {
 MONGODB_BINARIES = ["mongod"]
 LINUX_DISTRO = os.environ.get("FIFTYONE_DB_BUILD_LINUX_DISTRO")
 
-VERSION = "0.4.0"
+VERSION = "0.4.4"
 
 
 def get_version():
