66571ff9f763f9bc0a1f3467cc92f3103d00e956
Sashank Aryal <sashank2012@gmail.com>
2025-07-30 10:59:17 -0500

    fix ply points rendering bug

--- app/packages/looker-3d/src/fo3d/mesh/Ply.tsx
+++ app/packages/looker-3d/src/fo3d/mesh/Ply.tsx
@@ -1,12 +1,13 @@
 import { getSampleSrc } from "@fiftyone/state";
-import { useLoader } from "@react-three/fiber";
+import { ThreeEvent, useFrame } from "@react-three/fiber";
 import { useEffect, useMemo, useRef, useState } from "react";
+import { useRecoilState } from "recoil";
 import {
   type BufferGeometry,
   Mesh,
   Points,
   type Quaternion,
-  type Vector3,
+  Vector3,
 } from "three";
 import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 import type {
@@ -15,11 +16,12 @@ import type {
   FoPointcloudMaterialProps,
   PlyAsset,
 } from "../../hooks";
+import { useFoLoader } from "../../hooks/use-fo-loaders";
 import { useMeshMaterialControls } from "../../hooks/use-mesh-material-controls";
+import { currentHoveredPointAtom } from "../../state";
 import { useFo3dContext } from "../context";
 import { usePcdMaterial } from "../point-cloud/use-pcd-material";
 import { getBasePathForTextures, getResolvedUrlForFo3dAsset } from "../utils";
-import { useFoLoader } from "../../hooks/use-fo-loaders";
 
 interface PlyProps {
   name: string;
@@ -39,6 +41,11 @@ const PlyWithPointsMaterial = ({
   geometry: BufferGeometry;
   defaultMaterial: FoMeshMaterial;
 }) => {
+  const { pointCloudSettings, setHoverMetadata } = useFo3dContext();
+  const [currentHoveredPoint, setCurrentHoveredPoint] = useRecoilState(
+    currentHoveredPointAtom
+  );
+
   const overrideMaterial = {
     shadingMode: "height",
     customColor: defaultMaterial["color"] ?? "#ffffff",
@@ -49,7 +56,7 @@ const PlyWithPointsMaterial = ({
 
   const pointsContainerRef = useRef();
 
-  const pointsMaterialElement = usePcdMaterial(
+  const { pointsMaterial, shadingMode } = usePcdMaterial(
     name,
     geometry,
     overrideMaterial,
@@ -58,14 +65,115 @@ const PlyWithPointsMaterial = ({
 
   const mesh = useMemo(() => new Points(geometry), [geometry]);
 
-  if (!geometry || !pointsMaterialElement) {
+  const pointerMoveHandler = useMemo(
+    () => (e: ThreeEvent<MouseEvent>) => {
+      const idx = e.index;
+      if (idx === undefined) return;
+
+      const md: Record<string, any> = { index: idx };
+
+      if (geometry.hasAttribute("rgb")) {
+        const colorAttr = geometry.getAttribute("rgb");
+
+        md.rgb = [
+          colorAttr.getX(idx),
+          colorAttr.getY(idx),
+          colorAttr.getZ(idx),
+        ];
+      }
+
+      if (geometry.hasAttribute("position")) {
+        const posAttr = geometry.getAttribute("position");
+        md.coord = [posAttr.getX(idx), posAttr.getY(idx), posAttr.getZ(idx)];
+        setCurrentHoveredPoint(
+          new Vector3(posAttr.getX(idx), posAttr.getY(idx), posAttr.getZ(idx))
+        );
+      }
+
+      // dynamically handle all other attributes
+      Object.keys(geometry.attributes).forEach((attr) => {
+        if (attr === "rgb" || attr === "position") return;
+        md[attr] = geometry.attributes[attr].getX(idx);
+      });
+
+      setHoverMetadata({
+        assetName: name,
+        renderModeDescriptor: shadingMode,
+        attributes: md,
+      });
+    },
+    [geometry, setHoverMetadata, shadingMode, name]
+  );
+
+  const hoverProps = useMemo(() => {
+    if (!pointCloudSettings.enableTooltip) return {};
+
+    return {
+      // fires on *every* intersected point
+      onPointerMove: pointerMoveHandler,
+      onPointerOut: () => {
+        setCurrentHoveredPoint(null);
+      },
+    };
+  }, [pointCloudSettings.enableTooltip, pointerMoveHandler]);
+
+  useEffect(() => {
+    return () => {
+      setCurrentHoveredPoint(null);
+    };
+  }, [pointerMoveHandler]);
+
+  useEffect(() => {
+    setHoverMetadata((prev) => ({
+      ...prev,
+      renderModeDescriptor: shadingMode,
+    }));
+  }, [shadingMode, setHoverMetadata]);
+
+  const HoveredPointMarker = ({ position }: { position: Vector3 }) => {
+    const meshRef = useRef<any>(null);
+
+    // apply pulsating effect for scaling (based on distance from camera) and color
+    // so that the marker is visible from far away
+    useFrame(({ clock, camera }) => {
+      const t = clock.getElapsedTime();
+      const distance = camera.position.distanceTo(position);
+      const pulse = 0.5 + 0.3 * Math.sin(t * 4);
+      const scale = pulse * (distance * 0.1);
+
+      if (meshRef.current) {
+        meshRef.current.scale.set(scale, scale, scale);
+        const colorPhase = (Math.sin(t * 2) + 1) / 2;
+        meshRef.current.material.color.setRGB(1, colorPhase, 0);
+        meshRef.current.material.emissive.setRGB(1, colorPhase, 0);
+        meshRef.current.material.emissiveIntensity = 0.7 + 0.3 * colorPhase;
+      }
+    });
+    return (
+      <mesh ref={meshRef} position={position}>
+        <sphereGeometry args={[0.07, 32, 32]} />
+        <meshStandardMaterial
+          color="red"
+          emissive="orange"
+          emissiveIntensity={1}
+        />
+      </mesh>
+    );
+  };
+
+  if (!geometry || !pointsMaterial) {
     return null;
   }
 
   return (
-    <primitive ref={pointsContainerRef} object={mesh}>
-      {pointsMaterialElement}
-    </primitive>
+    <>
+      {currentHoveredPoint && (
+        <HoveredPointMarker position={currentHoveredPoint} />
+      )}
+      <primitive ref={pointsContainerRef} object={mesh} {...hoverProps}>
+        {pointsMaterial}
+      </primitive>
+    </>
   );
 };
 
