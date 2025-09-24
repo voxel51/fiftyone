54a2f764cce97dc4325682f9151e0190fbddde71
kacey <kacey@voxel51.com>
2025-09-24 09:17:50 -0700

    test query functionality

--- tests/no_wrapper/db_conn_tests.py
+++ tests/no_wrapper/db_conn_tests.py
@@ -6,22 +6,38 @@ Multiprocess tests.
 |
 """
 import unittest
+from pymongo.errors import InvalidOperation
+
 import fiftyone.core.odm as foo
+
+# from fiftyone.factory.repos.delegated_operation import DelegatedOperationRepo
 
 
 class GetDbConnTests(unittest.TestCase):
     def test_get_db_conn(self):
         # initial connection
         conn = foo.get_db_conn()
-        self.assertIsNotNone(conn)
+        # do_svc = DelegatedOperationRepo()
+        doc = conn.datasets.find_one({}, {"_id": 1})
+        self.assertIsNotNone(doc)
+        disconnected = False
 
         # close connection
         conn.client.close()
         self.assertTrue(conn.client._closed)
+        try:
+            conn.datasets.find_one({}, {"_id": 1})
+        except InvalidOperation:
+            disconnected = True
+            doc = None
+        self.assertTrue(disconnected)
+        self.assertIsNone(doc)
 
         # should reconnect
         conn = foo.get_db_conn()
+        doc = conn.datasets.find_one({}, {"_id": 1})
         self.assertFalse(conn.client._closed)
+        self.assertIsNotNone(doc)
 
 
 class GetAsyncDbConnTests(unittest.TestCase):
