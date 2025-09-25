b32e45600d49056572a15080bd217276af938afd
CamronStaley <camron_staley@live.com>
2025-09-25 18:51:24 -0500

    update log to debug to declutter logs

--- fiftyone/operators/delegated.py
+++ fiftyone/operators/delegated.py
@@ -734,7 +734,7 @@ class DelegatedOperationService(object):
                     )
                     return ExecutionResult(error=reason)
                 else:
-                    logger.info("Pinging operation %s", operation_id)
+                    logger.debug("Pinging operation %s", operation_id)
                     self._repo.ping(operation_id)
             except Exception as e:
                 reason = f"Error in monitoring loop: {e}"
