6291177e2947b67576cd058174343ddeef85055a
Tom Schmidt <tom.schmidt@voxel51.com>
2025-10-02 09:52:13 -0700

    update behavior to match server changes

--- app/packages/core/src/client/annotationClient.ts
+++ app/packages/core/src/client/annotationClient.ts
@@ -1,4 +1,10 @@
 import { encodeURIPath } from "./util";
+import { Sample } from "@fiftyone/looker";
+import {
+  MalformedRequestError,
+  NetworkError,
+  NotFoundError,
+} from "@fiftyone/utilities";
 
 /**
  * Label types which currently support mutation via {@link patchSample}.
@@ -12,25 +18,71 @@ export type MutableLabelTypes =
   | "Polylines";
 
 /**
- * A field specification can either be
- *  - `null` indicating the field should be deleted, OR
- *  - an object containing at least the `_cls` field to specify the label type
+ * Specification for a field which should be removed.
  */
-export type FieldSpecification = null | {
+export type NullField = null;
+
+/**
+ * Specification for a label field.
+ */
+export type LabelField = {
   _cls: MutableLabelTypes;
   [key: string]: unknown;
 };
 
+/**
+ * Specification for a top-level attribute field.
+ */
+export type AttributeField = Record<string, unknown>;
+
+/**
+ * A field specification can either be
+ *  - `null` indicating the field should be deleted, OR
+ *  - an object containing at least the `_cls` field to specify the label type, OR
+ *  - an arbitrary mapping to primitives or objects
+ */
+export type FieldSpecification = NullField | LabelField | AttributeField;
+
 export type PatchSampleRequest = {
   datasetId: string;
   sampleId: string;
-  deltas: Record<string, FieldSpecification>[];
+  delta: Record<string, FieldSpecification>;
 };
 
-export type PatchSampleResponse = {
-  status: string;
-  patched_sample_id: string;
+export type ErrorResponse = {
   errors: string[];
+};
+
+export type PatchSampleResponse = ErrorResponse | Sample;
+
+/**
+ * Error resulting from a failed update operation.
+ */
+export class PatchApplicationError extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = "Patch Application Error";
+  }
+}
+
+const handleErrorResponse = async (response: Response) => {
+  if (response.status === 400) {
+    let body: ErrorResponse;
+    try {
+      body = await response.json();
+    } catch (err) {
+      body = {} as ErrorResponse;
+    }
+    if (body.errors) {
+      throw new PatchApplicationError(body.errors.join(", "));
+    }
+
+    throw new MalformedRequestError();
+  } else if (response.status === 404) {
+    throw new NotFoundError({ path: "sample" });
+  }
+
+  throw new Error(`http error: ${response.status} ${response.statusText}`);
 };
 
 /**
@@ -45,7 +97,7 @@ export const patchSample = async (
     encodeURIPath(["dataset", request.datasetId, "sample", request.sampleId]),
     {
       method: "PATCH",
-      body: JSON.stringify(request.deltas),
+      body: JSON.stringify(request.delta),
       headers: {
         "content-type": "application/json",
       },
@@ -55,6 +107,6 @@ export const patchSample = async (
   if (httpResponse.ok) {
     return httpResponse.json();
   } else {
-    throw new Error(`http error: ${httpResponse.status}`);
+    await handleErrorResponse(httpResponse);
   }
 };
