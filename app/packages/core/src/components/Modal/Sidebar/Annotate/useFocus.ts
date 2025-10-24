01a7765710953ceea1b8e7046389c3b8339cca5c
Benjamin Kane <ben@voxel51.com>
2025-10-24 17:09:01 -0400

    linting

--- app/packages/core/src/components/Modal/Sidebar/Annotate/useFocus.ts
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/useFocus.ts
@@ -1,6 +1,6 @@
 import { LIGHTER_EVENTS, useLighter } from "@fiftyone/lighter";
 import { getDefaultStore } from "jotai";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import useConfirmExit from "./Confirmation/useConfirmExit";
 import { editing } from "./Edit";
 import { currentOverlay, hasChanges, savedLabel } from "./Edit/state";
@@ -8,73 +8,80 @@ import useExit from "./Edit/useExit";
 import useSave from "./Edit/useSave";
 import { labelMap } from "./useLabels";
 
+const STORE = getDefaultStore();
+
 export default function useFocus() {
   const { scene } = useLighter();
   const { confirmExit } = useConfirmExit(useExit, useSave);
   const selectId = useRef<string | null>(null);
   const onExit = useExit(false);
 
-  useEffect(() => {
-    const handler = (event) => {
-      if (event.detail.isBridgeLogicHandled) {
-        return;
-      }
-      const store = getDefaultStore();
-      if (!store.get(editing)) {
-        const label = store.get(labelMap)[event.detail.id];
+  const select = useCallback(() => {
+    const id = selectId.current;
+    if (!id) {
+      return;
+    }
 
-        if (label) {
-          store.set(savedLabel, store.get(label)?.data);
-          store.set(editing, label);
-        }
-      } else {
-        selectId.current = event.detail.id;
-
-        scene?.deselectOverlay(event.detail.id, { isBridgeLogicHandled: true });
-      }
-    };
-    scene?.on(LIGHTER_EVENTS.OVERLAY_SELECT, handler);
-
-    return () => {
-      scene?.off(LIGHTER_EVENTS.OVERLAY_SELECT, handler);
-    };
+    const label = STORE.get(labelMap)[id];
+    if (id && label) {
+      STORE.set(savedLabel, STORE.get(label)?.data);
+      STORE.set(editing, label);
+      scene?.selectOverlay(id, { isBridgeLogicHandled: true });
+    }
+    selectId.current = null;
   }, [scene]);
 
   useEffect(() => {
-    const store = getDefaultStore();
-    const select = (id: string) => {
-      const label = store.get(labelMap)[id];
-      if (label) {
-        store.set(savedLabel, store.get(label)?.data);
-        store.set(editing, label);
-        scene?.selectOverlay(id, { isBridgeLogicHandled: true });
-      }
-    };
-
     const handler = (event) => {
       if (event.detail.isBridgeLogicHandled) {
         return;
       }
-      selectId.current = event.detail.id;
-      if (store.get(hasChanges)) {
-        const current = store.get(currentOverlay).id;
-        scene?.selectOverlay(event.detail.id, { isBridgeLogicHandled: true });
-        confirmExit(() => {
-          scene?.deselectOverlay(current, {
-            isBridgeLogicHandled: true,
-          });
 
-          select(selectId.current);
-        });
+      selectId.current = event.detail.id;
+
+      const current = STORE.get(currentOverlay)?.id;
+
+      if (!current || !STORE.get(hasChanges)) {
+        // no unsaved changes, allow the exit
+        onExit();
         return;
       }
 
-      onExit();
+      // there are unsaved changes, ask for confirmation
+      scene?.selectOverlay(event.detail.id, { isBridgeLogicHandled: true });
+      confirmExit(() => {
+        scene?.deselectOverlay(current, {
+          isBridgeLogicHandled: true,
+        });
+        select();
+      });
     };
     scene?.on(LIGHTER_EVENTS.OVERLAY_DESELECT, handler);
 
     return () => {
       scene?.off(LIGHTER_EVENTS.OVERLAY_DESELECT, handler);
     };
-  }, [confirmExit, scene, onExit, selectId]);
+  }, [confirmExit, scene, onExit, select, selectId]);
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (event.detail.isBridgeLogicHandled) {
+        return;
+      }
+      selectId.current = event.detail.id;
+
+      if (STORE.get(editing)) {
+        // a label is already being edited, let the DESELECT event handle it
+        scene?.deselectOverlay(event.detail.id, { isBridgeLogicHandled: true });
+        return;
+      }
+
+      select();
+    };
+    scene?.on(LIGHTER_EVENTS.OVERLAY_SELECT, handler);
+
+    return () => {
+      scene?.off(LIGHTER_EVENTS.OVERLAY_SELECT, handler);
+    };
+  }, [scene, select]);
 }
