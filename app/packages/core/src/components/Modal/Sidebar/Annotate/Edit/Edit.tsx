4abe383680bdca99d72ea54df1220cf3d67d63e6
Benjamin Kane <ben@voxel51.com>
2025-10-27 11:59:05 -0400

    lint

--- app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/Edit.tsx
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/Edit.tsx
@@ -2,7 +2,7 @@ import { LIGHTER_EVENTS, useLighter } from "@fiftyone/lighter";
 import { useClearModal } from "@fiftyone/state";
 import { DETECTION } from "@fiftyone/utilities";
 import { getDefaultStore, useAtomValue } from "jotai";
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import styled from "styled-components";
 import Confirmation, { ConfirmationContext } from "../Confirmation";
 import useConfirmExit from "../Confirmation/useConfirmExit";
