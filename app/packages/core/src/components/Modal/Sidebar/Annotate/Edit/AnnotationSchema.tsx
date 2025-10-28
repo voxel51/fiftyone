0e49ea370761a802a6a1eaef767fe2a0ec6a4649
Sashank Aryal <sashank2012@gmail.com>
2025-10-28 13:27:03 -0500

    fix typo

--- app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/AnnotationSchema.tsx
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/AnnotationSchema.tsx
@@ -173,7 +173,7 @@ const AnnotationSchema = () => {
 
   useEffect(() => {
     const handler = (event) => {
-      // Here, this would be trye for `undo` or `redo`
+      // Here, this would be true for `undo` or `redo`
       if (!(event.detail?.command instanceof UpdateLabelCommand)) {
         const label = overlay?.label;
 
