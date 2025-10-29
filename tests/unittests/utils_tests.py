fa3c18206c47e3a26f5a8f281fbfae468c0f45d3
CamronStaley <camron_staley@live.com>
2025-10-29 11:24:58 -0500

    add tests and refactor sentinel

--- tests/unittests/utils_tests.py
+++ tests/unittests/utils_tests.py
@@ -353,6 +353,18 @@ class BatcherTests(unittest.TestCase):
             for batch in batcher:
                 self.assertEqual(len(batch), n // 2)
 
+        samples_with_none = [None] + samples
+        count = 0
+        batcher = fou.ContentSizeBatcher(
+            iter(samples_with_none), target_size=1
+        )
+        with batcher:
+            for batch in batcher:
+                if count == 0:
+                    self.assertIsNone(batch[0])
+                count += len(batch)
+        self.assertEqual(count, len(samples_with_none))
+
     def test_static_batcher_perfect_boundary(self):
         iterable = list(range(200))
         batcher = fou.StaticBatcher(iterable, batch_size=100, progress=False)
