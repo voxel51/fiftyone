66b26bb166f57c510ea68d37366286464c7386d3
Tom Schmidt <tom.schmidt@voxel51.com>
2025-11-04 16:34:13 -0800

    add tracking configuration to FeatureFlagged component

--- app/packages/feature-flags/src/components/FeatureFlagged.tsx
+++ app/packages/feature-flags/src/components/FeatureFlagged.tsx
@@ -9,19 +9,22 @@ import { FeatureFlag } from "../client";
  * @param children Component to render if the feature is enabled
  * @param fallback Optional fallback component to render if the feature is disabled
  * @param resolving Optional component to render while the check is resolving
+ * @param enableTracking If enabled, will emit tracking events
  */
 export const FeatureFlagged = ({
   feature,
   children,
   fallback,
   resolving,
+  enableTracking,
 }: {
   feature: FeatureFlag;
   children: React.ReactNode;
   fallback?: React.ReactNode;
   resolving?: React.ReactNode;
+  enableTracking?: boolean;
 }) => {
-  const { isEnabled, isResolved } = useFeature({ feature });
+  const { isEnabled, isResolved } = useFeature({ feature, enableTracking });
 
   if (!isResolved) {
     return resolving ?? <Fragment />;
