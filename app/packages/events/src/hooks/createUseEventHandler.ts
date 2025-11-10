dcb0ef2961cf6ccdad4280e61427ee7cdd1a6cb5
Sashank Aryal <sashank2012@gmail.com>
2025-11-10 13:30:52 -0600

    fix hook rule violation

--- app/packages/events/src/hooks/createUseEventHandler.ts
+++ app/packages/events/src/hooks/createUseEventHandler.ts
@@ -28,12 +28,12 @@ import { useEventBus } from "./useEventBus";
 export function createUseEventHandler<T extends EventGroup>(
   channelId = "default"
 ) {
-  const bus = useEventBus<T>({ channelId });
-
   return function useEventHandler<K extends keyof T>(
     event: K,
     handler: EventHandler<T[K]>
   ) {
+    const bus = useEventBus<T>({ channelId });
+
     useEffect(() => {
       bus.on(event, handler);
       return () => bus.off(event, handler);
