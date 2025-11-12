d7872e2b4f14f1ed05c93064ee087d28a696c330
Stuart Wheaton <stuart@voxel51.com>
2025-11-12 16:29:10 -0500

    code rabbit2

--- fiftyone/operators/executor.py
+++ fiftyone/operators/executor.py
@@ -621,7 +621,7 @@ class ExecutionContext(contextlib.AbstractContextManager):
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
     @property
