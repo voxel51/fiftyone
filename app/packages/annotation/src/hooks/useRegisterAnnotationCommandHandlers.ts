093744f686e25f07f0b232f286fef2a4be962182
Sashank Aryal <sashank2012@gmail.com>
2025-11-19 14:01:32 -0600

    fix circular dep

--- app/packages/annotation/src/hooks/useRegisterAnnotationCommandHandlers.ts
+++ app/packages/annotation/src/hooks/useRegisterAnnotationCommandHandlers.ts
@@ -16,8 +16,8 @@ import {
   useRefreshSample,
 } from "@fiftyone/state";
 import { Field } from "@fiftyone/utilities";
-import { useCallback, useMemo } from "react";
-import { useRecoilValue } from "recoil";
+import { useCallback } from "react";
+import { useRecoilCallback, useRecoilValue } from "recoil";
 import { DeleteAnnotationCommand, UpsertAnnotationCommand } from "../commands";
 import { OpType, buildJsonPath, buildLabelDeltas } from "../deltas";
 
@@ -27,7 +27,6 @@ import { OpType, buildJsonPath, buildLabelDeltas } from "../deltas";
  */
 export const useRegisterAnnotationCommandHandlers = () => {
   const datasetId = useRecoilValue(fosDatasetId);
-  const currentSample = useRecoilValue(modalSample)?.sample;
   const refreshSample = useRefreshSample();
   const eventBus = useAnnotationEventBus();
 
@@ -36,93 +35,104 @@ export const useRegisterAnnotationCommandHandlers = () => {
   // Updated version tokens are returned in the response body,
   // but the server also allows the current sample timestamp to be used as
   // a version token.
-  const versionToken = useMemo(() => {
-    const isoTimestamp = parseTimestamp(
-      currentSample?.last_modified_at as unknown as string
-    )?.toISOString();
+  const getVersionToken = useRecoilCallback(
+    ({ snapshot }) =>
+      async (): Promise<string | undefined> => {
+        const currentSample = (await snapshot.getPromise(modalSample))?.sample;
+        if (!currentSample?.last_modified_at) {
+          return undefined;
+        }
 
-    // server doesn't like the iso timestamp ending in 'Z'
-    if (isoTimestamp?.endsWith("Z")) {
-      return isoTimestamp.substring(0, isoTimestamp.length - 1);
-    } else {
-      return isoTimestamp;
-    }
-  }, [currentSample?.last_modified_at]);
+        const isoTimestamp = parseTimestamp(
+          currentSample.last_modified_at as unknown as string
+        )?.toISOString();
 
-  const handlePatchSample = useCallback(
-    /**
-     * Returns true if a patch was applied successfully
-     *
-     * @param sampleDeltas Deltas to apply
-     */
-    async (sampleDeltas: JSONDeltas): Promise<boolean> => {
-      if (!datasetId || !currentSample?._id || !versionToken) {
-        return false;
-      }
+        // server doesn't like the iso timestamp ending in 'Z'
+        if (isoTimestamp?.endsWith("Z")) {
+          return isoTimestamp.substring(0, isoTimestamp.length - 1);
+        } else {
+          return isoTimestamp;
+        }
+      },
+    []
+  );
 
-      if (sampleDeltas.length > 0) {
-        try {
-          const response = await patchSample({
-            datasetId,
-            sampleId: currentSample._id,
-            deltas: sampleDeltas,
-            versionToken,
-          });
+  const handlePatchSample = useRecoilCallback(
+    ({ snapshot }) =>
+      async (sampleDeltas: JSONDeltas): Promise<boolean> => {
+        const currentSample = (await snapshot.getPromise(modalSample))?.sample;
+        const versionToken = await getVersionToken();
 
-          // transform response data to match the graphql sample format
-          const cleanedSample = transformSampleData(response.sample);
-          if (isSampleIsh(cleanedSample)) {
-            refreshSample(cleanedSample as Sample);
-          } else {
-            console.error(
-              "response data does not adhere to sample format",
-              cleanedSample
-            );
-          }
-        } catch (error) {
-          console.error("error patching sample", error);
-
+        if (!datasetId || !currentSample?._id || !versionToken) {
           return false;
         }
-      }
 
-      return true;
-    },
-    [currentSample, datasetId, refreshSample, versionToken]
+        if (sampleDeltas.length > 0) {
+          try {
+            const response = await patchSample({
+              datasetId,
+              sampleId: currentSample._id,
+              deltas: sampleDeltas,
+              versionToken,
+            });
+
+            // transform response data to match the graphql sample format
+            const cleanedSample = transformSampleData(response.sample);
+            if (isSampleIsh(cleanedSample)) {
+              refreshSample(cleanedSample as Sample);
+            } else {
+              console.error(
+                "response data does not adhere to sample format",
+                cleanedSample
+              );
+            }
+          } catch (error) {
+            console.error("error patching sample", error);
+
+            return false;
+          }
+        }
+
+        return true;
+      },
+    [datasetId, refreshSample, getVersionToken]
   );
 
   // callback which handles both mutation (upsert) and deletion
-  const handlePersistence = useCallback(
-    async (
-      annotationLabel: AnnotationLabel,
-      schema: Field,
-      opType: OpType
-    ): Promise<boolean> => {
-      if (!currentSample) {
-        console.error("missing sample data!");
-        return false;
-      }
+  const handlePersistence = useRecoilCallback(
+    ({ snapshot }) =>
+      async (
+        annotationLabel: AnnotationLabel,
+        schema: Field,
+        opType: OpType
+      ): Promise<boolean> => {
+        const currentSample = (await snapshot.getPromise(modalSample))?.sample;
 
-      if (!annotationLabel) {
-        console.error("missing annotation label!");
-        return false;
-      }
+        if (!currentSample) {
+          console.error("missing sample data!");
+          return false;
+        }
 
-      // calculate label deltas between current sample data and new label data
-      const sampleDeltas = buildLabelDeltas(
-        currentSample,
-        annotationLabel,
-        schema,
-        opType
-      ).map((delta) => ({
-        ...delta,
-        // convert label delta to sample delta
-        path: buildJsonPath(annotationLabel.path, delta.path),
-      }));
+        if (!annotationLabel) {
+          console.error("missing annotation label!");
+          return false;
+        }
 
-      return await handlePatchSample(sampleDeltas);
-    },
-    [currentSample, handlePatchSample]
+        // calculate label deltas between current sample data and new label data
+        const sampleDeltas = buildLabelDeltas(
+          currentSample,
+          annotationLabel,
+          schema,
+          opType
+        ).map((delta) => ({
+          ...delta,
+          // convert label delta to sample delta
+          path: buildJsonPath(annotationLabel.path, delta.path),
+        }));
+
+        return await handlePatchSample(sampleDeltas);
+      },
+    [handlePatchSample]
   );
 
   useRegisterCommandHandler(
