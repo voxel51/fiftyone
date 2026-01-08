8c3ca11753600dbfc714d4d68b5c33befd5467b1
Sashank Aryal <sashank2012@gmail.com>
2026-01-07 18:34:47 -0600

    modify title at module level

--- app/packages/app/src/index.tsx
+++ app/packages/app/src/index.tsx
@@ -1,22 +1,20 @@
+/// <reference types="./env.d.ts" />
 import { ErrorBoundary, ThemeProvider } from "@fiftyone/components";
 import { BeforeScreenshotContext, screenshotCallbacks } from "@fiftyone/state";
 import { SnackbarProvider } from "notistack";
 import type React from "react";
-import { useLayoutEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import Network from "./Network";
 import "./index.css";
 import { useRouter } from "./routing";
 
+if (process.env.NODE_ENV === "development" && import.meta.env.VITE_DEV_WORKTREE_NAME) {
+  document.title = `${document.title} (${import.meta.env.VITE_DEV_WORKTREE_NAME})`;
+}
+
 const App: React.FC = () => {
   const { context, environment } = useRouter();
-
-  useLayoutEffect(() => {
-    if (process.env.NODE_ENV === "development" && import.meta.env?.VITE_DEV_WORKTREE_NAME) {
-      document.title = `${document.title} - (${import.meta.env?.VITE_DEV_WORKTREE_NAME})`;
-    }
-  }, []);
 
   return <Network environment={environment} context={context} />;
 };
