df5bb51a7315918f461b405aa5b066d279131e9c
erik <erik@voxel51.com>
2026-01-15 15:43:14 -0500

    fix bug where primitive count was not filtering by active fields

--- app/packages/core/src/components/Modal/Sidebar/Annotate/useSamplePrimitives.ts
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/useSamplePrimitives.ts
@@ -1,9 +1,12 @@
 import { Sample, State, fieldPaths } from "@fiftyone/state";
 import { DICT_FIELD, VALID_PRIMITIVE_TYPES } from "@fiftyone/utilities";
+import { useAtomValue } from "jotai";
 import { get } from "lodash";
 import { useRecoilValue } from "recoil";
+import { activeLabelSchemas } from "./state";
 
 const useSamplePrimitives = (currentSample: Sample): string[] => {
+  const activeFields = useAtomValue(activeLabelSchemas);
   const primitivePaths = useRecoilValue(
     fieldPaths({
       space: State.SPACE.SAMPLE,
@@ -11,11 +14,15 @@ const useSamplePrimitives = (currentSample: Sample): string[] => {
     })
   );
 
+  if (!activeFields) {
+    return [];
+  }
+
   // only top level primitives that exist - we want to keep null
   // values it just means the value has not been set yet
-  const validPrimitivePaths = primitivePaths.filter(
-    (path) => get(currentSample, path) !== undefined
-  );
+  const validPrimitivePaths = primitivePaths
+    .filter((path) => get(currentSample, path) !== undefined)
+    .filter((path) => activeFields.includes(path));
 
   return validPrimitivePaths;
 };
