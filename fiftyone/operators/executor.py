dd919c52684b51d3de8dc41064dd93f126e9d791
CamronStaley <camron_staley@live.com>
2026-01-20 13:30:49 -0600

    add rerunnable as part of delegation

--- fiftyone/operators/executor.py
+++ fiftyone/operators/executor.py
@@ -303,6 +303,16 @@ async def execute_or_delegate_operator(
                     raise TypeError(
                         "Pipeline stages must be of type PipelineStage"
                     )
+                registry = OperatorRegistry()
+                for stage in pipeline.stages:
+                    if stage.rerunnable is None:
+                        child = registry.get_operator(stage.operator_uri)
+                        if child:
+                            stage.rerunnable = child.config.rerunnable
+                        else:
+                            logger.warning(
+                                f"Pipeline stage operator '{stage.operator_uri}' does not exist"
+                            )
             except Exception as e:
                 return ExecutionResult(
                     executor=executor,
