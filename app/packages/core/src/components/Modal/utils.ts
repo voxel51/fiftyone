2c2713471a0af33c74699c74f0b8737bed75168f
Shugmi Shumunov <SShumunov@gmail.com>
2026-01-23 11:38:11 -0500

    feat: add Media Field shortcuts to help modal

--- app/packages/core/src/components/Modal/utils.ts
+++ app/packages/core/src/components/Modal/utils.ts
@@ -1,11 +1,32 @@
 interface ShortcutItem {
   shortcut: string;
+  title?: string;
+  detail?: string;
+}
+
+const MEDIA_FIELD_SHORTCUTS: ShortcutItem[] = [
+  {
+    shortcut: "PageUp",
+    title: "Previous media field",
+    detail: "Switch to the previous media field",
+  },
+  {
+    shortcut: "PageDown",
+    title: "Next media field",
+    detail: "Switch to the next media field",
+  },
+];
+
+export function getMediaFieldShortcuts(
+  hasMultipleMediaFields: boolean
+): ShortcutItem[] {
+  return hasMultipleMediaFields ? MEDIA_FIELD_SHORTCUTS : [];
 }
 
 type Shortcuts = { [key: string]: ShortcutItem };
 
 export function shortcutToHelpItems(SHORTCUTS: Shortcuts) {
-  const uniqueItems = {};
+  const uniqueItems: Record<string, ShortcutItem> = {};
   for (const item of Object.values(SHORTCUTS)) {
     uniqueItems[item.shortcut] = item;
   }
