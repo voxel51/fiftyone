bf049a12e5be9e2bc39a947129f17e6af7c2daa2
erik <erik@voxel51.com>
2026-01-23 10:35:56 -0500

    add support for radio and dropdown with numbers

--- app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/schemaHelpers.ts
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/schemaHelpers.ts
@@ -222,8 +222,12 @@ export function generatePrimitiveSchema(
   }
 
   if (schema.type === "float" || schema.type === "int") {
-    if (schema.range) {
+    if (schema.component === "slider" && schema.range) {
       return createSlider(name, schema.range);
+    } else if (schema.component === "dropdown") {
+      return createSelect(name, schema.values || []);
+    } else if (schema.component === "radio") {
+      return createRadio(name, schema.values || []);
     }
     return createText(name, "number");
   }
