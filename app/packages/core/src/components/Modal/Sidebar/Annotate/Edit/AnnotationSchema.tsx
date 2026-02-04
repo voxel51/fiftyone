ca6547dea5e1892a556a63cfbf133e1bf4dad017
erik <erik@voxel51.com>
2026-02-04 13:46:36 -0500

    fix text based label component

--- app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/AnnotationSchema.tsx
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/AnnotationSchema.tsx
@@ -21,6 +21,7 @@ const useSchema = (readOnly: boolean) => {
   // respect either the field OR the parent schema's readOnly flag
   const effectiveReadOnly = readOnly || isLabelReadOnly;
 
+  console.log("config", config);
   return useMemo(() => {
     const properties = config?.attributes
       .filter(({ name }) => name && !["id", "attributes"].includes(name))
@@ -35,7 +36,7 @@ const useSchema = (readOnly: boolean) => {
         {
           label: generatePrimitiveSchema("label", {
             type: "str",
-            component: "dropdown",
+            component: config?.component || "dropdown",
             values: config?.classes || [],
             readOnly: effectiveReadOnly,
           }),
@@ -54,26 +55,27 @@ const useSchema = (readOnly: boolean) => {
 
 const useHandleChanges = () => {
   return useRecoilCallback(
-    ({ snapshot }) => async (currentField: string, path: string, data) => {
-      const expanded = await snapshot.getPromise(expandPath(currentField));
-      const schema = await snapshot.getPromise(field(`${expanded}.${path}`));
+    ({ snapshot }) =>
+      async (currentField: string, path: string, data) => {
+        const expanded = await snapshot.getPromise(expandPath(currentField));
+        const schema = await snapshot.getPromise(field(`${expanded}.${path}`));
 
-      if (typeof data === "string") {
-        if (schema?.ftype === FLOAT_FIELD) {
-          if (!data.length) return null;
-          const parsed = Number.parseFloat(data);
-          return Number.isFinite(parsed) ? parsed : null;
+        if (typeof data === "string") {
+          if (schema?.ftype === FLOAT_FIELD) {
+            if (!data.length) return null;
+            const parsed = Number.parseFloat(data);
+            return Number.isFinite(parsed) ? parsed : null;
+          }
+
+          if (schema?.ftype === INT_FIELD) {
+            if (!data.length) return null;
+            const parsed = Number.parseInt(data);
+            return Number.isFinite(parsed) ? parsed : null;
+          }
         }
 
-        if (schema?.ftype === INT_FIELD) {
-          if (!data.length) return null;
-          const parsed = Number.parseInt(data);
-          return Number.isFinite(parsed) ? parsed : null;
-        }
-      }
-
-      return data;
-    },
+        return data;
+      },
     []
   );
 };
