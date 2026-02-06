7e3ed521eab4b01017f127f9be7bb1b0dd525f2d
Tom Schmidt <tom.schmidt@voxel51.com>
2026-02-06 14:08:33 -0800

    select 3d label when entering annotation via label quick edit

--- app/packages/annotation/src/hooks/useRegisterRendererEventHandlers.ts
+++ app/packages/annotation/src/hooks/useRegisterRendererEventHandlers.ts
@@ -1,6 +1,8 @@
 import { useAnnotationContextManager } from "@fiftyone/core/src/components/Modal/Sidebar/Annotate/useAnnotationContextManager";
 import { useLighter, useLighterEventHandler } from "@fiftyone/lighter";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+import { useSelect3DLabelForAnnotation } from "@fiftyone/looker-3d/src/hooks";
+import { useWorkingDetections, useWorkingPolylines } from "@fiftyone/looker-3d";
 
 /**
  * Hook which registers event handlers related to renderer events.
@@ -10,12 +12,19 @@ import { useCallback } from "react";
 export const useRegisterRendererEventHandlers = () => {
   const { scene } = useLighter();
   const handleLighterEvent = useLighterEventHandler(scene?.getEventChannel());
+
+  const select3DLabel = useSelect3DLabelForAnnotation();
+  const labels3D = [...useWorkingDetections(), ...useWorkingPolylines()];
+
   const { entranceLabelId, clearEntranceLabelId } =
     useAnnotationContextManager();
 
-  // If we entered annotation mode via direct label edit,
-  // we want to select the label once it's been initialized and added to the
-  // scene.
+  // If we entered annotation mode via direct label edit (e.g. via the label's tooltip),
+  // we want to open the label for editing once it's been initialized and
+  // added to the rendered scene.
+
+  // For 2D, we can listen to the overlay-added event and select the relevant
+  // label once it's available.
   handleLighterEvent(
     "lighter:overlay-added",
     useCallback(
@@ -31,5 +40,19 @@ export const useRegisterRendererEventHandlers = () => {
     )
   );
 
-  // todo 3d
+  // For 3D, we can listen to changes in the working label set and select
+  // the label once it's available.
+  useEffect(() => {
+    if (entranceLabelId && labels3D) {
+      const targetLabel = labels3D.find(
+        (label) => label._id === entranceLabelId
+      );
+
+      if (targetLabel) {
+        select3DLabel({ ...targetLabel, selected: true });
+
+        clearEntranceLabelId();
+      }
+    }
+  }, [entranceLabelId, labels3D]);
 };
