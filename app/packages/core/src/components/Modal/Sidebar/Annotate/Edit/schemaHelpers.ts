5f598ccc664ab3f4f7d33eff5cb9b4d6146fb9a3
lanzhenw <lanzhenwang9@gmail.com>
2026-02-11 17:10:30 -0600

    fix the annotation side display and selection

--- app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/schemaHelpers.ts
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/schemaHelpers.ts
@@ -99,9 +99,13 @@ export const createSlider = (
   };
 };
 
-export const createRadio = (name: string, choices: string[]) => {
+export const createRadio = (
+  name: string,
+  choices: string[],
+  type: string = "string"
+) => {
   return {
-    type: "string",
+    type,
     view: {
       name: "RadioGroup",
       label: name,
@@ -134,9 +138,13 @@ export const createTags = (name: string, choices: string[]) => {
   };
 };
 
-export const createSelect = (name: string, choices: string[]) => {
+export const createSelect = (
+  name: string,
+  choices: string[],
+  type: string = "string"
+) => {
   return {
-    type: "string",
+    type,
     view: {
       name: "SelectWidget",
       label: name,
@@ -245,9 +253,9 @@ export function generatePrimitiveSchema(
     if (schema.component === "slider" && schema.range) {
       return createSlider(name, schema.range);
     } else if (schema.component === "dropdown") {
-      return createSelect(name, schema.values || []);
+      return createSelect(name, schema.values || [], "number");
     } else if (schema.component === "radio") {
-      return createRadio(name, schema.values || []);
+      return createRadio(name, schema.values || [], "number");
     }
     return createText(name, "number");
   }
