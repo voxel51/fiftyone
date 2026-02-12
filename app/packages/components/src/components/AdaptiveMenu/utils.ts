cc976651329a31ebc2f22cea07e9048233e6bc7c
Nate Verjinski <nverjins@gmail.com>
2026-02-12 10:26:39 -0600

    fix: updated AdaptiveMenu to prefer rendering action buttons over show more button when possible

--- app/packages/components/src/components/AdaptiveMenu/utils.ts
+++ app/packages/components/src/components/AdaptiveMenu/utils.ts
@@ -1,8 +1,9 @@
 import { throttle } from "lodash";
 
-const ADAPT_THROTTLE = 250;
+const ADAPT_THROTTLE = 100;
 
 export const SHOW_MORE_ACTIONS_BUTTON_WIDTH = 48;
+export const ADAPTIVE_MENU_GAP = 8;
 
 export const hideOverflowingNodes = throttle(
   (container: HTMLDivElement, onHide: HideOverflowingNodesCallback) => {
@@ -10,7 +11,9 @@ export const hideOverflowingNodes = throttle(
     const containerWidth = container.offsetWidth;
     const itemsContainer = container.childNodes[0] as HTMLDivElement;
     const items = itemsContainer?.childNodes as NodeListOf<HTMLDivElement>;
-    const availableWidth = containerWidth - SHOW_MORE_ACTIONS_BUTTON_WIDTH;
+
+    let availableWidth =
+      containerWidth - SHOW_MORE_ACTIONS_BUTTON_WIDTH - ADAPTIVE_MENU_GAP;
     let hiddenItems = 0;
     let lastRightEdge = 0;
     let lastVisibleItemId = "";
@@ -27,6 +30,28 @@ export const hideOverflowingNodes = throttle(
         lastVisibleItemId = item.getAttribute("data-item-id") as string;
       }
     });
+
+    // If only one item would be hidden, check if we can fit it by not rendering the "More Items" button
+    if (hiddenItems === 1) {
+      availableWidth = containerWidth;
+      hiddenItems = 0;
+      lastRightEdge = 0;
+      lastVisibleItemId = "";
+
+      items.forEach((item: HTMLDivElement) => {
+        const itemWidth = item.offsetWidth;
+        const itemLeft = item.offsetLeft;
+        const rightEdge = itemWidth + itemLeft;
+        const overflown = rightEdge > availableWidth;
+        if (overflown) {
+          hiddenItems++;
+        } else {
+          lastRightEdge = rightEdge;
+          lastVisibleItemId = item.getAttribute("data-item-id") as string;
+        }
+      });
+    }
+
     itemsContainer.style.width = `${lastRightEdge - 4}px`;
     onHide(hiddenItems, lastVisibleItemId);
   },
