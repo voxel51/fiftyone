2c9ce6f2d91dffdd05a4988b37774c3560d3ad39
Benjamin Kane <ben@voxel51.com>
2026-03-11 10:56:54 -0400

    update test

--- e2e-pw/src/oss/specs/smoke-tests/lighter-toolbar.spec.ts
+++ e2e-pw/src/oss/specs/smoke-tests/lighter-toolbar.spec.ts
@@ -1,7 +1,7 @@
-import { test as base, expect } from "src/oss/fixtures";
+import { test as base } from "src/oss/fixtures";
 import { GridPom } from "src/oss/poms/grid";
 import { ModalPom } from "src/oss/poms/modal";
-import { ModalSidebarPom } from "src/oss/poms/modal/modal-sidebar";
+import { SampleCanvasType } from "src/oss/poms/modal/sample-canvas";
 import { getUniqueDatasetNameWithPrefix } from "src/oss/utils";
 
 const datasetName = getUniqueDatasetNameWithPrefix("smoke-lighter-toolbar");
@@ -9,16 +9,12 @@ const datasetName = getUniqueDatasetNameWithPrefix("smoke-lighter-toolbar");
 const test = base.extend<{
   grid: GridPom;
   modal: ModalPom;
-  modalSidebar: ModalSidebarPom;
 }>({
   grid: async ({ page, eventUtils }, use) => {
     await use(new GridPom(page, eventUtils));
   },
   modal: async ({ page, eventUtils }, use) => {
     await use(new ModalPom(page, eventUtils));
-  },
-  modalSidebar: async ({ page }, use) => {
-    await use(new ModalSidebarPom(page));
   },
 });
 
@@ -44,27 +40,18 @@ test.beforeEach(async ({ page, fiftyoneLoader }) => {
   await fiftyoneLoader.waitUntilGridVisible(page, datasetName);
 });
 
-test.afterEach(async ({ modal, page }) => {
-  await modal.close({ ignoreError: true });
-  await page.reload();
-});
-
 test.describe.serial("lighter-toolbar-smoke", () => {
   test("lighter toolbar appears when hovering over sample in annotate mode", async ({
     grid,
     modal,
-    modalSidebar,
   }) => {
     await grid.openFirstSample();
     await modal.waitForSampleLoadDomAttribute();
-    await modalSidebar.switchMode("annotate");
+    await modal.sidebar.switchMode("annotate");
 
-    const sampleRenderer = modal.sampleCanvas.locator.locator(
-      '[data-cy="lighter-sample-renderer"]'
-    );
-    await expect(sampleRenderer).toBeVisible();
+    await modal.sampleCanvas.assert.is(SampleCanvasType.LIGHTER);
     await modal.sampleCanvas.toolbar.assert.isVisible(false);
-    await sampleRenderer.hover();
+    await modal.sampleCanvas.move(0.5, 0.5);
     await modal.sampleCanvas.toolbar.assert.isVisible();
   });
 });
