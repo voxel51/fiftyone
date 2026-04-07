01dbb267755dff802b592abea3ddb399ce68dfa0
Tim Mendoza <tim@voxel51.com>
2026-04-07 15:48:15 -0600

    Update configs to be able to use mocks

--- app/packages/operators/babel.config.js
+++
@@ -1,7 +0,0 @@
-/* global module */
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-  ],
-};
--- app/packages/operators/vitest.config.ts
+++ app/packages/operators/vitest.config.ts
@@ -1,6 +1,8 @@
+import relay from "vite-plugin-relay";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [relay],
   test: {
     environment: "jsdom",
   },
