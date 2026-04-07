89b87a28247211954704eb87fa5fb2c5d04aa679
imanjra <ibrahim@voxel51.com>
2026-04-07 11:28:46 -0400

    move setup now button to its own component

--- app/packages/operators/src/components/RequiresOrchestrator.tsx
+++ app/packages/operators/src/components/RequiresOrchestrator.tsx
@@ -8,15 +8,18 @@ export default function RequiresOrchestrator() {
       description="Production workflows require dedicated compute resources."
       icon={IconName.Orchestrator}
       compact
-      action={
-        <Link
-          href="https://docs.voxel51.com/plugins/using_plugins.html#delegated-operations"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button variant={Variant.Secondary}>Set up now</Button>
-        </Link>
-      }
+      action={<SetupNow />}
     />
+  );
+}
+function SetupNow() {
+  return (
+    <Link
+      href="https://docs.voxel51.com/plugins/using_plugins.html#delegated-operations"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Button variant={Variant.Secondary}>Set up now</Button>
+    </Link>
   );
 }
