c9e806f441ea4e911b962ff48ad4d288f3649a2d
Tom Schmidt <tom.schmidt@voxel51.com>
2026-04-08 17:16:33 -0700

    add logic to upsert detection on segmentation inference result

--- app/packages/annotation/src/agents/hooks/useApplyInferenceResult.ts
+++ app/packages/annotation/src/agents/hooks/useApplyInferenceResult.ts
@@ -6,7 +6,7 @@ import {
 } from "../types";
 import { useCallback } from "react";
 import { useLighter } from "@fiftyone/lighter";
-import { useAnnotationContext } from "@fiftyone/core/src/components/Modal/Sidebar/Annotate/Edit/state";
+import { AnnotationLabel } from "@fiftyone/state";
 
 /**
  * Method which applies the provided {@link InferenceResult} to the current
@@ -20,27 +20,39 @@ export type InferenceResultHandler = (
  * Hook which returns a {@link InferenceResultHandler} bound to the current
  * annotation session.
  */
-export const useApplyInferenceResult = (): InferenceResultHandler => {
-  const { selectedLabel } = useAnnotationContext();
-  const { getOverlay } = useLighter();
+export const useApplyInferenceResult = (
+  createLabel: () => AnnotationLabel | null
+): InferenceResultHandler => {
+  const { getOverlay, scene } = useLighter();
 
   return useCallback(
     (result: InferenceResult<InferenceResultProxy>) => {
-      if (!selectedLabel || !getOverlay) {
-        return;
-      }
-
       if (result.type === "sync") {
         if (result.taskType === AgentTaskType.SEGMENT) {
-          const inferenceData = result.response as SegmentationInferenceResult;
+          if (scene) {
+            const inferenceData =
+              result.response as SegmentationInferenceResult;
 
-          const newLabelData = {
-            ...selectedLabel.data,
-            mask: inferenceData.detections?.[0]?.mask,
-          };
+            let overlay = getOverlay(result.labelId);
+            if (!overlay) {
+              // if there is no overlay yet, this is a new label
+              overlay = createLabel()?.overlay;
+            }
 
-          // todo integrate with undo/redo
-          getOverlay(selectedLabel.data._id)?.updateLabel(newLabelData);
+            if (overlay) {
+              const newLabelData = {
+                ...overlay.label,
+                // accept new bounding box and mask from inference result
+                bounding_box: inferenceData.detections?.[0]?.bounding_box,
+                mask: inferenceData.detections?.[0]?.mask,
+              };
+
+              // todo integrate with undo/redo
+              overlay.updateLabel(newLabelData);
+            } else {
+              console.warn("Unable to create overlay");
+            }
+          }
         } else {
           console.warn(`Unsupported task type: ${result.taskType}`);
         }
@@ -48,6 +60,6 @@ export const useApplyInferenceResult = (): InferenceResultHandler => {
         // todo async handling
       }
     },
-    [getOverlay, selectedLabel]
+    [createLabel, getOverlay, scene]
   );
 };
