f7b93a49ee02c13cdc48e72afa7fa07ae6eed140
Sashank Aryal <sashank2012@gmail.com>
2026-04-27 17:06:48 -0700

    basic unit tests for docs exclude decorator

---
+++ tests/unittests/internal_docs_tests.py
@@ -0,0 +1,86 @@
+"""
+Unit tests for internal documentation helpers.
+
+| Copyright 2017-2026, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+import unittest
+
+from fiftyone.internal.docs import hide_from_docs, is_hidden_from_docs
+
+
+class InternalDocsTests(unittest.TestCase):
+    def test_hides_function(self):
+        def visible():
+            pass
+
+        self.assertFalse(is_hidden_from_docs(visible))
+
+        hidden = hide_from_docs(visible)
+
+        self.assertIs(hidden, visible)
+        self.assertTrue(is_hidden_from_docs(visible))
+
+    def test_hides_class(self):
+        class Visible:
+            pass
+
+        self.assertFalse(is_hidden_from_docs(Visible))
+
+        hidden = hide_from_docs(Visible)
+
+        self.assertIs(hidden, Visible)
+        self.assertTrue(is_hidden_from_docs(Visible))
+
+    def test_hides_instance_method(self):
+        class Container:
+            @hide_from_docs
+            def method(self):
+                pass
+
+        self.assertTrue(is_hidden_from_docs(Container.method))
+        self.assertTrue(is_hidden_from_docs(Container().method))
+
+    def test_hides_property_with_inner_decorator(self):
+        class Container:
+            @property
+            @hide_from_docs
+            def value(self):
+                return 1
+
+        self.assertTrue(is_hidden_from_docs(Container.__dict__["value"]))
+
+    def test_hides_property_with_outer_decorator(self):
+        class Container:
+            @hide_from_docs
+            @property
+            def value(self):
+                return 1
+
+        self.assertTrue(is_hidden_from_docs(Container.__dict__["value"]))
+
+    def test_hides_classmethod(self):
+        class Container:
+            @hide_from_docs
+            @classmethod
+            def method(cls):
+                pass
+
+        self.assertTrue(is_hidden_from_docs(Container.__dict__["method"]))
+        self.assertTrue(is_hidden_from_docs(Container.method))
+
+    def test_hides_staticmethod(self):
+        class Container:
+            @hide_from_docs
+            @staticmethod
+            def method():
+                pass
+
+        self.assertTrue(is_hidden_from_docs(Container.__dict__["method"]))
+        self.assertTrue(is_hidden_from_docs(Container.method))
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
