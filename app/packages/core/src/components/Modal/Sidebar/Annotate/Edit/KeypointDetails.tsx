5222e45ef000f3c69a5724dd05ff89fb302947b7
Tom Schmidt <tom.schmidt@voxel51.com>
2026-04-27 17:08:54 -0700

    add skeleton information to keypoint details

--- app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/KeypointDetails.tsx
+++ app/packages/core/src/components/Modal/Sidebar/Annotate/Edit/KeypointDetails.tsx
@@ -1,17 +1,72 @@
 import { Box } from "@mui/material";
 import { useAnnotationContext } from "./state";
-import { Text, TextColor } from "@voxel51/voodo";
-import { KeypointAnnotationLabel } from "@fiftyone/state";
+import {
+  Align,
+  Orientation,
+  Spacing,
+  Stack,
+  Text,
+  TextColor,
+  Tooltip,
+  TooltipProps,
+} from "@voxel51/voodo";
+import { KeypointAnnotationLabel, useKeypointSkeleton } from "@fiftyone/state";
+import { useMemo } from "react";
+
+const ConditionalTooltip = ({
+  anchor,
+  children,
+  content,
+  enabled,
+}: Pick<TooltipProps, "anchor" | "children" | "content"> & {
+  enabled: boolean;
+}) => {
+  if (!enabled) {
+    return <>{children}</>;
+  }
+
+  return (
+    <Tooltip portal content={content} anchor={anchor}>
+      {children}
+    </Tooltip>
+  );
+};
 
 export const KeypointDetails = () => {
   const { selectedLabel } = useAnnotationContext();
   const currentData = selectedLabel?.data as KeypointAnnotationLabel["data"];
+  const getKeypointSkeleton = useKeypointSkeleton();
+
+  const keypointSkeleton = useMemo(() => {
+    if (selectedLabel?.path) {
+      return getKeypointSkeleton(selectedLabel.path);
+    }
+
+    return undefined;
+  }, [getKeypointSkeleton, selectedLabel.path]);
 
   const pointCount = currentData?.points?.length ?? 0;
 
   return (
-    <Box sx={{ px: 1.5, py: 1 }}>
-      <Text color={TextColor.Secondary}>{pointCount} points</Text>
+    <Box sx={{ py: 1 }}>
+      <Stack
+        orientation={Orientation.Row}
+        align={Align.Center}
+        spacing={Spacing.Sm}
+      >
+        <Text color={TextColor.Secondary}>
+          {keypointSkeleton.edges.length} edges
+        </Text>
+
+        <Text color={TextColor.Secondary}>&bull;</Text>
+
+        <ConditionalTooltip
+          enabled={keypointSkeleton?.labels?.length > 0}
+          content={<Text>{keypointSkeleton.labels.join(", ")}</Text>}
+        >
+          <Text color={TextColor.Secondary}>{pointCount} points</Text>
+        </ConditionalTooltip>
+      </Stack>
     </Box>
   );
 };
