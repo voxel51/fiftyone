6aa86c5b8eea6a258be7a37751dea26a79297f6b
Eric Shull <eric@exupero.org>
2026-05-04 17:05:41 -0400

    Prototype scene inventory to sample metadata

---
+++ fiftyone/multimodal/odm/base.py
@@ -0,0 +1,29 @@
+"""
+| Copyright 2017-2026, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+import abc
+from typing import List
+
+from fiftyone import Dataset
+from fiftyone.multimodal.schemas.v1 import SceneInventory
+
+
+class DatabaseAdapter(abc.ABC):
+    @classmethod
+    @abc.abstractmethod
+    def write_scene_inventories(
+        cls, dataset: Dataset, inventories: List[SceneInventory]
+    ) -> None:
+        """
+        Writes the given scene inventories to the dataset as samples with the
+        metadata field 'scene_inventory'.
+
+        Args:
+            inventories: a list of :class:`SceneInventory`
+        """
+        raise NotImplementedError(
+            "subclasses must implement write_scene_inventory()"
+        )
---
+++ fiftyone/multimodal/odm/mongo.py
@@ -0,0 +1,39 @@
+"""
+| Copyright 2017-2026, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+from typing import List
+
+from google.protobuf.json_format import MessageToDict
+
+from .base import DatabaseAdapter
+from fiftyone import Dataset, Sample
+from fiftyone.multimodal.schemas.v1 import SceneInventory
+
+
+class MongoAdapter(DatabaseAdapter):
+    @classmethod
+    def write_scene_inventories(
+        cls, dataset: Dataset, inventories: List[SceneInventory]
+    ) -> None:
+        """
+        Writes the given scene inventories to the dataset as samples with the
+        metadata field 'scene_inventory'.
+
+        Args:
+            inventories: a list of :class:`SceneInventory`
+        """
+        dataset.add_samples(
+            [
+                cls._scene_inventory_to_sample(inventory)
+                for inventory in inventories
+            ]
+        )
+
+    @classmethod
+    def _scene_inventory_to_sample(cls, inventory: SceneInventory) -> Sample:
+        sample = Sample(filepath=inventory.scene_id)
+        sample["scene_inventory"] = MessageToDict(inventory)
+        return sample
