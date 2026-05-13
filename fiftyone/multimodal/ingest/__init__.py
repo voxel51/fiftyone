e93ad562a3f316e808809adc741a99d44680e7fa
Eric Shull <eric.shull@voxel51.com>
2026-05-13 11:48:04 -0400

    Refactor filepath and directory crawling into generator

--- fiftyone/multimodal/ingest/__init__.py
+++ fiftyone/multimodal/ingest/__init__.py
@@ -9,6 +9,32 @@ Ingest scaffolding for multimodal workflows.
 from fiftyone.core import storage
 
 
+def _readable_paths(filepaths, *, adapter):
+    """
+    Returns a generator of filepaths in the given iterable that can be read by
+    the given adapter.
+
+    Args:
+        filepaths: an iterable of strings representing file paths or directory
+            locations to crawl
+        adapter: a subclass of :class:`MultimodalAdapter` that determines
+            whether a given file can be read
+
+    Returns:
+        a generator of strings representing the filepaths in the given iterable
+        that can be read by the given adapter
+    """
+    for filepath in filepaths:
+        if storage.isdir(filepath):
+            for path in storage.list_files(
+                filepath, abs_paths=True, recursive=True
+            ):
+                if adapter.can_read(path):
+                    yield path
+        elif storage.exists(filepath) and adapter.can_read(filepath):
+            yield filepath
+
+
 def _get_scene_inventories(filepaths, *, adapter):
     """
     Reads the given scene files using the given adapter class, and returns
@@ -24,19 +50,8 @@ def _get_scene_inventories(filepaths, *, adapter):
     Returns:
         a list of :class:`fiftyone.multimodal.SceneInventory` instances
     """
-    inventories = []
-
-    for filepath in filepaths:
-        if storage.isdir(filepath):
-            for path in storage.list_files(
-                filepath, abs_paths=True, recursive=True
-            ):
-                if adapter.can_read(path):
-                    inventories.append(adapter.get_scene_inventory(path))
-        elif storage.exists(filepath) and adapter.can_read(filepath):
-            inventories.append(adapter.get_scene_inventory(filepath))
-
-    return inventories
+    paths = _readable_paths(filepaths, adapter=adapter)
+    return [adapter.get_scene_inventory(path) for path in paths]
 
 
 __all__ = ["_get_scene_inventories"]
