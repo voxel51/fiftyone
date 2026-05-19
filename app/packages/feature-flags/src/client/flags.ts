66e121a469b97cb2a5e106b915602562113035e2
Tom Schmidt <tom.schmidt@voxel51.com>
2026-05-19 10:19:55 -0700

    remove feature flags for segmentation and polylines

--- app/packages/feature-flags/src/client/flags.ts
+++ app/packages/feature-flags/src/client/flags.ts
@@ -2,7 +2,5 @@
  * Enumeration of active feature flags.
  */
 export enum FeatureFlag {
-  VFF_AI_SEGMENTATION = "VFF_AI_SEGMENTATION",
   VFF_MULTIMODAL = "VFF_MULTIMODAL",
-  VFF_POLYLINE_ANNOTATION = "VFF_POLYLINE_ANNOTATION",
 }
