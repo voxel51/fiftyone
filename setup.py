6e1ddf1273b4a7e6d4e193c51a7993b41f8b0562
Benjamin Kane <ben@voxel51.com>
2026-08-10 16:34:38 -0400

    add Python 3.14 support
    - CI: add 3.14 to test/windows-test/build-ci-images/publish matrices;
      PR fast set becomes [3.10, 3.14]
    - tensorflow/tensorflow-datasets skipped on 3.14 via env markers until
      cp314 wheels ship
    - declare 3.14 classifiers (fiftyone, fiftyone-db) and update README

--- setup.py
+++ setup.py
@@ -118,6 +118,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     entry_points={"console_scripts": ["fiftyone=fiftyone.core.cli:main"]},
     python_requires=">=3.10",
