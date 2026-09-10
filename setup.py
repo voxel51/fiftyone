54beb7e571b46310af3259d002d454d07a0e66e9
Benjamin Kane <ben@voxel51.com>
2026-09-10 17:50:02 -0400

    Merge pull request #8437 from voxel51/bump/fiftyone-brain-0.25
    Bump fiftyone-brain to 0.25
    
    (cherry picked from commit 0a2a998a0b47449515eecfce288ec4ba8c369ffc)

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
         "scikit-image<1",
         "scipy<2",
         # internal packages
-        "fiftyone-brain>=0.24.0,<0.25",
+        "fiftyone-brain>=0.25.0,<0.26",
         "fiftyone-db>=0.4,<2.0",
         "voxel51-eta>=0.17,<0.18",
     ],
